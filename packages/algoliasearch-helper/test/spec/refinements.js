013e86b2e1b809f15e111d6a57db301c15e3f748
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2015-04-20 11:35:06 +0200

    Fix syntax

--- packages/algoliasearch-helper/test/spec/refinements.js
+++ packages/algoliasearch-helper/test/spec/refinements.js
@@ -1,11 +1,11 @@
-var test = require("tape");
+"use strict";
+var test = require( "tape" );
 var _ = require( "lodash" );
-var sinon = require("sinon"); 
 var algoliasearchHelper = require( "../../index" );
 
-test( "isDisjunctiveRefined", function( t ){
+test( "isDisjunctiveRefined", function( t ) {
   var helper = algoliasearchHelper( null, null, {} );
-  helper._search = function(){};
+  helper._search = function() {};
 
   var facet = "MyFacet";
   var value = "MyValue";
@@ -23,35 +23,35 @@ test( "isDisjunctiveRefined", function( t ){
 
 test( "Adding refinments should add an entry to the refinments attribute", function( t ) {
   var helper = algoliasearchHelper( {}, "index", {} );
-  helper._search = function(){};
+  helper._search = function() {};
 
   var facetName = "facet1";
   var facetValue = "42";
 
-  t.ok( _.isEmpty( helper.state.facetsRefinements ), "should be empty at first");
+  t.ok( _.isEmpty( helper.state.facetsRefinements ), "should be empty at first" );
   helper.addRefine( facetName, "42" );
   t.ok( _.size( helper.state.facetsRefinements ) === 1 &&
-          helper.state.facetsRefinements["facet1"] === facetValue,
+          helper.state.facetsRefinements.facet1 === facetValue,
           "when adding a refinment, should have one" );
   helper.addRefine( facetName, facetValue );
-  t.ok( _.size( helper.state.facetsRefinements ) === 1, "when adding the same, should still be one");
+  t.ok( _.size( helper.state.facetsRefinements ) === 1, "when adding the same, should still be one" );
   helper.removeRefine( facetName, facetValue );
-  t.ok( _.size( helper.state.facetsRefinements ) === 0, "Then empty ");
+  t.ok( _.size( helper.state.facetsRefinements ) === 0, "Then empty " );
   t.end();
 } );
 
-test( "IsRefined should return true if the ( facet, value ) is refined.", function( t ){
+test( "IsRefined should return true if the ( facet, value ) is refined.", function( t ) {
   var helper = algoliasearchHelper( null, null, {
     facets : [ "facet1" ]
   } );
 
   helper.addRefine( "facet1", "boom" );
 
-  t.ok( helper.isRefined( "facet1", "boom"), "the facet is refined >> true" );
+  t.ok( helper.isRefined( "facet1", "boom" ), "the facet is refined >> true" );
 
-  t.notOk( helper.isRefined( "facet1", "booohh" ), "not refined but is a facet");
+  t.notOk( helper.isRefined( "facet1", "booohh" ), "not refined but is a facet" );
   t.notOk( helper.isRefined( "notAFacet", "maoooh" ), "not refined because it's not a facet" );
-  t.notOk( helper.isRefined( null, null), "not even valid values" );
+  t.notOk( helper.isRefined( null, null ), "not even valid values" );
 
   t.end();
 } );
