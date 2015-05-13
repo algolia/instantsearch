55f5ec78811257b9a9a6c19e8cb52ef4b4dcc00a
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2015-05-13 11:49:02 +0200

    Bump version to 2.0.3

--- packages/algoliasearch-helper/index.js
+++ packages/algoliasearch-helper/index.js
@@ -16,6 +16,6 @@ function helper( client, index, opts ) {
  * The version currently used
  * @member module:algoliasearch-helper.version
  */
-helper.version = "2.0.2";
+helper.version = "2.0.3";
 
 module.exports = helper;
