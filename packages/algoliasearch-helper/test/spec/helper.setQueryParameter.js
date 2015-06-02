c2d046adb7b46bfaff8df4c03dc0b8c4aec3f31f
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2015-06-02 15:03:28 +0200

    Expose setQueryParameter directly on the helper

---
+++ packages/algoliasearch-helper/test/spec/helper.setQueryParameter.js
@@ -0,0 +1,39 @@
+"use strict";
+var test = require( "tape" );
+var algoliasearchHelper = require( "../../index" );
+
+test( "setChange should change the current state", function( t ) {
+  var helper = algoliasearchHelper( null, null, null );
+  var changed = false;
+
+  helper.on( "change", function() {
+    changed = true;
+  } );
+
+  t.equal( helper.getCurrentPage(), 0, "Initially page is 0" );
+  t.notOk( changed, "No changes called yet" );
+  helper.setQueryParameter( "page", 22 );
+  t.equal( helper.getCurrentPage(), 22, "After setting the page is 22" );
+  t.ok( changed, "Change event should have been triggered" );
+
+  t.end();
+} );
+
+test( "setChange should not change the current state : no real modification", function( t ) {
+  var helper = algoliasearchHelper( null, null, null );
+  var changed = false;
+  var initialState = helper.state;
+
+  helper.on( "change", function() {
+    changed = true;
+  } );
+
+  t.equal( helper.getCurrentPage(), 0, "Initially page is 0" );
+  t.notOk( changed, "No changes called yet" );
+  helper.setQueryParameter( "page", 0 );
+  t.equal( helper.getCurrentPage(), 0, "After setting the page is 0" );
+  t.notOk( changed, "Change event should not have been triggered" );
+  t.equal( helper.state, initialState, "The state instance should remain the same" );
+
+  t.end();
+} );
