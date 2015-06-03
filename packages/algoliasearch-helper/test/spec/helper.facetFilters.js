186eea19bb7c11a5ad06dafa20736843a5ce9548
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2015-06-03 12:47:31 +0200

    Test the generated filters

---
+++ packages/algoliasearch-helper/test/spec/helper.facetFilters.js
@@ -0,0 +1,29 @@
+//Make sure we do facet filters correctly before sending them to algolia
+
+"use strict";
+
+var test = require( "tape" );
+var algoliasearchHelper = require( "../../index" );
+
+test( "The filters should contain the different filters for a single conjunctive facet with multiple refinements",
+      function( t ) {
+  var facetName = "myFacet";
+  var helper = algoliasearchHelper( "", "", {
+    facets : [ facetName ]
+  } );
+
+  helper.addRefine( facetName, "value1" );
+  t.deepEqual( helper._getFacetFilters(),
+               [ facetName + ":value1" ],
+               "One value : one filter" );
+  helper.addRefine( facetName, "value2" );
+  t.deepEqual( helper._getFacetFilters(),
+               [ facetName + ":value1", facetName + ":value2" ],
+               "Two values : two filters" );
+  helper.addRefine( facetName, "value1" );
+  t.deepEqual( helper._getFacetFilters(),
+               [ facetName + ":value1", facetName + ":value2"],
+               "Add multiple times the same parameter : only two parameters" );
+
+  t.end();
+} );
