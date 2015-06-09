8bc87e0b059fb9c36e97e1165398886f6d8d032f
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2015-06-09 14:59:09 +0200

    Fix JSDoc

--- packages/algoliasearch-helper/index.js
+++ packages/algoliasearch-helper/index.js
@@ -1,11 +1,12 @@
 "use strict";
 var AlgoliaSearchHelper = require( "./src/algoliasearch.helper" );
+
 /**
  * The algoliasearch-helper module is a function that instanciate the helper.
- * @module algoliasearch-helper
+ * @module algoliasearchHelper
  * @param  {AlgoliaSearch} client an AlgoliaSearch client
  * @param  {string} index the index name to query
- * @param  {SearchParameters | object} options an object defining the initial config of the search. It doesn't have to be a {SearchParameters}, just an object containing the properties you need from it.
+ * @param  {SearchParameters|object} opts an object defining the initial config of the search. It doesn't have to be a {SearchParameters}, just an object containing the properties you need from it.
  * @return {AlgoliaSearchHelper}
  */
 function helper( client, index, opts ) {
