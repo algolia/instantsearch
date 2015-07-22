cb3d72a3c457a846b3f631f3e544a64bf4fabefc
vvo <vincent.voyer@algolia.com>
2015-07-22 11:33:51 +0200

    chore: update and pin all deps

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -2,4 +2,7 @@
 
 set -e # exit when error
 
-npm run js:watch & http-server . & sleep 1 && echo "Open http://127.0.0.1:8080/examples/" && wait
+npm run js:watch &
+  http-server . &
+  sleep 1 && echo "Open http://127.0.0.1:8080/examples/" &&
+  wait
