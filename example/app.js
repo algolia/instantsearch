82c60cb3bb73254cbd091266ef616bb217f05155
Sylvain UTARD <sylvain.utard@gmail.com>
2015-07-23 23:35:56 -0700

    s/instantSearch/instantsearch/

--- example/app.js
+++ example/app.js
@@ -1,17 +1,17 @@
-var instantSearch = require('../');
+var instantsearch = require('../');
 
-var instant = new instantSearch.InstantSearch(
+var instant = new instantsearch.instantsearch(
   'JTH1JDTDFT',
   '34a4c1b994546fbec45a670a06ba0c33',
   'Bibliotheques'
 );
 
-instant.addWidget(instantSearch.widgets.searchbox({
+instant.addWidget(instantsearch.widgets.searchbox({
   container: '#search-box',
   placeholder: 'Search for libraries in France...'
 }));
 
-instant.addWidget(instantSearch.widgets.results({
+instant.addWidget(instantsearch.widgets.results({
   container: '#hits',
   templates: {
     noResults: require('./templates/no-results.html'),
