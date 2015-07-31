32c64e6775a961aaaa467448a5bd7b21f2e6bd5d
vvo <vincent.voyer@algolia.com>
2015-07-31 17:08:17 +0200

    fix: do not use memoize & co
    Why use memoize here @bobylito? Is this some optimization?

--- components/templates/Hogan/index.js
+++ components/templates/Hogan/index.js
@@ -3,9 +3,6 @@
 var React = require('react');
 
 var hogan = require('hogan.js');
-var memoize = require('lodash/function/memoize');
-
-var memoHogan = memoize(hogan.compile.bind(hogan));
 
 var HoganResult = React.createClass({
   propTypes: {
@@ -14,12 +11,12 @@ var HoganResult = React.createClass({
   },
   componentWillMount: function() {
     this.setState({
-      template: memoHogan(this.props.template)
+      template: hogan.compile(this.props.template)
     });
   },
   render: function() {
     var content = this.state.template.render(this.props.data);
-    return <div dangerouslySetInnerHTML={{__html: content}} />;
+    return <div className="hit" dangerouslySetInnerHTML={{__html: content}} />;
   }
 });
 
