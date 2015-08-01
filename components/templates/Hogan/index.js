d8a3a4cd4f5d4ef6bd4374b6de6a0d179cfeece1
vvo <vincent.voyer@algolia.com>
2015-08-01 13:51:36 +0200

    fix: no state needed for Hogan component

--- components/templates/Hogan/index.js
+++ components/templates/Hogan/index.js
@@ -1,21 +1,15 @@
 'use strict';
 
-var React = require('react');
-
 var hogan = require('hogan.js');
+var React = require('react');
 
 var HoganResult = React.createClass({
   propTypes: {
     template: React.PropTypes.string,
     data: React.PropTypes.object
   },
-  componentWillMount: function() {
-    this.setState({
-      template: hogan.compile(this.props.template)
-    });
-  },
   render: function() {
-    var content = this.state.template.render(this.props.data);
+    var content = hogan.compile(this.props.template).render(this.props.data);
     return <div className="hit" dangerouslySetInnerHTML={{__html: content}} />;
   }
 });
