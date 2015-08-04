13066b955f0f288fa317c0b716d555394cb97eb9
vvo <vincent.voyer@algolia.com>
2015-08-04 15:17:30 +0200

    chore(hits): finish Results->Hits migration

--- widgets/hits/index.js
+++ widgets/hits/index.js
@@ -5,13 +5,13 @@ var React = require('react');
 var utils = require('../../lib/widgetUtils.js');
 
 function hits(params) {
-  var Results = require('../../components/Results');
+  var Hits = require('../../components/Hits');
   var containerNode = utils.getContainerNode(params.container);
 
   return {
     render: function(results, state, helper) {
       React.render(
-        <Results results={results}
+        <Hits results={results}
           helper={helper}
           noResultsTemplate={params.templates.noResults}
           hitTemplate={params.templates.hit} />,
