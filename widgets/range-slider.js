32ccd9352151b4bfc8dd50486a051738b1596889
vvo <vincent.voyer@algolia.com>
2015-09-15 10:41:26 +0200

    refactor(slider): some refine algorithm change after PR review

--- widgets/range-slider.js
+++ widgets/range-slider.js
@@ -18,33 +18,35 @@ function rangeSlider({
   var Slider = require('../components/Slider');
 
   var containerNode = utils.getContainerNode(container);
+  var currentValues = {
+    min: -Infinity,
+    max: Infinity
+  };
 
   return {
     getConfiguration: () => ({
       disjunctiveFacets: [facetName]
     }),
-    _currentValues: {
-      min: -Infinity,
-      max: Infinity
-    },
     _refine(helper, stats, newValues) {
       var min = helper.state.getNumericRefinement(facetName, '>=');
       var max = helper.state.getNumericRefinement(facetName, '<=');
 
-      this._currentValues = {
-        min: Math.max(min && min.length && min[0] || 0, stats.min),
-        max: Math.min(max && max.length && max[0] || Infinity, stats.max)
+      currentValues = {
+        // min: Math.max(min && min.length && min[0] || -Infinity, stats.min),
+        min: min && min.length ? Math.max.apply(Math, min.concat(stats.min)) : stats.min,
+        // max: Math.min(max && max.length && max[0] || Infinity, stats.max)
+        max: max && max.length ? Math.max.apply(Math, max.concat(stats.max)) : stats.max
       };
 
-      if (this._currentValues.min !== newValues[0] || this._currentValues.max !== newValues[1]) {
-        this._currentValues = {
+      if (currentValues.min !== newValues[0] || currentValues.max !== newValues[1]) {
+        currentValues = {
           min: newValues[0],
           max: newValues[1]
         };
 
         helper.clearRefinements(facetName);
-        helper.addNumericRefinement(facetName, '>=', this._currentValues.min);
-        helper.addNumericRefinement(facetName, '<=', this._currentValues.max);
+        helper.addNumericRefinement(facetName, '>=', currentValues.min);
+        helper.addNumericRefinement(facetName, '<=', currentValues.max);
         helper.search();
       }
     },
@@ -53,7 +55,7 @@ function rangeSlider({
 
       React.render(
         <Slider
-          start={[this._currentValues.min, this._currentValues.max]}
+          start={[currentValues.min, currentValues.max]}
           range={{min: stats.min, max: stats.max}}
           onChange={this._refine.bind(this, helper, stats)}
           tooltips={tooltips}
