20aedf58126d8f5f4d73ed961c64ee5fd13df4e3
vvo <vincent.voyer@algolia.com>
2015-10-22 14:28:54 +0200

    chore(build): silence react dev warning when using the production build
    fixes #330

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,20 +10,20 @@ bundle='instantsearch'
 
 printf "\n\nBuild"
 
-printf "\n\nBuild:webpack"
+printf "\n\nBuild: webpack\n"
 webpack
 
-printf "\n\nBuild: minify"
+printf "\n\nBuild: minify\n"
 cat dist/$bundle.js | uglifyjs -c warnings=false -m > dist/$bundle.min.js
 
-printf "\n\nBuild: CSS"
+printf "\n\nBuild: CSS\n"
 cp themes/default.css dist/themes/default.css
 cleancss dist/themes/default.css > dist/themes/default.min.css
 
-printf "\n\nBuild: prepend license"
+printf "\n\nBuild: prepend license\n"
 printf "$license" | cat - dist/"$bundle".js > /tmp/out && mv /tmp/out dist/"$bundle".js
 printf "$license" | cat - dist/"$bundle".min.js > /tmp/out && mv /tmp/out dist/"$bundle".min.js
 
-printf "\n\nBuild: filesize"
+printf "\n\nBuild: filesize\n"
 
 printf "=> $bundle.min.js gzipped will weight `cat dist/$bundle.min.js | gzip -9 | wc -c | pretty-bytes`\n"
