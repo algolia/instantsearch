773ff2e0b36ca003e45ba6f2748c664238eb3c13
Pixelastic <tim@pixelastic.com>
2015-10-28 11:58:31 +0100

    test(decorators): Use TestComponent instead of string

---
+++ decorators/__tests__/TestComponent.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+// This component is only used in tests, as a placeholder
+class TestComponent extends React.Component {
+  render() {
+    return <div {...this.props} />;
+  }
+}
+
+
+module.exports = TestComponent;
