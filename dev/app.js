a29e9c703fbf3433559728ac714f2531170f131c
Maxime Locqueville <maxiloc@gmail.com>
2015-11-03 18:10:11 +0100

    feat(numericRefinementList): create numericRefinementList widget using refinementList component

--- dev/app.js
+++ dev/app.js
@@ -93,6 +93,30 @@ search.addWidget(
 );
 
 search.addWidget(
+  instantsearch.widgets.numericRefinementList({
+    container: '#price-numeric-list',
+    attributeName: 'price',
+    operator: 'or',
+    options: [
+      {name: 'All'},
+      {end: 4, name: 'less than 4'},
+      {start: 4, end: 4, name: '4'},
+      {start: 5, end: 10, name: 'between 5 and 10'},
+      {start: 10, name: 'more than 10'}
+    ],
+    cssClasses: {
+      header: 'facet-title',
+      link: 'facet-value',
+      count: 'facet-count pull-right',
+      active: 'facet-active'
+    },
+    templates: {
+      header: 'Price numeric list'
+    }
+  })
+);
+
+search.addWidget(
   instantsearch.widgets.refinementList({
     container: '#price-range',
     facetName: 'price_range',
@@ -186,5 +210,23 @@ search.addWidget(
 search.once('render', function() {
   document.querySelector('.search').className = 'row search search--visible';
 });
+
+search.addWidget(
+  instantsearch.widgets.priceRanges({
+    container: '#price-ranges',
+    attributeName: 'price',
+    templates: {
+      header: 'Price ranges'
+    },
+    cssClasses: {
+      header: 'facet-title',
+      body: 'nav nav-stacked',
+      range: 'facet-value',
+      form: '',
+      input: 'fixed-input-sm',
+      button: 'btn btn-default btn-sm'
+    }
+  })
+);
 
 search.start();
