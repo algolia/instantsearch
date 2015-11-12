328c71bb7231739c39336e80557e8a46d804e7d8
vvo <vincent.voyer@algolia.com>
2015-11-12 15:37:05 +0100

    docs(gh-pages): its gh-pages not gh_pages

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -9,4 +9,4 @@ rm -rf node_modules/gh-pages/.cache
 rm -rf _site
 bundle install
 JEKYLL_ENV=production VERSION=${VERSION:-UNRELEASED} bundle exec jekyll build
-gh-pages --dist _site --branch gh_pages
+gh-pages --dist _site --branch gh-pages
