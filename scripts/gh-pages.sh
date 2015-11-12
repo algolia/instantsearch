d05fa0b33ec0c492025dca573a36a01b63861a02
Shipow <shipowlata@gmail.com>
2015-11-13 00:00:54 +0100

    chore(website): ecommerce example + examples

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -9,4 +9,10 @@ rm -rf node_modules/gh-pages/.cache
 rm -rf _site
 bundle install
 JEKYLL_ENV=production VERSION=${VERSION:-UNRELEASED} bundle exec jekyll build
+for example in _site/examples/*; do
+  if [ -d "$example" ]; then
+    name=`basename "$example"`
+    (cd "$example" && zip -r ../$name.zip *)
+  fi
+done
 gh-pages --dist _site --branch gh-pages
