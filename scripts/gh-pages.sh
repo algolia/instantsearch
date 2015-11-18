9705f803090dd42ad01ac8111b781d819a3e9abe
vvo <vincent.voyer@algolia.com>
2015-11-18 10:07:05 +0100

    docs(search): manual search index release for now

--- scripts/gh-pages.sh
+++ scripts/gh-pages.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+: ${VERSION?"Please provide the VERSION env variable"}
+
 set -e # exit when error
 
 printf "\nPublish website to gh-pages\n"
@@ -8,12 +10,11 @@ cd docs
 rm -rf node_modules/gh-pages/.cache
 rm -rf _site
 bundle install
-JEKYLL_ENV=production VERSION=${VERSION:-UNRELEASED} bundle exec jekyll build
+JEKYLL_ENV=production VERSION=${VERSION} bundle exec jekyll build
 for example in _site/examples/*; do
   if [ -d "$example" ]; then
     name=`basename "$example"`
     (cd "$example" && zip -r ../$name.zip *)
   fi
 done
-bundle exec jekyll algolia push
 gh-pages --dist _site --branch gh-pages
