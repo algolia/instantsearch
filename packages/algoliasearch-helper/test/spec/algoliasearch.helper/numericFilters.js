34a08765738792bef02a869c955a22266f79a5c9
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2016-02-17 11:16:01 +0100

    fix(numeric filters): Makes it possible to add then remove a string based numeric filter
    It also fixes the usage of arrays: we can now remove arrays from numeric
    filters.

--- packages/algoliasearch-helper/test/spec/algoliasearch.helper/numericFilters.js
+++ packages/algoliasearch-helper/test/spec/algoliasearch.helper/numericFilters.js
@@ -80,51 +80,57 @@ test('Should be able to remove values one by one even 0s', function(t) {
   t.end();
 });
 
-test('Should remove all the numeric values for a single operator if remove is called with two arguments', function(t) {
-  var helper = algoliasearchHelper(null, null, null);
+test(
+  'Should remove all the numeric values for a single operator if remove is called with two arguments',
+  function(t) {
+    var helper = algoliasearchHelper(null, null, null);
 
-  helper.addNumericRefinement('attribute', '>', 0);
-  helper.addNumericRefinement('attribute', '>', 4);
-  helper.addNumericRefinement('attribute', '<', 4);
-  t.deepEqual(
-    helper.state.numericRefinements.attribute,
-    {'>': [0, 4], '<': [4]},
-    'should be set to {">": [0, 4], "<":[4]} initially');
-  helper.removeNumericRefinement('attribute', '>');
-  t.equal(helper.state.numericRefinements.attribute['>'], undefined, 'should set to undefined');
-  t.deepEqual(
-    helper.state.numericRefinements.attribute['<'],
-    [4],
-    'the value of the other should be the same');
+    helper.addNumericRefinement('attribute', '>', 0);
+    helper.addNumericRefinement('attribute', '>', 4);
+    helper.addNumericRefinement('attribute', '<', 4);
+    t.deepEqual(
+      helper.state.numericRefinements.attribute,
+      {'>': [0, 4], '<': [4]},
+      'should be set to {">": [0, 4], "<":[4]} initially');
+    helper.removeNumericRefinement('attribute', '>');
+    t.equal(helper.state.numericRefinements.attribute['>'], undefined, 'should set to undefined');
+    t.deepEqual(
+      helper.state.numericRefinements.attribute['<'],
+      [4],
+      'the value of the other should be the same');
 
-  t.deepEqual(
-    helper.getRefinements('attribute'),
-    [{type: 'numeric', operator: '<', value: [4]}],
-    'should have the same result with getRefinements');
+    t.deepEqual(
+      helper.getRefinements('attribute'),
+      [{type: 'numeric', operator: '<', value: [4]}],
+      'should have the same result with getRefinements');
 
-  t.end();
-});
+    t.end();
+  }
+);
 
-test('Should remove all the numeric values for an attribute if remove is called with one argument', function(t) {
-  var helper = algoliasearchHelper(null, null, null);
+test(
+  'Should remove all the numeric values for an attribute if remove is called with one argument',
+  function(t) {
+    var helper = algoliasearchHelper(null, null, null);
 
-  helper.addNumericRefinement('attribute', '>', 0);
-  helper.addNumericRefinement('attribute', '>', 4);
-  helper.addNumericRefinement('attribute', '<', 4);
-  t.deepEqual(
-    helper.state.numericRefinements.attribute,
-    {'>': [0, 4], '<': [4]},
-    'should be set to {">": [0, 4], "<":[4]} initially');
-  helper.removeNumericRefinement('attribute');
-  t.equal(helper.state.numericRefinements.attribute, undefined, 'should set to undefined');
+    helper.addNumericRefinement('attribute', '>', 0);
+    helper.addNumericRefinement('attribute', '>', 4);
+    helper.addNumericRefinement('attribute', '<', 4);
+    t.deepEqual(
+      helper.state.numericRefinements.attribute,
+      {'>': [0, 4], '<': [4]},
+      'should be set to {">": [0, 4], "<":[4]} initially');
+    helper.removeNumericRefinement('attribute');
+    t.equal(helper.state.numericRefinements.attribute, undefined, 'should set to undefined');
 
-  t.deepEqual(
-    helper.getRefinements('attribute'),
-    [],
-    'should have the same result with getRefinements');
+    t.deepEqual(
+      helper.getRefinements('attribute'),
+      [],
+      'should have the same result with getRefinements');
 
-  t.end();
-});
+    t.end();
+  }
+);
 
 test('Should be able to get if an attribute has numeric filter with hasRefinements', function(t) {
   var helper = algoliasearchHelper(null, null, null);
@@ -132,6 +138,42 @@ test('Should be able to get if an attribute has numeric filter with hasRefinemen
   t.notOk(helper.hasRefinements('attribute'), 'not refined initially');
   helper.addNumericRefinement('attribute', '=', 42);
   t.ok(helper.hasRefinements('attribute'), 'should be refined');
+
+  t.end();
+});
+
+test('Should be able to remove the value even if it was a string used as a number', function(t) {
+  var attributeName = 'attr';
+  var n = '42';
+
+  var helper = algoliasearchHelper({}, 'index', {});
+
+  // add string - removes string
+  helper.addNumericRefinement(attributeName, '=', n);
+  t.ok(helper.state.isNumericRefined(attributeName, '=', n), 'should contain the numeric refinement "= 42"');
+  helper.removeNumericRefinement(attributeName, '=', n);
+  t.notOk(
+    helper.state.isNumericRefined(attributeName, '=', n),
+    'should not contain the numeric refinement = 42'
+  );
+
+  // add number - removes string
+  helper.addNumericRefinement(attributeName, '=', 42);
+  t.ok(helper.state.isNumericRefined(attributeName, '=', 42), 'should contain the numeric refinement "= 42"');
+  helper.removeNumericRefinement(attributeName, '=', n);
+  t.notOk(
+    helper.state.isNumericRefined(attributeName, '=', 42),
+    'should not contain the numeric refinement = 42'
+  );
+
+  // add string - removes number
+  helper.addNumericRefinement(attributeName, '=', n);
+  t.ok(helper.state.isNumericRefined(attributeName, '=', n), 'should contain the numeric refinement "= 42"');
+  helper.removeNumericRefinement(attributeName, '=', 42);
+  t.notOk(
+    helper.state.isNumericRefined(attributeName, '=', n),
+    'should not contain the numeric refinement = 42'
+  );
 
   t.end();
 });
