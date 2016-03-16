e62cfe503de56fc0764023046ba96afe956609e0
vvo <vincent.voyer@algolia.com>
2016-03-16 09:47:43 +0100

    chore(ci): if not a PR, we are good

--- scripts/validate-pr-done-on-develop.sh
+++ scripts/validate-pr-done-on-develop.sh
@@ -5,11 +5,11 @@ set -e # exit when error
 COMMIT_MSG=$(git log --format=%B --no-merges -n 1)
 [[ "$COMMIT_MSG" =~ hotfix ]] && is_hotfix=1 || is_hotfix=0
 [[ "$COMMIT_MSG" =~ /^docs/ ]] && is_doc=1 || is_doc=0
-[[ "$TRAVIS_PULL_REQUEST" == false ]] && is_travis=1 || is_travis=0
+[[ "$TRAVIS_PULL_REQUEST" == false ]] && is_pr=1 || is_pr=0
 [[ "$TRAVIS_BRANCH" == master ]] && is_master=1 || is_master=0
 [[ "$TRAVIS_BRANCH" == develop ]] && is_develop=1 || is_develop=0
 
-if [[ $is_travis == 0 ]]; then
+if [[ $is_pr == 1 ]]; then
   echo "No need to check pull request done on develop branch when not in a pull request"
   exit 0
 fi
