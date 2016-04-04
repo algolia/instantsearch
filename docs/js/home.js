486d961d1ce7ac39b50d9c169a8d0a76b0d0dff8
Sylvain UTARD <sylvain.utard@gmail.com>
2016-04-04 13:47:35 +0200

    docs(website): merge files + remove bootstrap (we use CDN)

--- docs/js/home.js
+++ docs/js/home.js
@@ -3,6 +3,20 @@
 /* global $, TweenMax, TimelineMax, ScrollMagic, Power2, Bounce, THREE*/
 'use strict';
 
+// Park-Miller-Carta Pseudo-Random Number Generator
+// https://github.com/pnitsch/BitmapData.js/blob/master/js/BitmapData.js
+var PRNG = function () {
+  this.seed = 1;
+  this.next = function() {
+    return ( this.gen() / 2147483647 );
+  };
+  this.nextRange = function( min, max ) {
+    return min + ( ( max - min ) * this.next() )
+  };
+  this.gen = function() {
+    return this.seed = ( this.seed * 16807 ) % 2147483647;
+  };
+};
 
 $(function () {
   // init scroll magic
