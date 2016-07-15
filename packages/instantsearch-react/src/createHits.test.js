4676c02f89222cd262e7fe06af747cbe03a00cfa
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-07-15 10:22:08 +0200

    Test createHits

--- packages/instantsearch-react/src/createHits.test.js
+++ packages/instantsearch-react/src/createHits.test.js
@@ -1,38 +1,17 @@
 /* eslint-env jest, jasmine */
 
-import React from 'react';
-import {shallow} from 'enzyme';
+jest.mock('algoliasearch-helper-provider/src/connect', () =>
+  mapStateToProps => state => mapStateToProps(state)
+);
 
 import createHits from './createHits';
 jest.unmock('./createHits');
 
-import connect from 'algoliasearch-helper-provider/src/connect';
-jest.mock('algoliasearch-helper-provider/src/connect', () => {
-  let stateInput = {};
-  const output = mapStateToProps => Composed => function Connected(props) {
-    return <Composed {...mapStateToProps(stateInput)} {...props} />;
-  };
-  output.__setState = state => {
-    stateInput = state;
-  };
-  return output;
-});
-
-function testConnect(hoc, input, testOutput) {
-  function Dummy() {
-    return <div />;
-  }
-  const Connected = hoc(Dummy);
-  connect.__setState(input);
-  const wrapper = shallow(<Connected />);
-  testOutput(wrapper.find(Dummy).props());
-}
-
 describe('createHits', () => {
   it('provides the current hits to the component', () => {
     const hits = {};
-    testConnect(createHits, {searchResults: {hits}}, props =>
-      expect(props.hits).toBe(hits)
-    );
+    const props = createHits({searchResults: {hits}});
+    expect(Object.keys(props).length).toBe(1);
+    expect(props.hits).toBe(hits);
   });
 });
