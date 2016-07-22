597310cbec244e1652f85a6bb3a21a88b4acf9e5
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-07-22 17:37:33 +0200

    Remove unused import

--- packages/instantsearch-react/src/createHOC.js
+++ packages/instantsearch-react/src/createHOC.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import {connect} from 'react-algoliasearch-helper';
-import {SearchParameters} from 'algoliasearch-helper';
 
 import {stateManagerPropType, configManagerPropType} from './propTypes';
 
