f054e48b2d2dd3d8481bcb47e0074999b8bb221a
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-07-26 10:56:38 +0200

    Test InstantSearch and createStateManager

--- packages/instantsearch-react/src/InstantSearch.test.js
+++ packages/instantsearch-react/src/InstantSearch.test.js
@@ -1,34 +1,298 @@
 /* eslint-env jest, jasmine */
 
 import React from 'react';
-import {mount} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 
-import {AlgoliaSearchHelper} from 'algoliasearch-helper';
-jest.unmock('algoliasearch-helper');
-jest.unmock('algoliasearch');
+import algoliasearch from 'algoliasearch';
 jest.unmock('debug');
+import algoliasearchHelper from 'algoliasearch-helper';
+
+import createStateManager from './createStateManager';
+import createConfigManager from './createConfigManager';
 
 import InstantSearch from './InstantSearch';
-jest.unmock('history');
-
 jest.unmock('./InstantSearch');
-jest.unmock('./createConfigManager');
-jest.unmock('./createStateManager');
+
+jest.mock('algoliasearch', () => jest.fn(() => ({
+  search: jest.fn(),
+})));
+
+jest.mock('algoliasearch-helper', () => {
+  const mock = jest.fn(() => {
+    let state = {};
+    const helper = {
+      setState: jest.fn(s => {
+        state = s;
+      }),
+      getState: jest.fn(() => state),
+      search: jest.fn(),
+    };
+    mock.__helpers.push(helper);
+    return helper;
+  });
+  mock.SearchParameters = function MockSearchParameters(s) {
+    Object.assign(this, s);
+  };
+  mock.__helpers = [];
+  return mock;
+});
+
+jest.mock('./createStateManager', () => {
+  let state = {};
+  const mock = jest.fn(() => ({
+    __isStateManager: true,
+    getState: jest.fn(() => state),
+    unlisten: jest.fn(),
+  }));
+  mock.__setState = s => {
+    state = s;
+  };
+  return mock;
+});
+
+jest.mock('./createConfigManager', () => {
+  let getState = s => s;
+  const mock = jest.fn(() => ({
+    __isConfigManager: true,
+    getState: jest.fn(s => getState(s)),
+  }));
+  mock.__setGetState = s => {
+    getState = s;
+  };
+  return mock;
+});
+
+jest.mock('history', () => ({
+  createHistory: jest.fn(() => ({
+    __isHistory: true,
+  })),
+  createMemoryHistory: jest.fn(() => ({
+    __isMemoryHistory: true,
+  })),
+}));
 
 describe('InstantSearch', () => {
+  afterEach(() => {
+    algoliasearch.mockClear();
+    algoliasearchHelper.mockClear();
+    algoliasearchHelper.__helpers = [];
+    createStateManager.mockClear();
+    createStateManager.__setState({});
+    createConfigManager.mockClear();
+    createConfigManager.__setGetState(s => s);
+  });
+
   it('correctly instantiates the API client and helper', () => {
+    mount(
+      <InstantSearch appId="foo" apiKey="bar" indexName="foobar">
+        <div />
+      </InstantSearch>
+    );
+    expect(algoliasearch.mock.calls[0]).toEqual(['foo', 'bar']);
+    expect(algoliasearchHelper.mock.calls[0][1]).toBe('foobar');
+  });
+
+  it('exposes a configManager and a stateManager in context', () => {
     const wrapper = mount(
       <InstantSearch appId="foo" apiKey="bar" indexName="foobar">
         <div />
       </InstantSearch>
     );
-    const {helper} = wrapper.instance();
-    expect(helper.getState().index).toBe('foobar');
-    expect(helper.client.applicationID).toBe('foo');
-    expect(helper.client.apiKey).toBe('bar');
+
+    const {
+      algoliaConfigManager,
+      algoliaStateManager,
+    } = wrapper.instance().getChildContext();
+    expect(algoliaConfigManager.__isConfigManager).toBe(true);
+    expect(algoliaStateManager.__isStateManager).toBe(true);
   });
 
-  it('exposes a configManager', () => {
+  it('accepts a custom history', () => {
+    const history = {};
+    mount(
+      <InstantSearch
+        appId="foo"
+        apiKey="bar"
+        indexName="foobar"
+        history={history}
+      >
+        <div />
+      </InstantSearch>
+    );
 
+    // Just test that it correctly passes it through to its state manager
+    expect(createStateManager.mock.calls[0][0]).toBe(history);
+  });
+
+  it('creates a memory history when no history and urlSync=false', () => {
+    mount(
+      <InstantSearch
+        appId="foo"
+        apiKey="bar"
+        indexName="foobar"
+        urlSync={false}
+      >
+        <div />
+      </InstantSearch>
+    );
+
+    expect(createStateManager.mock.calls[0][0].__isMemoryHistory).toBe(true);
+  });
+
+  it('creates a browser history when no history and urlSync=true', () => {
+    mount(
+      <InstantSearch
+        appId="foo"
+        apiKey="bar"
+        indexName="foobar"
+        urlSync={true}
+      >
+        <div />
+      </InstantSearch>
+    );
+
+    expect(createStateManager.mock.calls[0][0].__isHistory).toBe(true);
+  });
+
+  it('passes its treshold and createURL props to its stateManager', () => {
+    const createURL = jest.fn();
+    const treshold = 666;
+    mount(
+      <InstantSearch
+        appId="foo"
+        apiKey="bar"
+        indexName="foobar"
+        createURL={createURL}
+        treshold={treshold}
+      >
+        <div />
+      </InstantSearch>
+    );
+
+    expect(createStateManager.mock.calls[0][2].createURL).toBe(createURL);
+    expect(createStateManager.mock.calls[0][2].treshold).toBe(treshold);
+  });
+
+  it('calls search on didMount and not on willMount', () => {
+    let helper;
+
+    shallow(
+      <InstantSearch
+        appId="foo"
+        apiKey="bar"
+        indexName="foobar"
+      >
+        <div />
+      </InstantSearch>
+    );
+
+    helper = algoliasearchHelper.__helpers[0];
+    expect(helper.search.mock.calls.length).toBe(0);
+
+    mount(
+      <InstantSearch
+        appId="foo"
+        apiKey="bar"
+        indexName="foobar"
+      >
+        <div />
+      </InstantSearch>
+    );
+
+    helper = algoliasearchHelper.__helpers[1];
+    expect(helper.search.mock.calls.length).toBe(1);
+  });
+
+  function getHelper(configureState = null) {
+    mount(
+      <InstantSearch
+        appId="foo"
+        apiKey="bar"
+        indexName="foobar"
+        configureState={configureState}
+      >
+        <div />
+      </InstantSearch>
+    );
+
+    const helper = algoliasearchHelper.__helpers[0];
+    algoliasearchHelper.__helpers = [];
+    return helper;
+  }
+
+  it('correctly creates initial state', () => {
+    let helper;
+
+    helper = getHelper();
+    expect(helper.setState.mock.calls[0][0]).toEqual({index: 'foobar'});
+
+    helper = getHelper(s => ({...s, page: 1}));
+    expect(helper.setState.mock.calls[0][0]).toEqual({
+      index: 'foobar',
+      page: 1,
+    });
+
+    createStateManager.__setState({index: '', page: 2, hitsPerPage: 4});
+    helper = getHelper(s => ({...s, page: s.page + 1, hitsPerPage: 5}));
+    expect(helper.setState.mock.calls[0][0]).toEqual({
+      index: 'foobar',
+      hitsPerPage: 5,
+      page: 3,
+    });
+
+    createConfigManager.__setGetState(s => ({...s, hitsPerPage: 6}));
+    helper = getHelper(s => ({...s, page: s.page + 1, hitsPerPage: 5}));
+    expect(helper.setState.mock.calls[0][0]).toEqual({
+      index: 'foobar',
+      hitsPerPage: 6,
+      page: 3,
+    });
+  });
+
+  it('correctly updates its helper state upon config or state change', () => {
+    const helper = getHelper();
+    const onConfigChange = createConfigManager.mock.calls[0][0];
+    const onStateChange = createStateManager.mock.calls[0][1];
+
+    createConfigManager.__setGetState(s => ({...s, page: 1}));
+    onConfigChange();
+    expect(helper.setState.mock.calls[2][0]).toEqual({
+      index: 'foobar',
+      page: 1,
+    });
+    expect(helper.search.mock.calls.length).toBe(2);
+
+    onConfigChange();
+    expect(helper.search.mock.calls.length).toBe(2);
+
+    createStateManager.__setState({hitsPerPage: 5});
+    onStateChange();
+    expect(helper.setState.mock.calls[3][0]).toEqual({
+      index: 'foobar',
+      page: 1,
+      hitsPerPage: 5,
+    });
+    expect(helper.search.mock.calls.length).toBe(3);
+
+    onStateChange();
+    expect(helper.search.mock.calls.length).toBe(3);
+  });
+
+  it('cleans up after itself', () => {
+    const wrapper = mount(
+      <InstantSearch
+        appId="foo"
+        apiKey="bar"
+        indexName="foobar"
+      >
+        <div />
+      </InstantSearch>
+    );
+
+    const {algoliaStateManager} = wrapper.instance().getChildContext();
+
+    expect(algoliaStateManager.unlisten.mock.calls.length).toBe(0);
+    wrapper.unmount();
+    expect(algoliaStateManager.unlisten.mock.calls.length).toBe(1);
   });
 });
--- packages/instantsearch-react/src/createStateManager.test.js
+++ packages/instantsearch-react/src/createStateManager.test.js
@@ -1,20 +1,160 @@
 /* eslint-env jest, jasmine */
 
+import MockDate from 'mockdate';
+
 import createStateManager from './createStateManager';
 jest.unmock('./createStateManager');
 jest.unmock('algoliasearch-helper');
 jest.unmock('algoliasearch-helper/src/url');
 
+const location = {
+  search: '?q=hellooo',
+};
+
+const createMockHistory = (initialLocation, overrides) => ({
+  listen: jest.fn(),
+  push: jest.fn(),
+  replace: jest.fn(),
+  getCurrentLocation: jest.fn(() => initialLocation),
+  createHref: jest.fn(),
+  ...overrides,
+});
+
 describe('createStateManager', () => {
   it('retrieves the initial state from the URL', () => {
     const stateManager = createStateManager({
       listen: listener => {
-        listener({
-          search: '?q=hellooo',
-        });
+        listener(location);
       },
-    }, () => null);
+    });
 
     expect(stateManager.getState().query).toBe('hellooo');
+  });
+
+  it('also works with history v3', () => {
+    const stateManager = createStateManager({
+      getCurrentLocation: () => location,
+      listen: () => null,
+    });
+
+    expect(stateManager.getState().query).toBe('hellooo');
+  });
+
+  it('updates its state on setState', () => {
+    const stateManager = createStateManager(
+      createMockHistory(location),
+      () => null
+    );
+
+    stateManager.setState({query: 'goodbye'});
+    expect(stateManager.getState().query).toBe('goodbye');
+  });
+
+  it('calls its onChange callback on setState', () => {
+    const onChange = jest.fn();
+    const stateManager = createStateManager(
+      createMockHistory(location),
+      onChange
+    );
+
+    expect(onChange.mock.calls.length).toBe(0);
+    stateManager.setState({query: 'goodbye'});
+    expect(onChange.mock.calls.length).toBe(1);
+  });
+
+  it('pushes a new history entry on setState', () => {
+    const history = createMockHistory(location, {
+      createHref: a => a,
+    });
+    const stateManager = createStateManager(
+      history,
+      () => null
+    );
+
+    expect(history.push.mock.calls.length).toBe(0);
+    stateManager.setState({query: 'goodbye'});
+    expect(history.push.mock.calls.length).toBe(1);
+    expect(history.push.mock.calls[0][0].search).toBe('?q=goodbye');
+  });
+
+  it('replaces instead of pushing at intervals shorter than treshold', () => {
+    function testTreshold(treshold) {
+      const history = createMockHistory(location, {
+        createHref: a => a,
+      });
+
+      const stateManager = createStateManager(
+        history,
+        () => null,
+        {treshold}
+      );
+
+      MockDate.set(0);
+      expect(history.push.mock.calls.length).toBe(0);
+      stateManager.setState({query: 'goodbye'});
+      expect(history.push.mock.calls.length).toBe(1);
+      expect(history.push.mock.calls[0][0].search).toBe('?q=goodbye');
+      stateManager.setState({query: 'au revoir'});
+      expect(history.push.mock.calls.length).toBe(1);
+      expect(history.replace.mock.calls.length).toBe(1);
+      expect(history.replace.mock.calls[0][0].search).toBe('?q=au%20revoir');
+      MockDate.set(treshold + 1);
+      stateManager.setState({query: 'goodbye'});
+      expect(history.replace.mock.calls.length).toBe(1);
+      expect(history.push.mock.calls.length).toBe(2);
+      expect(history.push.mock.calls[1][0].search).toBe('?q=goodbye');
+      stateManager.setState({query: 'au revoir'});
+      expect(history.push.mock.calls.length).toBe(2);
+      expect(history.replace.mock.calls.length).toBe(2);
+      expect(history.replace.mock.calls[1][0].search).toBe('?q=au%20revoir');
+      MockDate.reset();
+    }
+
+    testTreshold(0);
+    testTreshold(500);
+  });
+
+  it('uses the createURL function option when provided', () => {
+    const history = createMockHistory(location, {
+      createHref: a => a,
+    });
+
+    const createURL = jest.fn(a => a);
+    const stateManager = createStateManager(
+      history,
+      () => null,
+      {createURL}
+    );
+
+    const state = {query: 'goodbye'};
+    stateManager.setState(state);
+    expect(history.push.mock.calls.length).toBe(1);
+    expect(history.push.mock.calls[0][0]).toBe(state);
+    expect(createURL.mock.calls[0][0]).toBe(state);
+
+    // And passes a stateToQueryString function
+    const stateToQueryString = createURL.mock.calls[0][1];
+    expect(typeof createURL.mock.calls[0][1]).toBe('function');
+    expect(stateToQueryString(state)).toBe('q=goodbye');
+    expect(stateToQueryString(state, {wat: 'wut'})).toBe('q=goodbye&wat=wut');
+
+    expect(stateManager.createURL(state)).toBe(state);
+    expect(createURL.mock.calls[1][0]).toBe(state);
+    expect(typeof createURL.mock.calls[1][1]).toBe('function');
+  });
+
+  it('correctly cleans up after itself when calling unlisten', () => {
+    const unlisten = jest.fn();
+    const history = createMockHistory(location, {
+      listen: () => unlisten,
+    });
+
+    const stateManager = createStateManager(
+      history,
+      () => null
+    );
+
+    stateManager.unlisten();
+    expect(unlisten.mock.calls.length).toBe(1);
   });
 });
