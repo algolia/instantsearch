b974ed84ec890b1ae317b2cbce31ab125595f0d1
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-08-02 11:29:18 +0200

    Clean up shared propTypes while we iterate on their API

--- packages/instantsearch-react/src/propTypes.js
+++ packages/instantsearch-react/src/propTypes.js
@@ -1,39 +1,5 @@
 import {PropTypes} from 'react';
 
-const valuePropTypes = PropTypes.oneOfType([
-  PropTypes.number,
-  PropTypes.string,
-]);
-
-const metadataItemPropType = PropTypes.shape({
-  value: valuePropTypes.isRequired,
-  count: PropTypes.number.isRequired,
-});
-
-export const hierarchicalItemPropType = PropTypes.shape({
-  label: PropTypes.node,
-  value: valuePropTypes.isRequired,
-  count: PropTypes.number.isRequired,
-  children: (...args) => PropTypes.arrayOf(hierarchicalItemPropType)(...args),
-});
-
-const valueItemPropType = valuePropTypes;
-
-export const itemPropType = PropTypes.oneOfType([
-  metadataItemPropType,
-  valueItemPropType,
-]);
-
-export const itemsPropType = PropTypes.oneOfType([
-  PropTypes.arrayOf(metadataItemPropType),
-  PropTypes.arrayOf(valueItemPropType),
-]);
-
-export const hierarchicalItemsPropType =
-  PropTypes.arrayOf(hierarchicalItemPropType);
-
-export const selectedItemsPropType = PropTypes.arrayOf(valuePropTypes);
-
 export const configManagerPropType = PropTypes.shape({
   register: PropTypes.func.isRequired,
   swap: PropTypes.func.isRequired,
