c9cd9be8f3b38b6b12a35dac5e0625f5f7e768dc
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-08-08 14:17:34 +0200

    Example fixes

--- packages/instantsearch-react/examples/Search.js
+++ packages/instantsearch-react/examples/Search.js
@@ -4,7 +4,6 @@ import {omit} from 'lodash';
 import qs from 'qs';
 
 import InstantSearch from '../src/InstantSearch';
-import connectCurrentFilters from '../src/connectors/connectCurrentFilters';
 import {
   Pagination,
   RefinementList,
@@ -16,6 +15,7 @@ import {
   NumericRefinementList,
   Range,
   CurrentFilters,
+  Toggle,
 } from '../';
 import history from './history';
 
@@ -35,31 +35,45 @@ class Search extends Component {
         apiKey="6be0576ff61c053d5f9a3225e2a90f76"
         indexName="instant_search"
         history={history}
-        createURL={this.createURL}
-        onStateChange={this.onStateChange}
-        state={{
-          ...this.props.location.query,
-          p: parseInt(this.props.params.page - 1, 10),
-        }}
+        treshold={700}
+        //createURL={this.createURL}
+        //onStateChange={this.onStateChange}
+        //state={{
+        //  ...this.props.location.query,
+        //  p: parseInt(this.props.params.page - 1, 10),
+        //}}
       >
         <div>
           <SearchBox />
           <CurrentFilters />
+          <Toggle
+            attributeName="free_shipping"
+            value={true}
+            label="Free shipping"
+          />
           <HitsPerPage values={[10, 20, 30]} defaultHitsPerPage={20} />
           <Range attributeName="price" />
-          {/*<NumericRefinementList
-            attributeName="price"
+          <NumericRefinementList
+            attributeName="rating"
             items={[
               {
                 label: 'All',
               },
               {
-                label: 'More than 60',
-                start: 60,
+                label: 'More than 5',
+                start: 5,
+              },
+              {
+                label: 'More than 3',
+                start: 3,
+              },
+              {
+                label: 'Between 1 and 4',
+                start: 1,
+                end: 4,
               },
             ]}
-            defaultSelectedItem="60:"
-          />*/}
+          />
           <RefinementList
             attributeName="categories"
             defaultSelectedItems={['Audio']}
