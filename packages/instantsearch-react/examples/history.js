239a6addfa09cdc98a97c5825eab659616cf924e
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-08-08 17:07:56 +0200

    Sort URL parameters for deterministic URLs

--- packages/instantsearch-react/examples/history.js
+++ packages/instantsearch-react/examples/history.js
@@ -2,7 +2,17 @@ import {useRouterHistory} from 'react-router';
 import {createHistory} from 'history';
 import qs from 'qs';
 
+function alphabeticalSort(a, b) {
+  if (a > b) {
+    return 1;
+  }
+  if (a === b) {
+    return 0;
+  }
+  return -1;
+}
+
 export default useRouterHistory(createHistory)({
   parseQueryString: qs.parse,
-  stringifyQuery: qs.stringify,
+  stringifyQuery: q => qs.stringify(q, {sort: alphabeticalSort}),
 });
