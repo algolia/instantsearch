bb9fa0c4692ced93638915fecbbed165fa60738b
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-08-09 10:50:13 +0200

    .toString() => .toLocaleString()

--- packages/instantsearch-react/src/impl/RefinementListLinks.js
+++ packages/instantsearch-react/src/impl/RefinementListLinks.js
@@ -89,6 +89,6 @@ export default themeable({
 })(
   translatable({
     showMore: extended => extended ? 'Show less' : 'Show more',
-    count: count => count.toString(),
+    count: count => count.toLocaleString(),
   })(RefinementListLinks)
 );
