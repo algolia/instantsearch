26858b33ce5e7ed1f4074c637e907c7533d274b7
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-08-09 17:59:07 +0200

    Fix withKeysPropType

--- packages/instantsearch-react/src/propTypes.js
+++ packages/instantsearch-react/src/propTypes.js
@@ -17,7 +17,7 @@ export const withKeysPropType = keys => (props, propName, componentName) => {
   const prop = props[propName];
   if (prop) {
     for (const key of Object.keys(prop)) {
-      if (!keys.indexOf(key)) {
+      if (keys.indexOf(key) === -1) {
         return new Error(
           `Unknown \`${propName}\` key \`${key}\`. Check the render method ` +
           `of \`${componentName}\`.`
