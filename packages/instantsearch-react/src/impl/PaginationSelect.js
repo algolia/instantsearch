2686ebec4e809cc3ee13a2e4563d4ca10d111475
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-08-11 17:23:35 +0200

    Refactor list components

--- packages/instantsearch-react/src/impl/PaginationSelect.js
+++ packages/instantsearch-react/src/impl/PaginationSelect.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import themeable from '../themeable';
-import translatable from '../translatable';
 
 import Pagination from './Pagination';
 import Select from './Select';
@@ -21,19 +20,4 @@ function PaginationSelect(props) {
 
 export default themeable({
   root: 'PaginationSelect',
-})(
-  translatable({
-    previous: 'Previous page',
-    next: 'Next page',
-    first: 'First page',
-    last: 'Last page',
-    page: page => (page + 1).toString(),
-    ariaPrevious: 'Previous page',
-    ariaNext: 'Next page',
-    ariaFirst: 'First page',
-    ariaLast: 'Last page',
-    ariaPage: page => `Page ${(page + 1).toString()}`,
-  })(
-    PaginationSelect
-  )
-);
+})(PaginationSelect);
