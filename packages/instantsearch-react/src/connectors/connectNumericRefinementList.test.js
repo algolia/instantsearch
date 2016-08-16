3c7eefcba47f929febf0d732ba31771661be316e
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-08-16 17:41:39 +0200

    Test connectNumericRefinementList

--- packages/instantsearch-react/src/connectors/connectNumericRefinementList.test.js
+++ packages/instantsearch-react/src/connectors/connectNumericRefinementList.test.js
@@ -6,104 +6,145 @@ jest.unmock('algoliasearch-helper');
 import connectNumericRefinementList from './connectNumericRefinementList';
 jest.unmock('./connectNumericRefinementList');
 
-const {mapStateToProps, refine} = connectNumericRefinementList;
+const {
+  getProps,
+  refine,
+  getSearchParameters: getSP,
+  getMetadata,
+} = connectNumericRefinementList;
+
+let props;
+let params;
 
 describe('connectNumericRefinementList', () => {
   it('provides the correct props to the component', () => {
-    let searchParameters;
-    let props;
-
-    searchParameters = new SearchParameters();
-    props = mapStateToProps({searchParameters}, {
+    props = getProps({
       items: [
         {label: 'All'},
       ],
-    });
+    }, {});
     expect(props).toEqual({
       items: [
-        {label: 'All', value: ':'},
+        {label: 'All', value: ''},
       ],
-      selectedItem: ':',
+      selectedItem: '',
     });
 
-    searchParameters = new SearchParameters()
-      .addNumericRefinement('facet', '>=', '100');
-    props = mapStateToProps({searchParameters}, {
-      attributeName: 'facet',
+    props = getProps({
       items: [
         {label: 'All'},
         {label: 'Ok', start: 100},
       ],
-    });
+    }, {});
     expect(props).toEqual({
       items: [
-        {label: 'All', value: ':'},
+        {label: 'All', value: ''},
         {label: 'Ok', value: '100:'},
       ],
-      selectedItem: '100:',
+      selectedItem: '',
     });
 
-    searchParameters = new SearchParameters()
-      .addNumericRefinement('facet', '<=', '200');
-    props = mapStateToProps({searchParameters}, {
-      attributeName: 'facet',
+    props = getProps({
       items: [
         {label: 'All'},
         {label: 'Not ok', end: 200},
       ],
-    });
+    }, {});
     expect(props).toEqual({
       items: [
-        {label: 'All', value: ':'},
+        {label: 'All', value: ''},
         {label: 'Not ok', value: ':200'},
       ],
-      selectedItem: ':200',
+      selectedItem: '',
     });
 
-    searchParameters = new SearchParameters()
-      .addNumericRefinement('facet', '>=', '100')
-      .addNumericRefinement('facet', '<=', '200');
-    props = mapStateToProps({searchParameters}, {
-      attributeName: 'facet',
+    props = getProps({
       items: [
         {label: 'All'},
         {label: 'Ok', start: 100},
         {label: 'Not ok', end: 200},
         {label: 'Maybe ok?', start: 100, end: 200},
       ],
-    });
+    }, {});
     expect(props).toEqual({
       items: [
-        {label: 'All', value: ':'},
+        {label: 'All', value: ''},
         {label: 'Ok', value: '100:'},
         {label: 'Not ok', value: ':200'},
         {label: 'Maybe ok?', value: '100:200'},
       ],
-      selectedItem: '100:200',
+      selectedItem: '',
+    });
+
+    props = getProps({id: 'ok', items: []}, {ok: 'wat'});
+    expect(props).toEqual({items: [], selectedItem: 'wat'});
+
+    props = getProps({attributeName: 'ok', items: []}, {ok: 'wat'});
+    expect(props).toEqual({items: [], selectedItem: 'wat'});
+
+    props = getProps({id: 'ok', items: [], defaultSelectedItem: 'wat'}, {});
+    expect(props).toEqual({items: [], selectedItem: 'wat'});
+  });
+
+  it('calling refine updates the widget\'s state', () => {
+    const nextState = refine({id: 'ok'}, {otherKey: 'val'}, 'yep');
+    expect(nextState).toEqual({
+      otherKey: 'val',
+      ok: 'yep',
     });
   });
 
   it('refines the corresponding numeric facet', () => {
-    const state = new SearchParameters();
-    let refinedState;
+    const initSP = new SearchParameters();
 
-    refinedState = refine(state, {attributeName: 'facet'}, ':');
-    expect(refinedState.getNumericRefinements('facet')).toEqual({});
+    params = getSP(initSP, {attributeName: 'facet'}, {facet: ''});
+    expect(params.getNumericRefinements('facet')).toEqual({});
 
-    refinedState = refine(state, {attributeName: 'facet'}, '100:');
-    expect(refinedState.getNumericRefinements('facet')).toEqual({
+    params = getSP(initSP, {attributeName: 'facet'}, {facet: '100:'});
+    expect(params.getNumericRefinements('facet')).toEqual({
       '>=': [100],
     });
 
-    refinedState = refine(state, {attributeName: 'facet'}, ':200');
-    expect(refinedState.getNumericRefinements('facet')).toEqual({
+    params = getSP(initSP, {attributeName: 'facet'}, {facet: ':200'});
+    expect(params.getNumericRefinements('facet')).toEqual({
       '<=': [200],
     });
 
-    refinedState = refine(state, {attributeName: 'facet'}, '100:200');
-    expect(refinedState.getNumericRefinements('facet')).toEqual({
+    params = getSP(initSP, {attributeName: 'facet'}, {facet: '100:200'});
+    expect(params.getNumericRefinements('facet')).toEqual({
       '>=': [100],
       '<=': [200],
     });
+  });
+
+  it('registers its id in metadata', () => {
+    const metadata = getMetadata({id: 'ok'}, {});
+    expect(metadata).toEqual({id: 'ok', filters: []});
+  });
+
+  it('registers its filter in metadata', () => {
+    const metadata = getMetadata(
+      {
+        attributeName: 'wot',
+        items: [{
+          label: 'YAY',
+          start: 100,
+          end: 200,
+        }],
+      },
+      {wot: '100:200'}
+    );
+    expect(metadata).toEqual({
+      id: 'wot',
+      filters: [{
+        key: 'wot.100:200',
+        label: 'wot: YAY',
+        // Ignore clear, we test it later
+        clear: metadata.filters[0].clear,
+      }],
+    });
+
+    const state = metadata.filters[0].clear({wot: '100:200'});
+    expect(state).toEqual({wot: ''});
   });
 });
