14faccaf8a56fc6affa5f73eec7414b193819ae0
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-08-19 11:49:43 +0200

    Fix a few tests

--- packages/instantsearch-react/src/impl/Hits.test.js
+++ packages/instantsearch-react/src/impl/Hits.test.js
@@ -1,7 +1,6 @@
 /* eslint-env jest, jasmine */
 
 import React from 'react';
-import {mount} from 'enzyme';
 import renderer from 'react-test-renderer';
 
 import Hits from './Hits';
@@ -12,30 +11,13 @@ jest.unmock('../propTypes');
 describe('Hits', () => {
   it('accepts a itemComponent prop', () => {
     const hits = [{objectID: 0}, {objectID: 1}, {objectID: 2}];
-    const Hit = () => null;
-    const wrapper = mount(
+    const Hit = 'Hit';
+    const tree = renderer.create(
       <Hits
         itemComponent={Hit}
         hits={hits}
       />
     );
-    expect(wrapper.find(Hit).length).toBe(3);
-    expect(wrapper.find(Hit).everyWhere((c, i) =>
-      c.props().hit === hits[i]
-    )).toBe(true);
-  });
-
-  it('applies theme', () => {
-    const tree = renderer.create(
-      <HitsPerPage
-        values={[111, 333, 666]}
-        theme={{
-          label: 'HITS_LABEL',
-          value: v => `HITS_VALUE_${v}`,
-        }}
-        hitsPerPage={111}
-      />
-    ).toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(tree.toJSON()).toMatchSnapshot();
   });
 });
