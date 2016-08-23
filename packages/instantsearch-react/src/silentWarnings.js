3af34280918c90f40c3031b260656d970555d305
Alexandre Kirszenberg <alexandre.kirszenberg@algolia.com>
2016-08-23 17:25:26 +0200

    Update React and remove warning silencer

--- packages/instantsearch-react/src/silentWarnings.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-console */
-
-const error = console.error;
-console.error = (message, ...args) => {
-  if (message.indexOf('Missing React element for debugID') === -1) {
-    error.call(console, message, ...args);
-  }
-};
