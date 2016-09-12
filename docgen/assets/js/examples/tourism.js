69c558fae15adcd0985c8da67266e3e99702b420
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2016-09-12 19:01:33 +0200

    Add map

--- docgen/assets/js/examples/tourism.js
+++ docgen/assets/js/examples/tourism.js
@@ -1,5 +1,7 @@
-import {InstantSearch, Hits, SearchBox, Pagination, Range} from 'react-instantsearch';
+import {InstantSearch, Hits, SearchBox, Pagination, Range, RefinementList} from 'react-instantsearch';
 import React, {Component, PropTypes} from 'react';
+import GoogleMap from 'google-map-react';
+import {fitBounds} from 'google-map-react/utils';
 import ReactDOM from 'react-dom';
 
 class TourismInstantsearchSample extends Component {
@@ -48,7 +50,11 @@ class Filters extends Component {
 
             <div className="aisdemo-filters">
               <DatesAndGuest />
-              <RoomType />
+              <RoomType
+                attributeName="room_type"
+                operator="or"
+                sortBy={['name:asc']}
+                limitMin={3} />
               <Price />
             </div>
 
@@ -58,13 +64,62 @@ class Filters extends Component {
           </div>
 
           <div className="col-sm-5 aisdemo--right-column">
-            <div id="map"></div>
+            <ConnectedHitsMap />
           </div>
         </div>
       </div>
     );
   }
 }
+
+function CustomMarker() {
+  return (
+    <svg width="60" height="102" viewBox="0 0 102 60" className="marker">
+      <g fill="none" fillRule="evenodd">
+        <g transform="translate(-60, 0)" stroke="#8962B2" id="pin" viewBox="0 0 100 100">
+          <path d="M157.39 34.315c0 18.546-33.825 83.958-33.825 83.958S89.74 52.86 89.74 34.315C89.74 15.768 104.885.73 123.565.73c18.68 0 33.825 15.038 33.825 33.585z" strokeWidth="5.53" fill="#E6D2FC"></path>
+          <path d="M123.565 49.13c-8.008 0-14.496-6.498-14.496-14.52 0-8.017 6.487-14.52 14.495-14.52s14.496 6.503 14.496 14.52c0 8.022-6.487 14.52-14.495 14.52z" strokeWidth="2.765" fill="#FFF"></path>
+        </g>
+      </g>
+    </svg>);
+}
+
+function HitsMap({hits}) {
+  const availableSpace = {
+    width: document.body.getBoundingClientRect().width * 5 / 12,
+    height: 400,
+  };
+  const boundingPoints = hits.reduce((bounds, hit) => {
+    const pos = hit;
+    if (pos.lat > bounds.nw.lat) bounds.nw.lat = pos.lat;
+    if (pos.lat < bounds.se.lat) bounds.se.lat = pos.lat;
+
+    if (pos.lng < bounds.nw.lng) bounds.nw.lng = pos.lng;
+    if (pos.lng > bounds.se.lng) bounds.se.lng = pos.lng;
+    return bounds;
+  }, {
+    nw: {lat: -85, lng: 180},
+    se: {lat: 85, lng: -180},
+  });
+  const boundsConfig = fitBounds(boundingPoints, availableSpace);
+  const markers = hits.map(hit => <CustomMarker lat={hit.lat} lng={hit.lng} key={hit.objectID}></CustomMarker>);
+  const options = {
+    minZoomOverride: true,
+    minZoom: 1,
+  };
+  return (
+    <GoogleMap
+      options={() => options}
+      bootstrapURLKeys={{
+        key: 'AIzaSyAl60n7p07HYQK6lVilAe_ggwbBJFktNw8',
+      }}
+      center={boundsConfig.center}
+      zoom={boundsConfig.zoom}
+    >{markers}</GoogleMap>
+  );
+}
+
+const ConnectedHitsMap = Hits.connect(HitsMap);
 
 class DatesAndGuest extends Component {
   render() {
@@ -79,16 +134,43 @@ class DatesAndGuest extends Component {
   }
 }
 
-class RoomType extends Component {
-  render() {
+const RoomType = RefinementList.connect(({items, refine, selectedItems}) => {
+  const itemComponents = items.map(item => {
+    const isSelected = selectedItems.indexOf(item.value) !== -1;
+    const value = isSelected ?
+      selectedItems.filter(v => v !== item.value) :
+      selectedItems.concat([item.value]);
+    const selectedClassName = isSelected ? ' ais-refinement-list--item__active' : '';
+    const itemClassName = `ais-refinement-list--item col-sm-3 ${selectedClassName}`;
     return (
-      <div className="row aisdemo-filter">
-        <div className="col-sm-2 aisdemo-filter-title">Room Type</div>
-        <div id="room_types"></div>
+      <div className={itemClassName} key={item.value}>
+        <div>
+          <label className="ais-refinement-list--label" onClick={e => {
+            e.preventDefault();
+            refine(value);
+          }}>
+            <input
+              type="checkbox"
+              className="ais-refinement-list--checkbox"
+              defaultChecked={isSelected ? 'checked' : ''}
+              />
+            {item.value}
+            <span className="ais-refinement-list--count">{item.count}</span>
+          </label>
+        </div>
       </div>
     );
-  }
-}
+  });
+
+  return (
+    <div className="row aisdemo-filter">
+      <div className="col-sm-2 aisdemo-filter-title">Room Type</div>
+      <div id="room_types">
+        {itemComponents}
+      </div>
+    </div>
+  );
+});
 
 class Price extends Component {
   render() {
@@ -101,8 +183,9 @@ class Price extends Component {
   }
 }
 
-const MyHits = Hits.connect(props => {
-  const hs = props.hits.map(hit => <HitComponent key={hit.objectID} hit={hit} />);
+
+const MyHits = Hits.connect(({hits}) => {
+  const hs = hits.map(hit => <HitComponent key={hit.objectID} hit={hit} />);
   return <div id="hits">{hs}</div>;
 });
 
