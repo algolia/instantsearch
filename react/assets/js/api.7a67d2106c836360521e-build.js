c02f4d8e61dde6df06e13bba1eff8acab784516c
vvo <vincent.voyer@algolia.com>
2016-09-14 17:39:32 +0200

    Updates

--- react/assets/js/api.7a67d2106c836360521e-build.js
+++
@@ -1,2 +0,0 @@
-!function(r){function t(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return r[o].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var e={};return t.m=r,t.c=e,t.p="/instantsearch.js/react/",t(0)}([function(r,t){"use strict";console.log("api")}]);
-//# sourceMappingURL=api.7a67d2106c836360521e-build.js.map
