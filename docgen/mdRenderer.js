e21ebb577fd6187ba200d2d016b015f5c449540d
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2016-09-15 16:54:06 +0200

    doc(generator): Add link in the heading of the documentation
    Fix #1283

--- docgen/mdRenderer.js
+++ docgen/mdRenderer.js
@@ -4,6 +4,10 @@ import unescape from 'unescape-html';
 import highlight from './syntaxHighlighting.js';
 
 const renderer = new marked.Renderer();
+renderer.heading = (text, level, raw) => {
+  const id = raw.toLowerCase().replace(/[^\w]+/g, '-');
+  return `<h${level} id="${id}" class="heading">${text}<a class="anchor" href="#${id}">#</a></h${level}>`;
+};
 renderer.code = (code, lang) => highlight(code, lang);
 renderer.codespan = code => highlight(unescape(code), 'js', true);
 
