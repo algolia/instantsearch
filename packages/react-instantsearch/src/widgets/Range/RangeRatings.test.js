5f5ff8dba2d14062297056bb30f000d7876cd272
Marie-Laure Thuret <mthuret@users.noreply.github.com>
2016-09-22 13:32:33 +0200

    feat(widget): add a new rating range (#1317)

---
+++ packages/react-instantsearch/src/widgets/Range/RangeRatings.test.js
@@ -0,0 +1,47 @@
+/* eslint-env jest, jasmine */
+
+import React from 'react';
+import renderer from 'react/lib/ReactTestRenderer';
+
+import RangeRatings from './RangeRatings';
+
+describe('Range', () => {
+  it('supports passing max/min values', () => {
+    const tree = renderer.create(
+      <RangeRatings
+        createURL={() => '#'}
+        refine={() => null}
+        min={1}
+        max={5}
+        value={{min: 1, max: 5}}
+        count={[{value: '1', count: 1},
+          {value: '2', count: 2},
+          {value: '3', count: 3},
+          {value: '4', count: 4},
+          {value: '5', count: 5}]}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('applies translations', () => {
+    const tree = renderer.create(
+      <RangeRatings
+        createURL={() => '#'}
+        refine={() => null}
+        translations={{
+          ratingLabel: ' & Up',
+        }}
+        min={1}
+        max={5}
+        value={{min: 1, max: 5}}
+        count={[{value: '1', count: 1},
+          {value: '2', count: 2},
+          {value: '3', count: 3},
+          {value: '4', count: 4},
+          {value: '5', count: 5}]}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
--- packages/react-instantsearch/src/widgets/Range/index.js
+++ packages/react-instantsearch/src/widgets/Range/index.js
@@ -1,8 +1,10 @@
 import connect from './connect';
 import RangeInput from './RangeInput';
+import RangeRatings from './RangeRatings';
 import Range from './Range';
 
 const Connected = connect(Range);
 Connected.Input = connect(RangeInput);
+Connected.Rating = connect(RangeRatings);
 Connected.connect = connect;
 export default Connected;
