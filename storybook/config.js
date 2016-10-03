2a4af36aa98c742705d97a4e3ae8d9fccf499bdf
mthuret <marielaure.thuret@algolia.com>
2016-10-03 18:42:30 +0200

    docs(storybook): add default style + add storybook to docs build

--- storybook/config.js
+++ storybook/config.js
@@ -3,15 +3,15 @@ import infoAddon from '@kadira/react-storybook-addon-info';
 
 setAddon(infoAddon);
 
-const req = require.context('../stories', true, /.stories.js$/)
+const req = require.context('../stories', true, /.stories.js$/);
 
 function loadStories() {
-  req.keys().forEach((filename) => req(filename))
+  req.keys().forEach(filename => req(filename));
 }
 
-//until snapshoting works with above conf, we need to
-//manually declare stories
-/*function loadStories() {
+// until snapshoting works with above conf, we need to
+// manually declare stories
+/* function loadStories() {
   require('../stories/SearchBox.stories.js');
   require('../stories/Pagination.stories.js');
   require('../stories/RangeRatings.stories.js');
