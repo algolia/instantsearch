cbdec9b9b466ce77012d64502baf6a18c3bab5cd
Mathias Stav <mathias@feed.no>
2016-10-18 11:25:30 +0200

    fix(numericRefinementList): classes on radio buttons (#1358)

--- src/widgets/numeric-refinement-list/defaultTemplates.js
+++ src/widgets/numeric-refinement-list/defaultTemplates.js
@@ -2,7 +2,7 @@
 export default {
   header: '',
   item: `<label class="{{cssClasses.label}}">
-  <input type="radio" class="{{cssClasses.checkbox}}" name="{{attributeName}}" {{#isRefined}}checked{{/isRefined}} />{{name}}
+  <input type="radio" class="{{cssClasses.radio}}" name="{{attributeName}}" {{#isRefined}}checked{{/isRefined}} />{{name}}
 </label>`,
   footer: ''
 };
