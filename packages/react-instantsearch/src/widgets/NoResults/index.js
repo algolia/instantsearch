a7edc36a9856875b32dd12dec9d8df0ca55d2a49
Marie-Laure Thuret <mthuret@users.noreply.github.com>
2016-10-19 10:50:56 +0200

    docs: add a guide to explain how to conditionally display content (#1434)
    also remove NoResults/EmptyQuery that partially fit the role of conditional display

--- packages/react-instantsearch/src/widgets/NoResults/index.js
+++
@@ -1,6 +0,0 @@
-import connect from './connect';
-import NoResults from './NoResults';
-
-const Connected = connect(NoResults);
-Connected.connect = connect;
-export default Connected;
---
+++ stories/Conditional.stories.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {storiesOf} from '@kadira/storybook';
+import {createConnector} from '../packages/react-instantsearch';
+import {WrapWithHits} from './util';
+
+const stories = storiesOf('Conditionals', module);
+
+stories.add('NoResults/HasResults', () => {
+  const Content = createConnector({
+    displayName: 'ConditionalResults',
+    getProps(props, state, search) {
+      const noResults = search.results ? search.results.nbHits === 0 : false;
+      return {query: state.q, noResults};
+    },
+  })(({noResults, query}) => {
+    const content = noResults
+      ? <div>No results has been found for {query}</div>
+      : <div>Some results</div>;
+    return <div>{content}</div>;
+  });
+  return <WrapWithHits>
+    <Content/>
+  </WrapWithHits>;
+}).add('NoQuery/HasQuery', () => {
+  const Content = createConnector({
+    displayName: 'ConditionalQuery',
+    getProps(props, state) {
+      return {query: state.q};
+    },
+  })(({query}) => {
+    const content = query
+      ? <div>The query {query} exists</div>
+      : <div>No query</div>;
+    return <div>{content}</div>;
+  });
+  return <WrapWithHits>
+    <Content/>
+  </WrapWithHits>;
+});
