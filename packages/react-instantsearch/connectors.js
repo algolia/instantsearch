652fa3b578ab5b8584f362179d40e3e3dd5f68cf
mthuret <marielaure.thuret@algolia.com>
2016-10-31 13:39:15 +0100

    feat(api): remove error widget
    BREAKING CHANGE:
    - error widget is removed. Please use the createConnector function to do conditional display.
    See our "Conditional Display" guide.

--- packages/react-instantsearch/connectors.js
+++ packages/react-instantsearch/connectors.js
@@ -1,5 +1,4 @@
 export {default as connectCurrentRefinements} from './src/connectors/connectCurrentRefinements.js';
-export {default as connectError} from './src/connectors/connectError.js';
 export {default as connectHierarchicalMenu} from './src/connectors/connectHierarchicalMenu.js';
 export {default as connectHits} from './src/connectors/connectHits.js';
 export {default as connectHitsPerPage} from './src/connectors/connectHitsPerPage.js';
