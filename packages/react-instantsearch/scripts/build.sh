2c7bc2668f11139fb7fa39b43cbe2b28ebc77d4b
mthuret <marielaure.thuret@algolia.com>
2016-11-08 11:47:01 +0100

    fix(build): wrong file were copied

--- packages/react-instantsearch/scripts/build.sh
+++ packages/react-instantsearch/scripts/build.sh
@@ -7,6 +7,6 @@ rm -rf dist/*
 cp package.json dist/
 cp README.md dist/
 babel -q index.js -o dist/index.js
-babel -q index.js -o dist/dom.js
-babel -q index.js -o dist/connectors.js
+babel -q dom.js -o dist/dom.js
+babel -q connectors.js -o dist/connectors.js
 babel -q --ignore test.js,__mocks__ --out-dir dist/src src
