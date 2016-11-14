dcce6ed5640d7fe81e0c1819f06090bbbe2c76c2
Alex S <bobylito@users.noreply.github.com>
2016-11-14 18:23:32 +0100

    fix(ClearAll): always display clearAll button, disabled when no filter (#1545)
    Fix #1530

--- packages/react-instantsearch/src/connectors/connectCurrentRefinements.js
+++ packages/react-instantsearch/src/connectors/connectCurrentRefinements.js
@@ -8,7 +8,7 @@ import createConnector from '../core/createConnector';
  * @kind connector
  * @category connector
  * @providedPropType {function} refine - a function to remove a single filter
- * @providedPropType {array.<{key: string, label: string}>} filters - all the filters, the key for calling the refine prop function, label is for the display.
+ * @providedPropType {array.<{key: string, label: string}>} items - all the filters, the key for calling the refine prop function, label is for the display.
  */
 export default createConnector({
   displayName: 'AlgoliaCurrentRefinements',
