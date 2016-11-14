5dd89143b35ba446c004daa4f524fbc4cd417756
Alex S <bobylito@users.noreply.github.com>
2016-11-14 16:28:37 +0100

    feat(InfiniteHits): add an infinite hits widgets with load more (algolia/react-instantsearch#1483)
    fixes algolia/react-instantsearch#1344

---
+++ packages/react-instantsearch/src/components/InfiniteHits.js
@@ -0,0 +1,50 @@
+/**
+ * @namespace widgets.Hits
+ */
+import React, {PropTypes, Component} from 'react';
+
+import themeable from '../core/themeable';
+import theme from './Hits.css';
+
+class InfiniteHits extends Component {
+  render() {
+    const {applyTheme, itemComponent: ItemComponent, hits, hasMore, refine} = this.props;
+    const renderedHits = hits.map(hit =>
+      <ItemComponent key={hit.objectID} hit={hit} />
+    );
+    const loadMoreButton = hasMore ?
+      <button onClick={() => refine()}>Load more</button> :
+      <button disabled>Load more</button>;
+
+    return (
+      <div {...applyTheme('root', 'root')}>
+        {renderedHits}
+        {loadMoreButton}
+      </div>
+    );
+  }
+}
+
+InfiniteHits.propTypes = {
+  applyTheme: PropTypes.func.isRequired,
+  hits: PropTypes.array,
+  itemComponent: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.func,
+  ]).isRequired,
+  hasMore: PropTypes.bool.isRequired,
+  refine: PropTypes.func.isRequired,
+};
+
+InfiniteHits.defaultProps = {
+  itemComponent: hit =>
+    <div
+      style={{
+        borderBottom: '1px solid #bbb',
+        paddingBottom: '5px',
+        marginBottom: '5px',
+      }}
+    >{JSON.stringify(hit).slice(0, 100)}...</div>,
+};
+
+export default themeable(theme)(InfiniteHits);
---
+++ packages/react-instantsearch/src/widgets/InfiniteHits.js
@@ -0,0 +1,39 @@
+import connectInfiniteHits from '../connectors/connectInfiniteHits.js';
+import InfiniteHitsComponent from '../components/InfiniteHits.js';
+
+/**
+ * Displays the list of hits for the current search parameters. This widget
+ * will also render a **load more** button that will add one to the current
+ * page and will trigger the search. The new results will be append in the
+ * list of results.
+ * @name InfiniteHits
+ * @kind component
+ * @category widget
+ * @propType {number} hitsPerPage - How many hits should be displayed for every page.
+ *   Ignored when a `HitsPerPage` component is also present.
+ * @propType {Component} itemComponent - Component used for rendering each hit from
+ *   the results. If it is not provided the rendering defaults to displaying the
+ *   hit in its JSON form. The component will be called with a `hit` prop.
+ * @themeKey root - the root of the component
+ * @example
+ * import React from 'react';
+
+ * import {
+ *   InstantSearch,
+ *   Hits,
+ * } from 'react-instantsearch/dom';
+ *
+ * export default function App() {
+ *   return (
+ *     <InstantSearch
+ *       className="container-fluid"
+ *       appId="latency"
+ *       apiKey="6be0576ff61c053d5f9a3225e2a90f76"
+ *       indexName="ikea"
+ *     >
+ *       <InfiniteHits />
+ *     </InstantSearch>
+ *   );
+ * }
+ */
+export default connectInfiniteHits(InfiniteHitsComponent);
