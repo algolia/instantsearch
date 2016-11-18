8ae9469d023f575b1e49bde277ed54e532f76be8
Marie-Laure Thuret <mthuret@users.noreply.github.com>
2016-11-18 18:21:26 +0100

    feat(react-native): make react-instantsearch compatible for native dev (algolia/react-instantsearch#1573)
    - add a native namespace
    - inject the right algoliaclient depending of dom or native
    - replace nextTick by setImmediate

--- packages/react-instantsearch/scripts/build.sh
+++ packages/react-instantsearch/scripts/build.sh
@@ -9,4 +9,5 @@ cp README.md dist/
 babel -q index.js -o dist/index.js
 babel -q dom.js -o dist/dom.js
 babel -q connectors.js -o dist/connectors.js
+babel -q native.js -o dist/native.js
 babel -q --ignore test.js,__mocks__ --out-dir dist/src src
