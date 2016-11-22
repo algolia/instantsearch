c45dae515daae1f6864ffc4c5ec8bc35c3ef0626
Chris Moyer <kopertop@gmail.com>
2016-11-22 10:03:01 -0500

    Adds a 'getSearchParameters' function to helper. (algolia/algoliasearch-helper-js#422)
    * Adds a 'getSearchParameters' function to helper.
    
    Closes algolia/algoliasearch-helper-js#421
    
    * Changed name to "getQuery"

--- packages/algoliasearch-helper/src/algoliasearch.helper.js
+++ packages/algoliasearch-helper/src/algoliasearch.helper.js
@@ -108,6 +108,16 @@ AlgoliaSearchHelper.prototype.search = function() {
 };
 
 /**
+ * Gets the search query parameters that would be sent to the Algolia Client
+ * for the hits
+ * @return {object} Query Parameters
+ */
+AlgoliaSearchHelper.prototype.getQuery = function() {
+  var state = this.state;
+  return requestBuilder._getHitsSearchParams(state);
+};
+
+/**
  * Start a search using a modified version of the current state. This method does
  * not trigger the helper lifecycle and does not modify the state kept internally
  * by the helper. This second aspect means that the next search call will be the
