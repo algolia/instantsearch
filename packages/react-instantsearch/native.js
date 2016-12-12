2d92c8d6f0c971a86d2258539d82946d94dc2082
mthuret <marielaure.thuret@algolia.com>
2016-12-12 12:00:42 +0100

    fix(react-native): add View as a container for react-native

--- packages/react-instantsearch/native.js
+++ packages/react-instantsearch/native.js
@@ -1,7 +1,7 @@
 import algoliasearch from 'algoliasearch/reactnative';
 import createInstantSearch from './src/core/createInstantSearch';
-
+import {View} from 'react-native';
 const InstantSearch = createInstantSearch(algoliasearch, {
-  Root: 'View',
+  Root: View,
 });
 export {InstantSearch};
