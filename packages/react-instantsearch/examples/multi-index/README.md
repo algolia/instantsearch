9e145df07f619cb6420970122dc991c05dbb0dd5
Marie-Laure Thuret <mthuret@users.noreply.github.com>
2016-12-14 16:29:31 +0100

    docs(examples): use only 'latest' and 'react' babel preset syntaxes (#1749)
    also add multi-index as a whole live example.

---
+++ packages/react-instantsearch/examples/multi-index/README.md
@@ -0,0 +1,8 @@
+This example shows how to target multiple indices using react-instantsearch
+
+To start the example: 
+
+* npm install
+* npm start
+
+Read more about react-instantsearch [in our documentation](https://community.algolia.com/instantsearch.js/react/).
---
+++ packages/react-instantsearch/examples/multi-index/src/App.js
@@ -0,0 +1,69 @@
+import React, {Component} from 'react';
+import {InstantSearch, Hits, SearchBox} from 'react-instantsearch/dom';
+import {connectSearchBox} from 'react-instantsearch/connectors';
+/* eslint-disable import/no-unresolved */
+import 'react-instantsearch-theme-algolia/style.css';
+/* eslint-enable import/no-unresolved */
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {searchState: {}};
+  }
+
+  onSearchStateChange(searchState) {
+    this.setState({searchState});
+  }
+
+  render() {
+    return (
+      <div>
+        {/* You can also nest `<InstantSearch>` components,
+         as long as you pass the right state and onStateChange function
+         */}
+        <FirstResults onSearchStateChange={this.onSearchStateChange.bind(this)}/>
+        <SecondResults searchState={this.state.searchState}/>
+      </div>
+    );
+  }
+}
+
+const FirstResults = props =>
+  <InstantSearch
+    appId="latency"
+    apiKey="6be0576ff61c053d5f9a3225e2a90f76"
+    indexName="bestbuy"
+    onSearchStateChange={props.onSearchStateChange}
+  >
+    <SearchBox/>
+    <p>Results in first dataset</p>
+    <Hits />
+  </InstantSearch>;
+
+FirstResults.propTypes = {
+  onSearchStateChange: React.PropTypes.func.isRequired,
+};
+
+/*
+ To perform the same query as the FirstResults instance we need a virtual SearchBox widget
+ to handle the search.
+ */
+const VirtualSearchBox = connectSearchBox(() => null);
+const SecondResults = props =>
+  <InstantSearch
+    appId="latency"
+    apiKey="6be0576ff61c053d5f9a3225e2a90f76"
+    indexName="ikea"
+    searchState={props.searchState}
+  >
+    <VirtualSearchBox/>
+    <p>Results in second dataset</p>
+    <Hits/>
+  </InstantSearch>;
+
+SecondResults.propTypes = {
+  searchState: React.PropTypes.object.isRequired,
+};
+
+export default App;
+
