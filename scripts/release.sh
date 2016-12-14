1bf8f44f1c9ce486a829631b390575ac8a92fcda
vvo <vincent.voyer@algolia.com>
2016-12-14 18:06:44 +0100

    chore(release): new release process given the lib is now published
    Similar to instantsearch.js v1.
    
    Also updated all libs

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,6 +7,11 @@ if [[ -n $(cd packages/react-instantsearch && npm owner add "$(npm whoami)") ]];
   exit 1
 fi
 
+if [[ -n $(cd packages/react-instantsearch-theme-algolia && npm owner add "$(npm whoami)") ]]; then
+  printf "Release: Not an owner of the npm repo, ask for it\n"
+  exit 1
+fi
+
 currentBranch=`git rev-parse --abbrev-ref HEAD`
 if [ $currentBranch != 'v2' ]; then
   printf "Release: You must be on v2\n"
@@ -26,64 +31,58 @@ git fetch origin --tags
 yarn
 
 # No need for complex release process for now, only patch releases should be ok
-# currentVersion=`cat package.json | json version`
+currentVersion=`cat package.json | json version`
 
 # header
-# printf "\n\nRelease: current version is %s" $currentVersion
-# printf "\nRelease: a changelog will be generated only if a fix/feat/performance/breaking token is found in git log"
-# printf "\nRelease: you must choose a new ve.rs.ion (semver)"
-# printf "\nRelease: press q to exit the next screen\n\n"
+printf "\n\nRelease: current version is %s" $currentVersion
+printf "\nRelease: a changelog will be generated only if a fix/feat/performance/breaking token is found in git log"
+printf "\nRelease: you must choose a new ve.rs.ion (semver)"
+printf "\nRelease: press q to exit the next screen\n\n"
 
 # preview changelog
-# read -p "=> Release: press [ENTER] to view changes since latest version.."
+read -p "=> Release: press [ENTER] to view changes since latest version.."
 
-# conventional-changelog --preset angular --output-unreleased | less
+conventional-changelog --preset angular --output-unreleased | less
 
 # choose and bump new version
-# printf "\n\nRelease: Please enter the new chosen version > "
-# printf "\n=> Release: please type the new chosen version > "
-# read -e newVersion
-
-newVersion=$(npm version major)
+printf "\n=> Release: please type the new chosen version > "
+read -e newVersion
 
 (
 cd packages/react-instantsearch
-npm version major
+npm version $newVersion
 yarn
 )
 
 (
 cd packages/react-instantsearch-theme-algolia
-npm version major
+npm version $newVersion
 )
 
 yarn
 npm run build
 
-# update changelog, not yet because of prototyping
-# printf "\n\nRelease: update changelog"
-# changelog=`conventional-changelog -p angular`
-# conventional-changelog --preset angular --infile CHANGELOG.md --same-file
+# update changelog
+printf "\n\nRelease: update changelog"
+changelog=`conventional-changelog -p angular`
+conventional-changelog --preset angular --infile CHANGELOG.md --same-file
 
 # regenerate readme TOC
 printf "\n\nRelease: generate TOCS"
 npm run doctoc
 
 # git add and tag
-# commitMessage="v$newVersion\n\n$changelog"
-commitMessage="$newVersion"
-git add package.json CHANGELOG.md README.md CONTRIBUTING.md packages/ yarn.lock
-printf "%s" $commitMessage | git commit --file -
-# not git tagging for now, still prototyping
-# git tag "v$newVersion"
+commitMessage="v$newVersion\n\n$changelog"
+git add src/lib/version.js yarn.lock package.json CHANGELOG.md README.md CONTRIBUTING.md docs/_includes/widget-jsdoc
+printf "$commitMessage" | git commit --file -
+git tag "v$newVersion"
 
 printf "\n\nRelease: almost done, check everything in another terminal tab if you want.\n"
 read -p "=> Release: when ready, press [ENTER] to push to github and publish the package"
 
 printf "\n\nRelease: pushed to github, publish on npm"
 git push origin v2
-# no git tagging for now
-# git push origin --tags
+git push origin --tags
 
 (
 cd packages/react-instantsearch/dist
