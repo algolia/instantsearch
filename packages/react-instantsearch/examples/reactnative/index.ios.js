5b76f7548b5ba1c1febbbd01be62a642e17b66ba
Alex S <bobylito@users.noreply.github.com>
2016-12-14 10:48:21 +0100

    fix(react-native): use View as a container for react-native (#1729)
    fixes #1730 
    
    Also fixes:
    - http://stackoverflow.com/questions/41064940/using-algolia-react-instantsearch-with-react-native
    - http://stackoverflow.com/questions/41066380/react-native-algolia-instantsearch-expected-a-component-class-got-object-objec

---
+++ packages/react-instantsearch/examples/reactnative/index.ios.js
@@ -0,0 +1,9 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+AppRegistry.registerComponent('reactnative', () => App);
