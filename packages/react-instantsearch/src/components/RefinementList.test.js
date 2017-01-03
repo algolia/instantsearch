mihilie/instantsearch.js

feat(SearchForFacetValues): connector functions + refinementList implementation

packages/react-instantsearch/src/components/RefinementList.test.js
@@ -6,11 +6,14 @@ import {mount} from 'enzyme';
 
 import RefinementList from './RefinementList';
 
+jest.mock('../widgets/Highlight');
+
 describe('RefinementList', () => {
   it('default refinement list', () => {
     const tree = renderer.create(
       <RefinementList
         refine={() => null}
+        searchForFacetValues={() => null}
         createURL={() => '#'}
         items={[
           {label: 'white', value: ['white'], count: 10, isRefined: true},
@@ -25,18 +28,38 @@ describe('RefinementList', () => {
     expect(tree).toMatchSnapshot();
   });
 
+  it('refinement list with search for facet values', () => {
+    const tree = renderer.create(
+      <RefinementList
+        refine={() => null}
+        searchForFacetValues={() => null}
+        createURL={() => '#'}
+        items={[
+          {label: 'white', value: ['white'], count: 10, isRefined: true},
+          {label: 'black', value: ['black'], count: 20, isRefined: false},
+          {label: 'blue', value: ['blue'], count: 30, isRefined: false},
+        ]}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
   it('applies translations', () => {
     const tree = renderer.create(
       <RefinementList
         refine={() => null}
+        searchForFacetValues={() => null}
         createURL={() => '#'}
         items={[
           {label: 'white', value: ['white'], count: 10, isRefined: false},
           {label: 'black', value: ['black'], count: 20, isRefined: false},
           {label: 'blue', value: ['blue'], count: 30, isRefined: false},
         ]}
+        isSearchable
+        facetValues={[]}
         translations={{
           showMore: ' display more',
+          noResults: ' no results',
         }}
       />
     ).toJSON();
@@ -46,16 +69,17 @@ describe('RefinementList', () => {
   it('refines its value on change', () => {
     const refine = jest.fn();
     const wrapper = mount(
-        <RefinementList
-          refine={refine}
-          createURL={() => '#'}
-          items={[
-            {label: 'white', value: ['white'], count: 10, isRefined: false},
-            {label: 'black', value: ['black'], count: 20, isRefined: false},
-            {label: 'blue', value: ['blue'], count: 30, isRefined: false},
-          ]}
-        />
-      );
+      <RefinementList
+        refine={refine}
+        searchForFacetValues={() => null}
+        createURL={() => '#'}
+        items={[
+          {label: 'white', value: ['white'], count: 10, isRefined: false},
+          {label: 'black', value: ['black'], count: 20, isRefined: false},
+          {label: 'blue', value: ['blue'], count: 30, isRefined: false},
+        ]}
+      />
+    );
 
     const items = wrapper.find('.ais-RefinementList__item');
 
@@ -74,21 +98,22 @@ describe('RefinementList', () => {
   it('should respect defined limits', () => {
     const refine = jest.fn();
     const wrapper = mount(
-        <RefinementList
-          refine={refine}
-          createURL={() => '#'}
-          items={[
-            {label: 'white', value: ['white'], count: 10, isRefined: false},
-            {label: 'black', value: ['black'], count: 20, isRefined: false},
-            {label: 'blue', value: ['blue'], count: 30, isRefined: false},
-            {label: 'red', value: ['red'], count: 30, isRefined: false},
-            {label: 'green', value: ['green'], count: 30, isRefined: false},
-          ]}
-          limitMin={2}
-          limitMax={4}
-          showMore={true}
-        />
-      );
+      <RefinementList
+        refine={refine}
+        searchForFacetValues={() => null}
+        createURL={() => '#'}
+        items={[
+          {label: 'white', value: ['white'], count: 10, isRefined: false},
+          {label: 'black', value: ['black'], count: 20, isRefined: false},
+          {label: 'blue', value: ['blue'], count: 30, isRefined: false},
+          {label: 'red', value: ['red'], count: 30, isRefined: false},
+          {label: 'green', value: ['green'], count: 30, isRefined: false},
+        ]}
+        limitMin={2}
+        limitMax={4}
+        showMore={true}
+      />
+    );
 
     const items = wrapper.find('.ais-RefinementList__item');
 
@@ -104,18 +129,19 @@ describe('RefinementList', () => {
   it('should disabled show more when no more item to display', () => {
     const refine = jest.fn();
     const wrapper = mount(
-        <RefinementList
-          refine={refine}
-          createURL={() => '#'}
-          items={[
-            {label: 'white', value: ['white'], count: 10, isRefined: false},
-            {label: 'black', value: ['black'], count: 20, isRefined: false},
-          ]}
-          limitMin={2}
-          limitMax={4}
-          showMore={true}
-        />
-      );
+      <RefinementList
+        refine={refine}
+        searchForFacetValues={() => null}
+        createURL={() => '#'}
+        items={[
+          {label: 'white', value: ['white'], count: 10, isRefined: false},
+          {label: 'black', value: ['black'], count: 20, isRefined: false},
+        ]}
+        limitMin={2}
+        limitMax={4}
+        showMore={true}
+      />
+    );
 
     const items = wrapper.find('.ais-RefinementList__item');
 
@@ -124,5 +150,94 @@ describe('RefinementList', () => {
     expect(wrapper.find('.ais-RefinementList__showMoreDisabled')).toBeDefined();
 
     wrapper.unmount();
+  });
+
+  describe('search for facets value', () => {
+    const refine = jest.fn();
+    const searchForFacetValues = jest.fn();
+    const refinementList = <RefinementList
+      refine={refine}
+      searchForFacetValues={searchForFacetValues}
+      createURL={() => '#'}
+      items={[
+        {label: 'white', value: ['white'], count: 10, isRefined: false},
+        {label: 'black', value: ['black'], count: 20, isRefined: false},
+      ]}
+      facetValues={[{
+        _highlightResult: {label: {value: 'wat'}},
+        count: 20,
+        isRefined: true,
+        label: 'wat',
+        value: ['wat'],
+      }]}
+      isSearchable
+    />;
+
+    it('a searchbox should be displayed if the feature is activated', () => {
+      const wrapper = mount(refinementList);
+
+      const searchBox = wrapper.find('.ais-RefinementList__SearchBox');
+
+      expect(searchBox).toBeDefined();
+
+      wrapper.unmount();
+    });
+
+    it('searching for a value should call searchForFacetValues', () => {
+      const wrapper = mount(refinementList);
+
+      wrapper.find('.ais-RefinementList__SearchBox input').simulate('change', {target: {value: 'query'}});
+
+      expect(searchForFacetValues.mock.calls.length).toBe(1);
+      expect(searchForFacetValues.mock.calls[0][0]).toBe('query');
+
+      wrapper.unmount();
+    });
+
+    it('a refinementList that has facet values and a search in progress, should display its facet values', () => {
+      const wrapper = mount(refinementList);
+      wrapper.find('.ais-RefinementList__SearchBox input').simulate('change', {target: {value: 'query'}});
+
+      const items = wrapper.find('.ais-RefinementList__item');
+
+      expect(items.length).toBe(1);
+
+      wrapper.unmount();
+    });
+
+    it('should refine the selected value and display selected refinement back', () => {
+      const wrapper = mount(refinementList);
+      wrapper.find('.ais-RefinementList__SearchBox input').simulate('change', {target: {value: 'query'}});
+
+      const firstItem = wrapper.find('.ais-RefinementList__item').first().find('.ais-RefinementList__itemCheckbox');
+
+      firstItem.simulate('change', {target: {checked: true}});
+
+      expect(refine.mock.calls.length).toBe(1);
+      expect(refine.mock.calls[0][0]).toEqual(['wat']);
+      expect(wrapper.find('.ais-RefinementList__SearchBox input').props().value).toBe('');
+
+      const selectedRefinements = wrapper.find('.ais-RefinementList__item');
+      expect(selectedRefinements.length).toBe(2);
+
+      wrapper.unmount();
+    });
+
+    it('hit enter on the search values results list should refine the first facet value', () => {
+      refine.mockClear();
+      const wrapper = mount(refinementList);
+      wrapper.find('.ais-RefinementList__SearchBox input').simulate('change', {target: {value: 'query'}});
+
+      wrapper.find('form').simulate('submit');
+
+      expect(refine.mock.calls.length).toBe(1);
+      expect(refine.mock.calls[0][0]).toEqual(['wat']);
+      expect(wrapper.find('.ais-RefinementList__SearchBox input').props().value).toBe('');
+
+      const selectedRefinements = wrapper.find('.ais-RefinementList__item');
+      expect(selectedRefinements.length).toBe(2);
+
+      wrapper.unmount();
+    });
   });
 });

packages/react-instantsearch/src/connectors/connectRefinementList.test.js
@@ -11,6 +11,7 @@ const {
   refine,
   getSearchParameters: getSP,
   getMetadata,
+  searchForFacetValues,
   cleanUp,
 } = connect;
 
@@ -18,12 +19,12 @@ let props;
 let params;
 
 describe('connectRefinementList', () => {
-  it('provides the correct props to the component', () => {
-    const results = {
-      getFacetValues: jest.fn(() => []),
-      getFacetByName: () => true,
-    };
+  const results = {
+    getFacetValues: jest.fn(() => []),
+    getFacetByName: () => true,
+  };
 
+  it('provides the correct props to the component', () => {
     props = getProvidedProps({attributeName: 'ok'}, {}, {results});
     expect(props).toEqual({items: [], currentRefinement: []});
 
@@ -85,6 +86,25 @@ describe('connectRefinementList', () => {
         count: 20,
       },
     ]);
+  });
+
+  it('facetValues results should be provided as props if they exists', () => {
+    props = getProvidedProps({attributeName: 'ok'}, {}, {results}, {}, {
+      ok: [{
+        value: ['wat'],
+        label: 'wat',
+        isRefined: true,
+        count: 20,
+        highlighted: 'wat',
+      }],
+    });
+    expect(props.facetValues).toEqual([{
+      _highlightResult: {label: {value: 'wat'}},
+      count: 20,
+      isRefined: true,
+      label: ['wat'],
+      value: [['wat']],
+    }]);
   });
 
   it('doesn\'t render when no results are available', () => {
@@ -202,5 +222,13 @@ describe('connectRefinementList', () => {
 
     searchState = cleanUp({attributeName: 'name2'}, searchState);
     expect(searchState).toEqual({another: {searchState: 'searchState'}});
+  });
+
+  it('calling searchForFacetValues return the right searchForFacetValues parameters', () => {
+    const parameters = searchForFacetValues({attributeName: 'ok'}, {}, 'yep');
+    expect(parameters).toEqual({
+      facetName: 'ok',
+      query: 'yep',
+    });
   });
 });

packages/react-instantsearch/src/core/createInstantSearchManager.js
@@ -116,6 +116,46 @@ export default function createInstantSearchManager({
     search();
   }
 
+  function onSearchForFacetValues(nextSearchState) {
+    const baseSP = new SearchParameters({
+      ...searchParameters,
+      index: indexName,
+      ...highlightTags,
+    });
+    const widgetSearchParameters = getSearchParameters(baseSP);
+    const searchForFacetValuesHelper = algoliasearchHelper(algoliaClient, indexName, widgetSearchParameters);
+    store.setState({
+      ...store.getState(),
+      searchingForFacetValues: true,
+    });
+    searchForFacetValuesHelper.searchForFacetValues(nextSearchState.facetName, nextSearchState.query)
+      .then(content => {
+        store.setState({
+          ...store.getState(),
+          resultsFacetValues: {
+            ...store.getState().resultsFacetValues,
+            [nextSearchState.facetName]: content.facetHits,
+          },
+          searchingForFacetValues: false,
+        });
+      }, error => {
+        store.setState({
+          ...store.getState(),
+          error,
+          searchingForFacetValues: false,
+        });
+      })
+      .catch(error => {
+        // Since setState is synchronous, any error that occurs in the render of a
+        // component will be swallowed by this promise.
+        // This is a trick to make the error show up correctly in the console.
+        // See http://stackoverflow.com/a/30741722/969302
+        setTimeout(() => {
+          throw error;
+        });
+      });
+  }
+
   function getWidgetsIds() {
     return store.getState().metadata.reduce((res, meta) =>
         typeof meta.id !== 'undefined' ? res.concat(meta.id) : res
@@ -128,5 +168,6 @@ export default function createInstantSearchManager({
     getWidgetsIds,
     onExternalStateUpdate,
     transitionState,
+    onSearchForFacetValues,
   };
 }

stories/RefinementList.stories.js
@@ -28,6 +28,10 @@ stories.add('default', () =>
       showMore={true}
     />
   </WrapWithHits>
+).add('with search for facets value', () =>
+  <WrapWithHits>
+    <RefinementList attributeName="category" isSearchable/>
+  </WrapWithHits>
 ).add('playground', () =>
   <WrapWithHits >
     <RefinementList

algolia/instantsearch

packages/react-instantsearch/src/components/RefinementList.test.js
@@ -6,6 +6,8 @@ import {mount} from 'enzyme';
 
 import RefinementList from './RefinementList';
 
+jest.mock('../widgets/Highlight');
+
 describe('RefinementList', () => {
   it('default refinement list', () => {
     const tree = renderer.create(
@@ -20,6 +22,24 @@ describe('RefinementList', () => {
         limitMin={2}
         limitMax={4}
         showMore={true}
+        isFromSearch={false}
+      />
+    ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('refinement list with search for facet values', () => {
+    const tree = renderer.create(
+      <RefinementList
+        refine={() => null}
+        searchForFacetValues={() => null}
+        createURL={() => '#'}
+        items={[
+          {label: 'white', value: ['white'], count: 10, isRefined: true},
+          {label: 'black', value: ['black'], count: 20, isRefined: false},
+          {label: 'blue', value: ['blue'], count: 30, isRefined: false},
+        ]}
+        isFromSearch={false}
       />
     ).toJSON();
     expect(tree).toMatchSnapshot();
@@ -29,14 +49,17 @@ describe('RefinementList', () => {
     const tree = renderer.create(
       <RefinementList
         refine={() => null}
+        searchForFacetValues={() => null}
         createURL={() => '#'}
         items={[
           {label: 'white', value: ['white'], count: 10, isRefined: false},
           {label: 'black', value: ['black'], count: 20, isRefined: false},
           {label: 'blue', value: ['blue'], count: 30, isRefined: false},
         ]}
+        isFromSearch={true}
         translations={{
           showMore: ' display more',
+          noResults: ' no results',
         }}
       />
     ).toJSON();
@@ -46,16 +69,18 @@ describe('RefinementList', () => {
   it('refines its value on change', () => {
     const refine = jest.fn();
     const wrapper = mount(
-        <RefinementList
-          refine={refine}
-          createURL={() => '#'}
-          items={[
-            {label: 'white', value: ['white'], count: 10, isRefined: false},
-            {label: 'black', value: ['black'], count: 20, isRefined: false},
-            {label: 'blue', value: ['blue'], count: 30, isRefined: false},
-          ]}
-        />
-      );
+      <RefinementList
+        refine={refine}
+        searchForFacetValues={() => null}
+        createURL={() => '#'}
+        items={[
+          {label: 'white', value: ['white'], count: 10, isRefined: false},
+          {label: 'black', value: ['black'], count: 20, isRefined: false},
+          {label: 'blue', value: ['blue'], count: 30, isRefined: false},
+        ]}
+        isFromSearch={false}
+      />
+    );
 
     const items = wrapper.find('.ais-RefinementList__item');
 
@@ -74,21 +99,23 @@ describe('RefinementList', () => {
   it('should respect defined limits', () => {
     const refine = jest.fn();
     const wrapper = mount(
-        <RefinementList
-          refine={refine}
-          createURL={() => '#'}
-          items={[
-            {label: 'white', value: ['white'], count: 10, isRefined: false},
-            {label: 'black', value: ['black'], count: 20, isRefined: false},
-            {label: 'blue', value: ['blue'], count: 30, isRefined: false},
-            {label: 'red', value: ['red'], count: 30, isRefined: false},
-            {label: 'green', value: ['green'], count: 30, isRefined: false},
-          ]}
-          limitMin={2}
-          limitMax={4}
-          showMore={true}
-        />
-      );
+      <RefinementList
+        refine={refine}
+        searchForFacetValues={() => null}
+        createURL={() => '#'}
+        items={[
+          {label: 'white', value: ['white'], count: 10, isRefined: false},
+          {label: 'black', value: ['black'], count: 20, isRefined: false},
+          {label: 'blue', value: ['blue'], count: 30, isRefined: false},
+          {label: 'red', value: ['red'], count: 30, isRefined: false},
+          {label: 'green', value: ['green'], count: 30, isRefined: false},
+        ]}
+        limitMin={2}
+        limitMax={4}
+        showMore={true}
+        isFromSearch={false}
+      />
+    );
 
     const items = wrapper.find('.ais-RefinementList__item');
 
@@ -104,18 +131,20 @@ describe('RefinementList', () => {
   it('should disabled show more when no more item to display', () => {
     const refine = jest.fn();
     const wrapper = mount(
-        <RefinementList
-          refine={refine}
-          createURL={() => '#'}
-          items={[
-            {label: 'white', value: ['white'], count: 10, isRefined: false},
-            {label: 'black', value: ['black'], count: 20, isRefined: false},
-          ]}
-          limitMin={2}
-          limitMax={4}
-          showMore={true}
-        />
-      );
+      <RefinementList
+        refine={refine}
+        searchForFacetValues={() => null}
+        createURL={() => '#'}
+        items={[
+          {label: 'white', value: ['white'], count: 10, isRefined: false},
+          {label: 'black', value: ['black'], count: 20, isRefined: false},
+        ]}
+        limitMin={2}
+        limitMax={4}
+        showMore={true}
+        isFromSearch={false}
+      />
+    );
 
     const items = wrapper.find('.ais-RefinementList__item');
 
@@ -124,5 +153,77 @@ describe('RefinementList', () => {
     expect(wrapper.find('.ais-RefinementList__showMoreDisabled')).toBeDefined();
 
     wrapper.unmount();
+  });
+
+  describe('search for facets value', () => {
+    const refine = jest.fn();
+    const searchForFacetValues = jest.fn();
+    const refinementList = <RefinementList
+      refine={refine}
+      searchForFacetValues={searchForFacetValues}
+      createURL={() => '#'}
+      items={[
+        {
+          label: 'white', value: ['white'], count: 10, isRefined: false, _highlightResult: {label: {value: 'white'}},
+        },
+        {
+          label: 'black', value: ['black'], count: 20, isRefined: false, _highlightResult: {label: {value: 'black'}},
+        },
+      ]}
+      isFromSearch={true}
+    />;
+
+    it('a searchbox should be displayed if the feature is activated', () => {
+      const wrapper = mount(refinementList);
+
+      const searchBox = wrapper.find('.ais-RefinementList__SearchBox');
+
+      expect(searchBox).toBeDefined();
+
+      wrapper.unmount();
+    });
+
+    it('searching for a value should call searchForFacetValues', () => {
+      const wrapper = mount(refinementList);
+
+      wrapper.find('.ais-RefinementList__SearchBox input').simulate('change', {target: {value: 'query'}});
+
+      expect(searchForFacetValues.mock.calls.length).toBe(1);
+      expect(searchForFacetValues.mock.calls[0][0]).toBe('query');
+
+      wrapper.unmount();
+    });
+
+    it('should refine the selected value and display selected refinement back', () => {
+      const wrapper = mount(refinementList);
+
+      const firstItem = wrapper.find('.ais-RefinementList__item').first().find('.ais-RefinementList__itemCheckbox');
+      firstItem.simulate('change', {target: {checked: true}});
+
+      expect(refine.mock.calls.length).toBe(1);
+      expect(refine.mock.calls[0][0]).toEqual(['white']);
+      expect(wrapper.find('.ais-RefinementList__SearchBox input').props().value).toBe('');
+
+      const selectedRefinements = wrapper.find('.ais-RefinementList__item');
+      expect(selectedRefinements.length).toBe(2);
+
+      wrapper.unmount();
+    });
+
+    it('hit enter on the search values results list should refine the first facet value', () => {
+      refine.mockClear();
+      const wrapper = mount(refinementList);
+
+      wrapper.find('form').simulate('submit');
+
+      expect(refine.mock.calls.length).toBe(1);
+      expect(refine.mock.calls[0][0]).toEqual(['white']);
+      expect(wrapper.find('.ais-RefinementList__SearchBox input').props().value).toBe('');
+
+      const selectedRefinements = wrapper.find('.ais-RefinementList__item');
+      expect(selectedRefinements.length).toBe(2);
+
+      wrapper.unmount();
+    });
   });
 });

packages/react-instantsearch/src/connectors/connectRefinementList.test.js
@@ -11,6 +11,7 @@ const {
   refine,
   getSearchParameters: getSP,
   getMetadata,
+  searchForFacetValues,
   cleanUp,
 } = connect;
 
@@ -18,20 +19,20 @@ let props;
 let params;
 
 describe('connectRefinementList', () => {
-  it('provides the correct props to the component', () => {
-    const results = {
-      getFacetValues: jest.fn(() => []),
-      getFacetByName: () => true,
-    };
+  const results = {
+    getFacetValues: jest.fn(() => []),
+    getFacetByName: () => true,
+  };
 
+  it('provides the correct props to the component', () => {
     props = getProvidedProps({attributeName: 'ok'}, {}, {results});
-    expect(props).toEqual({items: [], currentRefinement: []});
+    expect(props).toEqual({items: [], currentRefinement: [], isFromSearch: false});
 
     props = getProvidedProps({attributeName: 'ok'}, {refinementList: {ok: ['wat']}}, {results});
-    expect(props).toEqual({items: [], currentRefinement: ['wat']});
+    expect(props).toEqual({items: [], currentRefinement: ['wat'], isFromSearch: false});
 
     props = getProvidedProps({attributeName: 'ok', defaultRefinement: ['wat']}, {}, {results});
-    expect(props).toEqual({items: [], currentRefinement: ['wat']});
+    expect(props).toEqual({items: [], currentRefinement: ['wat'], isFromSearch: false});
 
     results.getFacetValues.mockClear();
     results.getFacetValues.mockImplementation(() => [
@@ -85,6 +86,26 @@ describe('connectRefinementList', () => {
         count: 20,
       },
     ]);
+  });
+
+  it('facetValues results should be provided as props if they exists', () => {
+    props = getProvidedProps({attributeName: 'ok'}, {}, {results}, {}, {
+      ok: [{
+        value: ['wat'],
+        label: 'wat',
+        isRefined: true,
+        count: 20,
+        highlighted: 'wat',
+      }],
+    });
+    expect(props.items).toEqual([{
+      _highlightResult: {label: {value: 'wat'}},
+      count: 20,
+      isRefined: true,
+      label: ['wat'],
+      value: [['wat']],
+    }]);
+    expect(props.isFromSearch).toBe(true);
   });
 
   it('doesn\'t render when no results are available', () => {
@@ -202,5 +223,13 @@ describe('connectRefinementList', () => {
 
     searchState = cleanUp({attributeName: 'name2'}, searchState);
     expect(searchState).toEqual({another: {searchState: 'searchState'}});
+  });
+
+  it('calling searchForFacetValues return the right searchForFacetValues parameters', () => {
+    const parameters = searchForFacetValues({attributeName: 'ok'}, {}, 'yep');
+    expect(parameters).toEqual({
+      facetName: 'ok',
+      query: 'yep',
+    });
   });
 });

packages/react-instantsearch/src/core/createInstantSearchManager.js
@@ -3,6 +3,7 @@ import algoliasearchHelper, {SearchParameters} from 'algoliasearch-helper';
 import createWidgetsManager from './createWidgetsManager';
 import createStore from './createStore';
 import highlightTags from './highlightTags.js';
+import {omit} from 'lodash';
 
 /**
  * Creates a new instance of the InstantSearchManager which controls the widgets and
@@ -63,19 +64,21 @@ export default function createInstantSearchManager({
   }
 
   function handleSearchSuccess(content) {
-    store.setState({
+    const nextState = omit({
       ...store.getState(),
       results: content,
       searching: false,
-    });
+    }, 'resultsFacetValues');
+    store.setState(nextState);
   }
 
   function handleSearchError(error) {
-    store.setState({
+    const nextState = omit({
       ...store.getState(),
       error,
       searching: false,
-    });
+    }, 'resultsFacetValues');
+    store.setState(nextState);
   }
 
   // Called whenever a widget has been rendered with new props.
@@ -115,6 +118,40 @@ export default function createInstantSearchManager({
     search();
   }
 
+  function onSearchForFacetValues(nextSearchState) {
+    store.setState({
+      ...store.getState(),
+      searchingForFacetValues: true,
+    });
+
+    helper.searchForFacetValues(nextSearchState.facetName, nextSearchState.query)
+      .then(content => {
+        store.setState({
+          ...store.getState(),
+          resultsFacetValues: {
+            ...store.getState().resultsFacetValues,
+            [nextSearchState.facetName]: content.facetHits,
+          },
+          searchingForFacetValues: false,
+        });
+      }, error => {
+        store.setState({
+          ...store.getState(),
+          error,
+          searchingForFacetValues: false,
+        });
+      })
+      .catch(error => {
+        // Since setState is synchronous, any error that occurs in the render of a
+        // component will be swallowed by this promise.
+        // This is a trick to make the error show up correctly in the console.
+        // See http://stackoverflow.com/a/30741722/969302
+        setTimeout(() => {
+          throw error;
+        });
+      });
+  }
+
   function getWidgetsIds() {
     return store.getState().metadata.reduce((res, meta) =>
         typeof meta.id !== 'undefined' ? res.concat(meta.id) : res
@@ -127,5 +164,6 @@ export default function createInstantSearchManager({
     getWidgetsIds,
     onExternalStateUpdate,
     transitionState,
+    onSearchForFacetValues,
   };
 }

stories/RefinementList.stories.js
@@ -28,6 +28,10 @@ stories.add('default', () =>
       showMore={true}
     />
   </WrapWithHits>
+).add('with search for facets value', () =>
+  <WrapWithHits>
+    <RefinementList attributeName="category" searchForFacetValues/>
+  </WrapWithHits>
 ).add('playground', () =>
   <WrapWithHits >
     <RefinementList