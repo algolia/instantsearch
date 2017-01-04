1696b61bdc073723ba647506b4274c0f6ee733b8
vvo <vincent.voyer@algolia.com>
2017-01-04 11:15:37 +0100

    fix lint

--- docgen/assets/js/sidebar.js
+++ docgen/assets/js/sidebar.js
@@ -105,7 +105,7 @@ function toggleDocumentationSidebar() {
   function init() {
     const bodySize = document.body.clientWidth;
     if (bodySize <= 960 && sidebarNav) {
-      trigger.addEventListener('click', (e) => {
+      trigger.addEventListener('click', () => {
         sidebarNav.classList.toggle('Showed');
         trigger.classList.toggle('Showed');
       });
