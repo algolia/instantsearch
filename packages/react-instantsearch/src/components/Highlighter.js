gianpaj/instantsearch.js

feat(snippet): add a snippet widget to be able to highlight snippet results

packages/react-instantsearch/src/components/Highlighter.js
@@ -1,0 +1,20 @@
+import React from 'react';
+
+export default function Highlighter({hit, attributeName, highlight, path}) {
+  const parsedHighlightedValue = highlight({hit, attributeName, path});
+  const reactHighlighted = parsedHighlightedValue.map((v, i) => {
+    const key = `split-${i}-${v.value}`;
+    if (!v.isHighlighted) {
+      return <span key={key} className="ais-Highlight__nonHighlighted">{v.value}</span>;
+    }
+    return <em key={key} className="ais-Highlight__highlighted">{v.value}</em>;
+  });
+  return <span className="ais-Highlight">{reactHighlighted}</span>;
+}
+
+Highlighter.propTypes = {
+  hit: React.PropTypes.object.isRequired,
+  attributeName: React.PropTypes.string.isRequired,
+  highlight: React.PropTypes.func.isRequired,
+  path: React.PropTypes.string.isRequired,
+};

packages/react-instantsearch/src/components/Snippet.js
@@ -1,0 +1,7 @@
+import React from 'react';
+
+import Highlighter from './Highlighter';
+
+export default function Snippet(props) {
+  return <Highlighter path="_snippetResult" {...props}/>;
+}

algolia/instantsearch

packages/react-instantsearch/src/components/Highlighter.js
@@ -1,0 +1,20 @@
+import React from 'react';
+
+export default function Highlighter({hit, attributeName, highlight, highlightProperty}) {
+  const parsedHighlightedValue = highlight({hit, attributeName, highlightProperty});
+  const reactHighlighted = parsedHighlightedValue.map((v, i) => {
+    const key = `split-${i}-${v.value}`;
+    if (!v.isHighlighted) {
+      return <span key={key} className="ais-Highlight__nonHighlighted">{v.value}</span>;
+    }
+    return <em key={key} className="ais-Highlight__highlighted">{v.value}</em>;
+  });
+  return <span className="ais-Highlight">{reactHighlighted}</span>;
+}
+
+Highlighter.propTypes = {
+  hit: React.PropTypes.object.isRequired,
+  attributeName: React.PropTypes.string.isRequired,
+  highlight: React.PropTypes.func.isRequired,
+  highlightProperty: React.PropTypes.string.isRequired,
+};

packages/react-instantsearch/src/components/Snippet.js
@@ -1,13 +1,13 @@
 import React from 'react';
+
 import Highlighter from './Highlighter';
 
-export default function Highlight(props) {
-  return <Highlighter highlightProperty="_highlightResult" {...props}/>;
+export default function Snippet(props) {
+  return <Highlighter highlightProperty="_snippetResult" {...props}/>;
 }
 
-Highlight.propTypes = {
+Snippet.propTypes = {
   hit: React.PropTypes.object.isRequired,
   attributeName: React.PropTypes.string.isRequired,
   highlight: React.PropTypes.func.isRequired,
 };
-