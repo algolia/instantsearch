58df900ccba1e18c5e53b410380d2e11f086afb7
Marie-Laure Thuret <mthuret@users.noreply.github.com>
2017-01-05 13:01:51 +0100

    chore: remove react-native dependencies from the root (#1805)

--- packages/react-instantsearch/examples/react-native/index.android.js
+++ packages/react-instantsearch/examples/react-native/index.android.js
@@ -4,6 +4,8 @@
  * @flow
  */
 
+/* eslint-disable import/no-unresolved */
 import {AppRegistry} from 'react-native';
+/* eslint-enable import/no-unresolved */
 import App from './App';
 AppRegistry.registerComponent('reactnative', () => App);
