5cb98e0b63d778bf7b4335553fbb0c9f6a375b31
Vincent Voyer <wouaren@gmail.com>
2017-01-20 09:09:09 +0100

    chore(netlify): avoid creating useless zip when deployed by netlify.com (algolia/react-instantsearch#1883)
    * chore(netlify): avoid creating useless zip when deployed by netlify.com

--- scripts/docs:build-preview.sh
+++ scripts/docs:build-preview.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 NODE_ENV=production yarn docs:build &&
-mv docs-production/react/_redirects docs-production/_redirects && # This is necessary so that the deployed preview lives at /react/
-rm -f preview.zip &&
-zip -r -9 preview.zip docs-production/
+mv docs-production/react/_redirects docs-production/_redirects # This is necessary so that the deployed preview lives at /react/
