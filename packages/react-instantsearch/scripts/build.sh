d62d8b7a84283882fe6c6f8bbaa7c19700769864
Vincent Voyer <wouaren@gmail.com>
2017-01-20 13:18:08 +0100

    fix(publish): publish react-instantsearch/dist instead of root (algolia/react-instantsearch#1884)
    Before this commit, we broke the publishing process

--- packages/react-instantsearch/scripts/build.sh
+++ packages/react-instantsearch/scripts/build.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-set -e # exit when error
-
-mkdir -p dist/
-rm -rf dist/*
-cp package.json dist/
-cp README.md dist/
-babel -q index.js -o dist/index.js
-babel -q dom.js -o dist/dom.js
-babel -q connectors.js -o dist/connectors.js
-babel -q native.js -o dist/native.js
+mkdir -p dist/ &&
+rm -rf dist/* &&
+cp package.json dist/ &&
+cp README.md dist/ &&
+babel -q index.js -o dist/index.js &&
+babel -q dom.js -o dist/dom.js &&
+babel -q connectors.js -o dist/connectors.js &&
+babel -q native.js -o dist/native.js &&
 babel -q --ignore test.js,__mocks__ --out-dir dist/src src
