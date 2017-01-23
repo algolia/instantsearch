7d0c1599886d06da800b58265a628682c7cd7461
Vincent Voyer <wouaren@gmail.com>
2017-01-23 13:45:33 +0100

    chore(website): fix production website paths (#1888)
    Before this commit I messed up with the production website and made
    the base href path /react/ instead of /instantsearch.js/react/
    
    Now both the preview and production website will reside at
    /instantsearch.js/react/.

--- scripts/docs:build-preview.sh
+++ scripts/docs:build-preview.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 NODE_ENV=production yarn docs:build &&
-mv docs-production/react/_redirects docs-production/_redirects # This is necessary so that the deployed preview lives at /react/
+mkdir -p docs-production/instantsearch.js/ &&
+mv docs-production/react/_redirects docs-production/ && # This is necessary so that the deployed preview lives at /react/
+mv docs-production/react docs-production/instantsearch.js
