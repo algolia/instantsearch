d95cfbfc7886b119d54785dc50ef251d1adc735c
Vincent Voyer <wouaren@gmail.com>
2017-01-27 13:39:24 +0100

    docs(examples): urlSync example implem now update URL every 700ms (#1916)
    Before this commit, we were advocating for updating the url at every keystrokes.
    We had multiple reports since releases of instansearch.js that this led to slowdowns
    of the browser UI.
    
    fixes #1856

--- docgen/src/examples/urlSync.js
+++ docgen/src/examples/urlSync.js
@@ -1,32 +1,38 @@
 import React, {Component} from 'react';
 import qs from 'qs';
 
+const updateAfter = 700;
+const searchStateToUrl =
+  searchState =>
+  searchState ? `${window.location.pathname}?${qs.stringify(searchState)}` : '';
+
 export const withUrlSync = App => class extends Component {
   constructor() {
     super();
     this.state = {searchState: qs.parse(window.location.search.slice(1))};
-    window.onpopstate = () => this.setState({searchState: qs.parse(window.location.search.slice(1))});
+    window.addEventListener(
+      'popstate',
+      ({state: searchState}) => this.setState({searchState})
+    );
   }
 
-  onSearchStateChange = nextSearchState => {
-    const THRESHOLD = 700;
-    const newPush = Date.now();
-    const search = nextSearchState ? `${window.location.pathname}?${qs.stringify(nextSearchState)}` : '';
-    if (this.state.lastPush && newPush - this.state.lastPush <= THRESHOLD) {
-      window.history.replaceState(null, null, search);
-    } else {
-      window.history.pushState(null, null, search);
-    }
-    this.setState({lastPush: newPush, searchState: nextSearchState});
+  onSearchStateChange = searchState => {
+    clearTimeout(this.debouncedSetState);
+    this.debouncedSetState = setTimeout(() => {
+      window.history.pushState(
+        searchState,
+        null,
+        searchStateToUrl(searchState)
+      );
+    }, updateAfter);
+    this.setState({searchState});
   };
-
-  createURL = searchState => `${window.location.pathname}?${qs.stringify(searchState)}`;
 
   render() {
     return <App {...this.props}
                 searchState={this.state.searchState}
                 onSearchStateChange={this.onSearchStateChange}
-                createURL={this.createURL}
+                createURL={searchStateToUrl}
     />;
   }
 };
