f552d0820291a627ed86ca3b137db2b659b71e5b
Marie-Laure Thuret <mthuret@users.noreply.github.com>
2017-02-06 14:01:14 +0100

    fix(Configure): add configure parameters in search state (#1935)
    fixes #1863

--- packages/react-instantsearch/src/connectors/connectConfigure.js
+++ packages/react-instantsearch/src/connectors/connectConfigure.js
@@ -1,11 +1,40 @@
 import createConnector from '../core/createConnector.js';
-import omit from 'lodash/omit';
+import {omit, isEmpty} from 'lodash';
+
+const namespace = 'configure';
+
+function getCurrentRefinement(props, searchState) {
+  return Object.keys(props).reduce((acc, item) => {
+    acc[item] = searchState[namespace] && searchState[namespace][item]
+     ? searchState[namespace][item] : props[item];
+    return acc;
+  }, {});
+}
 
 export default createConnector({
   displayName: 'AlgoliaConfigure',
-  getProvidedProps() { return {}; },
-  getSearchParameters(searchParameters, props) {
-    const configuration = omit(props, 'children');
+  getProvidedProps() {
+    return {};
+  },
+  getSearchParameters(searchParameters, props, searchState) {
+    const items = omit(props, 'children');
+    const configuration = getCurrentRefinement(items, searchState);
     return searchParameters.setQueryParameters(configuration);
+  },
+  transitionState(props, prevSearchState, nextSearchState) {
+    const items = omit(props, 'children');
+    return {
+      ...nextSearchState,
+      [namespace]: {...items, ...nextSearchState[namespace]},
+    };
+  },
+  cleanUp(props, searchState) {
+    const configureState = Object.keys(searchState[namespace]).reduce((acc, item) => {
+      if (!props[item]) {
+        acc[item] = searchState[namespace][item];
+      }
+      return acc;
+    }, {});
+    return isEmpty(configureState) ? omit(searchState, namespace) : {...searchState, [namespace]: configureState};
   },
 });
