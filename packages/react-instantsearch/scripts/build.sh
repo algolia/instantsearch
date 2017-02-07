18c9a736e65449b8f48fe0df4f4003233bb1d0db
vvo <vincent.voyer@algolia.com>
2017-02-07 18:30:28 +0100

    chore(dev tools): add standalone browser bundle to ease demos/fiddles
    This adds a standalone JavaScript bundle of React InstantSearch so that
    we can easily build demos and jsFiddles with it. Example usage:
    https://jsfiddle.net/o7kfw88g/ (to be added to the issue template
    afterwards).
    
    The build will be available on unpkg.com as usual.

--- packages/react-instantsearch/scripts/build.sh
+++ packages/react-instantsearch/scripts/build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e # exit when error
+
 mkdir -p dist/ &&
 rm -rf dist/* &&
 cp package.json dist/ &&
@@ -8,4 +10,32 @@ babel -q index.js -o dist/index.js &&
 babel -q dom.js -o dist/dom.js &&
 babel -q connectors.js -o dist/connectors.js &&
 babel -q native.js -o dist/native.js &&
-babel -q --ignore test.js,__mocks__ --out-dir dist/src src
+babel -q --ignore test.js,__mocks__ --out-dir dist/src src &&
+NODE_ENV=production webpack
+
+license="/*! ReactInstantSearch ${VERSION:-UNRELEASED} | © Algolia, inc. | https://community.algolia.com/instantsearch.js/react/ */"
+umd_dist="dist/umd"
+entries=( 'Core' 'Dom' )
+for entry in "${entries[@]}"
+do
+  dist_file="$umd_dist/${entry}.js"
+  dist_file_min="$umd_dist/${entry}.min.js"
+  source_map="${entry}.js.map"
+  source_map_min="${entry}.min.js.map"
+  dist_file_sourcemap="$umd_dist/${source_map}"
+  dist_file_sourcemap_min="$umd_dist/${source_map_min}"
+
+  echo "$license" | cat - "${dist_file}" > /tmp/out && mv /tmp/out "${dist_file}"
+
+  uglifyjs "${dist_file}" \
+    --in-source-map "${dist_file_sourcemap}" \
+    --source-map "${dist_file_sourcemap_min}" \
+    --source-map-url "${source_map_min}" \
+    --preamble "$license" \
+    -c warnings=false \
+    -m \
+    -o "${dist_file_min}"
+
+  gzip_size=$(gzip -9 < "$dist_file_min" | wc -c | pretty-bytes)
+  echo "=> $dist_file_min gzipped will weight $gzip_size"
+done
---
+++ packages/react-instantsearch/webpack.config.babel.js
@@ -0,0 +1,44 @@
+import {join} from 'path';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import webpack from 'webpack'; // this is available because of root package.json
+
+export default {
+  entry: {
+    Core: './index.js',
+    Dom: './dom.js',
+  },
+  devtool: 'source-map',
+  output: {
+    path: join(__dirname, 'dist/umd'),
+    filename: '[name].js',
+    library: ['ReactInstantSearch', '[name]'],
+    libraryTarget: 'umd',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/, exclude: /node_modules/, loader: 'babel',
+      },
+    ],
+  },
+  externals: {
+    'react': {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    }, 'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
+    }),
+  ],
+};
