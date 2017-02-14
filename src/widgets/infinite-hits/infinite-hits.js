745ed8986373a7a2535151ec3b44362c9e2e0ff3
Alex S <bobylito@users.noreply.github.com>
2017-02-14 03:43:46 +0100

    fix(infinite-hits): disable load more button when no more pages (#1973)
    fixes #1971

--- src/widgets/infinite-hits/infinite-hits.js
+++ src/widgets/infinite-hits/infinite-hits.js
@@ -13,19 +13,20 @@ const bem = bemHelper('ais-infinite-hits');
 
 /**
  * Display the list of results (hits) from the current search
- * @function hits
+ * @function infiniteHits
  * @param  {string|DOMElement} options.container CSS Selector or DOMElement to insert the widget
  * @param  {number} [options.hitsPerPage=20] The number of hits to display per page [*]
  * @param  {Object} [options.templates] Templates to use for the widget
- * @param  {string|Function} [options.templates.empty=''] Template to use when there are no results.
- * @param  {string|Function} [options.templates.item=''] Template to use for each result. This template will receive an object containing a single record.
+ * @param  {string|Function} [options.templates.empty=""] Template to use when there are no results.
+ * @param  {string|Function} [options.templates.item=""] Template to use for each result. This template will receive an object containing a single record.
+ * @param  {string} [options.showMoreLabel="Show more results"] label used on the show more button
  * @param  {Object} [options.transformData] Method to change the object passed to the templates
  * @param  {Function} [options.transformData.empty] Method used to change the object passed to the `empty` template
  * @param  {Function} [options.transformData.item] Method used to change the object passed to the `item` template
  * @param  {Object} [options.cssClasses] CSS classes to add
  * @param  {string|string[]} [options.cssClasses.root] CSS class to add to the wrapping element
  * @param  {string|string[]} [options.cssClasses.empty] CSS class to add to the wrapping element when no results
- * @param  {string|string[]} [options.7cssClasses.item] CSS class to add to each result
+ * @param  {string|string[]} [options.cssClasses.item] CSS class to add to each result
  * @return {Object}
  */
 const usage = `
@@ -34,6 +35,7 @@ infiniteHits({
   container,
   [ cssClasses.{root,empty,item}={} ],
   [ templates.{empty,item} | templates.{empty} ],
+  [ showMoreLabel="Show more results" ]
   [ transformData.{empty,item} | transformData.{empty} ],
   [ hitsPerPage=20 ]
 })`;
@@ -80,6 +82,8 @@ function infiniteHits({
 
       hitsCache = [...hitsCache, ...results.hits];
 
+      const isLastPage = results.nbPages <= results.page + 1;
+
       ReactDOM.render(
         <InfiniteHits
           cssClasses={cssClasses}
@@ -87,6 +91,7 @@ function infiniteHits({
           results={results}
           showMore={this.showMore}
           showMoreLabel={showMoreLabel}
+          isLastPage={isLastPage}
           templateProps={this._templateProps}
         />,
         containerNode
