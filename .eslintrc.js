58ece14e8ecb10c60c3eaa0f999f0747f0637e52
Marie-Laure Thuret <mthuret@MacBook-Pro-de-Marie-Laure.local>
2017-02-21 16:34:44 +0100

    chore(jest): update and add linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
-  "extends": "algolia",
+  "extends": ["algolia"],
+  "plugins": ["jest"],
   "rules": {
-    "space-before-blocks": ["error"]
+    "space-before-blocks": ["error"],
+    "jest/no-disabled-tests": "error",
+    "jest/no-focused-tests": "error",
+    "jest/no-identical-title": "error",
   }
 };
