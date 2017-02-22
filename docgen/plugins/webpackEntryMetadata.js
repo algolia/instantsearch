d8e17a2f81ad6e1317cb611e52251e3c36f1ff26
vvo <vincent.voyer@algolia.com>
2017-02-21 17:13:06 -0800

    chore(dev): reload in dev now takes ~500ms
    Before this commit, the `yarn start` workflow took 10s, now it takes
    500ms in average.
    
    Optimization was done via:
    - webpack 2 switch
    - dev env uses simpler sourcemap strategy
    - use happypack (https://github.com/amireh/happypack)
    - Create a common chunks plugin (this reduces the size of downloaded
    JavaScript files by a big factor).

--- docgen/plugins/webpackEntryMetadata.js
+++ docgen/plugins/webpackEntryMetadata.js
@@ -2,13 +2,17 @@
 // useful in dev mode when not using ms-webpack
 export default function webpackEntryMetadata(webpackConfig) {
   return (filenames, metalsmith, cb) => {
+    const assets = Object
+      .keys(webpackConfig.entry)
+      .reduce((memo, entryName) => ({
+        ...memo,
+        [`${entryName}.js`]: `${webpackConfig.output.publicPath}${entryName}.js`,
+      }), {});
     metalsmith.metadata().webpack = {
-      assets: Object
-        .keys(webpackConfig.entry)
-        .reduce((memo, entryName) => ({
-          ...memo,
-          [`${entryName}.js`]: `${webpackConfig.output.publicPath}${entryName}.js`,
-        }), {}),
+      assets: {
+        ...assets,
+        'js/common.js': `${webpackConfig.output.publicPath}js/common.js`,
+      },
     };
 
     cb();
