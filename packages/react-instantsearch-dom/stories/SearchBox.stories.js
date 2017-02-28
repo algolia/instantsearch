ca363da01e531d25aef5231ce8bc41dfe98bf090
Haroen Viaene <fingebimus@me.com>
2017-02-28 14:03:57 +0100

    feat(SearchBox): allow custom reset and submit components (algolia/react-instantsearch#1991)

--- packages/react-instantsearch-dom/stories/SearchBox.stories.js
+++ packages/react-instantsearch-dom/stories/SearchBox.stories.js
@@ -16,8 +16,18 @@ stories.add('default', () =>
   <WrapWithHits searchBox={false} hasPlayground={true} linkedStoryGroup="SearchBox">
     <SearchBox defaultRefinement="battery" />
   </WrapWithHits>
-)
-.add('playground', () =>
+).add('with submit and reset components', () =>
+  <WrapWithHits searchBox={false} hasPlayground={true} linkedStoryGroup="SearchBox" >
+    <SearchBox
+      submitComponent={<span>🔍</span>}
+      resetComponent={
+        <svg viewBox="200 198 108 122">
+          <path d="M200.8 220l45 46.7-20 47.4 31.7-34 50.4 39.3-34.3-52.6 30.2-68.3-49.7 51.7" />
+        </svg>
+      }
+    />
+  </WrapWithHits>
+).add('playground', () =>
   <WrapWithHits searchBox={false}>
     <SearchBox
       focusShortcuts={['s']}
