redsquare/instantsearch.js

feat(multi-index): ease multi index and auto complete

packages/react-instantsearch/connectors.js
@@ -3,6 +3,7 @@ export {default as connectCurrentRefinements} from './src/connectors/connectCurr
 export {default as connectHierarchicalMenu} from './src/connectors/connectHierarchicalMenu.js';
 export {default as connectHighlight} from './src/connectors/connectHighlight.js';
 export {default as connectHits} from './src/connectors/connectHits.js';
+export {default as connectMultiHits} from './src/connectors/connectMultiHits.js';
 export {default as connectHitsPerPage} from './src/connectors/connectHitsPerPage.js';
 export {default as connectInfiniteHits} from './src/connectors/connectInfiniteHits.js';
 export {default as connectMenu} from './src/connectors/connectMenu.js';

algolia/instantsearch

packages/react-instantsearch/connectors.js
@@ -3,6 +3,7 @@ export {default as connectCurrentRefinements} from './src/connectors/connectCurr
 export {default as connectHierarchicalMenu} from './src/connectors/connectHierarchicalMenu.js';
 export {default as connectHighlight} from './src/connectors/connectHighlight.js';
 export {default as connectHits} from './src/connectors/connectHits.js';
+export {default as connectAutoComplete} from './src/connectors/connectAutoComplete.js';
 export {default as connectHitsPerPage} from './src/connectors/connectHitsPerPage.js';
 export {default as connectInfiniteHits} from './src/connectors/connectInfiniteHits.js';
 export {default as connectMenu} from './src/connectors/connectMenu.js';