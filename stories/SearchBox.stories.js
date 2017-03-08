3ed97254347db5b0a527263291ae1a9bba9338a9
iam4x <iam4x@me.com>
2017-03-08 14:11:49 +0100

    doc(SearchBox): simplify logic of the story for example

--- stories/SearchBox.stories.js
+++ stories/SearchBox.stories.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {storiesOf} from '@kadira/storybook';
+import {storiesOf, action} from '@kadira/storybook';
 import {SearchBox} from '../packages/react-instantsearch/dom';
 import {withKnobs, object} from '@kadira/storybook-addon-knobs';
 import {WrapWithHits} from './util';
@@ -48,7 +48,7 @@ stories.add('default', () =>
 // --------------------
 class SearchBoxContainer extends Component {
 
-  state = {eventsHistory: [], selectedEvents: {onChange: true}}
+  state = {selectedEvents: {onChange: true}}
 
   get supportedEvents() {
     return [
@@ -58,48 +58,27 @@ class SearchBoxContainer extends Component {
     ];
   }
 
-  get time() {
-    const d = new Date();
-    const h = `0${d.getHours()}`.slice(-2);
-    const m = `0${d.getMinutes()}`.slice(-2);
-    const s = `0${d.getSeconds()}`.slice(-2);
-    return `${h}:${m}:${s}`;
-  }
-
   handleSelectedEvent = eventName => ({target: {checked}}) => {
     const {selectedEvents} = this.state;
     this.setState({selectedEvents: {...selectedEvents, [eventName]: checked}});
   }
 
-  handleInputEvent = eventName => event => {
-    // we dont want the page to reload while testing
-    if (eventName === 'onSubmit') event.preventDefault();
+  handleSubmit = event => {
+    // we dont want the page to reload after the submit event
+    event.preventDefault();
+    event.stopPropagation();
 
-    // we only want to log events which are selected
+    this.logAction('onSubmit')(event);
+  }
+
+  logAction = eventName => event => {
+    // we dont want to log unselected event
     if (this.state.selectedEvents[eventName]) {
-      const history = (() => {
-        switch (eventName) {
-        case 'onChange':
-          return `[${this.time}] event: onChange | e.target.value: ${event.target.value}`;
-        case 'onKeyDown':
-        case 'onKeyPress':
-          return `[${this.time}] event: ${eventName} | e.keyCode ${event.keyCode}`;
-        default:
-          return `[${this.time}] event: ${eventName}`;
-        }
-      })();
-
-      const eventsHistory = this.state.eventsHistory.length === 10
-        ? [history, ...(this.state.eventsHistory.slice(0, -1))]
-        : [history, ...this.state.eventsHistory];
-
-      this.setState({eventsHistory});
+      action(eventName)(event);
     }
   }
 
   render() {
-    const {eventsHistory} = this.state;
-
     return (
       <WrapWithHits searchBox={ false } hasPlayground={ true } linkedStoryGroup="searchBox">
         <div style={ {color: '#999', borderBottom: '1px solid #E4E4E4', marginBottom: 10} }>
@@ -115,17 +94,18 @@ class SearchBoxContainer extends Component {
                 { eventName }
               </label>
           ) }
-
-          {/* events history */}
-          <pre>{ eventsHistory.join('\n') }</pre>
         </div>
 
-        <SearchBox {
-          ...this.supportedEvents.reduce(
-            (events, eventName) =>
-              ({...events, [eventName]: this.handleInputEvent(eventName)})
-          , {})
-        } />
+        <SearchBox
+          onSubmit={ this.handleSubmit }
+          onReset={ this.logAction('onReset') }
+          onChange={ this.logAction('onChange') }
+          onFocus={ this.logAction('onFocus') }
+          onBlur={ this.logAction('onBlur') }
+          onSelect={ this.logAction('onSelect') }
+          onKeyDown={ this.logAction('onKeyDown') }
+          onKeyPress={ this.logAction('onKeyPress') }
+        />
       </WrapWithHits>
     );
   }
