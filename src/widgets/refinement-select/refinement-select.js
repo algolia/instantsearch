a9fb7402bbec8d0e2a84e1c6adb2b612dd0b4045
iam4x <iam4x@me.com>
2017-03-14 17:10:54 +0100

    doc(refinementSelect): describe widget options

--- src/widgets/refinement-select/refinement-select.js
+++ src/widgets/refinement-select/refinement-select.js
@@ -15,6 +15,28 @@ import {
 
 const bem = bemHelper('ais-refinement-select');
 
+/**
+ * Create a <select> menu out of a facet
+ * @function refinementSelect
+ * @param  {string|DOMElement} options.container CSS Selector or DOMElement to insert the widget
+ * @param  {string[]|Function} [options.sortBy=['count:desc', 'name:asc']] How to sort refinements. Possible values: `count|isRefined|name:asc|name:desc`.
+ *   You can also use a sort function that behaves like the standard Javascript [compareFunction](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Syntax). [*]
+ * @param  {string} [options.limit=10] How many facets values to retrieve [*]
+ * @param  {Object} [options.templates] Templates to use for the widget
+ * @param  {string|Function} [options.templates.header] Header template
+ * @param  {string|Function} [options.templates.footer] Footer template
+ * @param  {string} [options.templates.seeAllOption] First option of <select> template
+ * @param  {Function} [options.templates.selectOption] Other options of <select> template, provided with `name`, `count`, `isRefined` data properties
+ * @param  {Object} [options.cssClasses] CSS classes to add to the wrapping elements
+ * @param  {string|string[]} [options.cssClasses.root] CSS class to add to the root element
+ * @param  {string|string[]} [options.cssClasses.header] CSS class to add to the header element
+ * @param  {string|string[]} [options.cssClasses.body] CSS class to add to the body element
+ * @param  {string|string[]} [options.cssClasses.footer] CSS class to add to the footer element
+ * @param  {string|string[]} [options.cssClasses.select] CSS class to add to the <select> element
+ * @param  {string|string[]} [options.cssClasses.option] CSS class to add to the <option> element
+ * @param  {boolean} [options.autoHideContainer=true] Hide the container when there are no items in the menu
+ * @return {Object}
+**/
 const usage = `Usage:
 refinementSelect({
   container,
