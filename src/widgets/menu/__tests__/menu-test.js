bc72ba5ad4dc32a0a57dc57bff01c64ad28966fc
vvo <vincent.voyer@algolia.com>
2017-03-17 12:27:34 +0100

    test(menu): add snapshot

--- src/widgets/menu/__tests__/menu-test.js
+++ src/widgets/menu/__tests__/menu-test.js
@@ -1,7 +1,6 @@
-
-
-
 import menu from '../menu';
+
+import sinon from 'sinon';
 
 describe('menu', () => {
   it('throws an exception when no attributeName', () => {
@@ -13,5 +12,24 @@ describe('menu', () => {
     const attributeName = '';
     expect(menu.bind(null, {attributeName})).toThrow(/^Usage/);
   });
-});
 
+  it('snapshot', () => {
+    const data = {data: [{name: 'foo'}, {name: 'bar'}]};
+    const results = {getFacetValues: sinon.spy(() => data)};
+    const helper = {
+      toggleRefinement: sinon.stub().returnsThis(),
+      search: sinon.spy(),
+    };
+    const state = {
+      toggleRefinement: sinon.spy(),
+    };
+    const createURL = () => '#';
+    const ReactDOM = {render: sinon.spy()};
+    menu.__Rewire__('ReactDOM', ReactDOM);
+    const widget = menu({container: document.createElement('div'), attributeName: 'test'});
+    widget.init({helper, createURL});
+    widget.render({results, state});
+    expect(ReactDOM.render.firstCall.args[0]).toMatchSnapshot();
+    menu.__ResetDependency__('ReactDOM');
+  });
+});
