6d06173dde2e9545be64a74d18d0d9ef4726a9d6
iam4x <iam4x@me.com>
2017-03-22 17:16:13 +0100

    doc(connectors): connectStarRating jsdoc

--- src/connectors/star-rating/connectStarRating.js
+++ src/connectors/star-rating/connectStarRating.js
@@ -1,44 +1,13 @@
 import {checkRendering} from '../../lib/utils.js';
 
-/**
- * Instantiate a list of refinements based on a rating attribute
- * The ratings must be integer values. You can still keep the precise float value in another attribute
- * to be used in the custom ranking configuration. So that the actual hits ranking is precise.
- * @function starRating
- * @param  {string|DOMElement} options.container CSS Selector or DOMElement to insert the widget
- * @param  {string} options.attributeName Name of the attribute for filtering
- * @param  {number} [options.max] The maximum rating value
- * @param  {Object} [options.labels] Labels used by the default template
- * @param  {string} [options.labels.andUp] The label suffixed after each line
- * @param  {Object} [options.templates] Templates to use for the widget
- * @param  {string|Function} [options.templates.header] Header template
- * @param  {string|Function} [options.templates.item] Item template, provided with `name`, `count`, `isRefined`, `url` data properties
- * @param  {string|Function} [options.templates.footer] Footer template
- * @param  {Function} [options.transformData.item] Function to change the object passed to the `item` template
- * @param  {boolean} [options.autoHideContainer=true] Hide the container when no results match
- * @param  {Object} [options.cssClasses] CSS classes to add to the wrapping elements
- * @param  {string|string[]} [options.cssClasses.root] CSS class to add to the root element
- * @param  {string|string[]} [options.cssClasses.header] CSS class to add to the header element
- * @param  {string|string[]} [options.cssClasses.body] CSS class to add to the body element
- * @param  {string|string[]} [options.cssClasses.footer] CSS class to add to the footer element
- * @param  {string|string[]} [options.cssClasses.list] CSS class to add to the list element
- * @param  {string|string[]} [options.cssClasses.item] CSS class to add to each item element
- * @param  {string|string[]} [options.cssClasses.link] CSS class to add to each link element
- * @param  {string|string[]} [options.cssClasses.disabledLink] CSS class to add to each disabled link (when using the default template)
- * @param  {string|string[]} [options.cssClasses.star] CSS class to add to each star element (when using the default template)
- * @param  {string|string[]} [options.cssClasses.emptyStar] CSS class to add to each empty star element (when using the default template)
- * @param  {string|string[]} [options.cssClasses.active] CSS class to add to each active element
- * @param  {object|boolean} [options.collapsible=false] Hide the widget body and footer when clicking on header
- * @param  {boolean} [options.collapsible.collapsed] Initial collapsed state of a collapsible widget
- * @return {Object}
- */
 const usage = `Usage:
 var customStarRating = connectStarRating(function render(params, isFirstRendering) {
   // params = {
-  //   facetValues,
+  //   items,
   //   createURL,
+  //   refine,
   //   instantSearchInstance,
-  //   results,
+  //   hasNoResults,
   // }
 });
 search.addWidget(
@@ -49,6 +18,27 @@ search.addWidget(
 );
 Full documentation available at https://community.algolia.com/instantsearch.js/connectors/connectStarRating.html
 `;
+
+/**
+ * @typedef {Object} CustomStarRatingWidgetOptions
+ * @param {string} attributeName Name of the attribute for faceting (eg. "free_shipping")
+ * @param {number} [max = 5] the maximum rating value
+ */
+
+/**
+ * @typedef {Object} StarRatingRenderingOptions
+ * @property {Object[]} items
+ * @property {function} createURL
+ * @property {function} refine
+ * @property {boolean} hasNoResults
+ * @property {InstantSearch} instantSearchInstance
+ */
+
+/**
+ * Connects a rendering function with the star rating business logic.
+ * @param {function(StarRatingRenderingOptions)} renderFn function that renders the star rating widget
+ * @return {function(CustomStarRatingWidgetOptions)} a widget factory for star rating widget
+ */
 export default function connectStarRating(renderFn) {
   checkRendering(renderFn, usage);
 
