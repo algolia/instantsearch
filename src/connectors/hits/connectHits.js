b3e5f0b0b08dc171296ab39d156fd43f8a0dfc58
iam4x <iam4x@me.com>
2017-03-23 17:12:30 +0100

    doc(connectors): connectHits

--- src/connectors/hits/connectHits.js
+++ src/connectors/hits/connectHits.js
@@ -1,21 +1,60 @@
-const connectHits = renderHits => ({
-    hitsPerPage = 20,
-  } = {}) => ({
-    getConfiguration: () => ({hitsPerPage}),
+import {checkRendering} from '../../lib/utils.js';
+
+const usage = `Usage:
+var customHits = connectHits(function render(params, isFirstRendering) {
+  // params = {
+  //   hits,
+  //   results,
+  //   instantSearchInstance,
+  // }
+});
+search.addWidget(
+  customHits({
+    [ hitsPerPage = 20 ]
+  });
+);
+Full documentation available at https://community.algolia.com/instantsearch.js/connectors/connectHits.html
+`;
+
+/**
+ * @typedef {Object} CustomHitsWidgetOptions
+ * @param {number} [hitsPerPage = 20] The number of hits to display per page
+ */
+
+/**
+ * @typedef HitsRenderingOptions
+ * @property {Object[]} hits
+ * @property {Object} results
+ * @property {InstantSearch} instantSearchInstance
+ */
+
+ /**
+  * Connects a rendering function with the hits business logic.
+  * @param {function(HitsRenderingOptions)} renderFn function that renders the hits widget
+  * @return {function(CustomHitsWidgetOptions)} a widget factory for hits widget
+  */
+export default function connectHits(renderFn) {
+  checkRendering(renderFn, usage);
+
+  return ({hitsPerPage = 20}) => ({
+    getConfiguration() {
+      return {hitsPerPage};
+    },
+
     init({instantSearchInstance}) {
-      renderHits({
+      renderFn({
         hits: [],
         results: undefined,
         instantSearchInstance,
       }, true);
     },
+
     render({results, instantSearchInstance}) {
-      renderHits({
+      renderFn({
         hits: results.hits,
         results,
         instantSearchInstance,
       }, false);
     },
   });
-
-export default connectHits;
+}
