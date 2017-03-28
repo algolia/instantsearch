27940557e6305747a2432c0bcd6f62db43bf1210
vvo <vincent.voyer@algolia.com>
2017-03-28 14:21:56 +0200

    chore(release): fix release beta script

--- packages/react-instantsearch/scripts/build-and-publish.sh
+++ packages/react-instantsearch/scripts/build-and-publish.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 
 npmFlags=''
-yarnFlags=''
 while getopts 'n:y:' flag; do
   case "${flag}" in
     n) npmFlags="${OPTARG}" ;;
-    y) yarnFlags="${OPTARG}" ;;
     *) error "Unexpected option ${flag}" ;;
   esac
 done
 
-yarn build $yarnFlags &&
+yarn build &&
 cd dist/ &&
 npm publish $npmFlags &&
 rm -rf dist/
