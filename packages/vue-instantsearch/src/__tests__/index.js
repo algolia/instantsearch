c727eaf456951e84b5a170c40c425415b2d2b9c6
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-04-10 10:46:52 +0200

    test(instantsearch): add test

---
+++ packages/vue-instantsearch/src/__tests__/index.js
@@ -0,0 +1,30 @@
+import InstantSearch from 'vue-instantsearch';
+
+test('Should register all components when installed', () => {
+  const component = jest.fn();
+  const Vue = { component };
+
+  InstantSearch.install(Vue);
+
+  expect(component).toBeCalledWith('ais-store', expect.any(Object));
+  expect(component).toBeCalledWith('ais-highlight', expect.any(Object));
+  expect(component).toBeCalledWith('ais-snippet', expect.any(Object));
+  expect(component).toBeCalledWith('ais-input', expect.any(Object));
+  expect(component).toBeCalledWith('ais-results', expect.any(Object));
+  expect(component).toBeCalledWith('ais-stats', expect.any(Object));
+  expect(component).toBeCalledWith('ais-pagination', expect.any(Object));
+  expect(component).toBeCalledWith(
+    'ais-results-per-page-selector',
+    expect.any(Object)
+  );
+  expect(component).toBeCalledWith('ais-tree-menu', expect.any(Object));
+  expect(component).toBeCalledWith('ais-sort-by-selector', expect.any(Object));
+  expect(component).toBeCalledWith('ais-clear', expect.any(Object));
+  expect(component).toBeCalledWith('ais-rating', expect.any(Object));
+  expect(component).toBeCalledWith('ais-no-results', expect.any(Object));
+  expect(component).toBeCalledWith('ais-refinement-list', expect.any(Object));
+  expect(component).toBeCalledWith('ais-price-range', expect.any(Object));
+  expect(component).toBeCalledWith('ais-powered-by', expect.any(Object));
+
+  expect(component).toHaveBeenCalledTimes(16);
+});
