c52620bfa77d8f958b5b127e166c613900345e08
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-04-11 16:22:33 +0200

    feat(url-sync-example): add URL sync example with Vue Router
    Closes: algolia/vue-instantsearch#61

---
+++ packages/examples/vue-router/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+};
+</script>
+
+<style lang="scss"></style>
---
+++ packages/examples/vue-router/src/main.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import InstantSearch from 'vue-instantsearch';
+import App from './App.vue';
+import Search from './Search.vue';
+
+Vue.use(InstantSearch);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    {
+      name: 'search',
+      path: '/search',
+      component: Search,
+      props: route => ({ query: route.query.q })
+    },
+    { path: '/', redirect: '/search' }
+  ]
+});
+
+new Vue({
+  el: '#app',
+  render: h => h(App),
+  router
+});
