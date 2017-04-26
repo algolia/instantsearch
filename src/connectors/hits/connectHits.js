9f4c3833ea7495a752b60c8cdde726efefa13041
iam4x <iam4x@me.com>
2017-04-26 17:29:18 +0200

    doc: connectHits

--- src/connectors/hits/connectHits.js
+++ src/connectors/hits/connectHits.js
@@ -15,17 +15,39 @@ Full documentation available at https://community.algolia.com/instantsearch.js/c
 
 /**
  * @typedef {Object} HitsRenderingOptions
- * @property {Object[]} hits the hits contained in the last results from Algolia
- * @property {Object} results the complete results from Algolia
- * @property {InstantSearch} instantSearchInstance the instance of instantsearch on which the widget is attached
- * @property {Object} widgetParams all original options forwarded to rendering
+ * @property {Object[]} hits The matched hits from Algolia API.
+ * @property {Object} results The complete results response from Algolia API.
+ * @property {InstantSearch} instantSearchInstance The instance of instantsearch on which the widget is attached.
+ * @property {Object} widgetParams All original widget options forwarded to the `renderFn`.
  */
 
 /**
- * Connects a rendering function with the hits business logic.
+ * **Hits** connector provides the logic to create connected components that will render the results retrieved from Algolia.
  * @type {Connector}
  * @param {function(HitsRenderingOptions, boolean)} renderFn function that renders the hits widget
  * @return {function} a widget factory for hits widget
+ * @example
+ * var $ = window.$;
+ * var instantsearch = window.instantsearch;
+ *
+ * // custom `renderFn` to render the custom ClearAll widget
+ * function renderFn(HitsRenderingOptions) {
+ *   HitsRenderingOptions.containerNode.html(
+ *     HitsRenderingOptions.hits.map(function(hit) {
+ *       return '<div>' + hit._highlightResult.name.value + '</div>';
+ *     })
+ *   );
+ * }
+ *
+ * // connect `renderFn` to Hits logic
+ * var customHits = instantsearch.connectors.connectHits(renderFn);
+ *
+ * // mount widget on the page
+ * search.addWidget(
+ *   customHits({
+ *     containerNode: $('#custom-hits-container'),
+ *   })
+ * );
  */
 export default function connectHits(renderFn) {
   checkRendering(renderFn, usage);
