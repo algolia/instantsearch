46d6ad2649157f4310d595d73862dd84656f4176
iam4x <iam4x@me.com>
2017-04-26 17:20:11 +0200

    doc: connectHierarchicalMenu

--- src/connectors/hierarchical-menu/connectHierarchicalMenu.js
+++ src/connectors/hierarchical-menu/connectHierarchicalMenu.js
@@ -26,27 +26,30 @@ Full documentation available at https://community.algolia.com/instantsearch.js/c
 
 /**
  * @typedef {Object} CustomHierarchicalMenuWidgetOptions
- * @property {string[]} attributes Array of attributes to use to generate the hierarchy of the menu.
- * @property  {string} [separator='>'] Separator used in the attributes to separate level values. [*]
+ * @property {string[]} attributesof Attributes to use to generate the hierarchy of the menu.
+ * @property  {string} [separator='>'] Separator used in the attributes to separate level values.
  * @property  {string} [rootPath] Prefix path to use if the first level is not the root level.
- * @property  {string} [showParentLevel=false] Show the parent level of the current refined value
- * @property  {number} [limit=10] How much facet values to get [*]
- * @property  {string[]|Function} [sortBy=['isRefined', 'count:desc']] How to sort refinements. Possible values: `count|isRefined|name:asc|name:desc`.
+ * @property  {string} [showParentLevel=false] Show the parent level of the current refined value.
+ * @property  {number} [limit=10] How much facet values to get.
+ * @property  {string[]|function} [sortBy=['isRefined', 'count:desc']] How to sort refinements. Possible values: `count|isRefined|name:asc|name:desc`.
  *   You can also use a sort function that behaves like the standard Javascript [compareFunction](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Syntax).
  */
 
 /**
  * @typedef {Object} HierarchicalMenuRenderingOptions
- * @property {function} createURL function that create a url for the next state
- * @property {Object[]} items the values to be rendered
- * @property {function} refine set the path of the hierarchical filter and triggers a new search
- * @property {InstantSearch} instantSearchInstance the instance of instantsearch on which the widget is attached
- * @property {Object} widgetParams all original options forwarded to rendering
- * @property {Object} currentRefinement the refinement currently applied
+ * @property {function} createURL Create an url for the next state for a clicked item.
+ * @property {Object[]} items Values to be rendered.
+ * @property {function} refine Set the path of the hierarchical filter and triggers a new search.
+ * @property {InstantSearch} instantSearchInstance The instance of instantsearch on which the widget is attached.
+ * @property {Object} widgetParams All original `CustomHierarchicalMenuWidgetOptions` forwarded to the `renderFn`.
+ * @property {Object} currentRefinement The refinement currently applied.
  */
 
  /**
-  * Connects a rendering function with the hierarchical menu business logic.
+  * **HierarchicalMenu** connector provides the logic to build a widget that will give the user the ability to explore a tree-like structure.
+  * This is commonly used for multi-level categorization of products on e-commerce websites. From a UX point of view, we suggest not displaying more than two levels deep.
+  *
+  * There's a complete example available on how to write a custom **HierarchicalMenu**: [hierarchicalMenu.js](https://github.com/algolia/instantsearch.js/blob/feat/instantsearch.js/v2/dev/app/custom-widgets/jquery/hierarchicalMenu.js)
   * @type {Connector}
   * @param {function(HierarchicalMenuRenderingOptions)} renderFn function that renders the hierarchical menu widget
   * @return {function(CustomHierarchicalMenuWidgetOptions)} a custom hierarchical menu widget factory
