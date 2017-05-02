7187390da17eb1fe14e4c64e3560216f0f08f35b
iam4x <iam4x@me.com>
2017-05-02 11:37:37 +0200

    doc: connectHits

--- src/connectors/infinite-hits/connectInfiniteHits.js
+++ src/connectors/infinite-hits/connectInfiniteHits.js
@@ -19,19 +19,52 @@ Full documentation available at https://community.algolia.com/instantsearch.js/c
 
 /**
  * @typedef {Object} InfiniteHitsRenderingOptions
- * @property {Array} hits the aggregated hits of all the pages
- * @property {Object} results the last results
- * @property {function} showMore adds a new page and search
- * @property {boolean} isLastPage true if the last page has been reached
- * @property {InstantSearch} instantSearchInstance the instance of instantsearch on which the widget is attached
- * @property {Object} widgetParams all original options forwarded to rendering
+ * @property {Array<Object>} hits The aggregated matched hits from Algolia API of all pages.
+ * @property {Object} results The complete results response from Algolia API.
+ * @property {function} showMore Action to load next page of hits.
+ * @property {boolean} isLastPage Indicate if the last page of hits has been reached.
+ * @property {InstantSearch} instantSearchInstance The instance of instantsearch on which the widget is attached.
+ * @property {Object} widgetParams All original widget options forwarded to the `renderFn`.
  */
 
 /**
- * Connects a rendering function with the infinite hits business logic.
+ * **InfiniteHits** connector provides the logic to create custom widgets that will render an continuous list of results retrieved from Algolia. This connector provides a function to load more results.
  * @type {Connector}
- * @param {function(InfiniteHitsRenderingOptions, boolean)} renderFn function that renders the infinite hits widget
- * @return {function(object)} a widget factory for infinite hits widget
+ * @param {function(InfiniteHitsRenderingOptions, boolean)} renderFn Renders the infinite hits custom widget.
+ * @return {function(object)} A widget factory for infinite hits widget.
+ * @example
+ * var $ = window.$;
+ * var instantsearch = window.instantsearch;
+ *
+ * // custom `renderFn` to render the custom ClearAll widget
+ * function renderFn(InfiniteHitsRenderingOptions) {
+ *   if (isFirstRendering === true) {
+ *     InfiniteHitsRenderingOptions.widgetParams.containerNode
+ *       .html('<div id="hits"></div><button id="show-more"></button>');
+ *
+ *     InfiniteHitsRenderingOptions.widgetParams.containerNode
+ *       .find('#show-more')
+ *       .on('click', function(event) {
+ *         event.preventDefault();
+ *         InfiniteHitsRenderingOptions.showMore();
+ *       });
+ *   }
+ *
+ *   InfiniteHitsRenderingOptions.widgetParams.containerNode.find('#hits').html(
+ *     InfiniteHitsRenderingOptions.hits.map(function(hit) {
+ *       return '<div>' + hit._highlightResult.name.value + '</div>';
+ *     })
+ *   );
+ *
+ * // connect `renderFn` to Hits logic
+ * var customInfiniteHits = instantsearch.connectors.connectInfiniteHits(renderFn);
+ *
+ * // mount widget on the page
+ * search.addWidget(
+ *   customInfiniteHits({
+ *     containerNode: $('#custom-infinite-hits-container'),
+ *   })
+ * );
  */
 export default function connectInfiniteHits(renderFn) {
   checkRendering(renderFn, usage);
