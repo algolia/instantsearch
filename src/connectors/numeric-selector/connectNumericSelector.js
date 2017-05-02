5359128c234b6440ea8ca74d0ea7307fc49aa585
iam4x <iam4x@me.com>
2017-05-02 15:02:02 +0200

    doc: connectNumericSelector

--- src/connectors/numeric-selector/connectNumericSelector.js
+++ src/connectors/numeric-selector/connectNumericSelector.js
@@ -22,30 +22,78 @@ Full documentation available at https://community.algolia.com/instantsearch.js/c
 `;
 
 /**
+ * @typedef {Object} Option
+ * @property {number} value The numerical value to refine with.
+ * @property {string} label Label to display in the option.
+ */
+
+/**
  * @typedef {Object} CustomNumericSelectorWidgetOptions
- * @property {string} attributeName Name of the attribute for faceting (eg. "free_shipping")
- * @property  {Array} options Array of objects defining the different values and labels
- * @property  {number} options[i].value The numerical value to refine with
- * @property  {string} options[i].label Label to display in the option
- * @property  {string} [ operator='=' ] The operator to use to refine
+ * @property {string} attributeName Name of the attribute for faceting (eg. "free_shipping").
+ * @property {Option[]} options Array of objects defining the different values and labels.
+ * @property {string} [operator='='] The operator to use to refine.
  */
 
 /**
  * @typedef {Object} NumericSelectorRenderingOptions
- * @property {string} currentRefinement the currently selected value
- * @property {{ value: string, label: string }[]} the different values and labels of the selector
- * @property {function} refine updates the change with the selected value
- * @property {boolean} hasNoResults indicates if the last search returned any value
- * @property {InstantSearch} instantSearchInstance the instance of instantsearch on which the widget is attached
- * @property {Object} widgetParams all original options forwarded to rendering
+ * @property {string} currentRefinement The currently selected value.
+ * @property {Option[]} options The different values and labels of the selector.
+ * @property {function} refine Action to update the results with the selected value.
+ * @property {boolean} hasNoResults Indicates if the last search returned any value.
+ * @property {InstantSearch} instantSearchInstance Instance of instantsearch on which the widget is attached.
+ * @property {Object} widgetParams All original `CustomNumericSelectorWidgetOptions` forwarded to the `renderFn`.
  */
 
- /**
-  * Connects a rendering function with the numeric selector business logic.
-  * @type {Connector}
-  * @param {function(NumericSelectorRenderingOptions, boolean)} renderFn function that renders the numeric selector widget
-  * @return {function(CustomNumericSelectorWidgetOptions)} a widget factory for numeric selector widget
-  */
+/**
+ * **NumericSelector** connector connects privdes the logic to build a custom widget that will give the user the ability to limit results between numerical refinements.
+ * It provides a `NumericSelectorRenderingOptions.refine(option)` function to trigger a new search with selected option.
+ * @type {Connector}
+ * @param {function(NumericSelectorRenderingOptions, boolean)} renderFn Rendering function for the custom **NumericSelector** widget.
+ * @return {function(CustomNumericSelectorWidgetOptions)} Re-usable widget factory for a custom **NumericSelector** widget.
+ * @example
+ * var $ = window.$;
+ * var instantsearch = window.instantsearch;
+ *
+ * // custom `renderFn` to render the custom ClearAll widget
+ * function renderFn(NumericSelectorRenderingOptions, isFirstRendering) {
+ *   if (isFirstRendering) {
+ *     NumericSelectorRenderingOptions.widgetParams.containerNode.html('<select></select>');
+ *     NumericSelectorRenderingOptions.widgetParams.containerNode
+ *       .find('select')
+ *       .on('change', function(event) {
+ *         NumericSelectorRenderingOptions.refine(event.target.value);
+ *       })
+ *   }
+ *
+ *   var optionsHTML = NumericSelectorRenderingOptions.options.map(function(option) {
+ *     return '<option value="' + option.value + '"' +
+ *       (NumericSelectorRenderingOptions.currentRefinement === item.value ? ' selected' : '') + '>' +
+ *       item.label + '</option>';
+ *   });
+ *
+ *   NumericSelectorRenderingOptions.widgetParams.containerNode
+ *     .find('select')
+ *     .html(optionsHTML);
+ * }
+ *
+ * // connect `renderFn` to NumericSelector logic
+ * var customNumericSelector = instantsearch.connectors.connectNumericSelector(renderFn);
+ *
+ * // mount widget on the page
+ * search.addWidget(
+ *   customNumericSelector({
+ *     containerNode: $('#custom-numeric-selector-container'),
+ *     operator: '>=',
+ *     attributeName: 'popularity',
+ *     options: [
+ *       {label: 'Default', value: 0},
+ *       {label: 'Top 10', value: 9991},
+ *       {label: 'Top 100', value: 9901},
+ *       {label: 'Top 500', value: 9501},
+ *     ],
+ *   })
+ * );
+ */
 export default function connectNumericSelector(renderFn) {
   checkRendering(renderFn, usage);
 
