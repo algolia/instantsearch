c14b51de61fac64aa8e1d05634885d00903fcca1
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2017-05-09 17:19:16 +0200

    chore(build-doc): Update root url for prod

--- docgen/config.js
+++ docgen/config.js
@@ -13,6 +13,6 @@ export default {
   docsDist:  docsDist? docsDist :
              prod ? rootPath('docs') : // avoids publishing an `npm start`ed build if running.
              rootPath('docs-preview'),
-  publicPath: prod ? '/instantsearch-android/' : '/',
+  publicPath: prod ? '/instantsearch.js/' : '/',
   header: header
 };
