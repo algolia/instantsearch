0f05d28b92c37816351e624572ed531c2ed1c203
Haroen Viaene <fingebimus@me.com>
2017-05-10 09:37:41 +0200

    Update main.js

--- docgen/assets/js/main.js
+++ docgen/assets/js/main.js
@@ -5,7 +5,7 @@ var alg = require('algolia-frontend-components/javascripts.js');
 
 const docSearch = {
   apiKey: 'fd5e835f5153cad7d5ec0c3595dfa244',
-  indexName: 'instantsearch.js',
+  indexName: 'instantsearch.js-v2',
   inputSelector: '#searchbox',
 };
 
