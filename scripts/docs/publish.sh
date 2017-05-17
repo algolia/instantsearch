45fa7c10be55e9a5247abf97fd87bcd540649190
iam4x <iam4x@me.com>
2017-05-17 17:14:51 +0200

    chore(docs): provide NODE_ENV for local/dev build of docs

--- scripts/docs/publish.sh
+++ scripts/docs/publish.sh
@@ -17,30 +17,8 @@ set -e # exit when error
 
 printf "\nPublish website to gh-pages\n"
 
-# Build instantsearch.js library
-VERSION=preview-$(json version < package.json)
-NODE_ENV=production VERSION=${VERSION} yarn run build
-
-# Clean old ./docs folder if any
-rm -rf docs
-mkdir -p docs
-
 # Build docs for production
-(
-  cd docgen
-  yarn install && NODE_ENV=production yarn run build
-)
+NODE_ENV=production npm run docs:build:dev
 
-# Package the examples in the website
-for example in docs_preview/examples/*; do
-  if [ -d "$example" ]; then
-    name=$(basename "$example")
-    (cd "$example" && zip -r "../$name.zip" ./*)
-  fi
-done
-
-# Copy instantsearch.js builds to the website root
-cp dist/* docs
-
-# publish
+# Publish docs to github pages on `/v2`
 babel-node scripts/docs/gh-pages.js
