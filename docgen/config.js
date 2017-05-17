2b4afcdaa6442033fb3c260dfbb4e5411b199bbb
iam4x <iam4x@me.com>
2017-05-17 16:59:05 +0200

    chore(doc): always build into `/doc`

--- docgen/config.js
+++ docgen/config.js
@@ -4,15 +4,12 @@ var fs = require('fs');
 import {rootPath} from './path';
 
 const prod = process.env.NODE_ENV === 'production';
-const docsDist = process.env.DOCS_DIST;
 
 var content = JSON.parse(fs.readFileSync('./src/data/communityHeader.json', 'utf8').toString());
 var header = algoliaComponents.communityHeader(content);
 
 export default {
-  docsDist:  docsDist? docsDist :
-             prod ? rootPath('docs') : // avoids publishing an `npm start`ed build if running.
-             rootPath('docs-preview'),
-  publicPath: prod ? '/instantsearch.js/' : '/',
+  docsDist:  rootPath('docs'),
+  publicPath: prod ? '/instantsearch.js/v2/' : '/',
   header: header
 };
