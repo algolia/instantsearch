b776c583956a78665d49bb466da537fbb5b63910
iam4x <iam4x@me.com>
2017-07-04 16:44:55 +0200

    chore(build): provide ES6 build for enabling tree shaking

---
+++ index.es6.js
@@ -0,0 +1,59 @@
+/* eslint max-len: 0 */
+import toFactory from 'to-factory';
+
+import main from './src/lib/main.js';
+import InstantSearch from './src/lib/InstantSearch.js';
+
+// import instantsearch from 'instantsearch.js';
+// -> provides same instantsearch object as es5 or <script> build
+export default main;
+
+// import { instantsearch } from 'instantsearch.js';
+// -> provides instantsearch object without connectors and widgets
+export const instantsearch = Object.assign(toFactory(InstantSearch), {
+  version: main.version,
+  createQueryString: main.createQueryString,
+});
+
+// import { connectXXX } from 'instantsearch.js'
+// -> provides every available connectors
+export {default as connectClearAll} from './src/connectors/clear-all/connectClearAll.js';
+export {default as connectCurrentRefinedValues} from './src/connectors/current-refined-values/connectCurrentRefinedValues.js';
+export {default as connectHierarchicalMenu} from './src/connectors/hierarchical-menu/connectHierarchicalMenu.js';
+export {default as connectHits} from './src/connectors/hits/connectHits.js';
+export {default as connectHitsPerPage} from './src/connectors/hits-per-page/connectHitsPerPage.js';
+export {default as connectInfiniteHits} from './src/connectors/infinite-hits/connectInfiniteHits.js';
+export {default as connectMenu} from './src/connectors/menu/connectMenu.js';
+export {default as connectNumericRefinementList} from './src/connectors/numeric-refinement-list/connectNumericRefinementList.js';
+export {default as connectNumericSelector} from './src/connectors/numeric-selector/connectNumericSelector.js';
+export {default as connectPagination} from './src/connectors/pagination/connectPagination.js';
+export {default as connectPriceRanges} from './src/connectors/price-ranges/connectPriceRanges.js';
+export {default as connectRangeSlider} from './src/connectors/range-slider/connectRangeSlider.js';
+export {default as connectRefinementList} from './src/connectors/refinement-list/connectRefinementList.js';
+export {default as connectSearchBox} from './src/connectors/search-box/connectSearchBox.js';
+export {default as connectSortBySelector} from './src/connectors/sort-by-selector/connectSortBySelector.js';
+export {default as connectStarRating} from './src/connectors/star-rating/connectStarRating.js';
+export {default as connectStats} from './src/connectors/stats/connectStats.js';
+export {default as connectToggle} from './src/connectors/toggle/connectToggle.js';
+
+// import { searchBox } from 'instantsearch.js'
+// -> provides every available widgets
+export {default as clearAll} from './src/widgets/clear-all/clear-all.js';
+export {default as currentRefinedValues} from './src/widgets/current-refined-values/current-refined-values.js';
+export {default as hierarchicalMenu} from './src/widgets/hierarchical-menu/hierarchical-menu.js';
+export {default as hits} from './src/widgets/hits/hits.js';
+export {default as hitsPerPageSelector} from './src/widgets/hits-per-page-selector/hits-per-page-selector.js';
+export {default as infiniteHits} from './src/widgets/infinite-hits/infinite-hits.js';
+export {default as menu} from './src/widgets/menu/menu.js';
+export {default as refinementList} from './src/widgets/refinement-list/refinement-list.js';
+export {default as numericRefinementList} from './src/widgets/numeric-refinement-list/numeric-refinement-list.js';
+export {default as numericSelector} from './src/widgets/numeric-selector/numeric-selector.js';
+export {default as pagination} from './src/widgets/pagination/pagination.js';
+export {default as priceRanges} from './src/widgets/price-ranges/price-ranges.js';
+export {default as searchBox} from './src/widgets/search-box/search-box.js';
+export {default as rangeSlider} from './src/widgets/range-slider/range-slider.js';
+export {default as sortBySelector} from './src/widgets/sort-by-selector/sort-by-selector.js';
+export {default as starRating} from './src/widgets/star-rating/star-rating.js';
+export {default as stats} from './src/widgets/stats/stats.js';
+export {default as toggle} from './src/widgets/toggle/toggle.js';
+export {default as analytics} from './src/widgets/analytics/analytics.js';
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,14 +1,23 @@
-#! /usr/bin/env bash
+#!/bin/bash
 
-set -ev # exit when error
+set -e # exit when error
 
-rm -rf dist dist-es5-module
-mkdir -p dist dist-es5-module
+rm -rf dist dist-es5-module dist-es6-module
+mkdir -p dist dist-es5-module dist-es6-module
 
-webpack --config scripts/webpack.config.js &
-babel -q index.js -o dist-es5-module/index.js &
-babel src -d dist-es5-module/src --ignore *-test.js --quiet
+echo "➡️  Bundle instantsearch.js to UMD build './dist' via webpack"
+NODE_ENV=production BABEL_ENV=production webpack --config scripts/webpack.config.js --hide-modules
+
+printf "dist/instantsearch.min.js gzipped will weight `cat dist/instantsearch.min.js | gzip -9 | wc -c | pretty-bytes`\n\n"
+
+echo "➡️  Bundle instantsearch.js to ES5 build './dist-es5-module' via babel-cli"
+BABEL_ENV=production babel -q index.js -o dist-es5-module/index.js &
+BABEL_ENV=production babel src -d dist-es5-module/src --ignore *-test.js --quiet
 
 wait
 
-printf "=> instantsearch.min.js gzipped will weight `cat dist/instantsearch.min.js | gzip -9 | wc -c | pretty-bytes`\n"
+echo "➡️  Bundle instantsearch.js to ES6 build './dist-es6-module' via babel-cli"
+BABEL_ENV="production-es6" babel -q index.es6.js -o dist-es6-module/index.js &
+BABEL_ENV="production-es6" babel src -d dist-es6-module/src --ignore *-test.js --quiet
+
+wait
