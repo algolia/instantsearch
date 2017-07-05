566468711ab3c3ad9962ea284a749d07a6b3a702
iam4x <iam4x@me.com>
2017-07-05 15:24:40 +0200

    fix(es6-build): don't export the full `instantsearch.js` object
    Breaking changes for people already using rollup/webpack2+

--- index.es6.js
+++ index.es6.js
@@ -1,18 +1,29 @@
 /* eslint max-len: 0 */
+import algoliasearchHelper from 'algoliasearch-helper';
 import toFactory from 'to-factory';
 
-import main from './src/lib/main.js';
 import InstantSearch from './src/lib/InstantSearch.js';
+import version from './src/lib/version.js';
 
 // import instantsearch from 'instantsearch.js';
-// -> provides same instantsearch object as es5 or <script> build
-export default main;
-
-// import { instantsearch } from 'instantsearch.js';
 // -> provides instantsearch object without connectors and widgets
-export const instantsearch = Object.assign(toFactory(InstantSearch), {
-  version: main.version,
-  createQueryString: main.createQueryString,
+export default Object.assign(toFactory(InstantSearch), {
+  version,
+  createQueryString: algoliasearchHelper.url.getQueryStringFromState,
+
+  get widgets() {
+    throw new Error(`
+      You can't access to 'instantsearch.widgets' directly from the ES6 build.
+      Import the widgets this way 'import {SearchBox} from "instantsearch.js"'
+    `);
+  },
+
+  get connectors() {
+    throw new Error(`
+      You can't access to 'instantsearch.connectors' directly from the ES6 build.
+      Import the connectors this way 'import {connectSearchBox} from "instantsearch.js"'
+    `);
+  },
 });
 
 // import { connectXXX } from 'instantsearch.js'
