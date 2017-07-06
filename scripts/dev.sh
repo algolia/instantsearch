b2e3973fb9e6c94d060f66c6c6df19dff2f28bae
iam4x <iam4x@me.com>
2017-07-06 14:28:28 +0200

    chore(dev): refactor `yarn dev` to one script

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,6 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-set -ev # exit when error
+set -e # exit when error
 
-yarn
-NODE_ENV=development webpack-dev-server --config dev/webpack.config.js
+echo "➡️  Installing dependencies"
+
+(cd docgen && yarn) & yarn
+wait
+
+echo "➡️  Starting library dev server [ http://localhost:8080 ]"
+echo "➡️  Starting documentation dev server [ http://localhost:3000 ]"
+
+(cd docgen && yarn dev) & NODE_ENV=development webpack-dev-server --config dev/webpack.config.js
+wait
