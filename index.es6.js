0016617a049b89d261a73f05537018ec3c61c481
iam4x <iam4x@me.com>
2017-07-06 15:06:57 +0200

    fix(build-es6): use `Object.defineProperty` instead of class getter

--- index.es6.js
+++ index.es6.js
@@ -7,21 +7,27 @@ import version from './dist-es6-module/lib/version.js';
 
 // import instantsearch from 'instantsearch.js';
 // -> provides instantsearch object without connectors and widgets
-export default Object.assign(toFactory(InstantSearch), {
+const instantSearchFactory = Object.assign(toFactory(InstantSearch), {
   version,
   createQueryString: algoliasearchHelper.url.getQueryStringFromState,
+});
 
-  get widgets() {
-    throw new Error(`
+Object.defineProperty(instantSearchFactory, 'widgets', {
+  get() {
+    throw new ReferenceError(`
       You can't access to 'instantsearch.widgets' directly from the ES6 build.
       Import the widgets this way 'import {SearchBox} from "instantsearch.js/widgets"'
     `);
   },
+});
 
-  get connectors() {
-    throw new Error(`
+Object.defineProperty(instantSearchFactory, 'connectors', {
+  get() {
+    throw new ReferenceError(`
       You can't access to 'instantsearch.connectors' directly from the ES6 build.
       Import the connectors this way 'import {connectSearchBox} from "instantsearch.js/connectors"'
     `);
   },
 });
+
+export default instantSearchFactory;
