198beb4007acded36a56ad4b78cb53a615f6a903
iam4x <iam4x@me.com>
2017-07-06 11:54:19 +0200

    chore(pkg): remove `module` entry, include files to npm

--- index.es6.js
+++ index.es6.js
@@ -2,8 +2,8 @@
 import algoliasearchHelper from 'algoliasearch-helper';
 import toFactory from 'to-factory';
 
-import InstantSearch from './src/lib/InstantSearch.js';
-import version from './src/lib/version.js';
+import InstantSearch from './dist-es6-module/src/lib/InstantSearch.js';
+import version from './dist-es6-module/src/lib/version.js';
 
 // import instantsearch from 'instantsearch.js';
 // -> provides instantsearch object without connectors and widgets
@@ -14,14 +14,14 @@ export default Object.assign(toFactory(InstantSearch), {
   get widgets() {
     throw new Error(`
       You can't access to 'instantsearch.widgets' directly from the ES6 build.
-      Import the widgets this way 'import {SearchBox} from "instantsearch.js"'
+      Import the widgets this way 'import {SearchBox} from "instantsearch.js/widgets"'
     `);
   },
 
   get connectors() {
     throw new Error(`
       You can't access to 'instantsearch.connectors' directly from the ES6 build.
-      Import the connectors this way 'import {connectSearchBox} from "instantsearch.js"'
+      Import the connectors this way 'import {connectSearchBox} from "instantsearch.js/connectors"'
     `);
   },
 });
