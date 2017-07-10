8b0b8befbf9b72909dc027ff7bcf60e1a7e25025
Marie-Laure Thuret <mthuret@users.noreply.github.com>
2017-07-10 16:19:55 +0200

    docs(SSR): fix build of SSR example (algolia/react-instantsearch#153)

--- packages/react-instantsearch/examples/next-app/next.config.js
+++ packages/react-instantsearch/examples/next-app/next.config.js
@@ -10,7 +10,10 @@ module.exports = {
       test: /\.css$/,
       loader: ['style-loader', 'css-loader'],
     });
-
+    if (config.resolve.alias) {
+      delete config.resolve.alias.react;
+      delete config.resolve.alias['react-dom'];
+    }
     return config;
   },
 };
