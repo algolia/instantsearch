ce342d6f5fa80d7d658c18fb430f0b326cb7cc0d
Marie-Laure Thuret <mthuret@users.noreply.github.com>
2017-07-10 16:42:10 +0200

    docs(SSR): improve next.js example (#155)

--- packages/react-instantsearch/examples/next-app/pages/index.js
+++ packages/react-instantsearch/examples/next-app/pages/index.js
@@ -4,32 +4,43 @@ import PropTypes from 'prop-types';
 import Router from 'next/router';
 import qs from 'qs';
 
+const updateAfter = 700;
+
 const searchStateToUrl = searchState =>
   searchState ? `${window.location.pathname}?${qs.stringify(searchState)}` : '';
 
 export default class extends React.Component {
   static propTypes = {
     resultsState: PropTypes.object,
+    searchState: PropTypes.object,
   };
 
   constructor(props) {
     super(props);
     this.onSearchStateChange = this.onSearchStateChange.bind(this);
-    this.state = { searchState: {} };
   }
 
+  /*
+     nextjs params.query doesn't handle nested objects
+     once it does, params.query could be used directly here, but also inside the constructor
+     to initialize the searchState. 
+  */
   static async getInitialProps(params) {
-    const searchState = qs.parse(params.asPath.substring(2)); //nextjs params.query doesn't handle nested objects
+    const searchState = qs.parse(params.asPath.substring(2));
     const resultsState = await findResultsState(App, { searchState });
-    return { resultsState };
+    return { resultsState, searchState };
   }
 
-  onSearchStateChange(searchState) {
-    const href = searchStateToUrl(searchState);
-    Router.push(href, href, {
-      shallow: true,
-    });
-  }
+  onSearchStateChange = searchState => {
+    clearTimeout(this.debouncedSetState);
+    this.debouncedSetState = setTimeout(() => {
+      const href = searchStateToUrl(searchState);
+      Router.push(href, href, {
+        shallow: true,
+      });
+    }, updateAfter);
+    this.setState({ searchState });
+  };
 
   componentDidMount() {
     this.setState({ searchState: qs.parse(window.location.search.slice(1)) });
@@ -47,7 +58,11 @@ export default class extends React.Component {
           <App
             resultsState={this.props.resultsState}
             onSearchStateChange={this.onSearchStateChange}
-            searchState={this.state.searchState}
+            searchState={
+              this.state && this.state.searchState
+                ? this.state.searchState
+                : this.props.searchState
+            }
           />
         </div>
       </div>
