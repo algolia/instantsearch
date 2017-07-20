a7f9f3e41776e87fb804922a48816c3a9aec7999
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-07-20 15:21:46 +0200

    fix(pending-search): dispatch error event before searchQueueEmpty (algolia/algoliasearch-helper-js#503)
    * fix(pending-search): dispatch error event before searchQueueEmpty
    * misc(event-order): single event statement
    
    Move code around to make sure that we both have a proper event order with `searchQueueEmpty` after `result` or `error`.

--- packages/algoliasearch-helper/src/algoliasearch.helper.js
+++ packages/algoliasearch-helper/src/algoliasearch.helper.js
@@ -1250,26 +1250,24 @@ AlgoliaSearchHelper.prototype._dispatchAlgoliaResponse = function(states, queryI
   }
 
   this._currentNbQueries -= (queryId - this._lastQueryIdReceived);
-  if (this._currentNbQueries === 0) this.emit('searchQueueEmpty');
-
   this._lastQueryIdReceived = queryId;
 
   if (err) {
     this.emit('error', err);
-    return;
+  } else {
+    var results = content.results;
+    forEach(states, function(s) {
+      var state = s.state;
+      var queriesCount = s.queriesCount;
+      var helper = s.helper;
+      var specificResults = results.splice(0, queriesCount);
+
+      var formattedResponse = helper.lastResults = new SearchResults(state, specificResults);
+      helper.emit('result', formattedResponse, state);
+    });
   }
-
-  var results = content.results;
-  forEach(states, function(s) {
-    var state = s.state;
-    var queriesCount = s.queriesCount;
-    var helper = s.helper;
-
-    var specificResults = results.splice(0, queriesCount);
-
-    var formattedResponse = helper.lastResults = new SearchResults(state, specificResults);
-    helper.emit('result', formattedResponse, state);
-  });
+  
+  if (this._currentNbQueries === 0) this.emit('searchQueueEmpty');
 };
 
 AlgoliaSearchHelper.prototype.containsRefinement = function(query, facetFilters, numericFilters, tagFilters) {
