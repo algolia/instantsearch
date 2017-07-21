3560d4645d4cd137fc677c766df72f096900d5a1
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2017-07-21 17:57:08 +0200

    chore(doc): foldable sidebar on mobile
    fix #2221

--- docgen/assets/js/main.js
+++ docgen/assets/js/main.js
@@ -30,3 +30,28 @@ if(sidebarContainer) {
   fixSidebar({sidebarContainer, topOffset: headerHeight});
   followSidebarNavigation(sidebarContainer.querySelectorAll('a'), contentContainer.querySelectorAll('h2'));
 }
+
+// The Following function will make the '.sidebar-opener'
+// clickable and it will open/close the sidebar on the
+// documentations
+
+function toggleDocumentationSidebar() {
+  const sidebarNav = document.querySelector('nav.sidebar');
+  const trigger = document.querySelector('.sidebar-opener');
+
+  function init() {
+    const bodySize = document.body.clientWidth;
+    if (bodySize <= 960 && sidebarNav) {
+      trigger.addEventListener('click', () => {
+        sidebarNav.classList.toggle('Showed');
+        trigger.classList.toggle('Showed');
+      });
+    }
+  }
+  init();
+}
+toggleDocumentationSidebar();
+
+window.addEventListener('resize', () => {
+  toggleDocumentationSidebar();
+});
