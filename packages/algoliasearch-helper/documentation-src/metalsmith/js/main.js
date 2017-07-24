1ccd41b9420a37415f40e88897411ce306abd238
Jonas <jonas.badalic@algolia.com>
2017-07-24 16:53:31 +0200

    chore(doc): update header

--- packages/algoliasearch-helper/documentation-src/metalsmith/js/main.js
+++ packages/algoliasearch-helper/documentation-src/metalsmith/js/main.js
@@ -1,7 +1,13 @@
 import sidebar from './sidebar.js';
+import * as algolia from 'algolia-frontend-components/javascripts'
+
+window.addEventListener('load',() => {
+  var header = new algolia.communityHeader()
+})
 
 var container = document.querySelector('.documentation-container')
 var sidebarContainer = document.querySelector('.sidebar');
+
 
 if(container && sidebar) {
   sidebar({
