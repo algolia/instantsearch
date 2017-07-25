5b88160b2cc44127a2fb20f2b1eb86af2e04f8bd
Alex S <bobylito@users.noreply.github.com>
2017-07-25 11:27:55 +0200

    chore(tooling): conventional changelog integration (algolia/algoliasearch-helper-js#523)
    * chore(tooling): conventional changelog integration
    * chore(changelog): custom template
    * chore(changelog): add commands to generate and view changelog

---
+++ packages/algoliasearch-helper/scripts/conventional-changelog/index.js
@@ -0,0 +1,96 @@
+'use strict';
+var parserOpts = {
+  headerPattern: /^(\w*)(?:\((.*)\))?\: (.*)$/,
+  headerCorrespondence: [
+    'type',
+    'scope',
+    'subject'
+  ],
+  noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+  revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
+  revertCorrespondence: ['header', 'hash']
+};
+
+var writerOpts = {
+  transform: function(commit) {
+    return commit;
+  },
+  groupBy: 'type',
+  commitGroupsSort: 'title',
+  commitsSort: ['scope', 'subject'],
+  noteGroupsSort: 'title',
+  headerPartial: '{{version}} - {{date}}',
+  mainTemplate: `{{> header}}
+
+{{#each commitGroups}}
+{{#each commits}}
+{{> commit root=@root}}
+{{/each}}
+{{/each}}
+
+{{> footer}}
+`,
+  commitPartial: ` * {{header}}
+
+{{~!-- commit link --}} {{#if @root.linkReferences~}}
+  {{~#if @root.repository}}
+    {{~#if @root.host}}
+      {{~@root.host}}/
+    {{~/if}}
+    {{~#if @root.owner}}
+      {{~@root.owner}}/
+    {{~/if}}
+    {{~@root.repository}}
+  {{~else}}
+    {{~@root.repoUrl}}
+  {{~/if}}/
+  {{~@root.commit}}/{{hash}}
+{{~else}}
+  {{~hash}}
+{{~/if}}
+
+{{~!-- commit references --}}
+{{~#if references~}}
+  , closes
+  {{~#each references}} {{#if @root.linkReferences~}}
+    {{~#if @root.repository}}
+      {{~#if @root.host}}
+        {{~@root.host}}/
+      {{~/if}}
+      {{~#if this.repository}}
+        {{~#if this.owner}}
+          {{~this.owner}}/
+        {{~/if}}
+        {{~this.repository}}
+      {{~else}}
+        {{~#if @root.owner}}
+          {{~@root.owner}}/
+        {{~/if}}
+          {{~@root.repository}}
+        {{~/if}}
+    {{~else}}
+      {{~@root.repoUrl}}
+    {{~/if}}/
+    {{~@root.issue}}/{{this.issue}}
+  {{~else}}
+    {{~#if this.owner}}
+      {{~this.owner}}/
+    {{~/if}}
+    {{~this.repository}}#{{this.issue}}
+  {{~/if}}{{/each}}
+{{~/if}}
+
+`
+};
+
+/*
+  writerOpts.mainTemplate = template;
+  writerOpts.headerPartial = header;
+  writerOpts.commitPartial = commit;
+  writerOpts.footerPartial = footer;
+*/
+
+module.exports = {
+  parserOpts: parserOpts,
+  writerOpts: writerOpts
+};
