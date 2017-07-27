db593b1b159ebe7d4dceb4ef5a37c5182b4942ee
Alexandre Stanislawski <alexandre.stanislawski@algolia.com>
2017-07-27 10:27:48 +0200

    chore(tool): Let the user publish the website from any branch
    But we ask for confirmation before

--- scripts/docs/publish.sh
+++ scripts/docs/publish.sh
@@ -1,16 +1,24 @@
 #! /usr/bin/env bash
 
-set -ev # exit when error
+set -e # exit when error
 
-if [ -z $TRAVIS_BRANCH ]; then
-  currentBranch=`git rev-parse --abbrev-ref HEAD`
+echo Publishing InstantSearch.js website...
+
+if [ -z "$TRAVIS_BRANCH" ]; then
+  currentBranch=$(git rev-parse --abbrev-ref HEAD)
 else
   currentBranch=$TRAVIS_BRANCH
 fi
 
-if [ $currentBranch != 'master' ]; then
-  printf "update-website: You must be on master"
-  exit 1
+if [ "$currentBranch" != 'master' ]; then
+  echo "You are not running this script from master."
+  echo -n "Are you sure you want to proceed (y/n)? "
+  read -r answer
+  if echo "$answer" | grep -iq "^y" ;then
+    echo As you wish...
+  else
+    exit 1
+  fi
 fi
 
 set -e # exit when error
