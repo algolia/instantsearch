a82d3f43cc31dba23b287e595f0414745c0b7a5d
Marie-Laure Thuret <mthuret@users.noreply.github.com>
2017-07-31 16:41:16 +0200

    chore(examples): update ris version when new release (#208)

--- scripts/release.sh
+++ scripts/release.sh
@@ -118,3 +118,20 @@ npm run build-and-publish
 )
 
 printf "\n\nRelease: Package was published to npm."
+
+for d in packages/react-instantsearch/examples/* ; do 
+    cd $d
+    yarn upgrade react-instantsearch@$newVersion
+    cd ../../../..
+done
+
+commitMessage="chore(deps): update examples to react-instantsearch v$newVersion"
+git add .
+printf "$commitMessage" | git commit --file -
+
+printf "\n\nUpdate of react-instantsearch version in all examples\n" 
+printf "\n\nalmost done, check everything in another terminal tab if you want." 
+
+read -p "=> Update of react-instantsearch version in all examples: when ready, press [ENTER] to push to github"
+
+git push origin master
