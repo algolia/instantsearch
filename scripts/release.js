cfe259214ce3723ba55ed013823613c79e79266b
iam4x <iam4x@me.com>
2017-08-02 14:24:03 +0200

    refactor(release): use `shelljs`

--- scripts/release.js
+++ scripts/release.js
@@ -1,18 +1,19 @@
 #!/usr/bin/env node
 
-/* eslint-disable no-console */
 /* eslint-disable no-process-exit */
 /* eslint-disable import/no-commonjs */
 
-const { execSync } = require('child_process');
 const colors = require('colors/safe');
 const inquirer = require('inquirer');
 
+const shell = require('shelljs');
+shell.fatal = true;
+
 // check if user can publish new version to npm
 try {
-  execSync('$(npm owner add `npm whoami`)');
+  shell.exec('$(npm owner add `npm whoami`)');
 } catch (e) {
-  console.log(
+  shell.echo(
     colors.red(`
     You are not an owner of the npm repository,
     ask for it before trying to bundle a release.
@@ -22,9 +23,13 @@ try {
 }
 
 // check if branch is clean with all changes commited
-const uncommitedChanges = execSync('git status --porcelain').toString().trim();
+const uncommitedChanges = shell
+  .exec('git status --porcelain')
+  .toString()
+  .trim();
+
 if (uncommitedChanges) {
-  console.log(
+  shell.echo(
     colors.red(`
     Working tree is not clean, please commit all your changes before release.
   `)
@@ -32,13 +37,14 @@ if (uncommitedChanges) {
   process.exit(1);
 }
 
-const currentBranch = execSync('git rev-parse --abbrev-ref HEAD')
+const currentBranch = shell
+  .exec('git rev-parse --abbrev-ref HEAD')
   .toString()
   .trim();
 
 // check if we are not on branch master or exit
 if (currentBranch === 'master') {
-  console.log(
+  shell.echo(
     colors.red(`
     It's not possible to release a new version from the master branch.
     Please checkout any other branch:
@@ -56,15 +62,15 @@ const strategy = currentBranch === 'develop' ? 'stable' : 'beta';
 function rollback(newVersion) {
   if (strategy === 'stable') {
     // reset master
-    execSync('git reset --hard origin master');
-    execSync('git checkout develop');
+    shell.exec('git reset --hard origin master');
+    shell.exec('git checkout develop');
   } else {
     // remove last commit
-    execSync('git reset --hard HEAD~1');
+    shell.exec('git reset --hard HEAD~1');
   }
 
   // remove local created tag
-  execSync(`git tag -d v${newVersion}`);
+  shell.exec(`git tag -d v${newVersion}`);
   process.exit(1);
 }
 
@@ -88,19 +94,19 @@ inquirer
     if (strategy === 'stable') {
       // merge develop on master first
       // and start release from master branch
-      console.log(colors.blue('Updating working tree'));
-      execSync('git checkout master');
-      execSync('git pull origin master');
-      execSync('git fetch origin --tags');
-      execSync('git fetch origin develop');
-      execSync('git merge origin/develop');
+      shell.echo(colors.blue('Updating working tree'));
+      shell.exec('git checkout master');
+      shell.exec('git pull origin master');
+      shell.exec('git fetch origin --tags');
+      shell.exec('git fetch origin develop');
+      shell.exec('git merge origin/develop');
     }
 
-    console.log(colors.blue('Install dependencies'));
-    execSync('yarn');
+    shell.echo(colors.blue('Install dependencies'));
+    shell.exec('yarn');
 
     const { version: currentVersion } = require('../package.json');
-    console.log(
+    shell.echo(
       colors.blue(`
       - Current version is "${currentVersion}"
       - Changelog will be generated only if a fix/feat/performance/breaking is found in git log
@@ -108,10 +114,6 @@ inquirer
         ? 'with -beta.x suffix'
         : ''}
     `)
-    );
-
-    execSync(
-      'conventional-changelog --preset angular --output-unreleased | less'
     );
 
     inquirer
@@ -135,41 +137,45 @@ inquirer
       ])
       .then(({ newVersion }) => {
         // bump new version
-        console.log(colors.blue(`Bump version to "${newVersion}"`));
-        execSync(
+        shell.echo(colors.blue(`Bump version to "${newVersion}"`));
+        shell.exec(
           `VERSION=${newVersion} babel-node ./scripts/bump-package-version.js`
         );
 
         // build library new version
-        console.log(colors.blue('Building library new version'));
-        execSync(`NODE_ENV=production VERSION=${newVersion} npm run build`);
+        shell.echo(colors.blue('Building library new version'));
+        shell.exec(`NODE_ENV=production VERSION=${newVersion} npm run build`);
 
         // update changelog
-        console.log(colors.blue('Update changelog'));
-        const changelog = execSync('conventional-changelog -p angular')
+        shell.echo(colors.blue('Update changelog'));
+        const changelog = shell
+          .exec('conventional-changelog -p angular')
           .toString()
           .trim();
 
-        execSync(
-          'conventional-changelog --preset angular --infile CHANGELOG.md --same-file'
+        shell.echo('\nNext version changelog:');
+        const changelogContent = shell.exec(
+          'conventional-changelog -u -n scripts/conventional-changelog/',
+          { silent: true }
         );
+        shell.echo(colors.white(changelogContent));
 
         // regenerate README TOC
-        console.log(colors.blue('Generate TOCS'));
-        execSync('npm run doctoc');
+        shell.echo(colors.blue('Generate TOCS'));
+        shell.exec('npm run doctoc');
 
         // regenerate yarn.lock
-        execSync('yarn');
+        shell.exec('yarn');
 
         // git add and tag
         const commitMessage = `v${newVersion}\n\n${changelog}`;
-        execSync(
+        shell.exec(
           'git add src/lib/version.js yarn.lock package.json CHANGELOG.md README.md CONTRIBUTING.md'
         );
-        execSync(`git commit -m "${commitMessage}"`);
-        execSync(`git tag "v${newVersion}"`);
+        shell.exec(`git commit -m "${commitMessage}"`);
+        shell.exec(`git tag "v${newVersion}"`);
 
-        console.log(
+        shell.echo(
           colors.yellow.underline(
             'Almost done, check everything in another terminal tab'
           )
@@ -188,22 +194,22 @@ inquirer
           .then(({ publishNpm }) => {
             if (!publishNpm) return rollback(newVersion);
 
-            console.log(colors.blue('Push to github, publish on npm'));
+            shell.echo(colors.blue('Push to github, publish on npm'));
             if (strategy === 'stable') {
-              execSync('git push origin master');
-              execSync('git push origin --tags');
-              execSync('npm publish');
-              execSync('git checkout develop');
-              execSync('git pull origin develop');
-              execSync('git merge master');
-              execSync('git push origin develop');
+              shell.exec('git push origin master');
+              shell.exec('git push origin --tags');
+              shell.exec('npm publish');
+              shell.exec('git checkout develop');
+              shell.exec('git pull origin develop');
+              shell.exec('git merge master');
+              shell.exec('git push origin develop');
             } else {
-              execSync(`git push origin ${currentBranch}`);
-              execSync('git push origin --tags');
-              execSync('npm publish --tag beta');
+              shell.exec(`git push origin ${currentBranch}`);
+              shell.exec('git push origin --tags');
+              shell.exec('npm publish --tag beta');
             }
 
-            console.log(
+            shell.echo(
               colors.green(`
               Package was published to npm.
               A job on travis-ci will be automatically launched to finalize the release.
