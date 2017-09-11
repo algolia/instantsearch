4e0d4607ca894572aa5fa9794d9b8378a427a714
iam4x <iam4x@me.com>
2017-09-11 13:43:16 +0200

    doc(menuSelect): write documentation about menuSelect widget

--- src/widgets/menu-select/menu-select.js
+++ src/widgets/menu-select/menu-select.js
@@ -56,12 +56,66 @@ menuSelect({
   attributeName,
   [ sortBy=['name:asc'] ],
   [ limit=10 ],
-  [ cssClasses.{root,select,option} ]
+  [ cssClasses.{root,select,option,header,footer} ]
   [ templates.{header,item,footer,seeAllOption} ],
   [ transformData.{item} ],
   [ autoHideContainer ]
 })`;
 
+/**
+ * @typedef {Object} MenuCSSClasses
+ * @property {string|string[]} [root] CSS class to add to the root element.
+ * @property {string|string[]} [header] CSS class to add to the header element.
+ * @property {string|string[]} [select] CSS class to add to the select element.
+ * @property {string|string[]} [option] CSS class to add to the option element.
+ * @property {string|string[]} [footer] CSS class to add to the footer element.
+ */
+
+/**
+ * @typedef {Object} MenuSelectTemplates
+ * @property {string|Function} [header] Header template.
+ * @property {string|Function(name: string, count: number, isRefined: boolean)} [item] Item template, provided with `name`, `count`, `isRefined`, `url` data properties.
+ * @property {string} [seeAllOption='See all'] Label of the see all option in the select.
+ * @property {string|Function} [footer] Footer template.
+ */
+
+/**
+ * @typedef {Object} MenuSelectTransforms
+ * @property {function} [item] Method to change the object passed to the `item` template.
+ */
+
+/**
+  * @typedef {Object} MenuSelectWidgetOptions
+  * @property {string|HTMLElement} container CSS Selector or HTMLElement to insert the widget.
+  * @property {string} attributeName Name of the attribute for faceting
+  * @property {string[]|function} [sortBy=['name:asc']] How to sort refinements. Possible values: `count|isRefined|name:asc|name:desc`.
+  *
+  * You can also use a sort function that behaves like the standard Javascript [compareFunction](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#Syntax). [*]
+  * @property {MenuSelectTemplates} [templates] Customize the output through templating.
+  * @property {string} [limit=10] How many facets values to retrieve [*].
+  * @property {MenuSelectTransforms} [transformData] Set of functions to update the data before passing them to the templates.
+  * @property {boolean} [autoHideContainer=true] Hide the container when there are no items in the menu select.
+  * @property {MenuSelectCSSClasses} [cssClasses] CSS classes to add to the wrapping elements.
+ */
+
+/**
+ * Create a menu select out of a facet
+ * @type {WidgetFactory}
+ * @category filter
+ * @param {MenuSelectWidgetOptions} $0 The Menu select widget options.
+ * @return {Widget} Creates a new instance of the Menu select widget.
+ * @example
+ * search.addWidget(
+ *   instantsearch.widgets.menuSelect({
+ *     container: '#categories',
+ *     attributeName: 'hierarchicalCategories.lvl0',
+ *     limit: 10,
+ *     templates: {
+ *       header: 'Categories'
+ *     }
+ *   })
+ * );
+ */
 export default function menuSelect({
   container,
   attributeName,
