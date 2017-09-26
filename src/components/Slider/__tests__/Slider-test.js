509b7d4c8cc4fd277fdd108effaae1be4cae9738
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-09-26 17:52:21 +0200

    refactor(Slider): always call refine with values

--- src/components/Slider/__tests__/Slider-test.js
+++ src/components/Slider/__tests__/Slider-test.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow } from 'enzyme';
 
-import Slider from '../Slider';
+import Enhance, { Slider } from '../Slider';
 
 describe('Slider', () => {
   it('should render correctly', () => {
     const tree = renderer
       .create(
-        <Slider
+        <Enhance
           refine={() => undefined}
           min={0}
           max={500}
@@ -25,7 +26,7 @@ describe('Slider', () => {
   it('should render without pips', () => {
     const tree = renderer
       .create(
-        <Slider
+        <Enhance
           refine={() => undefined}
           min={0}
           max={500}
@@ -38,5 +39,26 @@ describe('Slider', () => {
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('expect to call handleChange on change', () => {
+    const props = {
+      refine: jest.fn(),
+      min: 0,
+      max: 500,
+      values: [0, 0],
+      pips: true,
+      step: 2,
+      tooltips: true,
+      shouldAutoHideContainer: false,
+    };
+
+    shallow(<Slider {...props} />)
+      .find('Rheostat')
+      .simulate('change', {
+        values: [0, 100],
+      });
+
+    expect(props.refine).toHaveBeenCalledWith([0, 100]);
   });
 });
