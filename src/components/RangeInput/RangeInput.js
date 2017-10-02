b78ea5cb49893c42be32c1df417f673ae076dfcf
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-10-02 12:19:53 +0200

    feat(RangeInput): add component

---
+++ src/components/RangeInput/RangeInput.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import _isFinite from 'lodash/isFinite';
+import autoHideContainerHOC from '../../decorators/autoHideContainer.js';
+import headerFooterHOC from '../../decorators/headerFooter.js';
+
+export class RawRangeInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      min: props.values.min,
+      max: props.values.max,
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      min: nextProps.values.min,
+      max: nextProps.values.max,
+    });
+  }
+
+  onChange = name => event => {
+    this.setState({
+      [name]: event.currentTarget.value,
+    });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    const { min, max } = this.state;
+    const { refine } = this.props;
+
+    const minAsNumber = parseFloat(min);
+    const maxAsNumber = parseFloat(max);
+
+    refine([
+      _isFinite(minAsNumber) ? minAsNumber : null,
+      _isFinite(maxAsNumber) ? maxAsNumber : null,
+    ]);
+  };
+
+  get isDisabled() {
+    return this.props.min >= this.props.max;
+  }
+
+  render() {
+    const { min: minValue, max: maxValue } = this.state;
+    const { min, max, step, cssClasses, labels } = this.props;
+
+    return (
+      <form className={cssClasses.form} onSubmit={this.onSubmit}>
+        <fieldset className={cssClasses.fieldset}>
+          <label className={cssClasses.labelMin}>
+            <input
+              className={cssClasses.inputMin}
+              type="number"
+              min={min}
+              max={max}
+              step={step}
+              value={minValue}
+              onChange={this.onChange('min')}
+              placeholder={min}
+              disabled={this.isDisabled}
+            />
+          </label>
+          <span className={cssClasses.separator}>{labels.separator}</span>
+          <label className={cssClasses.labelMax}>
+            <input
+              className={cssClasses.inputMax}
+              type="number"
+              min={min}
+              max={max}
+              step={step}
+              value={maxValue}
+              onChange={this.onChange('max')}
+              placeholder={max}
+              disabled={this.isDisabled}
+            />
+          </label>
+          <button
+            role="button"
+            className={cssClasses.submit}
+            disabled={this.isDisabled}
+          >
+            {labels.button}
+          </button>
+        </fieldset>
+      </form>
+    );
+  }
+}
+
+RawRangeInput.propTypes = {
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  step: PropTypes.number.isRequired,
+  values: PropTypes.shape({
+    min: PropTypes.number,
+    max: PropTypes.number,
+  }).isRequired,
+  cssClasses: PropTypes.shape({
+    form: PropTypes.string.isRequired,
+    fieldset: PropTypes.string.isRequired,
+    labelMin: PropTypes.string.isRequired,
+    inputMin: PropTypes.string.isRequired,
+    separator: PropTypes.string.isRequired,
+    labelMax: PropTypes.string.isRequired,
+    inputMax: PropTypes.string.isRequired,
+    submit: PropTypes.string.isRequired,
+  }).isRequired,
+  labels: PropTypes.shape({
+    button: PropTypes.string.isRequired,
+    separator: PropTypes.string.isRequired,
+  }).isRequired,
+  refine: PropTypes.func.isRequired,
+};
+
+export default autoHideContainerHOC(headerFooterHOC(RawRangeInput));
---
+++ src/components/RangeInput/__tests__/RangeInput-test.js
@@ -0,0 +1,283 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import serializer from 'enzyme-to-json/serializer';
+import RangeInput, { RawRangeInput } from '../RangeInput';
+
+expect.addSnapshotSerializer(serializer);
+
+describe('RawRangeInput', () => {
+  const defaultProps = {
+    min: 0,
+    max: 500,
+    step: 1,
+    values: {},
+    cssClasses: {
+      form: 'form',
+      fieldset: 'fieldset',
+      labelMin: 'labelMin',
+      inputMin: 'inputMin',
+      separator: 'separator',
+      labelMax: 'labelMax',
+      inputMax: 'inputMax',
+      submit: 'submit',
+    },
+    labels: {
+      button: 'Go',
+      separator: 'to',
+    },
+    refine: () => {},
+  };
+
+  const shallowRender = props =>
+    shallow(<RawRangeInput {...defaultProps} {...props} />);
+
+  it('expect to render', () => {
+    const props = {};
+    const component = shallowRender(props);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('expect to render with values', () => {
+    const props = {
+      values: {
+        min: 20,
+        max: 480,
+      },
+    };
+
+    const component = shallowRender(props);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it('expect to render with disabled state', () => {
+    const props = {
+      min: 480,
+      max: 20,
+    };
+
+    const component = shallowRender(props);
+
+    expect(component).toMatchSnapshot();
+  });
+
+  describe('willReceiveProps', () => {
+    it('expect to update the empty state from given props', () => {
+      const props = {};
+      const nextProps = {
+        values: {
+          min: 20,
+          max: 480,
+        },
+      };
+
+      const component = shallowRender(props);
+
+      expect(component).toMatchSnapshot();
+
+      component.instance().componentWillReceiveProps(nextProps);
+
+      expect(component).toMatchSnapshot();
+    });
+
+    it('expect to update the state from given props', () => {
+      const props = {
+        values: {
+          min: 40,
+          max: 460,
+        },
+      };
+
+      const nextProps = {
+        values: {
+          min: 20,
+          max: 480,
+        },
+      };
+
+      const component = shallowRender(props);
+
+      expect(component).toMatchSnapshot();
+
+      component.instance().componentWillReceiveProps(nextProps);
+
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('onChange', () => {
+    it('expect to update the state when min change', () => {
+      const props = {};
+      const component = shallowRender(props);
+
+      component
+        .find('input[type="number"]')
+        .first()
+        .simulate('change', {
+          currentTarget: {
+            value: 20,
+          },
+        });
+
+      expect(component).toMatchSnapshot();
+    });
+
+    it('expect to update the state when max change', () => {
+      const props = {};
+      const component = shallowRender(props);
+
+      component
+        .find('input[type="number"]')
+        .last()
+        .simulate('change', {
+          currentTarget: {
+            value: 480,
+          },
+        });
+
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('onSubmit', () => {
+    it('expect to call refine with min, max as integer', () => {
+      const props = {
+        refine: jest.fn(),
+      };
+
+      const event = {
+        preventDefault: jest.fn(),
+      };
+
+      const component = shallowRender(props);
+
+      component
+        .find('input[type="number"]')
+        .first()
+        .simulate('change', {
+          currentTarget: {
+            value: 20,
+          },
+        });
+
+      component
+        .find('input[type="number"]')
+        .last()
+        .simulate('change', {
+          currentTarget: {
+            value: 480,
+          },
+        });
+
+      component.find('form').simulate('submit', event);
+
+      expect(event.preventDefault).toHaveBeenCalled();
+      expect(props.refine).toHaveBeenCalledWith([20, 480]);
+    });
+
+    it('expect to call refine with min, max as float', () => {
+      const props = {
+        refine: jest.fn(),
+      };
+
+      const event = {
+        preventDefault: jest.fn(),
+      };
+
+      const component = shallowRender(props);
+
+      component
+        .find('input[type="number"]')
+        .first()
+        .simulate('change', {
+          currentTarget: {
+            value: 20.05,
+          },
+        });
+
+      component
+        .find('input[type="number"]')
+        .last()
+        .simulate('change', {
+          currentTarget: {
+            value: 480.05,
+          },
+        });
+
+      component.find('form').simulate('submit', event);
+
+      expect(event.preventDefault).toHaveBeenCalled();
+      expect(props.refine).toHaveBeenCalledWith([20.05, 480.05]);
+    });
+
+    it('expect to call refine with min only', () => {
+      const props = {
+        refine: jest.fn(),
+      };
+
+      const event = {
+        preventDefault: jest.fn(),
+      };
+
+      const component = shallowRender(props);
+
+      component
+        .find('input[type="number"]')
+        .first()
+        .simulate('change', {
+          currentTarget: {
+            value: 20,
+          },
+        });
+
+      component.find('form').simulate('submit', event);
+
+      expect(event.preventDefault).toHaveBeenCalled();
+      expect(props.refine).toHaveBeenCalledWith([20, null]);
+    });
+
+    it('expect to call refine with max only', () => {
+      const props = {
+        refine: jest.fn(),
+      };
+
+      const event = {
+        preventDefault: jest.fn(),
+      };
+
+      const component = shallowRender(props);
+
+      component
+        .find('input[type="number"]')
+        .last()
+        .simulate('change', {
+          currentTarget: {
+            value: 480,
+          },
+        });
+
+      component.find('form').simulate('submit', event);
+
+      expect(event.preventDefault).toHaveBeenCalled();
+      expect(props.refine).toHaveBeenCalledWith([null, 480]);
+    });
+
+    it('expect to call refine without values', () => {
+      const props = {
+        refine: jest.fn(),
+      };
+
+      const event = {
+        preventDefault: jest.fn(),
+      };
+
+      const component = shallowRender(props);
+
+      component.find('form').simulate('submit', event);
+
+      expect(event.preventDefault).toHaveBeenCalled();
+      expect(props.refine).toHaveBeenCalledWith([null, null]);
+    });
+  });
+});
