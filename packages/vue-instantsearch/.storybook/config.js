3f0d834aca32a9592a141de19b6e6246119a08aa
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-10-17 15:35:01 +0200

    chore(stories): add storybook

---
+++ packages/vue-instantsearch/.storybook/config.js
@@ -0,0 +1,14 @@
+import { configure } from "@storybook/vue";
+
+import Vue from "vue";
+import InstantSearch, { createStoreFromAlgoliaCredentials } from "../src/instantsearch";
+
+Vue.use(InstantSearch);
+
+const req = require.context("../stories", true, /\.stories\.js$/);
+
+function loadStories() {
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
