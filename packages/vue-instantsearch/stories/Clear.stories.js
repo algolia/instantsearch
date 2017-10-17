d7ca168c7f63651c749303f491623bd7508b520d
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-10-17 18:29:24 +0200

    chore(stories): add clear default story

---
+++ packages/vue-instantsearch/stories/Clear.stories.js
@@ -0,0 +1,6 @@
+import { previewWrapper } from './utils';
+import { storiesOf } from '@storybook/vue';
+
+storiesOf('Clear', module).addDecorator(previewWrapper).add('default', () => ({
+  template: '<ais-clear></ais-clear>',
+}));
