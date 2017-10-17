24c3d19118e2f3c6dd5b1278efa1d47619bbe8cc
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-10-17 19:57:32 +0200

    chore(stories): add custom display story for sortBySelector

--- packages/vue-instantsearch/stories/SortBySelector.stories.js
+++ packages/vue-instantsearch/stories/SortBySelector.stories.js
@@ -11,4 +11,16 @@ storiesOf('SortBySelector', module)
     ]">
     </ais-sort-by-selector>
     `,
+  }))
+  .add('custom display', () => ({
+    template: `<ais-sort-by-selector :indices="[
+      { name: 'ikea', label: 'Featured' },
+      { name: 'ikea_price_asc', label: 'Price asc.' },
+      { name: 'ikea_price_desc', label: 'Price desc.' },
+    ]">
+      <option slot-scope="{ indexName, label }" :value="indexName">
+        Sort by: {{ label }}
+      </option>
+    </ais-sort-by-selector>
+    `,
   }));
