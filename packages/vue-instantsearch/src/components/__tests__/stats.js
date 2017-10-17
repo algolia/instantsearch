63e4d861c39af3bba9229627fe0c3af822ef8582
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-10-17 13:13:21 +0200

    test(stats): add snaphost tests

---
+++ packages/vue-instantsearch/src/components/__tests__/stats.js
@@ -0,0 +1,32 @@
+import Vue from 'vue';
+import Stats from '../Stats.vue';
+
+const searchStore = {
+  query: 'search query',
+  totalResults: 10000,
+  processingTimeMS: 1000,
+};
+
+describe('Stats', () => {
+  test('renders proper HTML', () => {
+    const Component = Vue.extend(Stats);
+    const vm = new Component({
+      propsData: {
+        searchStore,
+      },
+    }).$mount();
+
+    expect(vm.$el.outerHTML).toMatchSnapshot();
+  });
+
+  test('should not be displayed if there are no results', () => {
+    const Component = Vue.extend(Stats);
+    const vm = new Component({
+      propsData: {
+        searchStore: Object.assign({}, searchStore, { totalResults: 0 }),
+      },
+    }).$mount();
+
+    expect(vm.$el.outerHTML).toMatchSnapshot();
+  });
+});
