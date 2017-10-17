6ef26675989b509b15ce023075744ab2bd408e57
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-10-17 18:07:13 +0200

    chore(stories): add some decorators

--- packages/vue-instantsearch/stories/utils.js
+++ packages/vue-instantsearch/stories/utils.js
@@ -10,3 +10,50 @@ export function defaultSearchStore() {
 
   return searchStore;
 }
+
+export function indexWrapper() {
+  return {
+    template: `
+      <ais-index :search-store="searchStore">
+        <story/>
+      </ais-index>
+    `,
+    data() {
+      return {
+        searchStore: defaultSearchStore(),
+      };
+    },
+  };
+}
+
+export function searchBoxWrapper() {
+  return {
+    template: `
+      <div>
+        <story/>
+        <ais-search-box></ais-search-box>
+      </div>
+    `,
+  };
+}
+
+export function hitsWrapper() {
+  return {
+    template: `
+      <div>
+        <story/>
+        <ais-results>
+          <div slot-scope="{ result }">
+            <h3 v-html="result._highlightResult.name.value"></h3>
+            <div v-html="result._highlightResult.description.value"></div>
+          </div>
+        </ais-results>
+      </div>
+    `,
+    data() {
+      return {
+        searchStore: defaultSearchStore(),
+      };
+    },
+  };
+}
