4bdd3687742fa39bbaf0ad5c8609ceffd9a4d990
Raymond Rutjes <raymond.rutjes@gmail.com>
2017-10-17 22:06:22 +0200

    style(stories): lint

--- packages/vue-instantsearch/stories/Clear.stories.js
+++ packages/vue-instantsearch/stories/Clear.stories.js
@@ -1,6 +1,8 @@
 import { previewWrapper } from './utils';
 import { storiesOf } from '@storybook/vue';
 
-storiesOf('Clear', module).addDecorator(previewWrapper).add('default', () => ({
-  template: '<ais-clear></ais-clear>',
-}));
+storiesOf('Clear', module)
+  .addDecorator(previewWrapper)
+  .add('default', () => ({
+    template: '<ais-clear></ais-clear>',
+  }));
