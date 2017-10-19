cd65f31984d3d4051ddf14e4683fa2a41e3d900b
iam4x <iam4x@me.com>
2017-10-19 16:07:38 +0200

    fix(connectRefinementList): set default value for limit

--- src/connectors/refinement-list/connectRefinementList.js
+++ src/connectors/refinement-list/connectRefinementList.js
@@ -58,7 +58,7 @@ export const checkUsage = ({
  * @typedef {Object} CustomRefinementListWidgetOptions
  * @property {string} attributeName The name of the attribute in the records.
  * @property {"and"|"or"} [operator = 'or'] How the filters are combined together.
- * @property {number} [limit = undefined] The max number of items to display when
+ * @property {number} [limit = 10] The max number of items to display when
  * `showMoreLimit` is not or if the widget is showing less value.
  * @property {number} [showMoreLimit] The max number of items to display if the widget
  * is showing more items.
@@ -148,7 +148,7 @@ export default function connectRefinementList(renderFn) {
     const {
       attributeName,
       operator = 'or',
-      limit,
+      limit = 10,
       showMoreLimit,
       sortBy = ['isRefined', 'count:desc', 'name:asc'],
     } = widgetParams;
