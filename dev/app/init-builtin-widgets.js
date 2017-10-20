d7bf5c659633fc3d7dfc548e7dd0e08c624e8321
Alex S <bobylito@algolia.com>
2017-10-20 17:23:18 +0200

    chore(dev-novel): add searchbox with loading indicator

--- dev/app/init-builtin-widgets.js
+++ dev/app/init-builtin-widgets.js
@@ -64,6 +64,17 @@ export default () => {
           })
         );
       })
+    )
+    .add(
+      'With loading indicator',
+      wrapWithHits(container => {
+        window.search.addWidget(
+          instantsearch.widgets.searchBox({
+            container,
+            showLoading: true,
+          })
+        );
+      })
     );
 
   storiesOf('Stats').add(
