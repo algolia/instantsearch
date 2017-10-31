aa5a9370163ebc345d3c5a6aa413cd6a8aa54ae6
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-10-31 16:18:57 +0100

    chore: move analytics stories to external file

--- dev/app/builtin/init-stories.js
+++ dev/app/builtin/init-stories.js
@@ -1,33 +1,14 @@
 /* eslint-disable import/default */
 
-import { action, storiesOf } from 'dev-novel';
+import { storiesOf } from 'dev-novel';
 import instantsearch from '../../../index.js';
 import wrapWithHits from '../utils/wrap-with-hits.js';
 import initInstantSearchStories from './stories/instantsearch.stories';
+import initAnalyticsStories from './stories/analytics.stories';
 
 export default () => {
   initInstantSearchStories();
-
-  storiesOf('Analytics').add(
-    'default',
-    wrapWithHits(container => {
-      const description = document.createElement('p');
-      description.innerText = 'Search for something, look into Action Logger';
-      container.appendChild(description);
-
-      window.search.addWidget(
-        instantsearch.widgets.analytics({
-          pushFunction(formattedParameters, state, results) {
-            action('pushFunction[formattedParameters]')(formattedParameters);
-            action('pushFunction[state]')(state);
-            action('pushFunction[results]')(results);
-          },
-          triggerOnUIInteraction: true,
-          pushInitialSearch: false,
-        })
-      );
-    })
-  );
+  initAnalyticsStories();
 
   storiesOf('SearchBox')
     .add(
