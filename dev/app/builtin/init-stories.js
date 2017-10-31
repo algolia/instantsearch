42544cca0ef053efe37e0d38f26b5adf1b2e0dc1
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-10-31 17:14:39 +0100

    chore: move Toggle stories to external file

--- dev/app/builtin/init-stories.js
+++ dev/app/builtin/init-stories.js
@@ -1,8 +1,3 @@
-/* eslint-disable import/default */
-
-import { storiesOf } from 'dev-novel';
-import instantsearch from '../../../index.js';
-import wrapWithHits from '../utils/wrap-with-hits.js';
 import initInstantSearchStories from './stories/instantsearch.stories';
 import initAnalyticsStories from './stories/analytics.stories';
 import initClearAllStories from './stories/clear-all.stories';
@@ -22,6 +17,7 @@ import initSearchBoxStories from './stories/search-box.stories';
 import initSortBySelectorStories from './stories/sort-by-selector.stories';
 import initStarRatingStories from './stories/star-rating.stories';
 import initStatsStories from './stories/stats.stories';
+import initToggleStories from './stories/toggle.stories';
 
 export default () => {
   initInstantSearchStories();
@@ -43,40 +39,5 @@ export default () => {
   initSortBySelectorStories();
   initStatsStories();
   initStarRatingStories();
-
-  storiesOf('Toggle')
-    .add(
-      'with single value',
-      wrapWithHits(container => {
-        window.search.addWidget(
-          instantsearch.widgets.toggle({
-            container,
-            attributeName: 'free_shipping',
-            label: 'Free Shipping (toggle single value)',
-            templates: {
-              header: 'Shipping',
-            },
-          })
-        );
-      })
-    )
-    .add(
-      'with on & off values',
-      wrapWithHits(container => {
-        window.search.addWidget(
-          instantsearch.widgets.toggle({
-            container,
-            attributeName: 'brand',
-            label: 'Canon (not checked) or sony (checked)',
-            values: {
-              on: 'Sony',
-              off: 'Canon',
-            },
-            templates: {
-              header: 'Google or amazon (toggle two values)',
-            },
-          })
-        );
-      })
-    );
+  initToggleStories();
 };
