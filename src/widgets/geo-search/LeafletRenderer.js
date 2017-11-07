8f38e2b442dc97c3f870116123e074aae4abbb9f
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-11-07 11:45:11 +0100

    refactor(LeafletRenderer): update for connector API

--- src/widgets/geo-search/LeafletRenderer.js
+++ src/widgets/geo-search/LeafletRenderer.js
@@ -1,12 +1,6 @@
 import L from 'leaflet';
 import { getContainerNode } from '../../lib/utils.js';
 
-const removeMarkers = (markers = []) =>
-  markers.forEach(marker => marker.remove());
-
-const addMarkers = (map, hits) =>
-  hits.map(({ _geoloc }) => L.marker([_geoloc.lat, _geoloc.lng]).addTo(map));
-
 const refineWithBounds = (refine, bounds) => {
   const ne = bounds.getNorthEast();
   const sw = bounds.getSouthWest();
@@ -17,14 +11,70 @@ const refineWithBounds = (refine, bounds) => {
   });
 };
 
-const renderRedoSearchButton = ({ renderState, refine }) => {
+const removeMarkers = (markers = []) =>
+  markers.forEach(marker => marker.remove());
+
+const addMarkers = (map, hits) =>
+  hits.map(({ _geoloc }) => L.marker([_geoloc.lat, _geoloc.lng]).addTo(map));
+
+const fitMarkersBounds = ({ renderState, isRefinedWithMap }) => {
+  if (renderState.markers.length && !isRefinedWithMap) {
+    // Ugly hack for enable to detect user interaction
+    // rather than interaction trigger by the program
+    // see: https://stackoverflow.com/questions/31992278/detect-user-initiated-pan-zoom-operations-on-leaflet
+    // see: https://github.com/Leaflet/Leaflet/issues/2934
+    renderState.isUserInteraction = false;
+    renderState.map.fitBounds(L.featureGroup(renderState.markers).getBounds(), {
+      // Disable the animation because the bounds may not be correctly
+      // updated with `fitBounds`
+      // see: https://github.com/Leaflet/Leaflet/issues/3249
+      animate: false,
+    });
+    renderState.isUserInteraction = true;
+  }
+};
+
+const renderClearRefinementButton = ({
+  renderState,
+  clearMapRefinement,
+  isRefinedWithMap,
+}) => {
+  renderState.containerClear.innerHTML = '';
+  const button = document.createElement('button');
+  button.textContent = 'Clear the refinement with the current map view';
+  button.disabled = !isRefinedWithMap;
+  button.addEventListener('click', clearMapRefinement);
+  renderState.containerClear.appendChild(button);
+};
+
+const renderControlElement = ({
+  renderState,
+  toggleRefineOnMapMove,
+  enableRefineOnMapMove,
+}) => {
+  renderState.containerControl.innerHTML = '';
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = enableRefineOnMapMove;
+  checkbox.addEventListener('change', toggleRefineOnMapMove);
+  label.appendChild(checkbox);
+  label.append('Search as I move the map');
+  renderState.containerControl.appendChild(label);
+};
+
+const renderRedoSearchButton = ({
+  renderState,
+  hasMapMoveSinceLastRefine,
+  refine,
+}) => {
   renderState.containerControl.innerHTML = '';
   const button = document.createElement('button');
   button.textContent = 'Redo search here';
-  button.addEventListener('click', () => {
-    renderState.hasMapMoveSinceLastRefine = false;
-    refineWithBounds(refine, renderState.map.getBounds());
-  });
+  button.disabled = !hasMapMoveSinceLastRefine;
+  button.addEventListener('click', () =>
+    refineWithBounds(refine, renderState.map.getBounds())
+  );
   renderState.containerControl.appendChild(button);
 };
 
@@ -32,21 +82,25 @@ const renderer = (
   {
     hits,
     refine,
-    clearRefinementWithMap,
+    clearMapRefinement,
+    toggleRefineOnMapMove,
+    setMapMoveSinceLastRefine,
     isRefinedWithMap,
-    isRefinePositionChanged,
     enableRefineOnMapMove,
-    enableControlRefineWithMap,
+    hasMapMoveSinceLastRefine,
     widgetParams,
   },
   isFirstRendering
 ) => {
-  const { container, cssClasses, renderState } = widgetParams;
+  const {
+    container,
+    cssClasses,
+    enableRefineControl,
+    renderState,
+  } = widgetParams;
 
   if (isFirstRendering) {
     // Inital component state
-    renderState.enableRefineOnMapMove = enableRefineOnMapMove;
-    renderState.hasMapMoveSinceLastRefine = false;
     renderState.isUserInteraction = true;
 
     const node = getContainerNode(container);
@@ -72,147 +126,84 @@ const renderer = (
     node.appendChild(containerButtonElement);
     renderState.containerButtonElement = containerButtonElement;
 
-    const buttonClearMapRefinement = document.createElement('button');
-    buttonClearMapRefinement.className = 'ais-button-clear';
-    buttonClearMapRefinement.textContent =
-      'Clear the refinement with the current map view';
-    containerButtonElement.appendChild(buttonClearMapRefinement);
-    buttonClearMapRefinement.addEventListener('click', () => {
-      renderState.hasMapMoveSinceLastRefine = false;
-      clearRefinementWithMap();
-    });
-    renderState.buttonClearMapRefinement = buttonClearMapRefinement;
+    const containerClear = document.createElement('div');
+    containerClear.className = 'ais-control';
+    containerButtonElement.appendChild(containerClear);
+    renderState.containerClear = containerClear;
 
-    if (enableControlRefineWithMap) {
-      const containerControl = document.createElement('div');
-      containerControl.className = 'ais-control';
-      containerButtonElement.appendChild(containerControl);
-      renderState.containerControl = containerControl;
-    }
+    const containerControl = document.createElement('div');
+    containerControl.className = 'ais-control';
+    containerButtonElement.appendChild(containerControl);
+    renderState.containerControl = containerControl;
 
-    if (!enableControlRefineWithMap && !renderState.enableRefineOnMapMove) {
-      const buttonRefineOnMapMove = document.createElement('button');
-      buttonRefineOnMapMove.className = 'ais-button-refine';
-      buttonRefineOnMapMove.textContent = 'Redo search here';
-      containerButtonElement.appendChild(buttonRefineOnMapMove);
-      buttonRefineOnMapMove.addEventListener('click', () => {
-        renderState.hasMapMoveSinceLastRefine = false;
-        refineWithBounds(refine, renderState.map.getBounds());
-      });
-      renderState.buttonRefineOnMapMove = buttonRefineOnMapMove;
-    }
-
-    // Setup the map
-    renderState.map = L.map(rootElement, {
-      // Disable the animation because the bounds may not be correctly
-      // updated with `fitBounds`
-      // see: https://github.com/Leaflet/Leaflet/issues/3249
-      zoomAnimation: false,
-    });
+    renderState.map = L.map(rootElement);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(renderState.map);
-
-    // Setup events on map
-    renderState.map.on('dragend', event => {
-      if (renderState.enableRefineOnMapMove) {
-        renderState.hasMapMoveSinceLastRefine = false;
-        refineWithBounds(refine, event.target.getBounds());
-      }
-    });
-
-    renderState.map.on('zoomend', event => {
-      if (renderState.isUserInteraction) {
-        renderState.hasMapMoveSinceLastRefine = true;
-
-        if (renderState.enableRefineOnMapMove) {
-          renderState.hasMapMoveSinceLastRefine = false;
-          refineWithBounds(refine, event.target.getBounds());
-        }
-
-        if (enableControlRefineWithMap && !renderState.enableRefineOnMapMove) {
-          renderRedoSearchButton({
-            renderState,
-            refine,
-          });
-        }
-
-        if (!enableControlRefineWithMap && !renderState.enableRefineOnMapMove) {
-          renderState.buttonRefineOnMapMove.removeAttribute('disabled');
-        }
-      }
-    });
   }
 
-  // Restore hasMapMoveSinceLastRefine when refinement change
-  // but the refinement is not with the map
-  // ex: with places we can set the refinement with the
-  // autocomplete bar & we can move the map without
-  // the refine action
-  if (isRefinePositionChanged && !isRefinedWithMap) {
-    renderState.hasMapMoveSinceLastRefine = false;
-  }
-
-  // Register the callback on each render
-  // to update the closure scope
-  renderState.map.off('dragstart');
-  renderState.map.on('dragstart', () => {
-    renderState.hasMapMoveSinceLastRefine = true;
-
-    if (enableControlRefineWithMap && !renderState.enableRefineOnMapMove) {
-      renderRedoSearchButton({
-        renderState,
-        refine,
-      });
-    }
-
-    if (!enableControlRefineWithMap && !renderState.enableRefineOnMapMove) {
-      renderState.buttonRefineOnMapMove.removeAttribute('disabled');
+  // Events apply on each render in order
+  // to update the scope each time
+  renderState.map.off('dragend zoomend');
+  renderState.map.on('dragend zoomend', event => {
+    if (renderState.isUserInteraction && enableRefineOnMapMove) {
+      refineWithBounds(refine, event.target.getBounds());
     }
   });
 
-  removeMarkers(renderState.markers);
+  renderState.map.off('dragstart');
+  renderState.map.on('dragstart', () => {
+    if (enableRefineControl && !enableRefineOnMapMove) {
+      renderRedoSearchButton({
+        renderState,
+        hasMapMoveSinceLastRefine,
+        refine,
+      });
+    }
+  });
 
+  renderState.map.off('move');
+  renderState.map.on('move', () => {
+    if (renderState.isUserInteraction) {
+      setMapMoveSinceLastRefine();
+    }
+  });
+
+  // Markers
+  removeMarkers(renderState.markers);
   renderState.markers = addMarkers(renderState.map, hits);
 
-  if (renderState.markers.length && !isRefinedWithMap) {
-    // Ugly hack for enable to detect user interaction
-    // rather than interaction trigger by the program
-    // see: https://stackoverflow.com/questions/31992278/detect-user-initiated-pan-zoom-operations-on-leaflet
-    // see: https://github.com/Leaflet/Leaflet/issues/2934
-    renderState.isUserInteraction = false;
-    renderState.map.fitBounds(L.featureGroup(renderState.markers).getBounds());
-    renderState.isUserInteraction = true;
-  }
+  fitMarkersBounds({
+    renderState,
+    isRefinedWithMap,
+  });
 
-  if (isRefinedWithMap) {
-    renderState.buttonClearMapRefinement.removeAttribute('disabled');
-  } else {
-    renderState.buttonClearMapRefinement.setAttribute('disabled', true);
-  }
+  // UI
+  renderClearRefinementButton({
+    renderState,
+    clearMapRefinement,
+    isRefinedWithMap,
+  });
 
-  if (enableControlRefineWithMap) {
-    renderState.containerControl.innerHTML = '';
-    const label = document.createElement('label');
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    checkbox.checked = renderState.enableRefineOnMapMove;
-    label.appendChild(checkbox);
-    label.append('Search as I move the map');
-    checkbox.addEventListener('change', event => {
-      renderState.enableRefineOnMapMove = event.currentTarget.checked;
+  if (enableRefineControl) {
+    renderControlElement({
+      renderState,
+      toggleRefineOnMapMove,
+      enableRefineOnMapMove,
     });
-    renderState.containerControl.appendChild(label);
   }
 
-  if (!enableControlRefineWithMap && !renderState.enableRefineOnMapMove) {
-    if (renderState.hasMapMoveSinceLastRefine) {
-      renderState.buttonRefineOnMapMove.removeAttribute('disabled');
-    } else {
-      renderState.buttonRefineOnMapMove.setAttribute('disabled', true);
-    }
+  if (
+    !enableRefineOnMapMove &&
+    (!enableRefineControl || (enableRefineControl && hasMapMoveSinceLastRefine))
+  ) {
+    renderRedoSearchButton({
+      renderState,
+      hasMapMoveSinceLastRefine,
+      refine,
+    });
   }
 };
 
