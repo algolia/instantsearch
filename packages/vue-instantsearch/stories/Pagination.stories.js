4a6a89361c3d3e392f9c53599a90e453c51a746d
Haroen Viaene <fingebimus@me.com>
2017-11-15 11:22:11 +0100

    chore(storybook): add some stories with custom rendering (algolia/vue-instantsearch#355)

--- packages/vue-instantsearch/stories/Pagination.stories.js
+++ packages/vue-instantsearch/stories/Pagination.stories.js
@@ -5,4 +5,12 @@ storiesOf('Pagination', module)
   .addDecorator(previewWrapper)
   .add('default', () => ({
     template: '<ais-pagination></ais-pagination>',
+  }))
+  .add('custom rendering', () => ({
+    template: `<ais-pagination>
+      <span slot="first">first</span>
+      <span slot="previous">previous</span>
+      <span slot="next">next</span>
+      <span slot="last">last</span>
+    </ais-pagination>`,
   }));
