d976ec8f10cb6f6ca216af439e849ec33d66388d
Samuel Vaillant <samuel.vllnt@gmail.com>
2017-11-17 11:43:47 +0100

    Chore: Generate UMD build with Rollup (algolia/react-instantsearch#603)
    * chore: add Rollup dependencies
    
    * chore: add Rollup configuration
    
    * refactor(build): use Rollup instead of Webpack
    
    * chore: remove the hard dependency on UglifyJS
    
    * chore: remove useless Webpack config
    
    * chore: reorder package.json
    
    * chore: add rollup plugin node globals
    
    * chore: rollup add globas for process.env
    
    * docs(rollup): update
    
    * docs(rollup): remove bitly link

---
+++ packages/react-instantsearch/rollup.config.js
@@ -0,0 +1,112 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import babel from 'rollup-plugin-babel';
+import globals from 'rollup-plugin-node-globals';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
+import filesize from 'rollup-plugin-filesize';
+/* eslint-enable import/no-extraneous-dependencies */
+
+const clear = x => x.filter(Boolean);
+
+const version = process.env.VERSION || 'UNRELEASED';
+const algolia = '© Algolia, inc.';
+const link = 'https://community.algolia.com/react-instantsearch';
+const license = `/*! ReactInstantSearch ${version} | ${algolia} | ${link} */`;
+
+const plugins = [
+  babel({
+    exclude: 'node_modules/**',
+    // see: https://github.com/rollup/rollup-plugin-babel#helpers
+    plugins: ['external-helpers'],
+  }),
+  resolve({
+    browser: true,
+    preferBuiltins: false,
+  }),
+  commonjs({
+    // Rollup only support ES modules so we need to transform commonjs modules to ES one.
+    // Can be magically done sometimes, but Rollup needs a bit of help in some use cases.
+    // see doc: https://github.com/rollup/rollup-plugin-commonjs#custom-named-exports
+    // see API: https://github.com/algolia/algoliasearch-helper-js/blob/develop/index.js
+    namedExports: {
+      'algoliasearch-helper': [
+        'version',
+        'AlgoliaSearchHelper',
+        'SearchParameters',
+        'SearchResults',
+        'url',
+      ],
+    },
+  }),
+  // Useful for define process.env variable, inside the AlgoliaSearchClient we
+  // have a dependency on it for RESET_APP_DATA_TIMER.
+  // see: https://github.com/algolia/algoliasearch-client-javascript/blob/bb5d2ce16cf4b5a6e6de8ebae2de2880f783d967/src/AlgoliaSearchCore.js#L13
+  globals(),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  }),
+  filesize(),
+];
+
+const configuration = ({ input, name, minify = false } = {}) => ({
+  input,
+  external: ['react', 'react-dom'],
+  output: {
+    file: `dist/umd/${name}${minify ? '.min' : ''}.js`,
+    name: `ReactInstantSearch.${name}`,
+    format: 'umd',
+    globals: {
+      react: 'React',
+      'react-dom': 'ReactDOM',
+    },
+    banner: license,
+    sourcemap: true,
+  },
+  plugins: plugins.concat(
+    clear([
+      minify &&
+        uglify({
+          output: {
+            preamble: license,
+          },
+        }),
+    ])
+  ),
+});
+
+export default [
+  // Core
+  configuration({
+    input: 'index.js',
+    name: 'Core',
+  }),
+  configuration({
+    input: 'index.js',
+    name: 'Core',
+    minify: true,
+  }),
+
+  // Dom
+  configuration({
+    input: 'dom.js',
+    name: 'Dom',
+  }),
+  configuration({
+    input: 'dom.js',
+    name: 'Dom',
+    minify: true,
+  }),
+
+  // Connectors
+  configuration({
+    input: 'connectors.js',
+    name: 'Connectors',
+  }),
+  configuration({
+    input: 'connectors.js',
+    name: 'Connectors',
+    minify: true,
+  }),
+];
--- packages/react-instantsearch/scripts/build.sh
+++ packages/react-instantsearch/scripts/build.sh
@@ -26,31 +26,4 @@ babel -q server.js -o dist/server.js &&
 babel -q --ignore test.js,__mocks__ --out-dir dist/src src
 
 # finally a UMD build
-NODE_ENV=production webpack
-
-license="/*! ReactInstantSearch ${VERSION:-UNRELEASED} | © Algolia, inc. | https://community.algolia.com/react-instantsearch/ */"
-umd_dist="dist/umd"
-entries=( 'Core' 'Dom' 'Connectors')
-for entry in "${entries[@]}"
-do
-  dist_file="$umd_dist/${entry}.js"
-  dist_file_min="$umd_dist/${entry}.min.js"
-  source_map="${entry}.js.map"
-  source_map_min="${entry}.min.js.map"
-  dist_file_sourcemap="$umd_dist/${source_map}"
-  dist_file_sourcemap_min="$umd_dist/${source_map_min}"
-
-  echo "$license" | cat - "${dist_file}" > /tmp/out && mv /tmp/out "${dist_file}"
-
-  uglifyjs "${dist_file}" \
-    --in-source-map "${dist_file_sourcemap}" \
-    --source-map "${dist_file_sourcemap_min}" \
-    --source-map-url "${source_map_min}" \
-    --preamble "$license" \
-    -c warnings=false \
-    -m \
-    -o "${dist_file_min}"
-
-  gzip_size=$(gzip -9 < "$dist_file_min" | wc -c | pretty-bytes)
-  echo "=> $dist_file_min gzipped will weigh $gzip_size"
-done
+BABEL_ENV=es rollup -c rollup.config.js
