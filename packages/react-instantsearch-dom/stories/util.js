1ba028c152100920143b83981e38fb7bb0e2291a
Marie-Laure Sin <marielaures@users.noreply.github.com>
2017-11-22 15:34:25 +0100

    docs(storybook): allow WrapWithHits usage with a different index (algolia/react-instantsearch#635)

--- packages/react-instantsearch-dom/stories/util.js
+++ packages/react-instantsearch-dom/stories/util.js
@@ -33,6 +33,9 @@ const WrapWithHits = ({
   hasPlayground = false,
   linkedStoryGroup,
   pagination = true,
+  appId = 'latency',
+  apiKey = '6be0576ff61c053d5f9a3225e2a90f76',
+  indexName = 'ikea',
 }) => {
   const sourceCodeUrl = `https://github.com/algolia/react-instantsearch/tree/master/stories/${
     linkedStoryGroup
@@ -61,11 +64,7 @@ const WrapWithHits = ({
   };
 
   return (
-    <InstantSearch
-      appId="latency"
-      apiKey="6be0576ff61c053d5f9a3225e2a90f76"
-      indexName="ikea"
-    >
+    <InstantSearch appId={appId} apiKey={apiKey} indexName={indexName}>
       <Configure {...searchParameters} />
       <div>
         <div className="container widget-container">{children}</div>
