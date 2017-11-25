ea2d14f8cce1e656bd0252d4de6064c7893d4436
Alex S <bobylito@algolia.com>
2017-11-26 00:34:21 +0100

    test(core): test new render step with stalled search

--- src/lib/__tests__/InstantSearch-test-2.js
+++ src/lib/__tests__/InstantSearch-test-2.js
@@ -1,7 +1,7 @@
-import sinon from 'sinon';
-
 // import algoliaSearchHelper from 'algoliasearch-helper';
 import InstantSearch from '../InstantSearch';
+
+jest.useFakeTimers();
 
 const appId = 'appId';
 const apiKey = 'apiKey';
@@ -9,12 +9,12 @@ const indexName = 'lifecycle';
 
 describe('InstantSearch lifecycle', () => {
   it('calls the provided searchFunction when used', () => {
-    const searchFunctionSpy = sinon.spy(h => {
+    const searchFunctionSpy = jest.fn(h => {
       h.setQuery('test').search();
     });
 
     const fakeClient = {
-      search: sinon.spy(),
+      search: jest.fn(),
       addAlgoliaAgent: () => {},
     };
 
@@ -26,13 +26,121 @@ describe('InstantSearch lifecycle', () => {
       createAlgoliaClient: () => fakeClient,
     });
 
-    expect(searchFunctionSpy.callCount).toBe(0);
-    expect(fakeClient.search.callCount).toBe(0);
+    expect(searchFunctionSpy).not.toHaveBeenCalled();
+    expect(fakeClient.search).not.toHaveBeenCalled();
 
     search.start();
 
-    expect(searchFunctionSpy.callCount).toBe(1);
+    expect(searchFunctionSpy).toHaveBeenCalledTimes(1);
     expect(search.helper.state.query).toBe('test');
-    expect(fakeClient.search.callCount).toBe(1);
+    expect(fakeClient.search).toHaveBeenCalledTimes(1);
+  });
+
+  const fakeResults = () => ({
+    results: [
+      {
+        hits: [{}, {}],
+        nbHits: 2,
+        page: 0,
+        nbPages: 1,
+        hitsPerPage: 4,
+        processingTimeMS: 1,
+        exhaustiveNbHits: true,
+        query: '',
+        params: '',
+        index: 'quick_links',
+      },
+    ],
+  });
+
+  it('triggers the stalled search rendering once if the search does not resolve in time', () => {
+    const searchResultsResolvers = [];
+    const searchResultsPromises = [];
+    const fakeClient = {
+      search: jest.fn((qs, cb) => {
+        const p = new Promise(resolve =>
+          searchResultsResolvers.push(resolve)
+        ).then(() => {
+          cb(null, fakeResults());
+        });
+        searchResultsPromises.push(p);
+      }),
+      addAlgoliaAgent: () => {},
+    };
+
+    const search = new InstantSearch({
+      appId,
+      apiKey,
+      indexName,
+      createAlgoliaClient: () => fakeClient,
+    });
+
+    const widget = {
+      getConfiguration: jest.fn(),
+      init: jest.fn(),
+      render: jest.fn(),
+    };
+
+    search.addWidget(widget);
+
+    // when a widget is added the methods of the widget are not called
+    expect(widget.getConfiguration).not.toHaveBeenCalled();
+    expect(widget.init).not.toHaveBeenCalled();
+    expect(widget.render).not.toHaveBeenCalled();
+
+    search.start();
+
+    // During start, IS.js calls the getConfiguration, init and then send a search
+    expect(widget.getConfiguration).toHaveBeenCalledTimes(1);
+    expect(widget.init).toHaveBeenCalledTimes(1);
+    expect(widget.render).not.toHaveBeenCalled();
+
+    // first results come back
+    searchResultsResolvers[0]();
+
+    return searchResultsPromises[0].then(() => {
+      // render has now been called
+      expect(widget.render).toHaveBeenCalledTimes(1);
+
+      expect(widget.render).toHaveBeenLastCalledWith(
+        expect.objectContaining({
+          searchMetadata: {
+            isSearchStalled: false,
+          },
+        })
+      );
+
+      // New search
+      search.helper.search();
+      // results are not back yet
+      expect(widget.render).toHaveBeenCalledTimes(1);
+      // delay is reached
+      jest.runAllTimers();
+
+      expect(widget.render).toHaveBeenCalledTimes(2);
+      expect(widget.render).toHaveBeenLastCalledWith(
+        expect.objectContaining({
+          searchMetadata: {
+            isSearchStalled: true,
+          },
+        })
+      );
+
+      searchResultsResolvers[1]();
+      return searchResultsPromises[1].then(() => {
+        expect(widget.render).toHaveBeenCalledTimes(3);
+        expect(widget.render).toHaveBeenLastCalledWith(
+          expect.objectContaining({
+            searchMetadata: {
+              isSearchStalled: false,
+            },
+          })
+        );
+
+        // getConfiguration and init are not called a second time
+        expect(widget.getConfiguration).toHaveBeenCalledTimes(1);
+        expect(widget.init).toHaveBeenCalledTimes(1);
+      });
+    });
   });
 });
