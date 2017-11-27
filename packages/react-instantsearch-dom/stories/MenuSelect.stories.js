dd4c1cb474d2b04fec01d96c8f1fa1241b5f0427
Steve Monsen <smonsen@devwax.com>
2017-11-27 03:20:36 -0800

    fix(localizecount): allow localized string for count in MenuSelect (algolia/react-instantsearch#657)
    * fix(localizecount): allow localized string for count in MenuSelect
    
    * Removes potentially confusing example
    
    * Avoids modifying an object inside a map

--- packages/react-instantsearch-dom/stories/MenuSelect.stories.js
+++ packages/react-instantsearch-dom/stories/MenuSelect.stories.js
@@ -105,4 +105,25 @@ stories
       displayName,
       filterProps,
     }
+  )
+  .addWithJSX(
+    'with localized count',
+    () => (
+      <WrapWithHits linkedStoryGroup="MenuSelect">
+        <MenuSelect
+          attributeName="category"
+          defaultRefinement={text('defaultSelectedItem', 'Bathroom')}
+          transformItems={items =>
+            items.map(({ count, ...item }) => ({
+              ...item,
+              count: (count + 1000).toLocaleString(),
+            }))
+          }
+        />
+      </WrapWithHits>
+    ),
+    {
+      displayName,
+      filterProps,
+    }
   );
