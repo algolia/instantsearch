a2cd8d8b15555128449e46caa53cbb04ae43d471
Samuel Vaillant <samuel.vllnt@gmail.com>
2017-12-01 10:22:55 +0100

    fix: track all index in the manager (#660)
    * fix(createInstantSearchManager): track all index
    
    * feat(IndexStories): add story for SortBy nested in Index

--- packages/react-instantsearch/src/core/createInstantSearchManager.js
+++ packages/react-instantsearch/src/core/createInstantSearchManager.js
@@ -121,6 +121,8 @@ export default function createInstantSearchManager({
         sharedParameters
       );
 
+    indexMapping[mainIndexParameters.index] = indexName;
+
     return { sharedParameters, mainIndexParameters, derivatedWidgets };
   }
 
