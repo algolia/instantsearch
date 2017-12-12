ae59844bfd28ab29c0460a93381ed1ec972f4335
Vaillant Samuel <samuel.vllnt@gmail.com>
2017-12-12 12:19:06 +0100

    refactor(connectGeoSearchTest): split renderering from configuration

--- src/connectors/geo-search/__tests__/connectGeoSearch-test.js
+++ src/connectors/geo-search/__tests__/connectGeoSearch-test.js
@@ -13,7 +13,7 @@ const createFakeHelper = client => {
   return helper;
 };
 
-describe('connectGeoSearch', () => {
+describe('connectGeoSearch - rendering', () => {
   it('expect to be a widget', () => {
     const render = jest.fn();
     const unmount = jest.fn();
@@ -566,254 +566,254 @@ describe('connectGeoSearch', () => {
       );
     });
   });
+});
 
-  describe('getConfiguration', () => {
-    describe('aroundLatLngViaIP', () => {
-      it('expect to set aroundLatLngViaIP', () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
+describe('connectGeoSearch - getConfiguration', () => {
+  describe('aroundLatLngViaIP', () => {
+    it('expect to set aroundLatLngViaIP', () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
 
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          enableGeolocationWithIP: true,
-        });
-
-        const expectation = {
-          aroundLatLngViaIP: true,
-        };
-
-        const actual = widget.getConfiguration({});
-
-        expect(actual).toEqual(expectation);
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        enableGeolocationWithIP: true,
       });
 
-      it('expect to not set aroundLatLngViaIP when position is given', () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
+      const expectation = {
+        aroundLatLngViaIP: true,
+      };
 
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          enableGeolocationWithIP: true,
-          position: {
-            lat: 12,
-            lng: 10,
-          },
-        });
+      const actual = widget.getConfiguration({});
 
-        const expectation = {
-          aroundLatLng: '12, 10',
-        };
-
-        const actual = widget.getConfiguration({});
-
-        expect(actual).toEqual(expectation);
-      });
-
-      it("expect to not set aroundLatLngViaIP when it's already set", () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
-
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          enableGeolocationWithIP: true,
-        });
-
-        const expectation = {};
-
-        const actual = widget.getConfiguration({
-          aroundLatLngViaIP: false,
-        });
-
-        expect(actual).toEqual(expectation);
-      });
-
-      it('expect to not set aroundLatLngViaIP when aroundLatLng is already set', () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
-
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          enableGeolocationWithIP: true,
-        });
-
-        const expectation = {};
-
-        const actual = widget.getConfiguration({
-          aroundLatLng: '10, 12',
-        });
-
-        expect(actual).toEqual(expectation);
-      });
+      expect(actual).toEqual(expectation);
     });
 
-    describe('aroundLatLng', () => {
-      it('expect to set aroundLatLng', () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
+    it('expect to not set aroundLatLngViaIP when position is given', () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
 
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          position: {
-            lat: 12,
-            lng: 10,
-          },
-        });
-
-        const expectation = {
-          aroundLatLng: '12, 10',
-        };
-
-        const actual = widget.getConfiguration({});
-
-        expect(actual).toEqual(expectation);
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        enableGeolocationWithIP: true,
+        position: {
+          lat: 12,
+          lng: 10,
+        },
       });
 
-      it('expect to set aroundLatLng when aroundLatLngViaIP is already set to false', () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
+      const expectation = {
+        aroundLatLng: '12, 10',
+      };
 
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          position: {
-            lat: 12,
-            lng: 10,
-          },
-        });
+      const actual = widget.getConfiguration({});
 
-        const expectation = {
-          aroundLatLng: '12, 10',
-        };
-
-        const actual = widget.getConfiguration({
-          aroundLatLngViaIP: false,
-        });
-
-        expect(actual).toEqual(expectation);
-      });
-
-      it("expect to not set aroundLatLng when it's already set", () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
-
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          position: {
-            lat: 12,
-            lng: 10,
-          },
-        });
-
-        const expectation = {};
-
-        const actual = widget.getConfiguration({
-          aroundLatLng: '12, 12',
-        });
-
-        expect(actual).toEqual(expectation);
-      });
-
-      it('expect to not set aroundLatLng when aroundLatLngViaIP is already set to true', () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
-
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          position: {
-            lat: 12,
-            lng: 10,
-          },
-        });
-
-        const expectation = {};
-
-        const actual = widget.getConfiguration({
-          aroundLatLngViaIP: true,
-        });
-
-        expect(actual).toEqual(expectation);
-      });
+      expect(actual).toEqual(expectation);
     });
 
-    describe('aroundRadius', () => {
-      it('expect to set aroundRadius', () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
+    it("expect to not set aroundLatLngViaIP when it's already set", () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
 
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          radius: 1000,
-        });
-
-        const expectation = {
-          aroundLatLngViaIP: true,
-          aroundRadius: 1000,
-        };
-
-        const actual = widget.getConfiguration({});
-
-        expect(actual).toEqual(expectation);
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        enableGeolocationWithIP: true,
       });
 
-      it("expect to not set aroundRadius when it's already defined", () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
+      const expectation = {};
 
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          radius: 1000,
-        });
-
-        const expectation = {
-          aroundLatLngViaIP: true,
-        };
-
-        const actual = widget.getConfiguration({
-          aroundRadius: 500,
-        });
-
-        expect(actual).toEqual(expectation);
+      const actual = widget.getConfiguration({
+        aroundLatLngViaIP: false,
       });
+
+      expect(actual).toEqual(expectation);
     });
 
-    describe('aroundPrecision', () => {
-      it('expect to set aroundPrecision', () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
+    it('expect to not set aroundLatLngViaIP when aroundLatLng is already set', () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
 
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          precision: 1000,
-        });
-
-        const expectation = {
-          aroundLatLngViaIP: true,
-          aroundPrecision: 1000,
-        };
-
-        const actual = widget.getConfiguration({});
-
-        expect(actual).toEqual(expectation);
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        enableGeolocationWithIP: true,
       });
 
-      it("expect to not set aroundPrecision when it's already defined", () => {
-        const render = jest.fn();
-        const unmount = jest.fn();
+      const expectation = {};
 
-        const customGeoSearch = connectGeoSearch(render, unmount);
-        const widget = customGeoSearch({
-          precision: 1000,
-        });
-
-        const expectation = {
-          aroundLatLngViaIP: true,
-        };
-
-        const actual = widget.getConfiguration({
-          aroundPrecision: 500,
-        });
-
-        expect(actual).toEqual(expectation);
+      const actual = widget.getConfiguration({
+        aroundLatLng: '10, 12',
       });
+
+      expect(actual).toEqual(expectation);
+    });
+  });
+
+  describe('aroundLatLng', () => {
+    it('expect to set aroundLatLng', () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
+
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        position: {
+          lat: 12,
+          lng: 10,
+        },
+      });
+
+      const expectation = {
+        aroundLatLng: '12, 10',
+      };
+
+      const actual = widget.getConfiguration({});
+
+      expect(actual).toEqual(expectation);
+    });
+
+    it('expect to set aroundLatLng when aroundLatLngViaIP is already set to false', () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
+
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        position: {
+          lat: 12,
+          lng: 10,
+        },
+      });
+
+      const expectation = {
+        aroundLatLng: '12, 10',
+      };
+
+      const actual = widget.getConfiguration({
+        aroundLatLngViaIP: false,
+      });
+
+      expect(actual).toEqual(expectation);
+    });
+
+    it("expect to not set aroundLatLng when it's already set", () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
+
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        position: {
+          lat: 12,
+          lng: 10,
+        },
+      });
+
+      const expectation = {};
+
+      const actual = widget.getConfiguration({
+        aroundLatLng: '12, 12',
+      });
+
+      expect(actual).toEqual(expectation);
+    });
+
+    it('expect to not set aroundLatLng when aroundLatLngViaIP is already set to true', () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
+
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        position: {
+          lat: 12,
+          lng: 10,
+        },
+      });
+
+      const expectation = {};
+
+      const actual = widget.getConfiguration({
+        aroundLatLngViaIP: true,
+      });
+
+      expect(actual).toEqual(expectation);
+    });
+  });
+
+  describe('aroundRadius', () => {
+    it('expect to set aroundRadius', () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
+
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        radius: 1000,
+      });
+
+      const expectation = {
+        aroundLatLngViaIP: true,
+        aroundRadius: 1000,
+      };
+
+      const actual = widget.getConfiguration({});
+
+      expect(actual).toEqual(expectation);
+    });
+
+    it("expect to not set aroundRadius when it's already defined", () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
+
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        radius: 1000,
+      });
+
+      const expectation = {
+        aroundLatLngViaIP: true,
+      };
+
+      const actual = widget.getConfiguration({
+        aroundRadius: 500,
+      });
+
+      expect(actual).toEqual(expectation);
+    });
+  });
+
+  describe('aroundPrecision', () => {
+    it('expect to set aroundPrecision', () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
+
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        precision: 1000,
+      });
+
+      const expectation = {
+        aroundLatLngViaIP: true,
+        aroundPrecision: 1000,
+      };
+
+      const actual = widget.getConfiguration({});
+
+      expect(actual).toEqual(expectation);
+    });
+
+    it("expect to not set aroundPrecision when it's already defined", () => {
+      const render = jest.fn();
+      const unmount = jest.fn();
+
+      const customGeoSearch = connectGeoSearch(render, unmount);
+      const widget = customGeoSearch({
+        precision: 1000,
+      });
+
+      const expectation = {
+        aroundLatLngViaIP: true,
+      };
+
+      const actual = widget.getConfiguration({
+        aroundPrecision: 500,
+      });
+
+      expect(actual).toEqual(expectation);
     });
   });
 });
