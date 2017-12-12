fb98324edf5e5d3560739873af8da835808bea28
Haroen Viaene <fingebimus@me.com>
2017-12-12 11:17:31 +0100

    fix(url): treat insideBoundingBox in float form  as number (algolia/algoliasearch-helper-js#554)
    fixes algolia/algoliasearch-helper-js#553
    
    There's two representation of insideBoundingBox, one is
    `'47.3165,4.9665,47.3424,5.0201'`, the other is `[[47.3165, 4.9665,
    47.3424, 5.0201], [40.9234, 2.1185, 38.6430, 1.9916]]`. If URLSync was
    used with the second form, it would be parsed as `[ ["51.1241999",
    "9.662499900000057", "41.3253001", "-5.559099999999944"] ]` instead of
    the original array of array of floats.
    
    This commit adds special handling for this kind of shape to the
    `_parseNumbers` of `SearchParameters`

---
+++ packages/algoliasearch-helper/test/spec/url.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var test = require('tape');
+var algoliasearchHelper = require('../../index');
+
+var fakeClient = {
+  addAlgoliaAgent: function() {}
+};
+
+test('getStateFromQueryString should parse insideBoundingBox as float georects and be consistent with the state', function(t) {
+  var index = 'indexNameInTheHelper';
+  var helper = algoliasearchHelper(fakeClient, index, {
+    insideBoundingBox: [[51.1241999, 9.662499900000057, 41.3253001, -5.559099999999944]]
+  });
+
+  var queryString = algoliasearchHelper.url.getQueryStringFromState(helper.getState());
+
+  var partialStateFromQueryString = algoliasearchHelper.url.getStateFromQueryString(
+    queryString
+  );
+
+  t.deepEquals(
+    partialStateFromQueryString.insideBoundingBox,
+    helper.state.insideBoundingBox,
+    'insideBoundingBox should be consistent through query string serialization/deserialization');
+  t.end();
+});
+
+test('getStateFromQueryString should parse insideBoundingBox as float georects and be consistent with the state', function(t) {
+  var index = 'indexNameInTheHelper';
+  var helper = algoliasearchHelper(fakeClient, index, {
+    insideBoundingBox: '51.1241999,9.662499900000057,41.3253001,-5.559099999999944'
+  });
+
+  var queryString = algoliasearchHelper.url.getQueryStringFromState(helper.getState());
+
+  var partialStateFromQueryString = algoliasearchHelper.url.getStateFromQueryString(
+    queryString
+  );
+
+  t.deepEquals(
+    partialStateFromQueryString.insideBoundingBox,
+    helper.state.insideBoundingBox,
+    'insideBoundingBox should be consistent through query string serialization/deserialization');
+  t.end();
+});
