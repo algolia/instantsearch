34868b9f465790d821fc15215244d83311bd2ef4
Samuel Vaillant <samuel.vllnt@gmail.com>
2018-01-03 11:00:48 +0100

    fix(stories): rename InstantSearch to <InstantSearch> (algolia/react-instantsearch#789)

--- packages/react-instantsearch-dom/stories/InstantSearch.stories.js
+++ packages/react-instantsearch-dom/stories/InstantSearch.stories.js
@@ -10,7 +10,7 @@ import { displayName, filterProps } from './util';
 
 setAddon(JSXAddon);
 
-const stories = storiesOf('InstantSearch', module);
+const stories = storiesOf('<InstantSearch>', module);
 
 stories
   .addWithJSX(
