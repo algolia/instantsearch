d4c57f91ce8a6a9d7e6bf9a25a96ac282105498a
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-01-22 17:37:09 +0100

    feat(GeoSearchToggle): add component

---
+++ src/components/GeoSearchControls/__tests__/GeoSearchToggle-test.js
@@ -0,0 +1,63 @@
+import React from 'preact-compat';
+import { shallow } from 'enzyme';
+import serializer from 'enzyme-to-json/serializer';
+import GeoSearchToggle from '../GeoSearchToggle';
+
+expect.addSnapshotSerializer(serializer);
+
+describe('GeoSearchToggle', () => {
+  const defaultProps = {
+    classNameLabel: 'label',
+    classNameInput: 'input',
+    checked: false,
+    onToggle: () => {},
+  };
+
+  it('expect to render', () => {
+    const props = {
+      ...defaultProps,
+    };
+
+    const wrapper = shallow(
+      <GeoSearchToggle {...props}>
+        Clear the current map refinement
+      </GeoSearchToggle>
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('expect to render checked', () => {
+    const props = {
+      ...defaultProps,
+      checked: true,
+    };
+
+    const wrapper = shallow(
+      <GeoSearchToggle {...props}>
+        Clear the current map refinement
+      </GeoSearchToggle>
+    );
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('expect to call onToggle when the input changed', () => {
+    const props = {
+      ...defaultProps,
+      onToggle: jest.fn(),
+    };
+
+    const wrapper = shallow(
+      <GeoSearchToggle {...props}>
+        Clear the current map refinement
+      </GeoSearchToggle>
+    );
+
+    expect(props.onToggle).not.toHaveBeenCalled();
+
+    wrapper.find('input').simulate('change');
+
+    expect(props.onToggle).toHaveBeenCalled();
+  });
+});
