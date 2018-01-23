b98713c1911352cc73b3f173a085148de6b34cae
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-01-23 10:18:29 +0100

    feat(GeoSearchControls): add component

---
+++ src/components/GeoSearchControls/GeoSearchControls.js
@@ -0,0 +1,91 @@
+import React from 'preact-compat';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+import Template from '../Template';
+import GeoSearchButton from './GeoSearchButton';
+import GeoSearchToggle from './GeoSearchToggle';
+
+const GeoSearchControls = ({
+  cssClasses,
+  enableRefineControl,
+  enableClearMapRefinement,
+  isRefineOnMapMove,
+  isRefinedWithMap,
+  hasMapMoveSinceLastRefine,
+  onRefineToggle,
+  onRefineClick,
+  onClearClick,
+  templateProps,
+}) => (
+  <div>
+    {enableRefineControl && (
+      <div className={cssClasses.control}>
+        {isRefineOnMapMove || !hasMapMoveSinceLastRefine ? (
+          <GeoSearchToggle
+            classNameLabel={cx(
+              cssClasses.toggleLabel,
+              isRefineOnMapMove && cssClasses.toggleLabelActive
+            )}
+            classNameInput={cssClasses.toggleInput}
+            checked={isRefineOnMapMove}
+            onToggle={onRefineToggle}
+          >
+            <Template {...templateProps} templateKey="toggle" />
+          </GeoSearchToggle>
+        ) : (
+          <GeoSearchButton
+            className={cssClasses.redo}
+            disabled={!hasMapMoveSinceLastRefine}
+            onClick={onRefineClick}
+          >
+            <Template {...templateProps} templateKey="redo" />
+          </GeoSearchButton>
+        )}
+      </div>
+    )}
+
+    {!enableRefineControl &&
+      !isRefineOnMapMove && (
+        <div className={cssClasses.control}>
+          <GeoSearchButton
+            className={cssClasses.redo}
+            disabled={!hasMapMoveSinceLastRefine}
+            onClick={onRefineClick}
+          >
+            <Template {...templateProps} templateKey="redo" />
+          </GeoSearchButton>
+        </div>
+      )}
+
+    {enableClearMapRefinement &&
+      isRefinedWithMap && (
+        <GeoSearchButton className={cssClasses.clear} onClick={onClearClick}>
+          <Template {...templateProps} templateKey="clear" />
+        </GeoSearchButton>
+      )}
+  </div>
+);
+
+const CSSClassesPropTypes = PropTypes.shape({
+  control: PropTypes.string.isRequired,
+  toggleLabel: PropTypes.string.isRequired,
+  toggleLabelActive: PropTypes.string.isRequired,
+  toggleInput: PropTypes.string.isRequired,
+  redo: PropTypes.string.isRequired,
+  clear: PropTypes.string.isRequired,
+});
+
+GeoSearchControls.propTypes = {
+  cssClasses: CSSClassesPropTypes.isRequired,
+  enableRefineControl: PropTypes.bool.isRequired,
+  enableClearMapRefinement: PropTypes.bool.isRequired,
+  isRefineOnMapMove: PropTypes.bool.isRequired,
+  isRefinedWithMap: PropTypes.bool.isRequired,
+  hasMapMoveSinceLastRefine: PropTypes.bool.isRequired,
+  onRefineToggle: PropTypes.func.isRequired,
+  onRefineClick: PropTypes.func.isRequired,
+  onClearClick: PropTypes.func.isRequired,
+  templateProps: PropTypes.object.isRequired,
+};
+
+export default GeoSearchControls;
---
+++ src/components/GeoSearchControls/__tests__/GeoSearchControls-test.js
@@ -0,0 +1,237 @@
+import React from 'preact-compat';
+import { shallow } from 'enzyme';
+import serializer from 'enzyme-to-json/serializer';
+import GeoSearchControls from '../GeoSearchControls';
+
+expect.addSnapshotSerializer(serializer);
+
+describe('GeoSearchControls', () => {
+  const CSSClassesDefaultProps = {
+    control: 'control',
+    toggleLabel: 'toggleLabel',
+    toggleLabelActive: 'toggleLabelActive',
+    toggleInput: 'toggleInput',
+    redo: 'redo',
+    clear: 'clear',
+  };
+
+  const defaultProps = {
+    cssClasses: CSSClassesDefaultProps,
+    enableRefineControl: true,
+    enableClearMapRefinement: true,
+    isRefineOnMapMove: true,
+    isRefinedWithMap: false,
+    hasMapMoveSinceLastRefine: false,
+    onRefineToggle: () => {},
+    onRefineClick: () => {},
+    onClearClick: () => {},
+    templateProps: {},
+  };
+
+  describe('Control enabled', () => {
+    it('expect to render the toggle checked when refine on map move is enabled', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: true,
+        isRefineOnMapMove: true,
+        hasMapMoveSinceLastRefine: false,
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to render the toggle checked when refine on map move is enabled even when the map has moved', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: true,
+        isRefineOnMapMove: true,
+        hasMapMoveSinceLastRefine: true,
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to render the toggle unchecked when refine on map move is disabled and the map has not moved', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: true,
+        isRefineOnMapMove: false,
+        hasMapMoveSinceLastRefine: false,
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to render the button when refine on map move is disabled and the map has moved', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: true,
+        isRefineOnMapMove: false,
+        hasMapMoveSinceLastRefine: true,
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to call onRefineToggle when the toggle is toggled', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: true,
+        isRefineOnMapMove: true,
+        hasMapMoveSinceLastRefine: false,
+        onRefineToggle: jest.fn(),
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(props.onRefineToggle).not.toHaveBeenCalled();
+
+      wrapper.find('GeoSearchToggle').simulate('toggle');
+
+      expect(props.onRefineToggle).toHaveBeenCalled();
+    });
+
+    it('expect to call onRefineClick when the button is clicked', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: true,
+        isRefineOnMapMove: false,
+        hasMapMoveSinceLastRefine: true,
+        onRefineClick: jest.fn(),
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(props.onRefineClick).not.toHaveBeenCalled();
+
+      wrapper.find('GeoSearchButton').simulate('click');
+
+      expect(props.onRefineClick).toHaveBeenCalled();
+    });
+  });
+
+  describe('Control disabled', () => {
+    it('expect to render the button enabled when refine on map move is disabled and the map as moved', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: false,
+        isRefineOnMapMove: false,
+        hasMapMoveSinceLastRefine: true,
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to render the button disabled when refine on map move is disabled and the map has not moved', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: false,
+        isRefineOnMapMove: false,
+        hasMapMoveSinceLastRefine: false,
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to render nothing when refine on map move is enabled', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: false,
+        isRefineOnMapMove: true,
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to call onRefineClick whe the button is clicked', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: false,
+        isRefineOnMapMove: false,
+        hasMapMoveSinceLastRefine: true,
+        onRefineClick: jest.fn(),
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(props.onRefineClick).not.toHaveBeenCalled();
+
+      wrapper.find('GeoSearchButton').simulate('click');
+
+      expect(props.onRefineClick).toHaveBeenCalled();
+    });
+  });
+
+  describe('Clear button', () => {
+    it('expect to render the button when the refinement come from the map', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: false,
+        enableClearMapRefinement: true,
+        isRefinedWithMap: true,
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to not render the button when the refinement is not coming from the map', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: false,
+        enableClearMapRefinement: true,
+        isRefinedWithMap: false,
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to not render the button when the options is disabled', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: false,
+        enableClearMapRefinement: false,
+        isRefinedWithMap: true,
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('expect to call onClearClick when the clear button is clicked', () => {
+      const props = {
+        ...defaultProps,
+        enableRefineControl: false,
+        enableClearMapRefinement: true,
+        isRefinedWithMap: true,
+        onClearClick: jest.fn(),
+      };
+
+      const wrapper = shallow(<GeoSearchControls {...props} />);
+
+      expect(props.onClearClick).not.toHaveBeenCalled();
+
+      wrapper.find('.clear').simulate('click');
+
+      expect(props.onClearClick).toHaveBeenCalled();
+    });
+  });
+});
