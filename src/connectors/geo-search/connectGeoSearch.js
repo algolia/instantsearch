7b4b19c900eb0885f52b313a4d86d6fc735c6692
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-02-16 14:37:48 +0100

    docs(connectGeoSearch): add docs & example

--- src/connectors/geo-search/connectGeoSearch.js
+++ src/connectors/geo-search/connectGeoSearch.js
@@ -15,7 +15,97 @@ search.addWidget(
 Full documentation available at https://community.algolia.com/instantsearch.js/connectors/connectGeoSearch.html
 `;
 
-export default function connectGeoSearch(renderFn, unmountFn) {
+/**
+ * @typedef {Object} LatLng
+ * @property {number} lat The latitude in degrees.
+ * @property {number} lng The longitude in degrees.
+ */
+
+/**
+ * @typedef {Object} Bounds
+ * @property {LatLng} northEast The top right corner of the map view.
+ * @property {LatLng} southWest The bottom left corner of the map view.
+ */
+
+/**
+ * @typedef {Object} CustomGeoSearchWidgetOptions
+ * @property {boolean} [enableRefineOnMapMove=true] If true, refine will be triggered as you move the map.
+ * @property {boolean} [enableGeolocationWithIP=true] If true, the IP will be use for the geolocation. If the `position` option is provided this option will be ignored, since we already refine the results around the given position. See [the documentation](https://www.algolia.com/doc/api-reference/api-parameters/aroundLatLngViaIP) for more informations.
+ * @property {LatLng} [position] Position that will be use to search around. <br />
+ * See [the documentation](https://www.algolia.com/doc/api-reference/api-parameters/aroundLatLng) for more informations.
+ * @property {number} [radius] Maximum radius to search around the position (in meters). <br />
+ * See [the documentation](https://www.algolia.com/doc/api-reference/api-parameters/aroundRadius) for more informations.
+ * @property {number} [precision] Precision of geo search (in meters). <br />
+ * See [the documentation](https://www.algolia.com/doc/api-reference/api-parameters/aroundPrecision) for more informations.
+ */
+
+/**
+ * @typedef {Object} GeoSearchRenderingOptions
+ * @property {Object[]} items The matched hits from Algolia API.
+ * @property {function(Bounds)} refine Sets a bounding box to filter the results from the given map bounds.
+ * @property {function()} clearMapRefinement Reset the current bounding box refinement.
+ * @property {function(): boolean} isRefinedWithMap Return true if the current refinement is set with the map bounds.
+ * @property {function()} toggleRefineOnMapMove Toggle the fact that the user is able to refine on map move.
+ * @property {function(): boolean} isRefineOnMapMove Return true if the user is able to refine on map move.
+ * @property {function()} setMapMoveSinceLastRefine Set the fact that the map has moved since the last refinement, should be call on each map move. The call to the function triggers a new rendering only when the value change.
+ * @property {function(): boolean} hasMapMoveSinceLastRefine Return true if the map has move since the last refinement.
+ * @property {Object} widgetParams All original `CustomGeoSearchWidgetOptions` forwarded to the `renderFn`.
+ * @property {LatLng} [position] The current position of the search.
+ */
+
+/**
+ * **GeoSearch** connector provides the logic to build a widget that will display results on a map. It also provides a way to search for results based on their position. The connector exposes APIs in order to manage the search experience (search on map interaction or control the interaction for example).
+ *
+ * Note that the GeoSearch connector takes leverage of the [geosearch](https://www.algolia.com/doc/guides/searching/geo-search) capabilities of Algolia. Your hits **must** have a `_geoloc` attribute in order to be passed to the rendering function.
+ *
+ * @type {Connector}
+ * @param {function(GeoSearchRenderingOptions, boolean)} renderFn Rendering function for the custom **GeoSearch** widget.
+ * @param {function} unmountFn Unmount function called when the widget is disposed.
+ * @return {function(CustomGeoSearchWidgetOptions)} Re-usable widget factory for a custom **GeoSearch** widget.
+ * @example
+ * // This example use Leaflet for the rendering, be sure to have the library correctly setup
+ * // before trying the demo. You can find more details in their documentation (link below).
+ * // We choose Leaflet for the example but you can use any libraries that you want.
+ * // See: http://leafletjs.com/examples/quick-start
+ *
+ * let map = null;
+ * let markers = [];
+ *
+ * // custom `renderFn` to render the custom GeoSearch widget
+ * function renderFn(GeoSearchRenderingOptions, isFirstRendering) {
+ *   const { items, widgetParams } = GeoSearchRenderingOptions;
+ *
+ *   if (isFirstRendering) {
+ *     map = L.map(widgetParams.container);
+ *
+ *     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ *       attribution:
+ *         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+ *     }).addTo(map);
+ *   }
+ *
+ *   markers.forEach(marker => marker.remove());
+ *
+ *   markers = items.map(({ _geoloc }) =>
+ *     L.marker([_geoloc.lat, _geoloc.lng]).addTo(map)
+ *   );
+ *
+ *   if (markers.length) {
+ *     map.fitBounds(L.featureGroup(markers).getBounds());
+ *   }
+ * }
+ *
+ * // connect `renderFn` to GeoSearch logic
+ * const customGeoSearch = instantsearch.connectors.connectGeoSearch(renderFn);
+ *
+ * // mount widget on the page
+ * search.addWidget(
+ *   customGeoSearch({
+ *     container: document.getElementById('custom-geo-search'),
+ *   })
+ * );
+ */
+const connectGeoSearch = (renderFn, unmountFn) => {
   checkRendering(renderFn, usage);
 
   return (widgetParams = {}) => {
@@ -220,4 +310,6 @@ export default function connectGeoSearch(renderFn, unmountFn) {
       },
     };
   };
-}
+};
+
+export default connectGeoSearch;
