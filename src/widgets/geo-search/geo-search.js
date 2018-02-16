096656465bd94f4edc8e0920e852d9e98f853a46
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-02-16 17:50:16 +0100

    feat(GeoSearch): add default value for initialZoom & initialPosition

--- src/widgets/geo-search/geo-search.js
+++ src/widgets/geo-search/geo-search.js
@@ -64,8 +64,8 @@ const bem = bemHelper('ais-geo-search');
  * @typedef {object} GeoSearchWidgetOptions
  * @property {string|HTMLElement} container CSS Selector or HTMLElement to insert the widget.
  * @property {object} googleReference Reference to the global `window.google` object. You can find more information about how to load the library in [the Google Maps documentation](https://developers.google.com/maps/documentation/javascript/tutorial).
- * @property {number} [initialZoom=TDB] By default the map will set the zoom accordingly to the markers displayed on it. When we refine it may happen that the results are empty. For those situtations we need to provide a zoom to render the map.
- * @property {LatLng} [initialPosition=TBD] By default the map will set the position accordingly to the markers displayed on it. When we refine it may happen that the results are empty. For those situtations we need to provide a position to render the map. This option is ignored when the `position` is provided.
+ * @property {number} [initialZoom=1] By default the map will set the zoom accordingly to the markers displayed on it. When we refine it may happen that the results are empty. For those situtations we need to provide a zoom to render the map.
+ * @property {LatLng} [initialPosition={ lat: 0, lng: 0 }] By default the map will set the position accordingly to the markers displayed on it. When we refine it may happen that the results are empty. For those situtations we need to provide a position to render the map. This option is ignored when the `position` is provided.
  * @property {Padding} [paddingBoundingBox={ top:0, right: 0, bottom:0, left: 0 }] Add an inner padding on the map when you refine.
  * @property {GeoSearchTemplates} [templates] Templates to use for the widget.
  * @property {GeoSeachCSSClasses} [cssClasses] CSS classes to add to the wrapping elements.
@@ -113,6 +113,11 @@ const geoSearch = (
     builtInMarker: userBuiltInMarker = {},
     customHTMLMarker: userCustomHTMLMarker = false,
     paddingBoundingBox: userPaddingBoundingBox = {},
+    initialZoom = 1,
+    initialPosition = {
+      lat: 0,
+      lng: 0,
+    },
     container,
     googleReference,
     ...widgetParams
@@ -217,6 +222,10 @@ const geoSearch = (
     return makeGeoSearch({
       ...widgetParams,
       renderState: {},
+      container,
+      googleReference,
+      initialZoom,
+      initialPosition,
       enableClearMapRefinement,
       enableRefineControl,
       cssClasses,
@@ -224,8 +233,6 @@ const geoSearch = (
       createMarker,
       markerOptions,
       paddingBoundingBox,
-      container,
-      googleReference,
     });
   } catch (e) {
     throw new Error(`See usage.`);
