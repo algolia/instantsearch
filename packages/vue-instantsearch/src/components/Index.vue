ea86bb3a6f4634719882f9a3df87d2ac71dead04
Haroen Viaene <hello@haroen.me>
2018-03-12 11:02:48 +0100

    chore(index): soon configurable

--- packages/vue-instantsearch/src/components/Index.vue
+++ packages/vue-instantsearch/src/components/Index.vue
@@ -110,6 +110,9 @@ export default {
 
     // todo: make it configurable
     const instance = instantsearch({
+      // appId: this.appId,
+      // apiKey: this.apiKey,
+      // indexName: this.indexName,
       appId: 'latency',
       apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
       indexName: 'ikea',
