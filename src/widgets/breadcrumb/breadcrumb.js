de75b5cbeeac220719376ae0a7539f2f2ff1e2e8
Haroen Viaene <hello@haroen.me>
2018-03-16 12:53:47 +0100

    chore: unmount widget on unmount

--- src/widgets/breadcrumb/breadcrumb.js
+++ src/widgets/breadcrumb/breadcrumb.js
@@ -2,7 +2,7 @@ import React, { render, unmountComponentAtNode } from 'preact-compat';
 import cx from 'classnames';
 
 import Breadcrumb from '../../components/Breadcrumb/Breadcrumb';
-import { connectBreadcrumb } from '../../connectors';
+import connectBreadcrumb from '../../connectors/breadcrumb/connectBreadcrumb';
 import defaultTemplates from './defaultTemplates.js';
 
 import {
