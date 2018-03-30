07ea18209e839bfdb1d02d69c6db03008af8861c
Alex S <bobylito@users.noreply.github.com>
2018-03-30 13:57:51 +0200

    chore(test): delete indices at the end of the tests (algolia/algoliasearch-helper-js#570)
    fix algolia/algoliasearch-helper-js#560

--- packages/algoliasearch-helper/test/run.js
+++ packages/algoliasearch-helper/test/run.js
@@ -1,10 +1,26 @@
 'use strict';
 
 var bulk = require('bulk-require');
+var test = require('tape');
+var algolia = require('algoliasearch');
 
 bulk(__dirname, ['spec/**/*.js']);
 
 if (process.env.ONLY_UNIT !== 'true' && process.env.INTEGRATION_TEST_API_KEY && process.env.INTEGRATION_TEST_APPID) {
   // usage: INTEGRATION_TEST_APPID=$APPID INTEGRATION_TEST_API_KEY=$APIKEY npm run dev
   bulk(__dirname, ['integration-spec/**/*.js']);
+
+  test.onFinish(cleanupIntegration);
+}
+
+function cleanupIntegration() {
+  console.log('Deleting all indices');
+  var client = algolia(process.env.INTEGRATION_TEST_APPID, process.env.INTEGRATION_TEST_API_KEY);
+  var maybeIndices = client.listIndexes();
+  maybeIndices.then(content => {
+    content.items
+      .map(i => i.name)
+      .filter(n => n.indexOf('_travis-algoliasearch-helper') !== -1)
+      .forEach(n => client.deleteIndex(n));
+  });
 }
