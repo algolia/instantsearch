e90d70f0b0dc8ef67d5f1c344d7221a4834b7898
Alex S <bobylito@algolia.com>
2018-04-04 15:30:04 +0200

    chore: refactor CI test into 2 parts: unit and functional
    Also this cleans up some other scripts:
     - validation of PR is now configurable in github
     - finish release is not used, doc is published directly and jsDeliver
     now publishes right away

--- scripts/ci/functional-test.sh
+++ scripts/ci/functional-test.sh
@@ -2,10 +2,6 @@
 
 set -ev # exit when error
 
-./scripts/validate-pr-done-on-develop.sh
-npm run lint
-npm test
-
 # we need to build to run functional tests
 NODE_ENV=production npm run build
 INDEX_PAGE=index npm run test:functional
@@ -15,7 +11,3 @@ npm run argos
 
 # check if the next size of the bundle is inside the threshold
 npm run test:size
-
-if [ $TRAVIS_PULL_REQUEST == 'false' ] && [ $TRAVIS_BRANCH == 'master' ]; then
-  npm run finish-release
-fi
