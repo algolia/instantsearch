1c54e53c58ee74e7ff84cc0b3786f31e3fed4118
Samuel Vaillant <samuel.vllnt@gmail.com>
2018-04-04 12:33:18 +0200

    chore: allow custom configuration for stories with <Wrap> (#1141)
    * feat(stories): allow custom configuration for <Wrap>
    
    * refactor(stories): reuse variable
    
    * refactor(stories) inline export
    
    * refactor(stories): re-order utils function

--- packages/react-instantsearch-dom/stories/util.js
+++ packages/react-instantsearch-dom/stories/util.js
@@ -12,30 +12,68 @@ import { connectHits } from '../packages/react-instantsearch/connectors';
 import { linkTo } from '@storybook/addon-links';
 import 'instantsearch.css/themes/algolia.css';
 
-const Wrap = props => (
-  <InstantSearch
-    appId="latency"
-    apiKey="6be0576ff61c053d5f9a3225e2a90f76"
-    indexName="ikea"
-  >
-    {props.children}
+export const filterProps = ['linkedStoryGroup', 'hasPlayground'];
+
+export const CustomHits = connectHits(({ hits }) => (
+  <div className="hits">
+    {hits.map(hit => (
+      <div key={hit.objectID} className="hit">
+        <div>
+          <div className="hit-picture">
+            <img
+              src={`https://res.cloudinary.com/hilnmyskv/image/fetch/h_100,q_100,f_auto/${
+                hit.image
+              }`}
+            />
+          </div>
+        </div>
+        <div className="hit-content">
+          <div>
+            <Highlight attribute="name" hit={hit} />
+            <span> - ${hit.price}</span>
+            <span> - {hit.rating} stars</span>
+          </div>
+          <div className="hit-type">
+            <Highlight attribute="type" hit={hit} />
+          </div>
+          <div className="hit-description">
+            <Highlight attribute="description" hit={hit} />
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+));
+
+export const Wrap = ({ appId, apiKey, indexName, children }) => (
+  <InstantSearch appId={appId} apiKey={apiKey} indexName={indexName}>
+    {children}
   </InstantSearch>
 );
 
 Wrap.propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node.isRequired,
+  appId: PropTypes.string,
+  apiKey: PropTypes.string,
+  indexName: PropTypes.string,
 };
 
-const WrapWithHits = ({
+Wrap.defaultProps = {
+  appId: 'latency',
+  apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
+  indexName: 'ikea',
+};
+
+export const WrapWithHits = ({
   searchParameters: askedSearchParameters = {},
   children,
   searchBox = true,
   hasPlayground = false,
   linkedStoryGroup,
   pagination = true,
-  appId = 'latency',
-  apiKey = '6be0576ff61c053d5f9a3225e2a90f76',
-  indexName = 'ikea',
+  appId,
+  apiKey,
+  indexName,
 }) => {
   const sourceCodeUrl = `https://github.com/algolia/react-instantsearch/tree/master/stories/${linkedStoryGroup}.stories.js`;
   const playgroundLink = hasPlayground ? (
@@ -94,37 +132,6 @@ const WrapWithHits = ({
   );
 };
 
-const CustomHits = connectHits(({ hits }) => (
-  <div className="hits">
-    {hits.map(hit => (
-      <div key={hit.objectID} className="hit">
-        <div>
-          <div className="hit-picture">
-            <img
-              src={`https://res.cloudinary.com/hilnmyskv/image/fetch/h_100,q_100,f_auto/${
-                hit.image
-              }`}
-            />
-          </div>
-        </div>
-        <div className="hit-content">
-          <div>
-            <Highlight attribute="name" hit={hit} />
-            <span> - ${hit.price}</span>
-            <span> - {hit.rating} stars</span>
-          </div>
-          <div className="hit-type">
-            <Highlight attribute="type" hit={hit} />
-          </div>
-          <div className="hit-description">
-            <Highlight attribute="description" hit={hit} />
-          </div>
-        </div>
-      </div>
-    ))}
-  </div>
-));
-
 WrapWithHits.propTypes = {
   appId: PropTypes.string,
   apiKey: PropTypes.string,
@@ -153,41 +160,46 @@ const getReactElementDisplayName = element =>
     : element.type);
 
 // displays the right name for the JSX addon in Storybook
-const displayName = element => {
+export const displayName = element => {
+  const reactElementDisplayName = getReactElementDisplayName(element);
+  const isWrap = reactElementDisplayName === 'Wrap';
+  const isWrapWithHits = reactElementDisplayName === 'WrapWithHits';
+
   // display 'InstantSearch' instead of 'WrapWithHits'
-  if (getReactElementDisplayName(element) === 'WrapWithHits') {
-    const instantSearch = 'InstantSearch';
-    return instantSearch;
+  if (isWrap || isWrapWithHits) {
+    return 'InstantSearch';
   }
+
   // wrapped component: AlgoliaWidgetName(Translatable..)" => "WidgetName"
   if (
     React.Component.isPrototypeOf(element.type) &&
-    getReactElementDisplayName(element).startsWith('Algolia')
+    reactElementDisplayName.startsWith('Algolia')
   ) {
     const innerComponentRegex = /\(([^()]+)\)/;
     const match = innerComponentRegex.exec(element.type.displayName);
     const innerComponentName = match[1];
     const rawName = element.type.displayName;
     const widgetName = rawName.split('(')[0].replace('Algolia', '');
+
     if (match) {
       // when a VirtualWidget is used, Algolia returns 'UnknownComponent' as the displayName
       if (innerComponentName === 'UnknownComponent') {
         return widgetName;
       }
+
       // when a Configure widget is used, Algolia returns '_default' as the displayName
       if (innerComponentName === '_default') {
         return widgetName;
       }
+
       // when a RangeInput widget is used, Algolia returns 'RawRangeInput' as the displayName
       if (innerComponentName === 'RawRangeInput') {
         return 'RangeInput';
       }
+
       return innerComponentName;
     }
   }
-  return getReactElementDisplayName(element);
+
+  return reactElementDisplayName;
 };
-
-const filterProps = ['linkedStoryGroup', 'hasPlayground'];
-
-export { CustomHits, displayName, filterProps, Wrap, WrapWithHits };
