0d96b5eca5546de15da5c3e23e793d2fa30a1f96
vvo <vincent.voyer@algolia.com>
2018-04-12 15:17:35 +0200

    test(Template): make template test flat

--- packages/vue-instantsearch/src/components/__tests__/__Template.js
+++ packages/vue-instantsearch/src/components/__tests__/__Template.js
@@ -2,57 +2,21 @@ import { mount } from '@vue/test-utils';
 import Template from '../__Template.vue';
 jest.mock('../../component');
 
-describe('Template', () => {
-  describe('html', () => {
-    it('should render correctly', () => {
-      const wrapper = mount(Template);
-      wrapper.setData({ state: { hits: [] } });
-      expect(wrapper.html()).toMatchSnapshot();
-    });
-
-    it('should render correctly when `hits` is different', () => {
-      const wrapper = mount(Template);
-      wrapper.setData({
-        state: {
-          hits: ['yo', 'how', 'are', 'you', 'doing', '?'],
-        },
-      });
-      expect(wrapper.html()).toMatchSnapshot();
-    });
+it('renders correctly', () => {
+  const wrapper = mount(Template);
+  wrapper.setData({
+    state: {
+      hits: ['yo', 'how', 'are', 'you', 'doing', '?'],
+    },
   });
+  expect(wrapper.html()).toMatchSnapshot();
+});
 
-  describe('widgetParams', () => {
-    it('correct defaults', () => {
-      const wrapper = mount(Template);
+// ☑️ add another rendering test if it's different given the propsData
 
-      const { widgetParams } = wrapper.vm;
-
-      expect(widgetParams).toEqual({
-        someProp: [],
-      });
-    });
-
-    it('allows overriding', () => {
-      const wrapper = mount(Template, {
-        propsData: {
-          someProp: ['hi'],
-        },
-      });
-
-      const { widgetParams } = wrapper.vm;
-
-      expect(widgetParams).toEqual({
-        someProp: ['hi'],
-      });
-    });
-  });
-
-  describe('behavior', () => {
-    it('calls refine with `hi` when button is clicked', () => {
-      const wrapper = mount(Template);
-      const button = wrapper.find('button');
-      button.trigger('click');
-      expect(wrapper.vm.state.refine).toHaveBeenLastCalledWith('hi');
-    });
-  });
+it('behaves correctly', () => {
+  const wrapper = mount(Template);
+  const button = wrapper.find('button');
+  button.trigger('click');
+  expect(wrapper.vm.state.refine).toHaveBeenLastCalledWith('hi');
 });
