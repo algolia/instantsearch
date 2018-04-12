de07117e914fed5fcda0ea061a05447edeee406d
Haroen Viaene <hello@haroen.me>
2018-04-12 17:20:18 +0200

    test(mock): allow setting of initial state with __setState

--- packages/vue-instantsearch/src/__mocks__/component.js
+++ packages/vue-instantsearch/src/__mocks__/component.js
@@ -1,11 +1,29 @@
 import suit from '../suit';
 
+let state = {
+  createURL: () => '#',
+  refine: jest.fn(),
+};
+
+// we need to have state given by `component` before it is mounted, otherwise
+// we can't render it in most cases (items, hits, etc. are used in the template)
+// so we share a (mock) global state during a whole test.
+//
+// (a mock is imported once per test file, so the state is isolated between tests)
+//
+// This allows us to import this `__setState` function and call it in the test
+// to give the necessary data before mounting.
+export function __setState(newState) {
+  state = {
+    ...state,
+    ...newState,
+  };
+}
+
 export default {
   data() {
     return {
-      state: {
-        refine: jest.fn(),
-      },
+      state,
     };
   },
   methods: {
---
+++ packages/vue-instantsearch/src/components/__tests__/.eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  rules: {
+    'import/named': 'off', // we import __setState and use it
+  },
+};
