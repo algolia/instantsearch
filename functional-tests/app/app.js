36379a326b7c5f4f6df28b131ab00585ba11f866
Alex S <bobylito@algolia.com>
2018-04-13 11:04:42 +0200

    test(functional): use routing instead of urlSync

--- functional-tests/app/app.js
+++ functional-tests/app/app.js
@@ -5,7 +5,7 @@ var search = instantsearch({ // eslint-disable-line
   appId: 'latency',
   apiKey: '6be0576ff61c053d5f9a3225e2a90f76',
   indexName: 'instant_search',
-  urlSync: true,
+  routing: true,
   searchParameters: {
     hitsPerPage: 6,
   },
