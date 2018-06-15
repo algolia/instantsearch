bff7dfc6e5faa4c3f6dac057c847d4da4dfe8b39
François Chalifour <francoischalifour@users.noreply.github.com>
2018-06-15 10:19:51 +0200

    feat(templates): Warn facets usage with React Native template (algolia/create-instantsearch-app#56)
    Since `attributesForFaceting` is not supported by the React Native template, warn the user about it.

--- templates/React InstantSearch Native/.template.js
+++ templates/React InstantSearch Native/.template.js
@@ -1,3 +1,4 @@
+const chalk = require('chalk');
 const install = require('../../packages/tasks/node/install');
 const teardown = require('../../packages/tasks/node/teardown');
 
@@ -5,8 +6,27 @@ module.exports = {
   libraryName: 'react-instantsearch-native',
   templateName: 'react-instantsearch-native',
   appName: 'react-instantsearch-native-app',
-  keywords: ['algolia', 'instantSearch', 'react', 'react-native', 'react-instantsearch-native'],
+  keywords: [
+    'algolia',
+    'instantSearch',
+    'react',
+    'react-native',
+    'react-instantsearch-native',
+  ],
   tasks: {
+    setup(config) {
+      if (!config.silent && config.attributesForFaceting) {
+        console.log();
+        console.log(
+          `⚠️   The ${chalk.cyan(
+            'attributesForFaceting'
+          )} option is not supported in this template.`
+        );
+        console.log();
+      }
+
+      return Promise.resolve();
+    },
     install,
     teardown,
   },
