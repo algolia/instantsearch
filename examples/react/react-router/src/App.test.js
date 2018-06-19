1b90df0fd1110e69cec7056221ddf9cc02f8dda0
Yotam <yotammanor@gmail.com>
2018-06-19 16:38:25 +0300

    chore(examples): use real location object in react-router example (#1349)
    * use real location object in example app test

--- examples/react/react-router/src/App.test.js
+++ examples/react/react-router/src/App.test.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import App from './App';
+import { createMemoryHistory } from 'history';
+
+const history = createMemoryHistory('/');
+const { location } = history;
 
 describe('react-router recipe', () => {
   it('App renders without crashing', () => {
-    const component = renderer.create(
-      <App location={{ search: { slice: jest.fn() } }} />
-    );
+    const component = renderer.create(<App location={location} />);
 
     expect(component.toJSON()).toMatchSnapshot();
   });
