0ba94723781c10c5380abb724f8dd9d83f23e49f
Samuel Vaillant <samuel.vllnt@gmail.com>
2018-06-22 14:55:16 +0200

    fix(example): update autocomplete example (algolia/react-instantsearch#1366)
    * fix(examples): update autocomplete example
    
    * feat(examples): add Highlighting to autocomplete
    
    * fix(examples): add propTypes for autocomplete
    
    * fix(examples): update autocomplete snapshots

--- examples/react/autocomplete/src/App-Multi-Index.js
+++ examples/react/autocomplete/src/App-Multi-Index.js
@@ -1,5 +1,11 @@
-import React from 'react';
-import { InstantSearch, Configure, Index } from 'react-instantsearch/dom';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+  InstantSearch,
+  Configure,
+  Index,
+  Highlight,
+} from 'react-instantsearch/dom';
 import { connectAutoComplete } from 'react-instantsearch/connectors';
 import Autosuggest from 'react-autosuggest';
 
@@ -16,28 +22,73 @@ const App = () => (
   </InstantSearch>
 );
 
-const AutoComplete = connectAutoComplete(
-  ({ hits, currentRefinement, refine }) => (
-    <Autosuggest
-      suggestions={hits}
-      multiSection={true}
-      onSuggestionsFetchRequested={({ value }) => refine(value)}
-      onSuggestionsClearRequested={() => refine('')}
-      getSuggestionValue={hit => hit.name}
-      renderSuggestion={hit => (
-        <div>
-          <div>{hit.name}</div>
-        </div>
-      )}
-      inputProps={{
-        placeholder: 'Type a product',
-        value: currentRefinement,
-        onChange: () => {},
-      }}
-      renderSectionTitle={section => section.index}
-      getSectionSuggestions={section => section.hits}
-    />
-  )
-);
+class Example extends Component {
+  static propTypes = {
+    hits: PropTypes.arrayOf(PropTypes.object).isRequired,
+    currentRefinement: PropTypes.string.isRequired,
+    refine: PropTypes.func.isRequired,
+  };
+
+  state = {
+    value: this.props.currentRefinement,
+  };
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue,
+    });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.props.refine(value);
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.props.refine();
+  };
+
+  getSuggestionValue(hit) {
+    return hit.name;
+  }
+
+  renderSuggestion(hit) {
+    return <Highlight attribute="name" hit={hit} tagName="mark" />;
+  }
+
+  renderSectionTitle(section) {
+    return section.index;
+  }
+
+  getSectionSuggestions(section) {
+    return section.hits;
+  }
+
+  render() {
+    const { hits } = this.props;
+    const { value } = this.state;
+
+    const inputProps = {
+      placeholder: 'Search for a product...',
+      onChange: this.onChange,
+      value,
+    };
+
+    return (
+      <Autosuggest
+        suggestions={hits}
+        multiSection={true}
+        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+        getSuggestionValue={this.getSuggestionValue}
+        renderSuggestion={this.renderSuggestion}
+        inputProps={inputProps}
+        renderSectionTitle={this.renderSectionTitle}
+        getSectionSuggestions={this.getSectionSuggestions}
+      />
+    );
+  }
+}
+
+const AutoComplete = connectAutoComplete(Example);
 
 export default App;
