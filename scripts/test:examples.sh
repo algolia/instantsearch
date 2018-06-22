d2beeb3f1ca4d456903346620858a75d39cb8fb0
Samuel Vaillant <samuel.vllnt@gmail.com>
2018-06-22 13:35:39 +0200

    chore: enable matrix on Travis (algolia/react-instantsearch#1365)
    * chore: rename test:recipes for test:examples
    
    * chore: revamp scripts name
    
    * chore(travis): use matrix
    
    * chore: remove useless test.sh

--- scripts/test:examples.sh
+++ scripts/test:examples.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# This script is used to test our recipes before upgrading their deps or adding a feature
-
-# cause test to fail if one fails
 set -e
 
 for example in examples/* ; do
