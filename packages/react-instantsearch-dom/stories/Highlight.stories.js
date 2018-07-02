a7d51300ca7c21e7ec9cd9eade42b4bfd0d4132f
Samuel Vaillant <samuel.vllnt@gmail.com>
2018-07-02 15:18:05 +0200

    chore: drop story for multi highlight for now (#1405)

--- packages/react-instantsearch-dom/stories/Highlight.stories.js
+++ packages/react-instantsearch-dom/stories/Highlight.stories.js
@@ -1,17 +1,9 @@
+import React from 'react';
 import PropTypes from 'prop-types';
-import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react';
 import { text } from '@storybook/addon-knobs';
-import {
-  ClearRefinements,
-  Highlight,
-  Hits,
-  InstantSearch,
-  Pagination,
-  SearchBox,
-  connectHits,
-} from 'react-instantsearch-dom';
-import { displayName, filterProps, WrapWithHits } from './util';
+import { Highlight, Hits } from 'react-instantsearch-dom';
+import { WrapWithHits } from './util';
 
 const stories = storiesOf('Highlight', module);
 
@@ -64,47 +56,3 @@ stories
       <Hits hitComponent={StrongHits} />
     </WrapWithHits>
   ));
-
-const CustomHits = connectHits(({ hits }) => (
-  <div className="hits">
-    {hits.map(hit => (
-      <div key={hit.objectID} className="hit">
-        <div className="hit-content">
-          <div>
-            <Highlight attribute="name" hit={hit} />
-          </div>
-
-          <div className="hit-tags">
-            <Highlight attribute="tags" hit={hit} />
-          </div>
-        </div>
-      </div>
-    ))}
-  </div>
-));
-
-class AppWithArray extends Component {
-  render() {
-    return (
-      <InstantSearch
-        appId="KY4PR9ORUL"
-        apiKey="a5ca312adab3b79e14054154efa00b37"
-        indexName="highlight_array"
-      >
-        <SearchBox
-          translations={{
-            placeholder: 'Search into our furnitures: chair, table, tv unit...',
-          }}
-        />
-        <ClearRefinements translations={{ reset: 'Clear all filters' }} />
-        <CustomHits />
-        <Pagination />
-      </InstantSearch>
-    );
-  }
-}
-
-stories.add('highlight on  array', () => <AppWithArray />, {
-  displayName,
-  filterProps,
-});
