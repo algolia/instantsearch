2bf0ca4f6432633e4bce1bae27380a4908aaac7a
François Chalifour <francoischalifour@users.noreply.github.com>
2018-07-04 14:36:25 +0200

    refactor(tasks): Simplify teardown task for iOS (algolia/create-instantsearch-app#71)

--- src/tasks/ios/teardown.js
+++ src/tasks/ios/teardown.js
@@ -2,28 +2,19 @@ const chalk = require('chalk');
 
 module.exports = function teardown(config) {
   if (!config.silent) {
-    try {
-      console.log();
-      console.log(
-        `🎉  Created ${chalk.bold.cyan(config.name)} at ${chalk.green(
-          config.path
-        )}.`
-      );
-      console.log();
+    console.log();
+    console.log(
+      `🎉  Created ${chalk.bold.cyan(config.name)} at ${chalk.green(
+        config.path
+      )}.`
+    );
+    console.log();
 
-      console.log(
-        `Begin by opening the workspace \`${chalk.green('App.xcworkspace')}\`.`
-      );
-      console.log();
-      console.log('⚡️  Start building something awesome!');
-    } catch (err) {
-      console.log();
-      console.error(chalk.red('🛑  The app generation failed.'));
-      console.error(err);
-      console.log();
-
-      return Promise.reject(err);
-    }
+    console.log(
+      `Begin by opening the workspace \`${chalk.green('App.xcworkspace')}\`.`
+    );
+    console.log();
+    console.log('⚡️  Start building something awesome!');
   }
 
   return Promise.resolve();
