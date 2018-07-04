6d3f7c14c271dd8c461680ccc72c74c0f4819452
Alex S <bobylito@algolia.com>
2018-07-04 10:16:24 +0200

    fix(stats): default template should integrate sub spans

--- src/widgets/stats/defaultTemplates.js
+++ src/widgets/stats/defaultTemplates.js
@@ -2,5 +2,5 @@ export default {
   text: `{{#hasNoResults}}No results{{/hasNoResults}}
   {{#hasOneResult}}1 result{{/hasOneResult}}
   {{#hasManyResults}}{{#helpers.formatNumber}}{{nbHits}}{{/helpers.formatNumber}} results{{/hasManyResults}}
-  <span class="{{cssClasses.time}}">found in {{processingTimeMS}}ms</span>`,
+  found in {{processingTimeMS}}ms`,
 };
