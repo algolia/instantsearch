706ad655937bec8a6430d785508a79dac2954f1d
François Chalifour <francoischalifour@users.noreply.github.com>
2018-07-09 17:38:10 +0200

    feat(templates): Use `searchClient` in Angular (algolia/create-instantsearch-app#114)

--- src/templates/Angular InstantSearch/src/app/app.component.ts
+++ src/templates/Angular InstantSearch/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as algoliasearch from "algoliasearch";
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   config = {
-    appId: '{{appId}}',
-    apiKey: '{{apiKey}}',
     indexName: '{{indexName}}',
+    searchClient: algoliasearch('{{appId}}', '{{apiKey}}'),
   };
 }
