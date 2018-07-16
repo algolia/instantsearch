8419e9e84fb27288a29211228edf4ea3cd1e4410
François Chalifour <francoischalifour@users.noreply.github.com>
2018-07-16 16:11:51 +0200

    fix(templates): Fix failing default tests in Angular (algolia/create-instantsearch-app#138)
    * fix(templates): Fix failing default tests in Angular
    
    * test(templates): Update snapshots

--- src/templates/Angular InstantSearch/src/app/app.component.spec.ts
+++ src/templates/Angular InstantSearch/src/app/app.component.spec.ts
@@ -1,13 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
+import { NgAisModule } from 'angular-instantsearch';
 import { AppComponent } from './app.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        NgAisModule.forRoot(),
+      ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
