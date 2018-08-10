e0e86d50322063ff25186c57e60130568c24a81e
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-08-10 12:15:33 +0200

    chore: remove old stories & docs

--- packages/vue-instantsearch/stories/TreeMenu.stories.js
+++
@@ -1,8 +0,0 @@
-import { storiesOf } from '@storybook/vue';
-import { previewWrapper } from './utils';
-
-storiesOf('TreeMenu', module)
-  .addDecorator(previewWrapper())
-  .add('default', () => ({
-    template: `<ais-tree-menu :attributes="['category', 'sub_category']"></ais-tree-menu>'`,
-  }));
