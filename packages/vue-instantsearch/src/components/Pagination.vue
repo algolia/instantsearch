9d15d2f6bbdc74ee43884ef1fe7c72824f82381e
Haroen Viaene <hello@haroen.me>
2018-08-13 15:23:35 +0200

    chore(Pagination): rename internals

--- packages/vue-instantsearch/src/components/Pagination.vue
+++ packages/vue-instantsearch/src/components/Pagination.vue
@@ -1,6 +1,15 @@
 <template>
   <div :class="suit()" v-if="state">
-    <slot v-bind="state">
+    <slot
+      :refine="refine"
+      :createURL="state.createURL"
+      :currentRefinement="state.currentRefinement"
+      :nbHits="state.nbHits"
+      :nbPages="state.nbPages"
+      :pages="state.pages"
+      :isFirstPage="state.isFirstPage"
+      :isLastPage="state.isLastPage"
+    >
       <ul :class="suit('list')">
         <li
           :class="{
@@ -14,7 +23,7 @@
               :class="suit('link')"
               aria-label="First"
               :href="state.createURL(0)"
-              @click.prevent="goToPage(0)"
+              @click.prevent="refine(0)"
             >‹‹</a>
           </template>
           <template v-else>
@@ -33,7 +42,7 @@
               :class="suit('link')"
               aria-label="Previous"
               :href="state.createURL(state.currentRefinement - 1)"
-              @click.prevent="goToPage(state.currentRefinement - 1)"
+              @click.prevent="refine(state.currentRefinement - 1)"
             >‹</a>
           </template>
           <template v-else>
@@ -51,7 +60,7 @@
           <a
             :class="suit('link')"
             :href="state.createURL(page)"
-            @click.prevent="goToPage(page)"
+            @click.prevent="refine(page)"
           >
             {{page + 1}}
           </a>
@@ -69,7 +78,7 @@
               :class="suit('link')"
               aria-label="Next"
               :href="state.createURL(state.currentRefinement + 1)"
-              @click.prevent="goToPage(state.currentRefinement + 1)"
+              @click.prevent="refine(state.currentRefinement + 1)"
             >›</a>
           </template>
           <template v-else>
@@ -88,7 +97,7 @@
               :class="suit('link')"
               aria-label="Last"
               :href="state.createURL(state.nbPages - 1)"
-              @click.prevent="goToPage(state.nbPages - 1)"
+              @click.prevent="refine(state.nbPages - 1)"
             >››</a>
           </template>
           <template v-else>
@@ -154,9 +163,10 @@ export default {
     },
   },
   methods: {
-    goToPage(page) {
+    refine(page) {
       const p = Math.min(Math.max(page, 0), this.state.nbPages - 1);
       this.state.refine(p);
+      // @TODO: do this in a general way
       this.$emit('page-change', p);
     },
   },
