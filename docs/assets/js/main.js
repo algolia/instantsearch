26bbd8fd8cbb1535f80122e4845d6802b1b77307
Haroen Viaene <hello@haroen.me>
2018-08-31 14:02:39 -0400

    docs: remove unused code

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,8 +1,6 @@
 import sidebar from './sidebar.js';
 import dropdowns from './dropdowns.js';
-// import move from './mover.js';
 import activateClipboard from './activateClipboard.js';
-// import bindRunExamples from './bindRunExamples.js';
 import {fixSidebar, followSidebarNavigation} from './fix-sidebar.js';
 
 var alg = require('algolia-frontend-components/javascripts.js');
@@ -19,7 +17,6 @@ const container = document.querySelector('.documentation-container');
 const codeSamples = document.querySelectorAll('.code-sample');
 
 dropdowns();
-// move();
 activateClipboard(codeSamples);
 
 
