bbbb5b73bcacf558028e09d076e6e326b6951384
Jure Sotošek <jure.sotosek@gmail.com>
2018-09-15 16:18:32 +0200

    fix: edit this page button (algolia/react-instantsearch#1562)

--- docgen/assets/js/editThisPage.js
+++ docgen/assets/js/editThisPage.js
@@ -5,26 +5,34 @@ if (document.querySelector('.documentation-container')) {
 
   let href = 'https://github.com/algolia/react-instantsearch/edit/master/';
   const doc = 'docgen/src';
-  const api = 'packages/react-instantsearch/src';
+  const apiCore = 'packages/react-instantsearch-core/src';
+  const apiDom = 'packages/react-instantsearch-dom/src';
 
   let pathname = document.location.pathname.replace('/react-instantsearch', '');
 
-  if (/^\/(?:widgets|connectors)\/.+/.test(pathname)) {
-    href += `${api}${pathname.replace('.html', '.js')}`;
+  if (/^\/(?:widgets)\/.+/.test(pathname)) {
+    href += `${apiDom}${pathname.replace('.html', '.js')}`;
 
     const instantsearchEncoded = encodeURIComponent('<InstantSearch>');
     const indexEncoded = encodeURIComponent('<Index>');
 
     if (pathname === `/widgets/${instantsearchEncoded}.html`) {
       href = href.replace(
-        `/widgets/${instantsearchEncoded}`,
-        '/core/InstantSearch'
+        `/react-instantsearch-dom/src/widgets/${instantsearchEncoded}`,
+        '/react-instantsearch-core/src/components/InstantSearch'
       );
     }
 
     if (pathname === `/widgets/${indexEncoded}.html`) {
-      href = href.replace(`/widgets/${indexEncoded}`, '/core/Index');
+      href = href.replace(
+        `/react-instantsearch-dom/src/widgets/${indexEncoded}`,
+        '/react-instantsearch-core/src/components/Index'
+      );
     }
+  } else if (/^\/(?:connectors)\/.+/.test(pathname)) {
+    href += `${apiCore}${pathname.replace('.html', '.js')}`;
+  } else if (/^\/(?:server-side-renderings)\/.+/.test(pathname)) {
+    href += `${apiDom}/core/findResultsState.js`;
   } else {
     if (/\/$/.test(pathname)) pathname += 'index.html';
     href += `${doc}${pathname.replace('.html', '.md')}`;
