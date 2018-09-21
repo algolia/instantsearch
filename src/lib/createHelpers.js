870d4249344d8b7f9bdee12d67c2ff80e6be8e1a
François Chalifour <francois.chalifour@gmail.com>
2018-09-21 11:26:44 +0200

    feat(highlight): throw error if uncorrect format

--- src/lib/createHelpers.js
+++ src/lib/createHelpers.js
@@ -6,20 +6,20 @@ export default function({ numberLocale }) {
       return Number(render(number)).toLocaleString(numberLocale);
     },
     highlight(options, render) {
-      let highlightOptions;
-
       try {
-        highlightOptions = JSON.parse(options);
-      } catch (error) {
-        return render('');
-      }
+        const highlightOptions = JSON.parse(options);
 
-      return render(
-        highlight({
-          ...highlightOptions,
-          hit: this,
-        })
-      );
+        return render(
+          highlight({
+            ...highlightOptions,
+            hit: this,
+          })
+        );
+      } catch (error) {
+        throw new Error(`
+The highlight helper expects a JSON object of the format:
+{ "attribute": "name", "highlightedTagName": "mark" }`);
+      }
     },
   };
 }
