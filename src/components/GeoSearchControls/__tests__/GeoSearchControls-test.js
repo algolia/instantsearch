1867d306a0b59bfb7433ddf77161f7c516466d76
Samuel Vaillant <samuel.vllnt@gmail.com>
2018-09-25 15:19:58 +0200

    feat(GeoSearch): implement InstantSearch.css (#3138)
    * feat(Geo): update classes
    
    * feat(Geo): update template
    
    * docs(Geo): add migration
    
    * chore: update InstantSearch.css to 7.1.0
    
    * fix(geo): update key names
    
    * refactor(Geo): use object notation for cx

--- src/components/GeoSearchControls/__tests__/GeoSearchControls-test.js
+++ src/components/GeoSearchControls/__tests__/GeoSearchControls-test.js
@@ -5,11 +5,12 @@ import GeoSearchControls from '../GeoSearchControls';
 describe('GeoSearchControls', () => {
   const CSSClassesDefaultProps = {
     control: 'control',
-    toggleLabel: 'toggleLabel',
-    toggleLabelActive: 'toggleLabelActive',
-    toggleInput: 'toggleInput',
+    label: 'label',
+    selectedLabel: 'label-selected',
+    input: 'input',
     redo: 'redo',
-    clear: 'clear',
+    disabledRedo: 'redo-disabled',
+    reset: 'reset',
   };
 
   const defaultProps = {
@@ -213,7 +214,7 @@ describe('GeoSearchControls', () => {
       expect(wrapper).toMatchSnapshot();
     });
 
-    it('expect to call onClearClick when the clear button is clicked', () => {
+    it('expect to call onClearClick when the reset button is clicked', () => {
       const props = {
         ...defaultProps,
         enableRefineControl: false,
@@ -226,7 +227,7 @@ describe('GeoSearchControls', () => {
 
       expect(props.onClearClick).not.toHaveBeenCalled();
 
-      wrapper.find('.clear').simulate('click');
+      wrapper.find('.reset').simulate('click');
 
       expect(props.onClearClick).toHaveBeenCalled();
     });
