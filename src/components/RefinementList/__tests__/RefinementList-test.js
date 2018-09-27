886be210d0adf77d59bec148c81cff81ecf2215f
François Chalifour <francois.chalifour@gmail.com>
2018-09-27 10:32:49 +0200

    feat(refinement-list): apply new options, markup and css

--- src/components/RefinementList/__tests__/RefinementList-test.js
+++ src/components/RefinementList/__tests__/RefinementList-test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import RefinementList from '../RefinementList';
 import RefinementListItem from '../RefinementListItem';
+import renderer from 'react-test-renderer';
 
 const defaultProps = {
   templateProps: {},
