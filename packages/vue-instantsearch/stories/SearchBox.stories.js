9e73994ec1c1c630c18dd2f563fa20922dffac31
Haroen Viaene <hello@haroen.me>
2018-10-30 09:09:12 +0100

    docs(SearchBox): better prop ordering

--- packages/vue-instantsearch/stories/SearchBox.stories.js
+++ packages/vue-instantsearch/stories/SearchBox.stories.js
@@ -16,10 +16,10 @@ storiesOf('ais-search-box', module)
     template: `
       <ais-search-box autofocus>
         <input
-          placeholder="Custom SearchBox"
           slot-scope="{ currentRefinement, refine }"
           :value="currentRefinement"
           @input="refine($event.currentTarget.value)"
+          placeholder="Custom SearchBox"
         />
       </ais-search-box>
     `,
