24d46992594171fa75cfb1dce61d94b50d1718be
François Chalifour <francois.chalifour@gmail.com>
2018-11-12 14:27:01 +0100

    feat(es): update ES export
    The widget, connectors and helpers are now available at the root level:
    
    ```js
    import instantsearch, { searchbox } from 'instantsearch.js';
    
    const search = instantsearch();
    search.addWidget(
      searchbox()
    );
    
    search.start();
    ```

--- index.es6.js
+++ index.es6.js
@@ -1,39 +1,19 @@
-/* eslint max-len: 0 */
 import algoliasearchHelper from 'algoliasearch-helper';
 import toFactory from 'to-factory';
 
+// This file gets moved to the `src/` folder before being compiled with Babel,
+// hence the base path is `src/`.
 /* eslint-disable import/no-unresolved */
-import InstantSearch from './lib/InstantSearch.js';
-import version from './lib/version.js';
-import { snippet, highlight } from './helpers';
-/* eslint-enable import/no-unresolved */
 
-// import instantsearch from 'instantsearch.js';
-// -> provides instantsearch object without connectors and widgets
+import InstantSearch from './lib/InstantSearch';
+import version from './lib/version';
+
 const instantSearchFactory = toFactory(InstantSearch);
-
 instantSearchFactory.version = version;
 instantSearchFactory.createQueryString =
   algoliasearchHelper.url.getQueryStringFromState;
-instantSearchFactory.snippet = snippet;
-instantSearchFactory.highlight = highlight;
 
-Object.defineProperty(instantSearchFactory, 'widgets', {
-  get() {
-    throw new ReferenceError(
-      `You can't access 'instantsearch.widgets' directly from the ES6 build.
-Import the widgets this way: 'import {searchBox} from "instantsearch.js/es/widgets"'`
-    );
-  },
-});
-
-Object.defineProperty(instantSearchFactory, 'connectors', {
-  get() {
-    throw new ReferenceError(
-      `You can't access 'instantsearch.connectors' directly from the ES6 build.
-Import the connectors this way: 'import {connectSearchBox} from "instantsearch.js/es/connectors"'`
-    );
-  },
-});
-
+export * from './helpers';
+export * from './connectors';
+export * from './widgets';
 export default instantSearchFactory;
