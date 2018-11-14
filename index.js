31c116689f581faba6e2ce6f96f63902f41c8cf4
François Chalifour <francois.chalifour@gmail.com>
2018-11-14 14:29:59 +0100

    fix(cjs): fix import

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
-import instantsearch from './src/lib/main.js';
+// This file gets moved to the `src/` folder before being compiled with Babel,
+// hence the base path is `src/`.
+/* eslint-disable import/no-unresolved */
+import instantsearch from './lib/main.js';
+/* eslint-enable import/no-unresolved */
 
 export default instantsearch;
