3ba438a395119c649694c4befb9e0f6a138a990c
François Chalifour <francois.chalifour@gmail.com>
2018-11-14 14:30:40 +0100

    feat(es): add warning when accessing widgets and connectors

--- index.es6.js
+++ index.es6.js
@@ -13,6 +13,29 @@ instantSearchFactory.version = version;
 instantSearchFactory.createQueryString =
   algoliasearchHelper.url.getQueryStringFromState;
 
+Object.defineProperty(instantSearchFactory, 'widgets', {
+  get() {
+    throw new ReferenceError(
+      `"instantsearch.widgets" are not available from the ES build.
+
+To import the widgets:
+
+import { searchBox } from 'instantsearch.js'`
+    );
+  },
+});
+Object.defineProperty(instantSearchFactory, 'connectors', {
+  get() {
+    throw new ReferenceError(
+      `"instantsearch.connectors" are not available from the ES build.
+
+To import the connectors:
+
+import { connectSearchBox } from 'instantsearch.js'`
+    );
+  },
+});
+
 export * from './helpers';
 export * from './connectors';
 export * from './widgets';
