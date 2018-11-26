5e367e1a7dd5666cba7fd2f46fdd63da96326c4e
Haroen Viaene <hello@haroen.me>
2018-11-26 13:13:05 +0100

    test: remove createQueryString

--- src/lib/__tests__/main-test.js
+++ src/lib/__tests__/main-test.js
@@ -10,21 +10,6 @@ describe('instantsearch()', () => {
     );
   });
 
-  it('statically creates a URL', () => {
-    expect(instantsearch.createQueryString({ hitsPerPage: 42 })).toEqual(
-      'hPP=42'
-    );
-  });
-
-  it('statically creates a complex URL', () => {
-    expect(
-      instantsearch.createQueryString({
-        hitsPerPage: 42,
-        facetsRefinements: { category: 'Home' },
-      })
-    ).toEqual('hPP=42&fR[category]=Home');
-  });
-
   it('includes the widget functions', () => {
     forEach(instantsearch.widgets, widget => {
       expect(typeof widget).toEqual('function', 'A widget must be a function');
