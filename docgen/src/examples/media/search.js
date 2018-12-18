dc6ce391f7f129899d03ebf3d9ba3b5905595cea
François Chalifour <francoischalifour@users.noreply.github.com>
2018-12-18 17:46:45 +0100

    chore(examples): migrate Media example (#3352)
    This uses the latest beta version for now (https://cdn.jsdelivr.net/npm/instantsearch.js@beta).

--- docgen/src/examples/media/search.js
+++ docgen/src/examples/media/search.js
@@ -3,30 +3,38 @@
 var search = instantsearch({
   indexName: 'movies',
   searchClient: algoliasearch('latency', '6be0576ff61c053d5f9a3225e2a90f76'),
-  routing: true
+  routing: true,
 });
 
 search.addWidget(
   instantsearch.widgets.searchBox({
-    container: '#q'
+    container: '#searchbox',
+    showReset: false,
+    cssClasses: {
+      root: 'root',
+      form: 'form',
+      input: 'input form-control',
+      submit: 'btn btn-default',
+      reset: 'btn btn-default',
+    },
   })
 );
 
 search.addWidget(
   instantsearch.widgets.stats({
-    container: '#stats'
+    container: '#stats',
   })
 );
 
 var hitTemplate =
   '<div class="hit media">' +
-    '<div class="media-left">' +
-      '<div class="media-object" style="background-image: url(\'{{image}}\');"></div>' +
-    '</div>' +
-    '<div class="media-body">' +
-      '<h4 class="media-heading">{{{_highlightResult.title.value}}} {{#stars}}<span class="ais-star-rating--star{{^.}}__empty{{/.}}"></span>{{/stars}}</h4>' +
-      '<p class="year">{{year}}</p><p class="genre">{{#genre}}<span class="badge">{{.}}</span> {{/genre}}</p>' +
-    '</div>' +
+  '<div class="media-left">' +
+  '<div class="media-object" style="background-image: url(\'{{image}}\');"></div>' +
+  '</div>' +
+  '<div class="media-body">' +
+  '<h4 class="media-heading">{{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}} {{#stars}}<svg class="starIcon ais-RatingMenu-starIcon ais-RatingMenu-starIcon{{#.}}--full{{/.}}{{^.}}--empty{{/.}}" aria-hidden="true" width="18" height="18"><use xlink:href="#ais-RatingMenu-{{#.}}starSymbol{{/.}}{{^.}}starEmptySymbol{{/.}}"></use></svg>{{/stars}}</h4>' +
+  '<p class="year">{{year}}</p><p class="genre">{{#genre}}<span class="badge">{{.}}</span> {{/genre}}</p>' +
+  '</div>' +
   '</div>';
 
 var noResultsTemplate =
@@ -38,15 +46,71 @@ search.addWidget(
     hitsPerPage: 10,
     templates: {
       empty: noResultsTemplate,
-      item: hitTemplate
+      item: hitTemplate,
     },
-    transformData: function(hit) {
-      hit.stars = [];
-      for (var i = 1; i <= 5; ++i) {
-        hit.stars.push(i <= hit.rating);
-      }
-      return hit;
-    }
+    transformItems: function(items) {
+      return items.map(function(item) {
+        item.stars = [];
+
+        for (var i = 1; i <= 5; ++i) {
+          item.stars.push(i <= item.rating);
+        }
+
+        return item;
+      });
+    },
+    cssClasses: {
+      list: 'list',
+    },
+  })
+);
+
+const genreList = instantsearch.widgets.panel({
+  templates: {
+    header: '<i class="fa fa-chevron-right"></i> Genres',
+  },
+  cssClasses: {
+    header: 'panel-header',
+  },
+})(instantsearch.widgets.refinementList);
+
+search.addWidget(
+  genreList({
+    container: '#genres',
+    attribute: 'genre',
+    operator: 'and',
+    limit: 10,
+    cssClasses: {
+      list: 'nav nav-list',
+      count: 'badge pull-right',
+      selectedItem: 'active',
+      item: 'item',
+    },
+  })
+);
+
+// TODO: use panel once https://github.com/algolia/instantsearch.js/pull/3347 is merged
+// const ratingList = instantsearch.widgets.panel({
+//   templates: {
+//     header: '<i class="fa fa-chevron-right"></i> Ratings',
+//   },
+//   cssClasses: {
+//     header: 'panel-header',
+//   },
+// })(instantsearch.widgets.ratingMenu);
+
+search.addWidget(
+  instantsearch.widgets.ratingMenu({
+    container: '#ratings',
+    attribute: 'rating',
+    cssClasses: {
+      list: 'nav',
+      link: 'link',
+      item: 'item',
+      selectedItem: 'selectedItem',
+      starIcon: 'starIcon',
+      count: 'badge pull-right',
+    },
   })
 );
 
@@ -54,34 +118,11 @@ search.addWidget(
   instantsearch.widgets.pagination({
     container: '#pagination',
     cssClasses: {
-      root: 'pagination',
-      active: 'active'
-    }
-  })
-);
-
-search.addWidget(
-  instantsearch.widgets.refinementList({
-    container: '#genres',
-    attributeName: 'genre',
-    operator: 'and',
-    limit: 10,
-    cssClasses: {
-      list: 'nav nav-list',
-      count: 'badge pull-right',
-      active: 'active'
-    }
-  })
-);
-
-search.addWidget(
-  instantsearch.widgets.starRating({
-    container: '#ratings',
-    attributeName: 'rating',
-    cssClasses: {
-      list: 'nav',
-      count: 'badge pull-right'
-    }
+      root: 'root',
+      list: 'pagination',
+      disabledItem: 'disabledItem',
+      selectedItem: 'selectedItem',
+    },
   })
 );
 
