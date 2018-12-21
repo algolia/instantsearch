465371d0ce7100513c0a53d02cc62fca9ee6953f
francoischalifour <francois.chalifour@gmail.com>
2018-12-21 11:56:52 +0100

    chore(stories): update stories

--- stories/menu-select.stories.js
+++ stories/menu-select.stories.js
@@ -1,7 +1,7 @@
 import { storiesOf } from '@storybook/html';
 import { withHits } from '../.storybook/decorators';
 
-storiesOf('Menu-select', module)
+storiesOf('MenuSelect', module)
   .add(
     'default',
     withHits(({ search, container, instantsearch }) => {
@@ -9,28 +9,6 @@ storiesOf('Menu-select', module)
         instantsearch.widgets.menuSelect({
           container,
           attribute: 'categories',
-        })
-      );
-    })
-  )
-  .add(
-    'with show more and header',
-    withHits(({ search, container, instantsearch }) => {
-      search.addWidget(
-        instantsearch.widgets.menuSelect({
-          container,
-          attribute: 'categories',
-          limit: 3,
-          showMore: {
-            templates: {
-              active: '<button>Show less</button>',
-              inactive: '<button>Show more</button>',
-            },
-            limit: 10,
-          },
-          templates: {
-            header: 'Categories (menu widget)',
-          },
         })
       );
     })
@@ -44,8 +22,22 @@ storiesOf('Menu-select', module)
           attribute: 'categories',
           limit: 10,
           templates: {
-            header: 'Categories (menu widget)',
             item: '{{label}}',
+          },
+        })
+      );
+    })
+  )
+  .add(
+    'with custom default option template',
+    withHits(({ search, container, instantsearch }) => {
+      search.addWidget(
+        instantsearch.widgets.menuSelect({
+          container,
+          attribute: 'categories',
+          limit: 10,
+          templates: {
+            defaultOption: 'Default choice',
           },
         })
       );
--- stories/panel.stories.js
+++ stories/panel.stories.js
@@ -1,21 +1,58 @@
 import { storiesOf } from '@storybook/html';
 import { withHits } from '../.storybook/decorators';
 
-storiesOf('Panel', module).add(
-  'with default',
-  withHits(({ search, container, instantsearch }) => {
-    search.addWidget(
-      instantsearch.widgets.panel({
-        templates: {
-          header: ({ results }) =>
-            `Header ${results ? `| ${results.nbHits} results` : ''}`,
-          footer: 'Footer',
-        },
-        hidden: ({ results }) => results.nbHits === 0,
-      })(instantsearch.widgets.refinementList)({
-        container,
-        attribute: 'brand',
-      })
-    );
-  })
-);
+storiesOf('Panel', module)
+  .add(
+    'with default',
+    withHits(({ search, container, instantsearch }) => {
+      search.addWidget(
+        instantsearch.widgets.panel({
+          templates: {
+            header: ({ results }) =>
+              `Header ${results ? `| ${results.nbHits} results` : ''}`,
+            footer: 'Footer',
+          },
+          hidden: ({ results }) => results.nbHits === 0,
+        })(instantsearch.widgets.refinementList)({
+          container,
+          attribute: 'brand',
+        })
+      );
+    })
+  )
+  .add(
+    'with ratingMenu',
+    withHits(({ search, container, instantsearch }) => {
+      search.addWidget(
+        instantsearch.widgets.panel({
+          templates: {
+            header: ({ results }) =>
+              `Header ${results ? `| ${results.nbHits} results` : ''}`,
+            footer: 'Footer',
+          },
+          hidden: ({ results }) => results.nbHits === 0,
+        })(instantsearch.widgets.ratingMenu)({
+          container,
+          attribute: 'price',
+        })
+      );
+    })
+  )
+  .add(
+    'with menu',
+    withHits(({ search, container, instantsearch }) => {
+      search.addWidget(
+        instantsearch.widgets.panel({
+          templates: {
+            header: ({ results }) =>
+              `Header ${results ? `| ${results.nbHits} results` : ''}`,
+            footer: 'Footer',
+          },
+          hidden: ({ results }) => results.nbHits === 0,
+        })(instantsearch.widgets.menu)({
+          container,
+          attribute: 'brand',
+        })
+      );
+    })
+  );
