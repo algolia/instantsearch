4e4d334725f5aa0fbd4c4d1a6efadd22ca6f4a6b
François Chalifour <francois.chalifour@gmail.com>
2019-01-18 17:01:51 +0100

    chore(jest): setup TypeScript

--- scripts/jestInit.js
+++ scripts/jestInit.js
@@ -1,6 +1,6 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-Enzyme.configure({ adapter: new Adapter() });
-
 import { createSerializer } from 'enzyme-to-json';
+
+Enzyme.configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
