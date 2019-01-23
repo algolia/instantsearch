f7dc2d41971bcd07220d27aea5f9cb30c8778b4f
François Chalifour <francoischalifour@users.noreply.github.com>
2019-01-23 13:44:31 +0100

    chore(typescript): setup TSLint (part 2) (#3412)
    * chore(typescript): setup TSLint
    
    * chore(typescript): setup Storybook (part 3) (#3413)
    
    * chore(typescript): setup Rollup and Babel builds (part 4) (#3414)
    
    * refactor(PoweredBy): migrate to TypeScript (part 5) (#3417)

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,16 @@ module.exports = {
     'import/no-extraneous-dependencies': 0,
     'react/no-string-refs': 1,
   },
+  settings: {
+    'import/resolver': {
+      node: {
+        // The migration is an incremental process so we import TypeScript modules
+        // from JavaScript files.
+        // By default, `import/resolver` only supports JavaScript modules.
+        extensions: ['.js', '.ts', '.tsx'],
+      },
+    },
+  },
   globals: {
     __DEV__: false,
   },
