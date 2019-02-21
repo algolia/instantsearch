61229c38b71404880ef43683858a9360d4e8708d
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-02-21 12:21:31 +0100

    wip(stories): enable add/remove controlled

--- stories/index.stories.ts
+++ stories/index.stories.ts
@@ -5,84 +5,115 @@ import index from '../src/widgets/index/index';
 storiesOf('Index', module).add(
   'with two indices',
   withHits(({ search, container, instantsearch }) => {
-    const instantSearchTitle = document.createElement('h2');
-    instantSearchTitle.innerHTML = '<code>instant_search</code>';
-    const instantSearchSearchBox = document.createElement('div');
-    instantSearchSearchBox.style.marginBottom = '15px';
-    const instantSearchHits = document.createElement('div');
+    const $buttonAddWidgets = document.createElement('button');
+    $buttonAddWidgets.textContent = 'Add widgets';
+    $buttonAddWidgets.style.marginRight = '10px';
 
-    const bestbuyTitle = document.createElement('h2');
-    bestbuyTitle.innerHTML = '<code>bestbuy</code>';
-    const bestbuySearchBox = document.createElement('div');
-    bestbuySearchBox.style.marginBottom = '15px';
-    const bestbuyHits = document.createElement('div');
+    const $buttonRemoveidgets = document.createElement('button');
+    $buttonRemoveidgets.textContent = 'Remove widgets';
 
-    container.appendChild(instantSearchTitle);
-    container.appendChild(instantSearchSearchBox);
-    container.appendChild(instantSearchHits);
+    const $instantSearchTitle = document.createElement('h2');
+    $instantSearchTitle.innerHTML = '<code>instant_search</code>';
+    const $instantSearchSearchBox = document.createElement('div');
+    $instantSearchSearchBox.style.marginBottom = '15px';
+    const $instantSearchHits = document.createElement('div');
 
-    container.appendChild(bestbuyTitle);
-    container.appendChild(bestbuySearchBox);
-    container.appendChild(bestbuyHits);
+    const $bestbuyTitle = document.createElement('h2');
+    $bestbuyTitle.innerHTML = '<code>bestbuy</code>';
+    const $bestbuySearchBox = document.createElement('div');
+    $bestbuySearchBox.style.marginBottom = '15px';
+    const $bestbuyHits = document.createElement('div');
 
+    container.appendChild($buttonAddWidgets);
+    container.appendChild($buttonRemoveidgets);
+
+    container.appendChild($instantSearchTitle);
+    container.appendChild($instantSearchSearchBox);
+    container.appendChild($instantSearchHits);
+
+    container.appendChild($bestbuyTitle);
+    container.appendChild($bestbuySearchBox);
+    container.appendChild($bestbuyHits);
+
+    // Top level
+    const topLevelConfigure = instantsearch.widgets.configure({
+      hitsPerPage: 3,
+    });
+
+    // InstantSearch
     const instantSearchIndex = index({
       indexName: 'instant_search',
     });
 
+    const instantSearchConfigure = instantsearch.widgets.configure({
+      hitsPerPage: 2,
+    });
+
+    const instantSearchSearchBox = instantsearch.widgets.searchBox({
+      container: $instantSearchSearchBox,
+    });
+
+    const instantSearchSearchHits = instantsearch.widgets.hits({
+      container: $instantSearchHits,
+      templates: {
+        item:
+          '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
+      },
+    });
+
+    instantSearchIndex.addWidgets([
+      instantSearchConfigure,
+      instantSearchSearchBox,
+    ]);
+
+    // BestBuy
     const bestbuyIndex = index({
       indexName: 'bestbuy',
     });
 
+    const bestbuyConfigure = instantsearch.widgets.configure({
+      hitsPerPage: 1,
+    });
+
+    const bestbuySearchBox = instantsearch.widgets.searchBox({
+      container: $bestbuySearchBox,
+    });
+
+    const bestbuyHits = instantsearch.widgets.hits({
+      container: $bestbuyHits,
+      escapeHTML: true,
+      templates: {
+        item:
+          '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
+      },
+    });
+
+    bestbuyIndex.addWidgets([bestbuyConfigure]);
+
+    // Search
     search.addWidgets([
-      instantsearch.widgets.configure({
-        hitsPerPage: 3,
-      }),
+      topLevelConfigure,
 
-      instantSearchIndex.addWidgets([
-        instantsearch.widgets.configure({
-          hitsPerPage: 2,
-        }),
-
-        instantsearch.widgets.searchBox({
-          container: instantSearchSearchBox,
-        }),
-
-        instantsearch.widgets.hits({
-          container: instantSearchHits,
-          templates: {
-            item:
-              '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
-          },
-        }),
-
-        bestbuyIndex.addWidgets([
-          instantsearch.widgets.configure({
-            hitsPerPage: 1,
-          }),
-
-          instantsearch.widgets.searchBox({
-            container: bestbuySearchBox,
-          }),
-
-          instantsearch.widgets.hits({
-            container: bestbuyHits,
-            templates: {
-              item:
-                '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
-            },
-          }),
-        ]),
-      ]),
+      instantSearchIndex.addWidgets([instantSearchSearchHits]),
     ]);
 
-    // bestbuyIndex.addWidgets([
-    //   instantsearch.widgets.hits({
-    //     container: bestbuyHits,
-    //     templates: {
-    //       item:
-    //         '{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}',
-    //     },
-    //   }),
-    // ]);
+    // Add widgets dynamically
+    $buttonAddWidgets.addEventListener('click', () => {
+      instantSearchIndex.addWidgets([
+        bestbuyIndex.addWidgets([bestbuySearchBox, bestbuyHits]),
+      ]);
+    });
+
+    // Remove widgets dynamically
+    $buttonRemoveidgets.addEventListener('click', () => {
+      // remove a top level widget
+      search.removeWidgets([topLevelConfigure]);
+
+      // remove a widget of an index
+      // instantSearchIndex.removeWidgets([instantSearchConfigure]);
+
+      // remove an index
+      // remove bestbuyIndex -> drop bestby queries
+    });
   })
 );
