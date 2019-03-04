f1b0f0dd6037a93829d072b09dacd3e1ed003269
François Chalifour <francois.chalifour@gmail.com>
2019-03-04 15:42:36 +0100

    chore(jest): move Jest config

--- scripts/jest/config.js
+++ scripts/jest/config.js
@@ -1,12 +1,13 @@
 /* eslint-disable import/no-commonjs */
 
 module.exports = {
+  rootDir: process.cwd(),
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
     '<rootDir>/dist*',
     '<rootDir>/functional-tests',
   ],
-  setupFilesAfterEnv: ['./scripts/jest/init.js'],
+  setupFilesAfterEnv: ['./scripts/jest/setupTests.js'],
   watchPlugins: [
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
