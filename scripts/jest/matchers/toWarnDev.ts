adebe3853f0115e3c337658666a3f582900df42f
François Chalifour <francoischalifour@users.noreply.github.com>
2019-03-04 17:27:44 +0100

    test: add toWarnDev custom Jest matcher (#3574)

---
+++ scripts/jest/matchers/toWarnDev.ts
@@ -0,0 +1,64 @@
+/* tslint:disable:no-console */
+import jestDiff from 'jest-diff';
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toWarnDev(expectedMessage?: string): R;
+    }
+  }
+}
+
+const matcher = {
+  toWarnDev: (callback: () => void, expectedMessage: string) => {
+    if (expectedMessage !== undefined && typeof expectedMessage !== 'string') {
+      throw new Error(
+        `toWarnDev() requires a parameter of type string but was given ${typeof expectedMessage}.`
+      );
+    }
+
+    if (!__DEV__) {
+      callback();
+
+      return { pass: true };
+    }
+
+    const originalWarnMethod = console.warn;
+    let calledTimes = 0;
+    let actualWarning = '';
+
+    console.warn = (message: string) => {
+      calledTimes++;
+      actualWarning = message;
+    };
+
+    callback();
+
+    console.warn = originalWarnMethod;
+
+    // Expectation without any message.
+    // We only check that `console.warn` was called.
+    if (expectedMessage === undefined && calledTimes === 0) {
+      return {
+        pass: false,
+        message: () => 'No warning recorded.',
+      };
+    }
+
+    // Expectation with a message.
+    if (expectedMessage !== undefined && actualWarning !== expectedMessage) {
+      return {
+        pass: false,
+        message: () => `Unexpected warning recorded.
+
+Difference:
+
+${jestDiff(expectedMessage, actualWarning)}`,
+      };
+    }
+
+    return { pass: true };
+  },
+};
+
+export default matcher;
