5a33dc4d8fed147c79a0ee4702931bfdb973d991
François Chalifour <francois.chalifour@gmail.com>
2019-03-07 10:50:51 +0100

    fix(panel): update stories

--- stories/panel.stories.js
+++ stories/panel.stories.js
@@ -5,15 +5,17 @@ storiesOf('Panel', module)
   .add(
     'default',
     withHits(({ search, container, instantsearch }) => {
-      search.addWidget(
-        instantsearch.widgets.panel({
-          templates: {
-            header: ({ results }) =>
-              `Header ${results ? `| ${results.nbHits} results` : ''}`,
-            footer: 'Footer',
+      const brandList = instantsearch.widgets.panel({
+        templates: {
+          header: 'Brand',
+          footer: ({ results }) => {
+            return results ? `${results.nbHits} results` : '';
           },
-          hidden: ({ results }) => results.nbHits === 0,
-        })(instantsearch.widgets.refinementList)({
+        },
+      })(instantsearch.widgets.refinementList);
+
+      search.addWidget(
+        brandList({
           container,
           attribute: 'brand',
         })
@@ -23,14 +25,15 @@ storiesOf('Panel', module)
   .add(
     'with range input',
     withHits(({ search, container, instantsearch }) => {
+      const priceList = instantsearch.widgets.panel({
+        templates: {
+          header: 'Price',
+          footer: 'Footer',
+        },
+      })(instantsearch.widgets.rangeInput);
+
       search.addWidget(
-        instantsearch.widgets.panel({
-          templates: {
-            header: 'Price',
-            footer: 'Footer',
-          },
-          hidden: ({ results }) => results.nbHits === 0,
-        })(instantsearch.widgets.rangeInput)({
+        priceList({
           container,
           attribute: 'price',
         })
@@ -40,14 +43,15 @@ storiesOf('Panel', module)
   .add(
     'with range slider',
     withHits(({ search, container, instantsearch }) => {
+      const priceSlider = instantsearch.widgets.panel({
+        templates: {
+          header: 'Price',
+          footer: 'Footer',
+        },
+      })(instantsearch.widgets.rangeSlider);
+
       search.addWidget(
-        instantsearch.widgets.panel({
-          templates: {
-            header: 'Price',
-            footer: 'Footer',
-          },
-          hidden: ({ results }) => results.nbHits === 0,
-        })(instantsearch.widgets.rangeSlider)({
+        priceSlider({
           container,
           attribute: 'price',
           tooltips: {
@@ -59,19 +63,41 @@ storiesOf('Panel', module)
       );
     })
   )
+
+  .add(
+    'with hidden',
+    withHits(({ search, container, instantsearch }) => {
+      const priceList = instantsearch.widgets.panel({
+        templates: {
+          header: 'Price',
+          footer: 'Footer',
+        },
+        hidden: ({ results }) => results.nbHits === 0,
+      })(instantsearch.widgets.rangeInput);
+
+      search.addWidget(
+        priceList({
+          container,
+          attribute: 'price',
+        })
+      );
+    })
+  )
   .add(
     'with collapsed',
     withHits(({ search, container, instantsearch }) => {
+      const brandList = instantsearch.widgets.panel({
+        collapsed: options => {
+          return options && options.state && options.state.query.length === 0;
+        },
+        templates: {
+          header: 'Brand (collapsible)',
+          footer: 'The panel collapses on empty query until controlled',
+        },
+      })(instantsearch.widgets.refinementList);
+
       search.addWidget(
-        instantsearch.widgets.panel({
-          collapsed: options => {
-            return options && options.state && options.state.query.length === 0;
-          },
-          templates: {
-            header: 'Collapsible panel',
-            footer: 'Footer',
-          },
-        })(instantsearch.widgets.refinementList)({
+        brandList({
           container,
           attribute: 'brand',
         })
@@ -79,20 +105,21 @@ storiesOf('Panel', module)
     })
   )
   .add(
-    'with collapsed and templates',
+    'with collapsed and custom templates',
     withHits(({ search, container, instantsearch }) => {
+      const brandList = instantsearch.widgets.panel({
+        collapsed: options => {
+          return options && options.state && options.state.query.length === 0;
+        },
+        templates: {
+          header: 'Collapsible panel',
+          footer: 'The panel collapses on empty query until controlled',
+          collapseButtonText: ({ collapsed }) => (collapsed ? 'More' : 'Less'),
+        },
+      })(instantsearch.widgets.refinementList);
+
       search.addWidget(
-        instantsearch.widgets.panel({
-          collapsed: options => {
-            return options && options.state && options.state.query.length === 0;
-          },
-          templates: {
-            header: 'Collapsible panel',
-            footer: 'Footer',
-            collapseButtonText: ({ collapsed }) =>
-              collapsed ? 'More' : 'Less',
-          },
-        })(instantsearch.widgets.refinementList)({
+        brandList({
           container,
           attribute: 'brand',
         })
