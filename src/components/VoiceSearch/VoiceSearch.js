75597c74541672b48adc70015d3ee1f63a3841b7
Eunjae Lee <eunjae.lee@algolia.com>
2019-03-18 10:26:05 +0100

    feat(voiceSearch): move WebSpeech-related code to a separate file under lib

--- src/components/VoiceSearch/VoiceSearch.js
+++ src/components/VoiceSearch/VoiceSearch.js
@@ -2,23 +2,7 @@ import React, { Component } from 'preact-compat';
 import PropTypes from 'prop-types';
 import noop from 'lodash/noop';
 import Template from '../Template/Template';
-
-const STATUS_INITIAL = 'initial';
-const STATUS_ASKING_PERMISSION = 'askingPermission';
-const STATUS_WAITING_FOR_AUDIO = 'waitingForAudio';
-const STATUS_WAITING_FOR_VOICE = 'waitingForVoice';
-const STATUS_GETTING_INPUTS = 'gettingInputs';
-const STATUS_FINISHED = 'finished';
-
-// https://w3c.github.io/speech-api/#speechreco-error
-// const ERROR_NO_SPEECH = 'no-speech';
-// const ERROR_ABORTED = 'aborted';
-// const ERROR_AUDIO_CAPTURE = 'audio-capture';
-// const ERROR_NETWORK = 'network';
-// const ERROR_NOT_ALLOWED = 'not-allowed';
-// const ERROR_SERVICE_NOT_ALLOWED = 'service-not-allowed';
-// const ERROR_BAD_GRAMMAR = 'bad-grammar';
-// const ERROR_LANGUAGE_NOT_SUPPORTED = 'language-not-supported';
+import voiceSearchHelper from '../../lib/voiceSearchHelper';
 
 const VoiceSearchCSSClasses = PropTypes.shape({
   root: PropTypes.string.isRequired,
@@ -45,91 +29,33 @@ class VoiceSearch extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
-    this.resetState();
+    this.helper = voiceSearchHelper({
+      onQueryChange: query => this.props.refine(query),
+      onStateChange: state => this.setState(state),
+    });
     window.SpeechRecognition =
       window.webkitSpeechRecognition || window.SpeechRecognition;
     this.isSupportedBrowser = 'SpeechRecognition' in window;
   }
 
-  resetState() {
-    this.setState({
-      status: STATUS_INITIAL,
-      transcript: '',
-      isSpeechFinal: undefined,
-      errorCode: undefined,
-    });
-  }
-
-  stopRecognition() {
-    if (this.recognition) {
-      this.recognition.stop();
-      this.recognition = undefined;
-    }
-  }
-
-  canStartRecognition() {
-    return (
-      this.state.status === STATUS_INITIAL ||
-      this.state.status === STATUS_FINISHED
-    );
-  }
-
-  startRecognition() {
-    this.setState({
-      status: STATUS_ASKING_PERMISSION,
-    });
-    this.recognition = new window.SpeechRecognition();
-    // this.recognition.continuous = true;
-    this.recognition.interimResults = this.props.searchAsYouSpeak;
-    this.recognition.onend = () => {
-      if (
-        !this.state.errorCode &&
-        this.state.transcript &&
-        !this.props.searchAsYouSpeak
-      ) {
-        this.props.refine(this.state.transcript);
-      }
-      this.setState({ status: STATUS_FINISHED });
-    };
-    this.recognition.onerror = e => {
-      this.setState({ errorCode: e.error });
-    };
-    this.recognition.onresult = e => {
-      this.setState({
-        status: STATUS_GETTING_INPUTS,
-        transcript: e.results[0][0].transcript,
-        isSpeechFinal: e.results[0].isFinal,
-      });
-      this.props.refine(this.state.transcript);
-    };
-    this.recognition.onsoundstart = () => {
-      this.setState({ status: STATUS_WAITING_FOR_VOICE });
-    };
-    this.recognition.onstart = () => {
-      this.setState({
-        status: STATUS_WAITING_FOR_AUDIO,
-      });
-    };
-    this.recognition.start();
-  }
-
   handleClick() {
     this.button.blur();
-    if (this.canStartRecognition()) {
-      this.resetState();
-      this.startRecognition();
+    const { canStart, start, stop } = this.helper;
+    const { searchAsYouSpeak } = this.props;
+    if (canStart()) {
+      start(searchAsYouSpeak);
     } else {
-      this.stopRecognition();
-      this.resetState();
+      stop();
     }
   }
 
   render() {
     const { cssClasses, templates } = this.props;
     const { status, transcript, isSpeechFinal, errorCode } = this.state;
-    const isListening = status !== STATUS_INITIAL && status !== STATUS_FINISHED;
+    const { isSupportedBrowser } = this.helper;
+    const isListening = this.helper.isListening();
     return (
-      this.isSupportedBrowser && (
+      isSupportedBrowser() && (
         <div className={cssClasses.root}>
           <Template
             ref={buttonRef => (this.button = buttonRef.base)}
---
+++ src/lib/voiceSearchHelper/index.js
@@ -0,0 +1,87 @@
+const STATUS_INITIAL = 'initial';
+const STATUS_ASKING_PERMISSION = 'askingPermission';
+const STATUS_WAITING_FOR_AUDIO = 'waitingForAudio';
+const STATUS_WAITING_FOR_VOICE = 'waitingForVoice';
+const STATUS_GETTING_INPUTS = 'gettingInputs';
+const STATUS_FINISHED = 'finished';
+
+export default function voiceSearchHelper({ onQueryChange, onStateChange }) {
+  window.SpeechRecognition =
+    window.webkitSpeechRecognition || window.SpeechRecognition;
+  let state = {};
+  let recognition;
+
+  const isSupportedBrowser = () => 'SpeechRecognition' in window;
+
+  const isListening = () =>
+    state.status !== STATUS_INITIAL && state.status !== STATUS_FINISHED;
+
+  const setState = (newState = {}) => {
+    state = { ...state, ...newState };
+    onStateChange(state);
+  };
+
+  const resetState = (status = STATUS_INITIAL) => {
+    setState({
+      status,
+      transcript: '',
+      isSpeechFinal: undefined,
+      errorCode: undefined,
+    });
+  };
+
+  const stop = () => {
+    if (recognition) {
+      recognition.stop();
+      recognition = undefined;
+    }
+    resetState();
+  };
+
+  const canStart = () =>
+    state.status === STATUS_INITIAL || state.status === STATUS_FINISHED;
+
+  const start = searchAsYouSpeak => {
+    resetState(STATUS_ASKING_PERMISSION);
+    recognition = new window.SpeechRecognition();
+    // recognition.continuous = true;
+    recognition.interimResults = searchAsYouSpeak;
+    recognition.onend = () => {
+      if (!state.errorCode && state.transcript && !searchAsYouSpeak) {
+        onQueryChange(this.state.transcript);
+      }
+      setState({ status: STATUS_FINISHED });
+    };
+    recognition.onerror = e => {
+      setState({ errorCode: e.error });
+    };
+    recognition.onresult = e => {
+      setState({
+        status: STATUS_GETTING_INPUTS,
+        transcript: e.results[0][0].transcript,
+        isSpeechFinal: e.results[0].isFinal,
+      });
+      if (searchAsYouSpeak) {
+        onQueryChange(state.transcript);
+      }
+    };
+    recognition.onsoundstart = () => {
+      setState({ status: STATUS_WAITING_FOR_VOICE });
+    };
+    recognition.onstart = () => {
+      setState({
+        status: STATUS_WAITING_FOR_AUDIO,
+      });
+    };
+    recognition.start();
+  };
+
+  resetState();
+  return {
+    isSupportedBrowser,
+    isListening,
+    canStart,
+    start,
+    stop,
+  };
+}
