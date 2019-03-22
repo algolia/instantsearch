c8d83900b9e425cfccf1a3b6364a3f87d89434d1
François Chalifour <francoischalifour@users.noreply.github.com>
2019-03-22 15:41:12 +0100

    feat(queryRules): add queryRuleCustomData widget (#3600)
    * chore(storybook): add support for playgrounds (#3599)
    
    * feat(queryRules): add queryRuleCustomData widget (#3600)

--- src/widgets/index.js
+++ src/widgets/index.js
@@ -37,3 +37,6 @@ export { default as breadcrumb } from './breadcrumb/breadcrumb';
 export { default as menuSelect } from './menu-select/menu-select';
 export { default as poweredBy } from './powered-by/powered-by';
 export { default as panel } from './panel/panel';
+export {
+  default as queryRuleCustomData,
+} from './query-rule-custom-data/query-rule-custom-data';
