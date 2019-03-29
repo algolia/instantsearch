d87269c2f3aaa81f4ba59472f32f82daae15332d
Eunjae Lee <eunjae.lee@algolia.com>
2019-03-29 16:33:43 +0100

    chore(voiceSearch): update style for a test story

--- stories/voice-search.stories.ts
+++ stories/voice-search.stories.ts
@@ -126,24 +126,10 @@ storiesOf('VoiceSearch', module)
       window.document.head.appendChild(style);
       [
         `.voice-search-button {
-          border: none;
-          background: none;
           position: absolute;
-          right: 40px;
-          top: 52px;
+          right: 43px;
+          top: 53px;
           z-index: 3;
-        }`,
-        `.voice-search-button svg {
-          fill: #666;
-        }`,
-        `.voice-search-button:hover {
-          cursor: pointer;
-        }`,
-        `.voice-search-button:hover .listening-false svg {
-          fill: #444;
-        }`,
-        `.voice-search-button .listening-true svg {
-          fill: #d83636;
         }`,
         `.voice-search-status .layer {
           position: absolute;
@@ -174,11 +160,6 @@ storiesOf('VoiceSearch', module)
             status: 'voice-search-status',
           },
           templates: {
-            buttonText: `
-              <span class="listening-{{isListening}}">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
-              </span>
-            `,
             status({ isListening, transcript }) {
               return `
                 <div class="layer listening-${isListening}">
