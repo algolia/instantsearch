cea150c76916ba0646deaed2c454cfc522bbf171
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-04-08 12:14:54 +0200

    WIP

--- .storybook/decorators.js
+++ .storybook/decorators.js
@@ -2,45 +2,7 @@ import { action } from '@storybook/addon-actions';
 import algoliasearch from 'algoliasearch/lite';
 import instantsearch from '../src/index';
 
-export const withHits = (storyFn, searchOptions = {}) => () => {
-  const {
-    appId = 'latency',
-    apiKey = '6be0576ff61c053d5f9a3225e2a90f76',
-    indexName = 'instant_search',
-    searchParameters = {},
-    ...instantsearchOptions
-  } = searchOptions;
-
-  const urlLogger = action('Routing state');
-  const search = instantsearch({
-    indexName,
-    searchClient: algoliasearch(appId, apiKey),
-    searchParameters: {
-      hitsPerPage: 4,
-      attributesToSnippet: ['description:15'],
-      snippetEllipsisText: '[…]',
-      ...searchParameters,
-    },
-    routing: {
-      router: {
-        write: routeState => {
-          urlLogger(JSON.stringify(routeState, null, 2));
-        },
-        read: () => ({}),
-        createURL: () => '',
-        onUpdate: () => {},
-      },
-    },
-    ...instantsearchOptions,
-  });
-
-  const containerElement = document.createElement('div');
-
-  // Add the preview container to add the stories in
-  const previewElement = document.createElement('div');
-  previewElement.classList.add('container', 'container-preview');
-  containerElement.appendChild(previewElement);
-
+const withPlaygound = ({ containerElement, search }) => {
   // Add the playground container to add widgets into
   const playgroundElement = document.createElement('div');
   playgroundElement.classList.add('container', 'container-playground');
@@ -71,44 +33,45 @@ export const withHits = (storyFn, searchOptions = {}) => () => {
     })
   );
 
-  const numericMenu = document.createElement('div');
-  leftPanelPlaygroundElement.appendChild(numericMenu);
+  // NOT IMPLEMENTED
+  // const numericMenu = document.createElement('div');
+  // leftPanelPlaygroundElement.appendChild(numericMenu);
 
-  const priceMenu = instantsearch.widgets.panel({
-    templates: {
-      header: 'Price',
-    },
-  })(instantsearch.widgets.numericMenu);
+  // const priceMenu = instantsearch.widgets.panel({
+  //   templates: {
+  //     header: 'Price',
+  //   },
+  // })(instantsearch.widgets.numericMenu);
 
-  search.addWidget(
-    priceMenu({
-      container: numericMenu,
-      attribute: 'price',
-      items: [
-        { label: 'All' },
-        { label: '≤ 10$', end: 10 },
-        { label: '10–100$', start: 10, end: 100 },
-        { label: '100–500$', start: 100, end: 500 },
-        { label: '≥ 500$', start: 500 },
-      ],
-    })
-  );
+  // search.addWidget(
+  //   priceMenu({
+  //     container: numericMenu,
+  //     attribute: 'price',
+  //     items: [
+  //       { label: 'All' },
+  //       { label: '≤ 10$', end: 10 },
+  //       { label: '10–100$', start: 10, end: 100 },
+  //       { label: '100–500$', start: 100, end: 500 },
+  //       { label: '≥ 500$', start: 500 },
+  //     ],
+  //   })
+  // );
 
-  const ratingMenu = document.createElement('div');
-  leftPanelPlaygroundElement.appendChild(ratingMenu);
+  // const ratingMenu = document.createElement('div');
+  // leftPanelPlaygroundElement.appendChild(ratingMenu);
 
-  const ratingList = instantsearch.widgets.panel({
-    templates: {
-      header: 'Rating',
-    },
-  })(instantsearch.widgets.ratingMenu);
+  // const ratingList = instantsearch.widgets.panel({
+  //   templates: {
+  //     header: 'Rating',
+  //   },
+  // })(instantsearch.widgets.ratingMenu);
 
-  search.addWidget(
-    ratingList({
-      container: ratingMenu,
-      attribute: 'rating',
-    })
-  );
+  // search.addWidget(
+  //   ratingList({
+  //     container: ratingMenu,
+  //     attribute: 'rating',
+  //   })
+  // );
 
   const searchBox = document.createElement('div');
   searchBox.classList.add('searchbox');
@@ -140,24 +103,24 @@ export const withHits = (storyFn, searchOptions = {}) => () => {
       container: hits,
       templates: {
         item: `
-<div
-  class="hits-image"
-  style="background-image: url({{image}})"
-></div>
-<article>
-  <header>
-    <strong>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</strong>
-  </header>
-  <p>
-    {{#helpers.snippet}}{ "attribute": "description" }{{/helpers.snippet}}
-  </p>
-  <footer>
-    <p>
-      <strong>{{price}}$</strong>
-    </p>
-  </footer>
-</article>
-          `,
+          <div
+            class="hits-image"
+            style="background-image: url({{image}})"
+          ></div>
+          <article>
+            <header>
+              <strong>{{#helpers.highlight}}{ "attribute": "name" }{{/helpers.highlight}}</strong>
+            </header>
+            <p>
+              {{#helpers.snippet}}{ "attribute": "description" }{{/helpers.snippet}}
+            </p>
+            <footer>
+              <p>
+                <strong>{{price}}$</strong>
+              </p>
+            </footer>
+          </article>
+        `,
       },
       cssClasses: {
         item: 'hits-item',
@@ -173,6 +136,53 @@ export const withHits = (storyFn, searchOptions = {}) => () => {
       container: pagination,
     })
   );
+};
+
+export const withHits = (storyFn, searchOptions = {}) => () => {
+  const {
+    appId = 'latency',
+    apiKey = '6be0576ff61c053d5f9a3225e2a90f76',
+    indexName = 'instant_search',
+    searchParameters = {},
+    ...instantsearchOptions
+  } = searchOptions;
+
+  const urlLogger = action('Routing state');
+  const search = instantsearch({
+    indexName,
+    searchClient: algoliasearch(appId, apiKey, {
+      _useRequestCache: true,
+    }),
+    searchParameters: {
+      hitsPerPage: 4,
+      attributesToSnippet: ['description:15'],
+      snippetEllipsisText: '[…]',
+      ...searchParameters,
+    },
+    routing: {
+      router: {
+        write: routeState => {
+          urlLogger(JSON.stringify(routeState, null, 2));
+        },
+        read: () => ({}),
+        createURL: () => '',
+        onUpdate: () => {},
+      },
+    },
+    ...instantsearchOptions,
+  });
+
+  const containerElement = document.createElement('div');
+
+  // Add the preview container to add the stories in
+  const previewElement = document.createElement('div');
+  previewElement.classList.add('container', 'container-preview');
+  containerElement.appendChild(previewElement);
+
+  withPlaygound({
+    containerElement,
+    search,
+  });
 
   storyFn({
     container: previewElement,
