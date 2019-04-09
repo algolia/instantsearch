8db6aa9ec6765962e1da3da1306e821d1172af81
François Chalifour <francois.chalifour@gmail.com>
2019-04-09 17:01:28 +0200

    chore(routing): add comment on arrayLimit

--- src/lib/routers/history.js
+++ src/lib/routers/history.js
@@ -16,6 +16,9 @@ function defaultParseURL({ qsModule, location }) {
   // We want to avoid this because the data structure manipulated can therefore vary.
   // Setting the limit to `100` seems a good number because the engine's default is 100
   // (it can go up to 1000 but it is very unlikely to select more than 100 items in the UI).
+  //
+  // Using an `arrayLimit` of `n` allows `n + 1` items.
+  //
   // See:
   //   - https://github.com/ljharb/qs#parsing-arrays
   //   - https://www.algolia.com/doc/api-reference/api-parameters/maxValuesPerFacet/
