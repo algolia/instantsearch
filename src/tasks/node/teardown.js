cd6122a51ed0f1413ebdce0cd0bf5c3c0a57dee6
François Chalifour <francoischalifour@users.noreply.github.com>
2019-04-11 12:59:23 +0200

    feat(api): run Prettier after templates generation (algolia/create-instantsearch-app#398)

--- src/tasks/node/teardown.js
+++ src/tasks/node/teardown.js
@@ -1,18 +1,42 @@
 const path = require('path');
+const { execSync } = require('child_process');
 const chalk = require('chalk');
 const { isYarnAvailable } = require('../../utils');
 
 module.exports = function teardown(config) {
+  const hasYarn = isYarnAvailable();
+  const currentDirectory = process.cwd();
+  const cdPath =
+    path.join(currentDirectory, config.name) === config.path
+      ? config.name
+      : config.path;
+
+  try {
+    const command = hasYarn ? 'yarn' : 'npx';
+
+    // This runs the Prettier dependency from Create InstantSearch App (not the template itself)
+    // with the template's Prettier configuration.
+    // We use the "global" Prettier dependency because it is installed for sure at this step,
+    // while the template's Prettier dependency might not be installed if `config.installation`
+    // is `false`.
+    execSync(
+      `${command} prettier "${cdPath}/src/**/*.{json,html,css,js,vue,ts,tsx}" --write --config "${cdPath}/.prettierrc"`,
+      {
+        stdio: 'ignore',
+      }
+    );
+  } catch (error) {
+    // We swallow Prettier's errors because we're not totally in control of what might happen.
+    // Besides, prettifying the files in not necessary in the app generation lifecycle.
+    // Prettier might throw for these known reasons:
+    //  - there's no `.prettierrc` file in the template
+  }
+
   if (!config.silent) {
     try {
-      const hasYarn = isYarnAvailable();
-      const installCommand = hasYarn ? 'yarn' : 'npm install';
-      const startCommand = hasYarn ? 'yarn start' : 'npm start';
-      const currentDirectory = process.cwd();
-      const cdPath =
-        path.join(currentDirectory, config.name) === config.path
-          ? config.name
-          : config.path;
+      const command = hasYarn ? 'yarn' : 'npm';
+      const installCommand = `${command} install`;
+      const startCommand = `${command} start`;
 
       console.log();
       console.log(
