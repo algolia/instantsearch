fe5be97f4174603aeef20ed65f29859444b40ed9
Eunjae Lee <eunjae.lee@algolia.com>
2019-04-16 15:34:36 +0200

    fix(voiceSearch): add extra description on title on unsupported browser

--- src/components/VoiceSearch/VoiceSearch.tsx
+++ src/components/VoiceSearch/VoiceSearch.tsx
@@ -42,7 +42,9 @@ const VoiceSearch = ({
         rootProps={{
           className: cssClasses.button,
           type: 'button',
-          title: 'Search by voice',
+          title:
+            'Search by voice' +
+            (!isBrowserSupported ? ' (not supported on this browser)' : ''),
           onClick: handleClick,
           disabled: !isBrowserSupported,
         }}
