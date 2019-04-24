5d0cfb04ead1dabbe7c0963c536c8ef902b6f801
François Chalifour <francoischalifour@users.noreply.github.com>
2019-04-24 11:20:52 +0200

    refactor(lodash): add documentation for isFiniteNumber (#3722)

--- src/lib/utils/isFiniteNumber.ts
+++ src/lib/utils/isFiniteNumber.ts
@@ -1,3 +1,6 @@
+// This is the `Number.isFinite()` polyfill recommended by MDN.
+// We do not provide any tests for this function.
+// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/isFinite#Polyfill
 function isFiniteNumber(value: any): value is number {
   return typeof value === 'number' && isFinite(value);
 }
