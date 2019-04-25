7f601a81fbddeb05769dcdb5ce6f0da1af8bf755
François Chalifour <francois.chalifour@gmail.com>
2019-04-25 11:19:35 +0200

    docs(find): fix typo

--- src/lib/utils/find.ts
+++ src/lib/utils/find.ts
@@ -1,6 +1,6 @@
 // We aren't using the native `Array.prototype.find` because the refactor away from Lodash is not
 // published as a major version.
-// Relying on the `find` polyfillon user-land, which before was only required for niche use-cases,
+// Relying on the `find` polyfill on user-land, which before was only required for niche use-cases,
 // was decided as too risky.
 // @MAJOR Replace with the native `Array.prototype.find` method
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find
