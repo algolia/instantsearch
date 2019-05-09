fafb2e5773c67f6d75d91f3a953305e71a10cf6d
Haroen Viaene <hello@haroen.me>
2019-05-09 13:04:12 +0200

    refactor(lodash): remove flatten (algolia/algoliasearch-helper-js#695)
    derivedQueries is an array, where each element is an array as well.
    
    mainQueries is an array.

--- packages/algoliasearch-helper/src/algoliasearch.helper.js
+++ packages/algoliasearch-helper/src/algoliasearch.helper.js
@@ -8,7 +8,6 @@ var requestBuilder = require('./requestBuilder');
 var events = require('events');
 var inherits = require('./functions/inherits');
 
-var flatten = require('lodash/flatten');
 var isEmpty = require('lodash/isEmpty');
 
 var version = require('./version');
@@ -1259,7 +1258,7 @@ AlgoliaSearchHelper.prototype._search = function() {
     return queries;
   });
 
-  var queries = mainQueries.concat(flatten(derivedQueries));
+  var queries = Array.prototype.concat.apply(mainQueries, derivedQueries);
   var queryId = this._queryId++;
 
   this._currentNbQueries++;
