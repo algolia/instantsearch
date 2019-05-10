1270178757af66a9482924e37d950a3eb5f17f3d
Haroen Viaene <hello@haroen.me>
2019-05-10 14:50:03 +0200

    fix(defaults): remove const

--- packages/algoliasearch-helper/src/functions/defaultsPure.js
+++ packages/algoliasearch-helper/src/functions/defaultsPure.js
@@ -2,7 +2,7 @@
 
 // NOTE: this behaves like lodash/defaults, but doesn't mutate the target
 module.exports = function defaultsPure() {
-  const sources = Array.prototype.slice.call(arguments);
+  var sources = Array.prototype.slice.call(arguments);
   return sources.reduceRight(function(acc, source) {
     Object.keys(Object(source)).forEach(function(key) {
       if (source[key] !== undefined) {
