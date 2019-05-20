49744e51a450bd4ae5433ca83a8b62da0c10f868
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-05-20 14:47:19 +0200

    refactor(change): use object vs list of arguments

--- src/connectors/query-rules/connectQueryRules.ts
+++ src/connectors/query-rules/connectQueryRules.ts
@@ -18,6 +18,10 @@ import {
   NumericRefinement as InternalNumericRefinement,
 } from '../../lib/utils/getRefinements';
 
+type HelperChangeEvent = {
+  state: SearchParameters;
+};
+
 type TrackedFilterRefinement = string | number | boolean;
 
 export type ParamTrackedFilters = {
@@ -126,7 +130,7 @@ function applyRuleContexts(
     trackedFilters: ParamTrackedFilters;
     transformRuleContexts: ParamTransformRuleContexts;
   },
-  sharedHelperState: SearchParameters
+  event: HelperChangeEvent
 ): void {
   const {
     helper,
@@ -135,6 +139,7 @@ function applyRuleContexts(
     transformRuleContexts,
   } = this;
 
+  const sharedHelperState = event.state;
   const previousRuleContexts: string[] = sharedHelperState.ruleContexts || [];
   const newRuleContexts = getRuleContextsFromTrackedFilters({
     helper,
@@ -186,7 +191,7 @@ const connectQueryRules: QueryRulesConnector = (render, unmount = noop) => {
     // We store the initial rule contexts applied before creating the widget
     // so that we do not override them with the rules created from `trackedFilters`.
     let initialRuleContexts: string[] = [];
-    let onHelperChange: (state: SearchParameters) => void;
+    let onHelperChange: (event: HelperChangeEvent) => void;
 
     return {
       init({ helper, state, instantSearchInstance }) {
@@ -207,11 +212,13 @@ const connectQueryRules: QueryRulesConnector = (render, unmount = noop) => {
             hasStateRefinements(state) ||
             Boolean(widgetParams.transformRuleContexts)
           ) {
-            onHelperChange(state);
+            onHelperChange({ state });
           }
 
           // We track every change in the helper to override its state and add
           // any `ruleContexts` needed based on the `trackedFilters`.
+          // @ts-ignore
+          // @TODO: we have to update the definition to make it work
           helper.on('change', onHelperChange);
         }
 
