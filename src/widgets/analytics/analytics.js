08d9f9ef435878015c80f4c838428c97a8eb4e69
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-05-21 09:59:23 +0200

    refactor(analytics): update page usage

--- src/widgets/analytics/analytics.js
+++ src/widgets/analytics/analytics.js
@@ -149,7 +149,7 @@ function analytics({
 
     let dataToSend = `Query: ${state.state.query}, ${formattedParams}`;
     if (pushPagination === true) {
-      dataToSend += `, Page: ${state.state.page}`;
+      dataToSend += `, Page: ${state.state.page || 0}`;
     }
 
     if (lastSentData !== dataToSend) {
