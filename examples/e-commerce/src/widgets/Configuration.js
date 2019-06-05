83740055ae6ab6091573552201c38cd389e2670f
François Chalifour <francois.chalifour@gmail.com>
2019-06-05 15:39:01 +0200

    fix: don't snippet product names

--- examples/e-commerce/src/widgets/Configuration.js
+++ examples/e-commerce/src/widgets/Configuration.js
@@ -1,7 +1,7 @@
 import { configure } from 'instantsearch.js/es/widgets';
 
 const configuration = configure({
-  attributesToSnippet: ['name:10', 'description:10'],
+  attributesToSnippet: ['description:10'],
   snippetEllipsisText: '…',
 });
 
