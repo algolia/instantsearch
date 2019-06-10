14289d9a0764f1a594164caa2027cecaca4443f6
François Chalifour <francoischalifour@users.noreply.github.com>
2019-06-10 11:06:52 +0200

    chore(examples): update e-commerce design [part 5] (#3843)

--- examples/e-commerce/src/app.js
+++ examples/e-commerce/src/app.js
@@ -1,8 +1,3 @@
 import search from './search';
-import { spyCssClassMutationsAndCopy } from './ui';
-
-const rangeInput = document.querySelector('#price-input > .ais-Panel');
-const rangeSlider = document.querySelector('#price-range > .ais-Panel');
-spyCssClassMutationsAndCopy({ source: rangeInput, target: rangeSlider });
 
 search.start();
