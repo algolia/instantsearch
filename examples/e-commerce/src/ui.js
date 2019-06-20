c9954ad89f76b3c3ce91fd83a26dabde15aa12cf
François Chalifour <francois.chalifour@gmail.com>
2019-06-20 15:30:58 +0200

    refactor: rename function onKeyUp

--- examples/e-commerce/src/ui.js
+++ examples/e-commerce/src/ui.js
@@ -9,16 +9,16 @@ const resultsContainer = document.querySelector('.container-results');
 function openFilters() {
   document.body.classList.add('filtering');
   window.scrollTo(0, 0);
-  window.addEventListener('keyup', onEscape);
+  window.addEventListener('keyup', onKeyUp);
 }
 
 function closeFilters() {
   document.body.classList.remove('filtering');
   resultsContainer.scrollIntoView();
-  window.removeEventListener('keyup', onEscape);
+  window.removeEventListener('keyup', onKeyUp);
 }
 
-function onEscape(event) {
+function onKeyUp(event) {
   if (event.key !== 'Escape') {
     return;
   }
