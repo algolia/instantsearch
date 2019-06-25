56a6f1c9256bea3f4caff2ae260372fe0ea238e9
François Chalifour <francois.chalifour@gmail.com>
2019-06-25 10:57:32 +0200

    chore(examples): format price in hits

--- examples/e-commerce/src/widgets/Products.js
+++ examples/e-commerce/src/widgets/Products.js
@@ -16,7 +16,7 @@ const products = hits({
 
     <footer>
       <p>
-        <span class="hit-em">$</span> <strong>{{price}}</strong>
+        <span class="hit-em">$</span> <strong>{{#helpers.formatNumber}}{{price}}{{/helpers.formatNumber}}</strong>
         <span class="hit-em hit-rating">
         <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 16 16">
           <path fill="#e2a400" fill-rule="evenodd" d="M10.472 5.008L16 5.816l-4 3.896.944 5.504L8 12.616l-4.944 2.6L4 9.712 0 5.816l5.528-.808L8 0z"/>
