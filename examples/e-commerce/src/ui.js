006f1f31dafd199b81005832cd079e8896efc047
François Chalifour <francois.chalifour@gmail.com>
2019-06-26 10:43:07 +0200

    chore(examples): close panel on outter click

--- examples/e-commerce/src/ui.js
+++ examples/e-commerce/src/ui.js
@@ -4,22 +4,33 @@ const filtersButtons = [
 const closeOverlayButtons = [
   ...document.querySelectorAll('[data-action="close-overlay"]'),
 ];
+const header = document.querySelector('#header');
 const resultsContainer = document.querySelector('.container-results');
 
 function openFilters() {
   document.body.classList.add('filtering');
   window.scrollTo(0, 0);
   window.addEventListener('keyup', onKeyUp);
+  window.addEventListener('click', onClick);
 }
 
 function closeFilters() {
   document.body.classList.remove('filtering');
   resultsContainer.scrollIntoView();
   window.removeEventListener('keyup', onKeyUp);
+  window.removeEventListener('click', onClick);
 }
 
 function onKeyUp(event) {
   if (event.key !== 'Escape') {
+    return;
+  }
+
+  closeFilters();
+}
+
+function onClick(event) {
+  if (event.target !== header) {
     return;
   }
 
