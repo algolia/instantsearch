e7b52837d1c1becb64b9e03ebe9f0d883c0ff2eb
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-06-28 12:16:57 +0200

    feat(index): add mergeSearchParameters function
    ```js
    mergeSearchParameters(a, b, c) // new search parameters
    ```
    
    - overrides left-to-right on uncontrolled parameters
    - merges controlled parameters
    - overrides left-to-right on duplicate attributes
    
    Co-authored-by: samouss <samouss@users.noreply.github.com>

---
+++ src/lib/utils/__tests__/mergeSearchParameters-test.ts
@@ -0,0 +1,370 @@
+import algoliasearchHelper from 'algoliasearch-helper';
+import merge from '../mergeSearchParameters';
+
+describe('mergeSearchParameters', () => {
+  it('merges non-managed parameters', () => {
+    const actual = merge(
+      algoliasearchHelper.SearchParameters.make({
+        // Inherit
+        hitsPerPage: 2,
+        attributesToSnippet: ['description'],
+        // Overridden
+        query: 'Samsung',
+        attributesToHighlight: ['name'],
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        // hitsPerPage: 2,
+        // attributesToSnippet: ['description'],
+        query: 'Apple',
+        attributesToHighlight: ['name', 'author'],
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        // hitsPerPage: 2,
+        // attributesToSnippet: ['description'],
+        // attributesToHighlight: ['name', 'author'],
+        query: 'Apple iPhone',
+        distinct: true,
+      })
+    );
+
+    expect(actual).toEqual(
+      algoliasearchHelper.SearchParameters.make({
+        hitsPerPage: 2,
+        attributesToSnippet: ['description'],
+        attributesToHighlight: ['name', 'author'],
+        query: 'Apple iPhone',
+        distinct: true,
+      })
+    );
+  });
+
+  it('merges `facets` parameters', () => {
+    const actual = merge(
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['brand'],
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['categories'],
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['brand', 'colors'],
+      })
+    );
+
+    expect(actual).toEqual(
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['brand', 'categories', 'colors'],
+      })
+    );
+  });
+
+  it('merges `disjunctiveFacets` parameters', () => {
+    const actual = merge(
+      algoliasearchHelper.SearchParameters.make({
+        disjunctiveFacets: ['brand'],
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        disjunctiveFacets: ['categories'],
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        disjunctiveFacets: ['brand', 'colors'],
+      })
+    );
+
+    expect(actual).toEqual(
+      algoliasearchHelper.SearchParameters.make({
+        disjunctiveFacets: ['brand', 'categories', 'colors'],
+      })
+    );
+  });
+
+  it('merges `facetsRefinements` parameters', () => {
+    const actual = merge(
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['brand'],
+        facetsRefinements: {
+          brand: ['Samsung'],
+        },
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['categories'],
+        facetsRefinements: {
+          categories: ['TVs'],
+        },
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['brand', 'colors'],
+        facetsRefinements: {
+          brand: ['Apple'],
+          colors: ['Red'],
+        },
+      })
+    );
+
+    expect(actual).toEqual(
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['brand', 'categories', 'colors'],
+        facetsRefinements: {
+          brand: ['Apple'],
+          categories: ['TVs'],
+          colors: ['Red'],
+        },
+      })
+    );
+  });
+
+  it('merges `facetsExcludes` parameters', () => {
+    const actual = merge(
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['brand'],
+        facetsExcludes: {
+          brand: ['Samsung'],
+        },
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['categories'],
+        facetsExcludes: {
+          categories: ['TVs'],
+        },
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['brand', 'colors'],
+        facetsExcludes: {
+          brand: ['Apple'],
+          colors: ['Red'],
+        },
+      })
+    );
+
+    expect(actual).toEqual(
+      algoliasearchHelper.SearchParameters.make({
+        facets: ['brand', 'categories', 'colors'],
+        facetsExcludes: {
+          brand: ['Apple'],
+          categories: ['TVs'],
+          colors: ['Red'],
+        },
+      })
+    );
+  });
+
+  it('merges `disjunctiveFacetsRefinements` parameters', () => {
+    const actual = merge(
+      algoliasearchHelper.SearchParameters.make({
+        disjunctiveFacets: ['brand'],
+        disjunctiveFacetsRefinements: {
+          brand: ['Samsung'],
+        },
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        disjunctiveFacets: ['categories'],
+        disjunctiveFacetsRefinements: {
+          categories: ['TVs'],
+        },
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        disjunctiveFacets: ['brand', 'colors'],
+        disjunctiveFacetsRefinements: {
+          brand: ['Apple'],
+          colors: ['Red'],
+        },
+      })
+    );
+
+    expect(actual).toEqual(
+      algoliasearchHelper.SearchParameters.make({
+        disjunctiveFacets: ['brand', 'categories', 'colors'],
+        disjunctiveFacetsRefinements: {
+          brand: ['Apple'],
+          categories: ['TVs'],
+          colors: ['Red'],
+        },
+      })
+    );
+  });
+
+  it('merges `numericRefinements` parameters', () => {
+    const actual = merge(
+      algoliasearchHelper.SearchParameters.make({
+        numericRefinements: {
+          price: {
+            '>=': [10],
+            '<=': [100],
+          },
+        },
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        numericRefinements: {
+          rating: {
+            '>=': [3],
+          },
+        },
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        numericRefinements: {
+          price: {
+            '>': [100],
+          },
+          vote: {
+            '>=': [50],
+          },
+        },
+      })
+    );
+
+    expect(actual).toEqual(
+      algoliasearchHelper.SearchParameters.make({
+        numericRefinements: {
+          price: {
+            '>': [100],
+          },
+          rating: {
+            '>=': [3],
+          },
+          vote: {
+            '>=': [50],
+          },
+        },
+      })
+    );
+  });
+
+  it('merges `tagRefinements` parameters', () => {
+    const actual = merge(
+      algoliasearchHelper.SearchParameters.make({
+        tagRefinements: ['brand'],
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        tagRefinements: ['categories'],
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        tagRefinements: ['brand', 'colors'],
+      })
+    );
+
+    expect(actual).toEqual(
+      algoliasearchHelper.SearchParameters.make({
+        tagRefinements: ['brand', 'categories', 'colors'],
+      })
+    );
+  });
+
+  it('merges `hierarchicalFacets` parameters', () => {
+    const actual = merge(
+      algoliasearchHelper.SearchParameters.make({
+        hierarchicalFacets: [
+          {
+            name: 'categories',
+            attributes: ['categories.lvl0', 'categories.lvl1'],
+          },
+        ],
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        hierarchicalFacets: [
+          {
+            name: 'department',
+            attributes: ['department.lvl0', 'department.lvl1'],
+          },
+        ],
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        hierarchicalFacets: [
+          {
+            name: 'categories',
+            attributes: ['topLevelCategories', 'subLevelCategories'],
+          },
+          {
+            name: 'folders',
+            attributes: ['folders.lvl0', 'folders.lvl1'],
+          },
+        ],
+      })
+    );
+
+    expect(actual).toEqual(
+      algoliasearchHelper.SearchParameters.make({
+        hierarchicalFacets: [
+          {
+            name: 'categories',
+            attributes: ['topLevelCategories', 'subLevelCategories'],
+          },
+          {
+            name: 'department',
+            attributes: ['department.lvl0', 'department.lvl1'],
+          },
+          {
+            name: 'folders',
+            attributes: ['folders.lvl0', 'folders.lvl1'],
+          },
+        ],
+      })
+    );
+  });
+
+  it('merges `hierarchicalFacetsRefinements` parameters', () => {
+    const actual = merge(
+      algoliasearchHelper.SearchParameters.make({
+        hierarchicalFacets: [
+          {
+            name: 'categories',
+            attributes: ['categories.lvl0', 'categories.lvl1'],
+          },
+        ],
+        hierarchicalFacetsRefinements: {
+          categories: ['Appliances > Fans'],
+        },
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        hierarchicalFacets: [
+          {
+            name: 'department',
+            attributes: ['department.lvl0', 'department.lvl1'],
+          },
+        ],
+        hierarchicalFacetsRefinements: {
+          department: ['Engineering > Squad'],
+        },
+      }),
+      algoliasearchHelper.SearchParameters.make({
+        hierarchicalFacets: [
+          {
+            name: 'categories',
+            attributes: ['topLevelCategories', 'subLevelCategories'],
+          },
+          {
+            name: 'folders',
+            attributes: ['folders.lvl0', 'folders.lvl1'],
+          },
+        ],
+        hierarchicalFacetsRefinements: {
+          categories: ['Cell Phones > Prepaid Phones'],
+          folders: ['Music > Artist > Mike Miller'],
+        },
+      })
+    );
+
+    expect(actual).toEqual(
+      algoliasearchHelper.SearchParameters.make({
+        hierarchicalFacets: [
+          {
+            name: 'categories',
+            attributes: ['topLevelCategories', 'subLevelCategories'],
+          },
+          {
+            name: 'department',
+            attributes: ['department.lvl0', 'department.lvl1'],
+          },
+          {
+            name: 'folders',
+            attributes: ['folders.lvl0', 'folders.lvl1'],
+          },
+        ],
+        hierarchicalFacetsRefinements: {
+          categories: ['Cell Phones > Prepaid Phones'],
+          department: ['Engineering > Squad'],
+          folders: ['Music > Artist > Mike Miller'],
+        },
+      })
+    );
+  });
+});
---
+++ src/lib/utils/mergeSearchParameters.ts
@@ -0,0 +1,125 @@
+import { SearchParameters } from '../../types';
+
+type Merge = (value: SearchParameters) => SearchParameters;
+
+type Merger = (right: SearchParameters) => Merge;
+
+const compose = <TValue>(...fns: Array<(x: TValue) => TValue>) => (
+  initial: TValue
+) => fns.reduceRight((_, fn) => fn(_), initial);
+
+const mergeWithRest: Merger = right => left => {
+  const {
+    facets,
+    disjunctiveFacets,
+    facetsRefinements,
+    facetsExcludes,
+    disjunctiveFacetsRefinements,
+    numericRefinements,
+    tagRefinements,
+    hierarchicalFacets,
+    hierarchicalFacetsRefinements,
+    ...rest
+  } = right;
+
+  return left.setQueryParameters(rest);
+};
+
+// Merge facets
+const mergeFacets: Merger = right => left =>
+  right.facets!.reduce((_, name) => _.addFacet(name), left);
+
+const mergeDisjunctiveFacets: Merger = right => left =>
+  right.disjunctiveFacets!.reduce(
+    (_, name) => _.addDisjunctiveFacet(name),
+    left
+  );
+
+const mergeTagRefinements: Merger = right => left =>
+  right.tagRefinements!.reduce((_, value) => _.addTagRefinement(value), left);
+
+type HierarchicalFacet = {
+  name: string;
+  attributes: string[];
+  separator: string;
+};
+
+const mergeHierarchicalFacets: Merger = right => left =>
+  left.setQueryParameters({
+    // @TODO: remove this cast when typings in helper are merged
+    hierarchicalFacets: (right.hierarchicalFacets as HierarchicalFacet[]).reduce(
+      (facets, facet) => {
+        const index = facets.findIndex(_ => _.name === facet.name);
+
+        if (index === -1) {
+          return facets.concat(facet);
+        }
+
+        const nextFacets = facets.slice();
+        nextFacets.splice(index, 1, facet);
+
+        return nextFacets;
+      },
+      left.hierarchicalFacets as HierarchicalFacet[]
+    ),
+  });
+
+// Merge facets refinements
+const mergeFacetRefinements: Merger = right => left =>
+  left.setQueryParameters({
+    facetsRefinements: {
+      ...left.facetsRefinements,
+      ...right.facetsRefinements,
+    },
+  });
+
+const mergeFacetsExcludes: Merger = right => left =>
+  left.setQueryParameters({
+    facetsExcludes: {
+      ...left.facetsExcludes,
+      ...right.facetsExcludes,
+    },
+  });
+
+const mergeDisjunctiveFacetsRefinements: Merger = right => left =>
+  left.setQueryParameters({
+    disjunctiveFacetsRefinements: {
+      ...left.disjunctiveFacetsRefinements,
+      ...right.disjunctiveFacetsRefinements,
+    },
+  });
+
+const mergeNumericRefinements: Merger = right => left =>
+  left.setQueryParameters({
+    numericRefinements: {
+      ...left.numericRefinements,
+      ...right.numericRefinements,
+    },
+  });
+
+const mergeHierarchicalFacetsRefinements: Merger = right => left =>
+  left.setQueryParameters({
+    hierarchicalFacetsRefinements: {
+      ...left.hierarchicalFacetsRefinements,
+      ...right.hierarchicalFacetsRefinements,
+    },
+  });
+
+const merge = (...parameters: SearchParameters[]): SearchParameters => {
+  return parameters.reduce((left, right) =>
+    compose(
+      mergeHierarchicalFacetsRefinements(right),
+      mergeHierarchicalFacets(right),
+      mergeTagRefinements(right),
+      mergeNumericRefinements(right),
+      mergeDisjunctiveFacetsRefinements(right),
+      mergeFacetsExcludes(right),
+      mergeFacetRefinements(right),
+      mergeDisjunctiveFacets(right),
+      mergeFacets(right),
+      mergeWithRest(right)
+    )(left)
+  );
+};
+
+export default merge;
