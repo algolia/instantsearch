016dce079928de4d370d4d63a23f689e5878fe5f
Samuel Vaillant <samuel.vllnt@gmail.com>
2019-07-03 15:39:25 +0200

    feat(defer): implement cancellable callback (#3916)

--- src/lib/utils/defer.ts
+++ src/lib/utils/defer.ts
@@ -1,19 +1,38 @@
 const nextMicroTask = Promise.resolve();
 
 type Callback = (...args: any[]) => void;
+type Cancellable = Callback & {
+  cancel(): void;
+};
 
-const defer = (callback: Callback): Callback => {
+const defer = (callback: Callback): Cancellable => {
   let progress: Promise<void> | null = null;
-  return (...args) => {
+  let cancelled = false;
+
+  const fn: Cancellable = (...args) => {
     if (progress !== null) {
       return;
     }
 
     progress = nextMicroTask.then(() => {
+      if (cancelled) {
+        progress = null;
+        cancelled = false;
+        return;
+      }
+
       callback(...args);
       progress = null;
     });
   };
+
+  fn.cancel = () => {
+    if (progress !== null) {
+      cancelled = true;
+    }
+  };
+
+  return fn;
 };
 
 export default defer;
