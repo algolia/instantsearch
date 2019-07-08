5f483cf16ca6aa4efc9b97532e99917f6ec18b27
François Chalifour <francois.chalifour@gmail.com>
2019-07-08 15:19:53 +0200

    chore(examples): update design elements

--- examples/media/src/widgets/SearchBox.ts
+++ examples/media/src/widgets/SearchBox.ts
@@ -4,5 +4,4 @@ export const searchBox = searchBoxWidget({
   container: '[data-widget="searchbox"]',
   placeholder: 'Articles, categories, topics or content',
   showSubmit: false,
-  showReset: false,
 });
