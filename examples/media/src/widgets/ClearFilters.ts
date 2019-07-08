02dedc899600762f33790ce1779a28b6ae7cdaac
François Chalifour <francois.chalifour@gmail.com>
2019-07-08 15:19:53 +0200

    chore(examples): update design elements

--- examples/media/src/widgets/ClearFilters.ts
+++ examples/media/src/widgets/ClearFilters.ts
@@ -4,17 +4,6 @@ export const clearFilters = clearRefinements({
   container: '[data-widget="clear-filters"]',
   excludedAttributes: ['topics', 'query'],
   templates: {
-    resetLabel: `
-<div class="clear-filters">
-  <svg xmlns="http://www.w3.org/2000/svg" width="15" height="14" viewBox="0 0 15 14">
-    <g fill="none" fill-rule="evenodd" stroke="#6E7070" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-      <path d="M1 1.65v3.9h3.9"/>
-      <path d="M2.631 8.8a5.85 5.85 0 1 0 1.385-6.084L1 5.55"/>
-    </g>
-  </svg>
-
-  Clear filters
-</div>
-`,
+    resetLabel: 'Clear filters',
   },
 });
