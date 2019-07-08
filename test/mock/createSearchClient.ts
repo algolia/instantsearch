b5e3cec7ec0420a3912883038b3badd88a8ef111
Samuel Vaillant <samuel.vllnt@gmail.com>
2019-07-08 16:23:08 +0200

    fix(InstantSearch): cancel scheduled operations (#3930)
    * fix(InstantSearch): cancel scheduled render
    
    * fix(InstantSearch): cancel scheduled search
    
    * fix(InstantSearch): cancel scheduled stalled render
    
    * test(utils): create controlled search client

--- test/mock/createSearchClient.ts
+++ test/mock/createSearchClient.ts
@@ -1,3 +1,4 @@
+import { MultiResponse } from 'algoliasearch';
 import { Client } from '../../src/types';
 import {
   createSingleSearchResponse,
@@ -19,3 +20,39 @@ export const createSearchClient = (args: Partial<Client> = {}): Client =>
     ),
     ...args,
   } as Client);
+
+type ControlledClient = {
+  searchClient: Client;
+  searches: Array<{
+    promise: Promise<MultiResponse>;
+    resolver: () => void;
+  }>;
+};
+
+export const createControlledSearchClient = (
+  args: Partial<Client> = {}
+): ControlledClient => {
+  const searches: ControlledClient['searches'] = [];
+  const searchClient = createSearchClient({
+    search: jest.fn(() => {
+      let resolver: () => void;
+      const promise: Promise<MultiResponse> = new Promise(resolve => {
+        resolver = () => resolve(createMutliSearchResponse());
+      });
+
+      searches.push({
+        promise,
+        // @ts-ignore
+        resolver,
+      });
+
+      return promise;
+    }),
+    ...args,
+  });
+
+  return {
+    searchClient,
+    searches,
+  };
+};
