9f344d128393643b95601170763aa583d101e981
Yannick Croissant <yannick.croissant@gmail.com>
2019-07-09 17:00:14 +0200

    refactor(test): replace jest-dom by @testing-library/jest-dom

--- scripts/jest/setupTests.js
+++ scripts/jest/setupTests.js
@@ -2,7 +2,7 @@ import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { createSerializer } from 'enzyme-to-json';
 import { toWarnDev } from './matchers';
-import 'jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 
 Enzyme.configure({ adapter: new Adapter() });
 expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
