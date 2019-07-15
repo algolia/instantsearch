6111a27cb2e4bcff8737babac2c7800c61d4809f
Haroen Viaene <hello@haroen.me>
2019-07-15 17:27:11 +0200

    fix(menu): prevent error on stale search (#3934)
    * fix(menu): prevent error on stale search
    
    If a menu is added while the search is being stale, we will  recieve undefined (currently an error), so we can not fetch `.data` from it.
    
    requires https://github.com/algolia/algoliasearch-helper-js/pull/720
    
    * test: add a test declaring the use case with wrong helper state

--- src/connectors/menu/connectMenu.js
+++ src/connectors/menu/connectMenu.js
@@ -190,8 +190,10 @@ export default function connectMenu(renderFn, unmountFn = noop) {
       },
 
       render({ results, instantSearchInstance }) {
+        const facetValues = results.getFacetValues(attribute, { sortBy });
         const facetItems =
-          results.getFacetValues(attribute, { sortBy }).data || [];
+          facetValues && facetValues.data ? facetValues.data : [];
+
         const items = transformItems(
           facetItems
             .slice(0, this.getLimit())
