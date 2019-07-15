706c1bc949a5e276266a881f72799b82bb869ff5
Haroen Viaene <hello@haroen.me>
2019-07-15 17:27:25 +0200

    chore(enhanceConfiguration): swap mergeDeep for mergeSearchParameters (#3945)
    * chore(enhanceConfiguration): swap mergeDeep for mergeSearchParameters
    
    This is a follow-up on #3917, to easily be able to see the perf impact this has. It's expected that we lose some performance, but it's a tradeoff we can make to fix some bugs in wrongly merging parameters
    
    * chore: remove mergeDeep

--- src/lib/utils/enhanceConfiguration.ts
+++ src/lib/utils/enhanceConfiguration.ts
@@ -1,7 +1,8 @@
-import { PlainSearchParameters } from 'algoliasearch-helper';
+import algoliasearchHelper, {
+  PlainSearchParameters,
+} from 'algoliasearch-helper';
 import { Widget } from '../../types';
-import findIndex from './findIndex';
-import mergeDeep from './mergeDeep';
+import mergeSearchParameters from './mergeSearchParameters';
 
 export function enhanceConfiguration(
   configuration: PlainSearchParameters,
@@ -14,45 +15,9 @@ export function enhanceConfiguration(
   // Get the relevant partial configuration asked by the widget
   const partialConfiguration = widget.getConfiguration(configuration);
 
-  if (!partialConfiguration) {
-    return configuration;
-  }
-
-  if (!partialConfiguration.hierarchicalFacets) {
-    return mergeDeep(configuration, partialConfiguration);
-  }
-
-  const {
-    hierarchicalFacets,
-    ...partialWithoutHierarchcialFacets
-  } = partialConfiguration;
-
-  // The `mergeDeep` function uses a `uniq` function under the hood, but the
-  // implementation does not support arrays of objects (we also had the issue
-  // with the Lodash version). The `hierarchicalFacets` attribute is an array
-  // of objects, which means that this attribute is never deduplicated. It
-  // becomes problematic when widgets are frequently added/removed, since the
-  // function `enhanceConfiguration` is called at each operation.
-  // https://github.com/algolia/instantsearch.js/issues/3278
-  const configurationWithHierarchicalFacets = {
-    ...configuration,
-    hierarchicalFacets: hierarchicalFacets.reduce((facets, facet) => {
-      const index = findIndex(facets, _ => _.name === facet.name);
-
-      if (index === -1) {
-        return facets.concat(facet);
-      }
-
-      const nextFacets = facets.slice();
-      nextFacets.splice(index, 1, facet);
-
-      return nextFacets;
-    }, configuration.hierarchicalFacets || []),
-  };
-
-  return mergeDeep(
-    configurationWithHierarchicalFacets,
-    partialWithoutHierarchcialFacets
+  return mergeSearchParameters(
+    new algoliasearchHelper.SearchParameters(configuration),
+    new algoliasearchHelper.SearchParameters(partialConfiguration)
   );
 }
 
