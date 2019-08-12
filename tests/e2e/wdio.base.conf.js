ac26bf2d5fcad6deed53e93afb68c2f8ddbb68cc
Yannick Croissant <yannick.croissant@gmail.com>
2019-08-12 15:35:25 +0200

    fix(typescript): fix TypeScript configuration to transpile tests when in node_modules

--- tests/e2e/wdio.base.conf.js
+++ tests/e2e/wdio.base.conf.js
@@ -74,8 +74,12 @@ module.exports = {
      * https://webdriver.io/docs/typescript.html
      */
     require('ts-node').register({
-      files: true,
+      // Only transpile our files without typechecking them, it makes test
+      // faster and exempts us from installing type definitions to run them
       transpileOnly: true,
+      // Force `ts-node` to compile files in `node_modules` directory
+      // (ignored by default), since our tests will be stored in it
+      ignore: [],
       // Force `ts-node` to use the config file from the package root
       // instead of the directory from which `wdio` was called
       project: path.join(__dirname, './tsconfig.json'),
