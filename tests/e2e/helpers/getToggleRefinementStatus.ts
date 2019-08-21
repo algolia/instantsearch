fa431f397b2b6c2b16eb07fbb1b8098c2a3ca2af
Yannick Croissant <yannick.croissant@gmail.com>
2019-08-21 17:05:24 +0200

    feat(helper): add getToggleRefinementStatus helper

---
+++ tests/e2e/helpers/getToggleRefinementStatus.ts
@@ -0,0 +1,11 @@
+declare namespace WebdriverIOAsync {
+  interface Browser {
+    getToggleRefinementStatus(): Promise<boolean>;
+  }
+}
+
+browser.addCommand('getToggleRefinementStatus', async () => {
+  const checkbox = await browser.$('.ais-ToggleRefinement-checkbox');
+
+  return checkbox.isSelected();
+});
