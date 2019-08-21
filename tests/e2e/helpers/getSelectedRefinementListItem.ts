92198963ab72a4fcec227f73c9bbc1813ef4ad64
Yannick Croissant <yannick.croissant@gmail.com>
2019-08-21 17:01:53 +0200

    feat(helper): add getSelectedRefinementListItem helper

---
+++ tests/e2e/helpers/getSelectedRefinementListItem.ts
@@ -0,0 +1,12 @@
+declare namespace WebdriverIOAsync {
+  interface Browser {
+    getSelectedRefinementListItem(): Promise<string>;
+  }
+}
+
+browser.addCommand('getSelectedRefinementListItem', async () => {
+  const item = await browser.$(
+    '.ais-RefinementList-item--selected .ais-RefinementList-labelText'
+  );
+  return await item.getText();
+});
