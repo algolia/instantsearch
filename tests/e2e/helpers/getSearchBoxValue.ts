46f6ad0fb5125bed2b90cee00fce60093d24dbaa
Yannick Croissant <yannick.croissant@gmail.com>
2019-08-21 17:00:11 +0200

    feat(helper): add getSearchBoxValue helper

---
+++ tests/e2e/helpers/getSearchBoxValue.ts
@@ -0,0 +1,11 @@
+declare namespace WebdriverIOAsync {
+  interface Browser {
+    getSearchBoxValue(): Promise<string>;
+  }
+}
+
+browser.addCommand('getSearchBoxValue', async () => {
+  const searchBox = await browser.$('.ais-SearchBox [type=search]');
+
+  return searchBox.getValue();
+});
