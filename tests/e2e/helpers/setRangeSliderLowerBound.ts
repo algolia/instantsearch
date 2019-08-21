24ab14a3b25d3e67f6e40c7d6f6038e381ad58ea
Yannick Croissant <yannick.croissant@gmail.com>
2019-08-21 14:27:08 +0200

    feat(helper): add setRangeSliderLowerBound helper

---
+++ tests/e2e/helpers/setRangeSliderLowerBound.ts
@@ -0,0 +1,37 @@
+declare namespace WebdriverIOAsync {
+  interface Browser {
+    setRangeSliderLowerBound(value: number): Promise<number>;
+  }
+}
+
+browser.addCommand('setRangeSliderLowerBound', async (value: number) => {
+  const slider = await browser.$('.rheostat-horizontal');
+  let lowerHandle = await browser.$('.ais-RangeSlider .rheostat-handle-lower');
+  const upperHandle = await browser.$(
+    '.ais-RangeSlider .rheostat-handle-upper'
+  );
+  const { width: sliderWidth } = await slider.getSize();
+  const { x: lowerHandleX } = await lowerHandle.getLocation();
+  const { x: sliderX } = await slider.getLocation();
+  const lowerHandleMin = Number(
+    await lowerHandle.getAttribute('aria-valuemin')
+  );
+  const upperHandleMax = Number(
+    await upperHandle.getAttribute('aria-valuemax')
+  );
+
+  // Get the size of a step (in pixels)
+  const step = sliderWidth / (upperHandleMax - lowerHandleMin);
+  // Get the horizontal difference between the handle and the slider (can happen if the handle have some margins)
+  const xDifference = sliderX - lowerHandleX;
+  // Calculate the final offset
+  const offset = value * step + xDifference;
+
+  await browser.dragAndDropByOffset(lowerHandle, offset);
+
+  // Depending of the steps calculation there can be a difference between
+  // the wanted value and the actual value of the slider, so we return
+  // the actual value in case we need it in the rest of the tests
+  lowerHandle = await browser.$('.ais-RangeSlider .rheostat-handle-lower');
+  return Number(await lowerHandle.getAttribute('aria-valuenow'));
+});
