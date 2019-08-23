0e3d8fc3fa1bf68a3557a635bf02b94e746e14f6
Yannick Croissant <yannick.croissant@gmail.com>
2019-08-23 13:31:17 +0200

    fix(helper): fix getSelectedHierarchicalMenuItems to wait for the elements
    Sometimes the menu was not quite ready yet and getSelectedHierarchicalMenuItems was returning an
    empty array

--- tests/e2e/helpers/getSelectedHierarchicalMenuItems.ts
+++ tests/e2e/helpers/getSelectedHierarchicalMenuItems.ts
@@ -5,8 +5,13 @@ declare namespace WebdriverIOAsync {
 }
 
 browser.addCommand('getSelectedHierarchicalMenuItems', async () => {
+  await browser.waitForElement(
+    '.ais-HierarchicalMenu-item--selected .ais-HierarchicalMenu-label'
+  );
+
   const items = await browser.$$(
     '.ais-HierarchicalMenu-item--selected .ais-HierarchicalMenu-label'
   );
+
   return await browser.getTextFromElements(items);
 });
