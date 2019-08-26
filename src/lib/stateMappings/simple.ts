132c249e277149f241896a66814ee84cba377b8f
Haroen Viaene <hello@haroen.me>
2019-08-26 16:35:26 +0200

    Update src/lib/stateMappings/simple.ts

--- src/lib/stateMappings/simple.ts
+++ src/lib/stateMappings/simple.ts
@@ -3,7 +3,7 @@ import { UiState, StateMapping } from '../../types';
 // technically a URL could contain any key, since users provide it,
 // which is why the input to this function is UiState, not something
 // which excludes "configure" as this function does.
-export default function SimpleStateMapping(): StateMapping<UiState> {
+export default function simpleStateMapping(): StateMapping<UiState> {
   return {
     stateToRoute(uiState) {
       const { configure, ...trackedRouteState } = uiState;
