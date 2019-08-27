be836a52617228fefbc658135f722734d55dd62e
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-08-27 18:55:01 +0200

    feat(InstantSearch): add stories for initialUiState

--- stories/instantsearch.stories.js
+++ stories/instantsearch.stories.js
@@ -1,17 +1,31 @@
 import { storiesOf } from '@storybook/html';
 import { withHits } from '../.storybook/decorators';
 
-storiesOf('InstantSearch', module).add(
-  'with searchfunction to prevent search',
-  withHits(() => {}, {
-    searchFunction: helper => {
-      const query = helper.state.query;
+storiesOf('InstantSearch', module)
+  .add(
+    'with searchfunction to prevent search',
+    withHits(() => {}, {
+      searchFunction: helper => {
+        const query = helper.state.query;
 
-      if (query === '') {
-        return;
-      }
+        if (query === '') {
+          return;
+        }
 
-      helper.search();
-    },
-  })
-);
+        helper.search();
+      },
+    })
+  )
+  .add(
+    'with initialUiState',
+    withHits(() => {}, {
+      initialUiState: {
+        // eslint-disable-next-line @typescript-eslint/camelcase
+        instant_search: {
+          refinementList: {
+            brand: ['Apple'],
+          },
+        },
+      },
+    })
+  );
