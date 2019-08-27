8085d88868960e73471e57f772630ded5fd42e67
François Chalifour <francois.chalifour@gmail.com>
2019-08-27 11:34:08 +0200

    chore(hitsPerPage): remove story

--- stories/hits-per-page.stories.js
+++ stories/hits-per-page.stories.js
@@ -10,23 +10,7 @@ storiesOf('HitsPerPage', module)
           container,
           items: [
             { value: 3, label: '3 per page', default: true },
-            { value: 4, label: '4 per page' },
             { value: 5, label: '5 per page' },
-            { value: 10, label: '10 per page' },
-          ],
-        })
-      );
-    })
-  )
-  .add(
-    'with default hitPerPage to 5',
-    withHits(({ search, container, instantsearch }) => {
-      search.addWidget(
-        instantsearch.widgets.hitsPerPage({
-          container,
-          items: [
-            { value: 3, label: '3 per page' },
-            { value: 5, label: '5 per page', default: true },
             { value: 10, label: '10 per page' },
           ],
         })
