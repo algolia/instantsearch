107734057c798c48f22a0d51f2741d01fbe140fc
Yannick Croissant <yannick.croissant@algolia.com>
2019-09-04 11:30:35 +0200

    fix(connectInfiniteHits): fix page state when adding or removing widgets (#4104)

--- src/connectors/infinite-hits/connectInfiniteHits.ts
+++ src/connectors/infinite-hits/connectInfiniteHits.ts
@@ -84,6 +84,18 @@ const connectInfiniteHits: InfiniteHitsConnector = (
     const getShowMore = (helper: Helper): (() => void) => () => {
       helper.setPage(lastReceivedPage + 1).search();
     };
+    const filterEmptyRefinements = (refinements = {}) => {
+      return Object.keys(refinements)
+        .filter(key =>
+          Array.isArray(refinements[key])
+            ? refinements[key].length
+            : Object.keys(refinements[key]).length
+        )
+        .reduce((obj, key) => {
+          obj[key] = refinements[key];
+          return obj;
+        }, {});
+    };
 
     return {
       $$type: 'ais.infiniteHits',
@@ -131,7 +143,22 @@ const connectInfiniteHits: InfiniteHitsConnector = (
         // We're doing this to "reset" the widget if a refinement or the
         // query changes between renders, but we want to keep it as is
         // if we only change pages.
-        const { page = 0, ...currentState } = state;
+        const {
+          page = 0,
+          hierarchicalFacets,
+          disjunctiveFacets,
+          ...currentState
+        } = state;
+
+        currentState.hierarchicalFacetsRefinements = filterEmptyRefinements(
+          currentState.hierarchicalFacetsRefinements
+        );
+        currentState.disjunctiveFacetsRefinements = filterEmptyRefinements(
+          currentState.disjunctiveFacetsRefinements
+        );
+        currentState.numericRefinements = filterEmptyRefinements(
+          currentState.numericRefinements
+        );
 
         if (!isEqual(currentState, prevState)) {
           hitsCache = [];
@@ -230,7 +257,11 @@ const connectInfiniteHits: InfiniteHitsConnector = (
           );
         }
 
-        if (hasShowPrevious && uiState.page) {
+        if (!hasShowPrevious) {
+          return widgetSearchParameters;
+        }
+
+        if (uiState.page) {
           // The page in the search parameters is decremented by one
           // to get to the actual parameter value from the UI state.
           return widgetSearchParameters.setQueryParameter(
