edd8923780b6ac663fc4a2c97c1c5777e30b5292
Yannick Croissant <yannick.croissant@algolia.com>
2019-09-11 17:13:11 +0200

    chore(examples): update examples to match the ones on instantsearch.js (#2823)

--- examples/.eslintrc.js
+++ examples/.eslintrc.js
@@ -5,5 +5,6 @@ module.exports = {
   rules: {
     'import/no-unresolved': 'off',
     'import/named': 'off',
+    'react/prop-types': 'off',
   },
 };
--- scripts/test-examples.js
+++ scripts/test-examples.js
@@ -2,7 +2,14 @@ const path = require('path');
 const glob = require('glob');
 const execSync = require('child_process').execSync;
 
-const examples = glob.sync(path.join(__dirname, '..', 'examples', '*'));
+const examples = glob.sync(
+  path.join(
+    __dirname,
+    '..',
+    'examples',
+    '!(default-theme|e-commerce|media|tourism)'
+  )
+);
 
 examples.forEach(example => {
   execSync(`cd ${example} && yarn && yarn build && yarn test`, {
