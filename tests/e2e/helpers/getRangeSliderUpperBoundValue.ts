eb896dc9057f0891a124759d85e8017aedeee653
Yannick Croissant <yannick.croissant@algolia.com>
2019-09-11 15:30:49 +0200

    fix(helper): fix helpers to handle RangeSlider in react-instantsearch (algolia/instantsearch-e2e-tests#9)

--- tests/e2e/helpers/getRangeSliderUpperBoundValue.ts
+++ tests/e2e/helpers/getRangeSliderUpperBoundValue.ts
@@ -5,8 +5,7 @@ declare namespace WebdriverIOAsync {
 }
 
 browser.addCommand('getRangeSliderUpperBoundValue', async () => {
-  const upperHandle = await browser.$(
-    '.ais-RangeSlider .rheostat-handle-upper'
-  );
+  await browser.waitForElement('.slider-handle, .rheostat-handle');
+  const [, upperHandle] = await browser.$$('.slider-handle, .rheostat-handle');
   return Number(await upperHandle.getAttribute('aria-valuenow'));
 });
