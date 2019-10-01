d6a95060b0c3832dc662ad2d68cf8f364a93b4c9
Haroen Viaene <hello@haroen.me>
2019-10-01 10:16:15 +0200

    Update addWidget-to-addWidgets.test.js

--- scripts/transforms/__tests__/addWidget-to-addWidgets.test.js
+++ scripts/transforms/__tests__/addWidget-to-addWidgets.test.js
@@ -1,25 +1,25 @@
 const defineTest = require('jscodeshift/dist/testUtils').defineTest;
 
-// defineTest(
-//   __dirname,
-//   'addWidget-to-addWidgets',
-//   null,
-//   'addWidget-to-addWidgets/global'
-// );
+defineTest(
+  __dirname,
+  'addWidget-to-addWidgets',
+  null,
+  'addWidget-to-addWidgets/global'
+);
 
-// defineTest(
-//   __dirname,
-//   'addWidget-to-addWidgets',
-//   null,
-//   'addWidget-to-addWidgets/imported'
-// );
+defineTest(
+  __dirname,
+  'addWidget-to-addWidgets',
+  null,
+  'addWidget-to-addWidgets/imported'
+);
 
-// defineTest(
-//   __dirname,
-//   'addWidget-to-addWidgets',
-//   null,
-//   'addWidget-to-addWidgets/mixed'
-// );
+defineTest(
+  __dirname,
+  'addWidget-to-addWidgets',
+  null,
+  'addWidget-to-addWidgets/mixed'
+);
 
 defineTest(
   __dirname,
