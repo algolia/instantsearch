f2585815450bf5cb69889d709c3bbf2c5d9bfd41
François Chalifour <francois.chalifour@gmail.com>
2019-10-03 13:37:23 +0200

    chore(deps): add typings for testing library

--- scripts/jest/setupTests.ts
+++ scripts/jest/setupTests.ts
@@ -1,6 +1,7 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-preact-pure';
 import { createSerializer } from 'enzyme-to-json';
+import '@testing-library/jest-dom/extend-expect';
 import { toWarnDev } from './matchers';
 
 Enzyme.configure({ adapter: new Adapter() });
