2bc94e61297143f05f8959c0415ad9e56dff399e
Yannick Croissant <yannick.croissant@algolia.com>
2019-10-14 15:21:27 +0200

    fix(helper): fix helpers to handle RangeSlider in vue-instantsearch (algolia/instantsearch-e2e-tests#12)

--- tests/e2e/helpers/getRangeSliderUpperBoundValue.ts
+++ tests/e2e/helpers/getRangeSliderUpperBoundValue.ts
@@ -1,11 +1,13 @@
-declare namespace WebdriverIOAsync {
+import { RANGE_SLIDER_HANDLE_SELECTOR } from './selectors';
+
+declare module 'webdriverio' {
   interface Browser {
     getRangeSliderUpperBoundValue(): Promise<number>;
   }
 }
 
 browser.addCommand('getRangeSliderUpperBoundValue', async () => {
-  await browser.waitForElement('.slider-handle, .rheostat-handle');
-  const [, upperHandle] = await browser.$$('.slider-handle, .rheostat-handle');
+  await browser.waitForElement(RANGE_SLIDER_HANDLE_SELECTOR);
+  const [, upperHandle] = await browser.$$(RANGE_SLIDER_HANDLE_SELECTOR);
   return Number(await upperHandle.getAttribute('aria-valuenow'));
 });
