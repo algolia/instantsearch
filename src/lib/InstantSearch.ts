3083ad98e714e1c7cface1ed51e894f36292c88f
Youcef Mammar <tkrugg@users.noreply.github.com>
2019-12-02 17:52:26 +0100

    Update src/lib/InstantSearch.ts
    Co-Authored-By: François Chalifour <francoischalifour@users.noreply.github.com>

--- src/lib/InstantSearch.ts
+++ src/lib/InstantSearch.ts
@@ -188,7 +188,7 @@ See: https://www.algolia.com/doc/guides/building-search-ui/going-further/backend
     warning(
       Boolean(insightsClient) || !hasDetectedInsightsClient(),
       `InstantSearch detected the Insights client in the global scope.
-To connect InstantSearch to insights client, make sure you provide it to your InstantSearch instance:
+To connect InstantSearch to the Insights client, make sure to specify the `insightsClient` option:
 
 const search = instantsearch({
   /* ... */
