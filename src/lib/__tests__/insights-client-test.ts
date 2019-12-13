algolia/instantsearch

commit 9cac5a3aa4af616ec7913c17ed7388134c5e7f0a

fix(insights): detect clicks on children of [data-insights] HTML elements (#4197)
* fix(insights): detect clicks on children of [data-insights] HTML elements

* renamed realTarget to insightsTarget

src/lib/insights/listener.tsx
@@ -9,22 +9,36 @@ type WithInsightsListenerProps = {
   insights: InsightsClientWrapper;
 };
 
+const findInsightsTarget = (
+  startElement: HTMLElement | null,
+  endElement: HTMLElement | null
+): HTMLElement | null => {
+  let element: HTMLElement | null = startElement;
+  while (element && !hasDataAttributes(element)) {
+    if (element === endElement) {
+      return null;
+    }
+    element = element.parentElement;
+  }
+  return element;
+};
+
 const insightsListener = (BaseComponent: any) => {
   function WithInsightsListener(props: WithInsightsListenerProps) {
     const handleClick = (event: MouseEvent): void => {
-      if (!hasDataAttributes(event.target as HTMLElement)) {
-        return;
-      }
-
       if (!props.insights) {
         throw new Error(
           'The `insightsClient` option has not been provided to `instantsearch`.'
         );
       }
-
-      const { method, payload } = readDataAttributes(
-        event.target as HTMLElement
+      const insightsTarget = findInsightsTarget(
+        event.target as HTMLElement | null,
+        event.currentTarget as HTMLElement | null
       );
+
+      if (!insightsTarget) return;
+
+      const { method, payload } = readDataAttributes(insightsTarget);
 
       props.insights(method, payload);
     };

src/lib/__tests__/insights-listener-test.tsx
@@ -61,6 +61,62 @@ describe('withInsightsListener', () => {
     });
   });
 
+  it('should capture clicks performed on inner elements whose parents have data-insights-method defined', () => {
+    const payload = btoa(
+      JSON.stringify({ objectIDs: ['1'], eventName: 'Product Clicked' })
+    );
+
+    const Hits = () => (
+      <div>
+        <a
+          data-insights-method="clickedObjectIDsAfterSearch"
+          data-insights-payload={payload}
+        >
+          <h1> Product 1 </h1>
+        </a>
+      </div>
+    );
+
+    const insights = jest.fn();
+
+    const hits = [
+      { objectID: '1' },
+      { objectID: '2' },
+      { objectID: '3' },
+      { objectID: '4' },
+      { objectID: '5' },
+    ];
+
+    const results = {
+      index: 'theIndex',
+      queryID: 'theQueryID',
+      hitsPerPage: 2,
+      page: 1,
+    };
+
+    const instantSearchInstance = {
+      insightsClient: jest.fn(),
+    };
+    const HitsWithInsightsListener: any = withInsightsListener(Hits);
+    const { container } = render(
+      <HitsWithInsightsListener
+        hits={hits}
+        results={results}
+        instantSearchInstance={instantSearchInstance}
+        insights={insights}
+      />
+    );
+    const innerChild = container.querySelector('h1') as HTMLElement;
+
+    fireEvent.click(innerChild);
+
+    expect(insights).toHaveBeenCalledTimes(1);
+    expect(insights).toHaveBeenCalledWith('clickedObjectIDsAfterSearch', {
+      eventName: 'Product Clicked',
+      objectIDs: ['1'],
+    });
+  });
+
   it('should not capture clicks performed on inner elements with no data-insights-method defined', () => {
     const payload = btoa(
       JSON.stringify({ objectIDs: ['1'], eventName: 'Add to Cart' })

commit b3c45a3deae3e4083c8ce9308adebce707f5ebfa

fix(insights): move 'insightsClient not provided error' to wrapper level
Prior to this change, this error was fired in the click listeners. As a result, users who used a custom component (and therefore a custom listeners) could never see it (they saw instead: insights is `TypeError: insights is not a function`).

Now the insightsClient wrapper itself is responsible for throwing this error.

This also fixes a bug introduced in https://github.com/algolia/instantsearch.js/pull/4197 when the `if (!props.insights) { ... }` was accidentaly brought on top of the handler.

This is not breaking change.

src/lib/insights/client.ts
@@ -75,13 +75,18 @@ export const inferPayload = ({
 };
 
 const wrapInsightsClient = (
-  aa: InsightsClient,
+  aa: InsightsClient | null,
   results: SearchResults,
   hits: Hits
 ): InsightsClientWrapper => (
   method: InsightsClientMethod,
   payload: Partial<InsightsClientPayload>
 ) => {
+  if (!aa) {
+    throw new Error(
+      'The `insightsClient` option has not been provided to `instantsearch`.'
+    );
+  }
   if (!Array.isArray(payload.objectIDs)) {
     throw new TypeError('Expected `objectIDs` to be an array.');
   }
@@ -109,12 +114,7 @@ export default function withInsights(
     isFirstRender: boolean
   ) => {
     const { results, hits, instantSearchInstance } = renderOptions;
-    if (
-      results &&
-      hits &&
-      instantSearchInstance &&
-      instantSearchInstance.insightsClient /* providing the insightsClient is optional */
-    ) {
+    if (results && hits && instantSearchInstance) {
       const insights = wrapInsightsClient(
         instantSearchInstance.insightsClient,
         results,

src/lib/insights/listener.tsx
@@ -26,11 +26,6 @@ const findInsightsTarget = (
 const insightsListener = (BaseComponent: any) => {
   function WithInsightsListener(props: WithInsightsListenerProps) {
     const handleClick = (event: MouseEvent): void => {
-      if (!props.insights) {
-        throw new Error(
-          'The `insightsClient` option has not been provided to `instantsearch`.'
-        );
-      }
       const insightsTarget = findInsightsTarget(
         event.target as HTMLElement | null,
         event.currentTarget as HTMLElement | null

src/lib/__tests__/insights-client-test.ts
@@ -86,10 +86,11 @@ describe('withInsights', () => {
       expect(renderProps).toHaveProperty('insights');
       expect(renderProps.insights).toBeInstanceOf(Function);
     });
-
-    it('should not expose the insights client wrapper to renderOptions if not passed to instantSearchInstance', () => {
+    it('should expose the insights client wrapper even when insightsClient was not provided', () => {
       const renderFn = jest.fn();
-      const instantSearchInstance = { insightsClient: undefined };
+      const instantSearchInstance = {
+        /* insightsClient was not passed */
+      };
       const results = {
         index: 'theIndex',
         hits: [
@@ -102,7 +103,34 @@ describe('withInsights', () => {
       createWidgetWithInsights({ renderFn, instantSearchInstance, results });
 
       const [renderProps] = renderFn.mock.calls[0];
-      expect(renderProps).not.toHaveProperty('insights');
+      expect(renderProps).toHaveProperty('insights');
+      expect(renderProps.insights).toBeInstanceOf(Function);
+    });
+    it('should expose the insights client wrapper that throws when insightsClient was not provided', () => {
+      const renderFn = jest.fn();
+      const instantSearchInstance = {
+        /* insightsClient was not passed */
+      };
+      const results = {
+        index: 'theIndex',
+        hits: [
+          { objectID: '1', __queryID: 'theQueryID', __position: 9 },
+          { objectID: '2', __queryID: 'theQueryID', __position: 10 },
+          { objectID: '3', __queryID: 'theQueryID', __position: 11 },
+          { objectID: '4', __queryID: 'theQueryID', __position: 12 },
+        ],
+      };
+      createWidgetWithInsights({ renderFn, instantSearchInstance, results });
+
+      const [renderProps] = renderFn.mock.calls[0];
+      expect(() => {
+        renderProps.insights('clickedObjectIDsAfterSearch', {
+          eventName: 'add to favorites',
+          objectIDs: ['1'],
+        });
+      }).toThrowErrorMatchingInlineSnapshot(
+        `"The \`insightsClient\` option has not been provided to \`instantsearch\`."`
+      );
     });
   });
 