3f1315cabdcc94c3c7bb21b30b49f1cda627b5e8
Youcef Mammar <youcef.mammar@algolia.com>
2020-01-08 14:47:35 +0100

    fix linting

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,6 +3,4 @@ export * from './snippet';
 export { default as highlight } from './highlight';
 export { default as snippet } from './snippet';
 export { default as insights } from './insights';
-export {
-  default as getInsightsAnonymousUserToken
-} from './get-insights-anonymous-user-token';
+export { default as getInsightsAnonymousUserToken } from './get-insights-anonymous-user-token';
