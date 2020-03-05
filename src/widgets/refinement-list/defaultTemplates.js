bc589dd5dea60987f5f6679fdfd17e06b5680ab1
François Chalifour <francois.chalifour@gmail.com>
2020-03-05 14:46:43 +0100

    fix(refinementList): prevent XSS via routing

--- src/widgets/refinement-list/defaultTemplates.js
+++ src/widgets/refinement-list/defaultTemplates.js
@@ -4,7 +4,7 @@ export default {
          class="{{cssClasses.checkbox}}"
          value="{{value}}"
          {{#isRefined}}checked{{/isRefined}} />
-  <span class="{{cssClasses.labelText}}">{{{highlighted}}}</span>
+  <span class="{{cssClasses.labelText}}">{{#isFromSearch}}{{{highlighted}}}{{/isFromSearch}}{{^isFromSearch}}{{highlighted}}{{/isFromSearch}}</span>
   <span class="{{cssClasses.count}}">{{#helpers.formatNumber}}{{count}}{{/helpers.formatNumber}}</span>
 </label>`,
   showMoreText: `
