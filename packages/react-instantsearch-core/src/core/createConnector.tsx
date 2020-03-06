176fdca5e6fbd817a338096a263e1576ab5da2fb
François Chalifour <francoischalifour@users.noreply.github.com>
2020-03-06 11:11:50 +0100

    fix(deps): fix "too much recursion" error with circular deps (algolia/react-instantsearch#2899)

--- packages/react-instantsearch-core/src/core/createConnector.tsx
+++ packages/react-instantsearch-core/src/core/createConnector.tsx
@@ -1,5 +1,5 @@
 import React, { Component, ReactType } from 'react';
-import isEqual from 'fast-deep-equal';
+import isEqual from 'react-fast-compare';
 import { shallowEqual, getDisplayName, removeEmptyKey } from './utils';
 import {
   InstantSearchConsumer,
