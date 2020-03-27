a544ca9fcf45cc8fff6caae693a2dc104badc859
Nuno Maduro <enunomaduro@gmail.com>
2020-03-27 15:04:31 +0100

    chore(ts-migration): refactors connector types (#4361)

--- src/connectors/infinite-hits/connectInfiniteHits.ts
+++ src/connectors/infinite-hits/connectInfiniteHits.ts
@@ -3,13 +3,7 @@ import {
   AlgoliaSearchHelper as Helper,
   SearchParameters,
 } from 'algoliasearch-helper';
-import {
-  Renderer,
-  RendererOptions,
-  WidgetFactory,
-  Hits,
-  Unmounter,
-} from '../../types';
+import { Hits, Connector } from '../../types';
 import {
   checkRendering,
   createDocumentationMessageGenerator,
@@ -24,37 +18,27 @@ export type InfiniteHitsConnectorParams = Partial<
   InfiniteHitsRendererWidgetParams
 >;
 
-export type InfiniteHitsRendererOptions<TInfiniteHitsWidgetParams> = {
+export type InfiniteHitsRendererOptions = {
   showPrevious: () => void;
   showMore: () => void;
   isFirstPage: boolean;
   isLastPage: boolean;
-} & RendererOptions<TInfiniteHitsWidgetParams>;
-
-export type InfiniteHitsRenderer<TInfiniteHitsWidgetParams> = Renderer<
-  InfiniteHitsRendererOptions<
-    InfiniteHitsConnectorParams & TInfiniteHitsWidgetParams
-  >
->;
-
-export type InfiniteHitsWidgetFactory<
-  TInfiniteHitsWidgetParams
-> = WidgetFactory<InfiniteHitsConnectorParams & TInfiniteHitsWidgetParams>;
-
-export type InfiniteHitsConnector = <TInfiniteHitsWidgetParams>(
-  render: InfiniteHitsRenderer<TInfiniteHitsWidgetParams>,
-  unmount?: Unmounter
-) => InfiniteHitsWidgetFactory<TInfiniteHitsWidgetParams>;
+};
 
 const withUsage = createDocumentationMessageGenerator({
   name: 'infinite-hits',
   connector: true,
 });
 
-const connectInfiniteHits: InfiniteHitsConnector = (
+export type InfiniteHitsConnector = Connector<
+  InfiniteHitsRendererOptions,
+  InfiniteHitsConnectorParams
+>;
+
+const connectInfiniteHits: InfiniteHitsConnector = function connectInfiniteHits(
   renderFn,
   unmountFn = noop
-) => {
+) {
   checkRendering(renderFn, withUsage());
 
   return widgetParams => {
