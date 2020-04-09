571d60a7bde3824d497072624aa1aedf7b9ee477
Nuno Maduro <enunomaduro@gmail.com>
2020-04-09 12:54:28 +0200

    chore(ts-migration): adds api extrator for public definitions files and strip Internals (#4374)

--- scripts/build/types.js
+++ scripts/build/types.js
@@ -2,54 +2,51 @@
 
 /* eslint-disable import/no-commonjs, no-console */
 
-const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
-const tmpdir = `${require('os').tmpdir()}/instantsearch.js/types`;
-shell.exec(`rm -rf ${tmpdir}`);
 
 shell.exec(
-  `tsc -p tsconfig.declaration.json --outDir ${tmpdir}; mv ${tmpdir}/index.es.d.ts ${tmpdir}/index.d.ts`
+  `tsc -p tsconfig.declaration.json --outDir es/; mv es/index.es.d.ts es/index.d.ts`
 );
 
 const pkgDir = path.resolve('');
-const pkg = require(`${pkgDir}/package.json`);
 
 console.log();
-console.log(`Rolling up type definitions for InstantSearch...`);
+console.log(`Validating definitions...`);
 
 const { Extractor, ExtractorConfig } = require('@microsoft/api-extractor');
 
 const extractorConfigPath = path.resolve(pkgDir, `api-extractor.json`);
 const extractorConfig = ExtractorConfig.loadFileAndPrepare(extractorConfigPath);
 
-extractorConfig.mainEntryPointFilePath = `${tmpdir}/index.d.ts`;
+const publicExports = [
+  'index.d.ts',
+  // 'components/index.d.ts' -> does not contains index.d.ts yet
+  'connectors/index.d.ts',
+  // 'lib/main.d.ts', -> Api extrator "import * as ___ from ___;" is not supported yet for local files
+  'middleware/index.d.ts',
+  'types/index.d.ts',
+  'widgets/index.d.ts', //  -> It does not compile as WidgetFactory is not imported in all files
+];
 
-const result = Extractor.invoke(extractorConfig, {
-  localBuild: true,
-  showVerboseMessages: true,
-});
-
-if (result.succeeded) {
-  if (pkg.buildOptions && pkg.buildOptions.dts) {
-    const dtsPath = path.resolve(pkgDir, pkg.types);
-
-    (async () => {
-      const existing = await fs.readFile(dtsPath, 'utf-8');
-      const toAdd = await Promise.all(
-        pkg.buildOptions.dts.map(file => {
-          return fs.readFile(path.resolve(pkgDir, file), 'utf-8');
-        })
-      );
-      await fs.writeFile(dtsPath, `${existing}\n${toAdd.join('\n')}`);
-      console.log(`API Extractor completed successfully.`);
-    })();
-  }
-} else {
-  console.error(
-    `API Extractor completed with ${result.errorCount} errors` +
-      ` and ${result.warningCount} warnings`
+const validateExport = publicExport => {
+  extractorConfig.mainEntryPointFilePath = `${pkgDir}/es/${publicExport}`;
+  console.log(
+    `Validating type definitions of: ${extractorConfig.mainEntryPointFilePath}`
   );
+  const result = Extractor.invoke(extractorConfig, {
+    localBuild: true,
+    showVerboseMessages: true,
+  });
 
-  process.exitCode = 1;
-}
+  if (!result.succeeded) {
+    console.error(
+      `API Extractor completed with ${result.errorCount} errors` +
+        ` and ${result.warningCount} warnings`
+    );
+
+    process.exitCode = 1;
+  }
+};
+
+publicExports.forEach(validateExport);
