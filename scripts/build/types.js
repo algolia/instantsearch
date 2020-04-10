7eb0f754c12d3b193c9ae022f39c7e85375420f5
Nuno Maduro <enunomaduro@gmail.com>
2020-04-10 14:47:05 +0200

    chore(ts-migration): improves validating of types with api extractor (#4380)

--- scripts/build/types.js
+++ scripts/build/types.js
@@ -2,6 +2,7 @@
 
 /* eslint-disable import/no-commonjs, no-console */
 
+const fs = require('fs');
 const path = require('path');
 const shell = require('shelljs');
 
@@ -9,44 +10,49 @@ shell.exec(
   `tsc -p tsconfig.declaration.json --outDir es/; mv es/index.es.d.ts es/index.d.ts`
 );
 
-const pkgDir = path.resolve('');
-
 console.log();
 console.log(`Validating definitions...`);
 
 const { Extractor, ExtractorConfig } = require('@microsoft/api-extractor');
 
-const extractorConfigPath = path.resolve(pkgDir, `api-extractor.json`);
-const extractorConfig = ExtractorConfig.loadFileAndPrepare(extractorConfigPath);
-
 const publicExports = [
-  'index.d.ts',
-  // 'components/index.d.ts' -> does not contains index.d.ts yet
-  'connectors/index.d.ts',
-  // 'lib/main.d.ts', -> Api extrator "import * as ___ from ___;" is not supported yet for local files
-  'middleware/index.d.ts',
-  'types/index.d.ts',
-  'widgets/index.d.ts', //  -> It does not compile as WidgetFactory is not imported in all files
+  '',
+  // 'components' -> does not contains index.d.ts yet
+  'connectors',
+  // 'lib', -> Api extrator "import * as ___ from ___;" is not supported yet for local files
+  // 'middleware',
+  'helpers',
+  'types',
+  'widgets', //  -> It does not compile as WidgetFactory is not imported in all files
 ];
 
-const validateExport = publicExport => {
-  extractorConfig.mainEntryPointFilePath = `${pkgDir}/es/${publicExport}`;
-  console.log(
-    `Validating type definitions of: ${extractorConfig.mainEntryPointFilePath}`
+shell.cd(__dirname);
+shell.mkdir('-p', '.temp');
+
+fs.writeFileSync(
+  '.temp/index.d.ts',
+  publicExports
+    .map(publicExport => `../../../es/${publicExport}`)
+    .map(exportedFile => {
+      return `export * from '${exportedFile}';`;
+    })
+    .join('\r\n')
+);
+
+const extractorConfig = ExtractorConfig.loadFileAndPrepare(
+  path.resolve(path.join(__dirname, 'api-extractor.json'))
+);
+
+const result = Extractor.invoke(extractorConfig, {
+  localBuild: true,
+  showVerboseMessages: true,
+});
+
+if (!result.succeeded) {
+  console.error(
+    `API Extractor completed with ${result.errorCount} errors` +
+      ` and ${result.warningCount} warnings`
   );
-  const result = Extractor.invoke(extractorConfig, {
-    localBuild: true,
-    showVerboseMessages: true,
-  });
 
-  if (!result.succeeded) {
-    console.error(
-      `API Extractor completed with ${result.errorCount} errors` +
-        ` and ${result.warningCount} warnings`
-    );
-
-    process.exitCode = 1;
-  }
-};
-
-publicExports.forEach(validateExport);
+  process.exitCode = 1;
+}
