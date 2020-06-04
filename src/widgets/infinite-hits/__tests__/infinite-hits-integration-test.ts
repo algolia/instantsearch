da267dad1413d8e5db3759061aacc3a6de332575
eunjae-lee <karis612@gmail.com>
2020-06-04 16:35:17 +0200

    chore: fix lint error

--- src/widgets/infinite-hits/__tests__/infinite-hits-integration-test.ts
+++ src/widgets/infinite-hits/__tests__/infinite-hits-integration-test.ts
@@ -1,18 +1,5 @@
 import algoliasearch from 'algoliasearch/lite';
-import {
-  getByLabelText,
-  getAllByText,
-  getByText,
-  getByTestId,
-  queryByTestId,
-  // Tip: all queries are also exposed on an object
-  // called "queries" which you could import here as well
-  wait,
-  waitFor,
-  fireEvent,
-} from '@testing-library/dom';
-// adds special assertions like toHaveTextContent
-import '@testing-library/jest-dom/extend-expect';
+import { getByText, waitFor, fireEvent } from '@testing-library/dom';
 
 import instantsearch from '../../../index.es';
 import { infiniteHits, configure } from '../../';
