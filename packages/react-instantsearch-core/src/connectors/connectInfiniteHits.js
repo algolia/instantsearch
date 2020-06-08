e58333a7af485ddbbab09f7000151733b0de3b0d
Eunjae Lee <eunjae.lee@algolia.com>
2020-06-08 14:19:46 +0200

    feat(infinite-hits): support cache (#2921)
    Co-authored-by: Yannick Croissant <yannick.croissant@algolia.com>

--- packages/react-instantsearch-core/src/connectors/connectInfiniteHits.js
+++ packages/react-instantsearch-core/src/connectors/connectInfiniteHits.js
@@ -29,6 +29,36 @@ function getCurrentRefinement(props, searchState, context) {
   return currentRefinement;
 }
 
+function getStateWithoutPage(state) {
+  const { page, ...rest } = state || {};
+  return rest;
+}
+
+function getInMemoryCache() {
+  let cachedHits = undefined;
+  let cachedState = undefined;
+  return {
+    read({ state }) {
+      return isEqual(cachedState, getStateWithoutPage(state))
+        ? cachedHits
+        : null;
+    },
+    write({ state, hits }) {
+      cachedState = getStateWithoutPage(state);
+      cachedHits = hits;
+    },
+  };
+}
+
+function extractHitsFromCachedHits(cachedHits) {
+  return Object.keys(cachedHits)
+    .map(Number)
+    .sort((a, b) => a - b)
+    .reduce((acc, page) => {
+      return acc.concat(cachedHits[page]);
+    }, []);
+}
+
 /**
  * InfiniteHits connector provides the logic to create connected
  * components that will render an continuous list of results retrieved from
@@ -48,12 +78,16 @@ export default createConnector({
       multiIndexContext: props.indexContextValue,
     });
 
-    this._allResults = this._allResults || [];
     this._prevState = this._prevState || {};
+
+    const cache = props.cache || getInMemoryCache();
+    if (this._cachedHits === undefined) {
+      this._cachedHits = cache.read({ state: searchState }) || {};
+    }
 
     if (!results) {
       return {
-        hits: [],
+        hits: extractHitsFromCachedHits(this._cachedHits),
         hasPrevious: false,
         hasMore: false,
         refine: () => {},
@@ -76,32 +110,34 @@ export default createConnector({
       results.queryID
     );
 
-    if (
-      this._firstReceivedPage === undefined ||
-      !isEqual(currentState, this._prevState)
-    ) {
-      this._allResults = [...hitsWithPositionsAndQueryID];
-      this._firstReceivedPage = page;
-      this._lastReceivedPage = page;
-    } else if (this._lastReceivedPage < page) {
-      this._allResults = [...this._allResults, ...hitsWithPositionsAndQueryID];
-      this._lastReceivedPage = page;
-    } else if (this._firstReceivedPage > page) {
-      this._allResults = [...hitsWithPositionsAndQueryID, ...this._allResults];
-      this._firstReceivedPage = page;
+    if (!isEqual(currentState, this._prevState)) {
+      this._cachedHits = cache.read({ state: searchState }) || {};
+    }
+    if (this._cachedHits[page] === undefined) {
+      this._cachedHits[page] = hitsWithPositionsAndQueryID;
+      cache.write({ state: searchState, hits: this._cachedHits });
     }
 
     this._prevState = currentState;
+    /*
+      Math.min() and Math.max() returns Infinity or -Infinity when no argument is given.
+      But there is always something in this point because of `this._cachedHits[page]`.
+    */
+    const firstReceivedPage = Math.min(
+      ...Object.keys(this._cachedHits).map(Number)
+    );
+    const lastReceivedPage = Math.max(
+      ...Object.keys(this._cachedHits).map(Number)
+    );
 
-    const hasPrevious = this._firstReceivedPage > 0;
+    const hasPrevious = firstReceivedPage > 0;
     const lastPageIndex = nbPages - 1;
-    const hasMore = page < lastPageIndex;
-    const refinePrevious = event =>
-      this.refine(event, this._firstReceivedPage - 1);
-    const refineNext = event => this.refine(event, this._lastReceivedPage + 1);
+    const hasMore = lastReceivedPage < lastPageIndex;
+    const refinePrevious = event => this.refine(event, firstReceivedPage - 1);
+    const refineNext = event => this.refine(event, lastReceivedPage + 1);
 
     return {
-      hits: this._allResults,
+      hits: extractHitsFromCachedHits(this._cachedHits),
       hasPrevious,
       hasMore,
       refinePrevious,
@@ -120,8 +156,13 @@ export default createConnector({
   },
 
   refine(props, searchState, event, index) {
-    if (index === undefined && this._lastReceivedPage !== undefined) {
-      index = this._lastReceivedPage + 1;
+    const pages = Object.keys(this._cachedHits || {}).map(Number);
+    const lastReceivedPage =
+      pages.length === 0 ? undefined : Math.max(...pages);
+    // If there is no key in `this._cachedHits`,
+    // then `lastReceivedPage` should be `undefined`.
+    if (index === undefined && lastReceivedPage !== undefined) {
+      index = lastReceivedPage + 1;
     } else if (index === undefined) {
       index = getCurrentRefinement(props, searchState, {
         ais: props.contextValue,
---
+++ packages/react-instantsearch-dom/src/lib/infiniteHitsCache/index.js
@@ -0,0 +1,3 @@
+export {
+  default as createInfiniteHitsSessionStorageCache,
+} from './sessionStorage';
