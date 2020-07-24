10ce292051818384700fbee8b0b9c746483d3f4f
eunjae-lee <karis612@gmail.com>
2020-07-24 17:20:41 +0200

    inline functions

--- src/middleware/insights.ts
+++ src/middleware/insights.ts
@@ -1,5 +1,5 @@
 import { Middleware } from '.';
-import { InsightsClient, InstantSearch } from '../types';
+import { InsightsClient } from '../types';
 import { getInsightsAnonymousUserToken } from '../helpers';
 
 export type InsightsProps = {
@@ -14,27 +14,54 @@ export const createInsightsMiddleware: CreateInsightsMiddleware = props => {
     throw new Error('The `insightsClient` option is required.');
   }
   return ({ instantSearchInstance }) => {
-    const setUserToken = (userToken?: string) => {
-      // At the time this middleware is subscribed, `mainIndex.init()` is already called.
-      // It means `mainIndex.getHelper()` exists.
-      if (userToken) {
-        instantSearchInstance.mainIndex
-          .getHelper()!
-          .setQueryParameter('userToken', userToken);
-      }
-    };
-
     return {
       onStateChange() {},
       subscribe() {
-        updateQueryParameter(instantSearchInstance);
-        ensureInitialization({
-          instantSearchInstance,
-          insightsClient,
-          callback: () => {
-            setUserTokenFromQueueOrCookie(insightsClient, setUserToken);
-            watchUserToken(insightsClient, setUserToken);
-          },
+        const setUserTokenToSearch = (userToken?: string) => {
+          // At the time this middleware is subscribed, `mainIndex.init()` is already called.
+          // It means `mainIndex.getHelper()` exists.
+          if (userToken) {
+            instantSearchInstance.mainIndex
+              .getHelper()!
+              .setQueryParameter('userToken', userToken);
+          }
+        };
+
+        instantSearchInstance.mainIndex
+          .getHelper()!
+          .setQueryParameter('clickAnalytics', true);
+
+        insightsClient('_get', '_hasCredentials', (hasCredentials: boolean) => {
+          if (!hasCredentials) {
+            const [appId, apiKey] = getAppIdAndApiKey(
+              instantSearchInstance.client
+            );
+            insightsClient('init', { appId, apiKey });
+          }
+
+          // When `aa('init', { ... })` is called, it creates an anonymous user token in cookie.
+          // We can set it as userToken.
+          setUserTokenToSearch(getInsightsAnonymousUserToken());
+
+          if (Array.isArray((insightsClient as any).queue)) {
+            // Context: The umd build of search-insights is asynchronously loaded by the snippet.
+            //
+            // When user called `aa('setUserToken', 'my-user-token')` before `search-insights` is loaded,
+            // it is stored in `aa.queue` and we are reading it to set userToken to search call.
+            // This queue is meant to be consumed whenever `search-insights` is loaded and when it runs `processQueue()`.
+            // But the reason why we handle it here is to prevent the first search API from being triggered
+            // without userToken because search-insights is not loaded yet.
+            (insightsClient as any).queue.forEach(([method, firstArgument]) => {
+              if (method === 'setUserToken') {
+                setUserTokenToSearch(firstArgument);
+              }
+            });
+          }
+
+          // This updates userToken which is set explicitly by `aa('setUserToken', userToken)`
+          insightsClient('onUserTokenChange', setUserTokenToSearch, {
+            immediate: true,
+          });
         });
       },
       unsubscribe() {
@@ -43,26 +70,6 @@ export const createInsightsMiddleware: CreateInsightsMiddleware = props => {
     };
   };
 };
-
-function ensureInitialization({
-  instantSearchInstance,
-  insightsClient,
-  callback,
-}: {
-  instantSearchInstance: InstantSearch;
-  insightsClient: InsightsClient;
-  callback: () => void;
-}) {
-  insightsClient('_get', '_hasCredentials', (hasCredentials: boolean) => {
-    if (hasCredentials) {
-      callback();
-    } else {
-      const [appId, apiKey] = getAppIdAndApiKey(instantSearchInstance.client);
-      insightsClient('init', { appId, apiKey });
-      callback();
-    }
-  });
-}
 
 function getAppIdAndApiKey(searchClient) {
   if (searchClient.transporter) {
@@ -77,43 +84,4 @@ function getAppIdAndApiKey(searchClient) {
     // searchClient v3
     return [searchClient.applicationID, searchClient.apiKey];
   }
-}
-
-function updateQueryParameter(instantSearchInstance) {
-  instantSearchInstance.mainIndex
-    .getHelper()
-    .setQueryParameter('clickAnalytics', true);
-}
-
-function setUserTokenFromQueueOrCookie(
-  insightsClient,
-  setUserToken: (userToken?: string) => void
-) {
-  if (Array.isArray(insightsClient.queue)) {
-    // Context: The umd build of search-insights is asynchronously loaded by the snippet.
-    //
-    // When user called `aa('setUserToken', 'my-user-token')` before `search-insights` is loaded,
-    // it is stored in `aa.queue` and we can read it to set user token.
-    // This queue is meant to be consumed whenever `search-insights` is loaded and when it runs `processQueue()`.
-    // But the reason why we handle it here is to prevent the first search API from triggering
-    // without userToken because search-insights is not loaded yet.
-    insightsClient.queue.forEach(([method, firstArgument]) => {
-      if (method === 'setUserToken') {
-        setUserToken(firstArgument);
-        return;
-      }
-    });
-  }
-
-  // When `aa('init', { ... })` is called, it creates an anonymous user token in cookie.
-  // We can set it as userToken first.
-  setUserToken(getInsightsAnonymousUserToken());
-}
-
-// This function updates userToken which is set explicitly by `aa('setUserToken', userToken)`
-function watchUserToken(
-  insightsClient: InsightsClient,
-  setUserToken: (userToken?: string) => void
-) {
-  insightsClient('onUserTokenChange', setUserToken, { immediate: true });
 }
