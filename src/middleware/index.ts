000d7083a3cb52840746430afd59d8654965d2d7
eunjae-lee <karis612@gmail.com>
2020-07-29 22:02:49 +0200

    clean up types

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,16 +1,1 @@
-import { UiState, InstantSearch } from '../types';
-
-export type MiddlewareDefinition = {
-  onStateChange(options: { uiState: UiState }): void;
-  subscribe(): void;
-  unsubscribe(): void;
-};
-
-export type MiddlewareOptions = {
-  instantSearchInstance: InstantSearch;
-};
-
-export type Middleware = (options: MiddlewareOptions) => MiddlewareDefinition;
-
-export { createRouter, RouterProps } from './createRouter';
 export { createInsightsMiddleware } from './insights';
