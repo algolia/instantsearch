0a6cbaed676c2bcda7b732f9ac75dca68e16193b
Eunjae Lee <eunjae.lee@algolia.com>
2020-09-29 11:17:36 +0200

    Update src/helpers/get-insights-anonymous-user-token.ts
    Co-authored-by: François Chalifour <francoischalifour@users.noreply.github.com>

--- src/helpers/get-insights-anonymous-user-token.ts
+++ src/helpers/get-insights-anonymous-user-token.ts
@@ -27,7 +27,7 @@ export function getInsightsAnonymousUserTokenInternal(): string | undefined {
 export default function getInsightsAnonymousUserToken(): string | undefined {
   warning(
     false,
-    `\`getInsightsAnonymousUserToken\` function has been deprecated. It is still supported in 4.x releases, but not further. It is replaced by \`insights\` middleware.
+    `\`getInsightsAnonymousUserToken\` function has been deprecated. It is still supported in 4.x releases, but not further. It is replaced by the \`insights\` middleware.
 
 For more information, visit https://www.algolia.com/doc/guides/getting-insights-and-analytics/search-analytics/click-through-and-conversions/how-to/send-click-and-conversion-events-with-instantsearch/js/`
   );
