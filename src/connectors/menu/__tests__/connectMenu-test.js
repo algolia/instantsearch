239906c7fdb36c691b9a9aca343802a8ccc616c8
Clément Vannicatte <20689156+shortcuts@users.noreply.github.com>
2020-10-30 11:31:43 +0100

    feat(menu): implement `getRenderState` and `getWidgetRenderState` (#4540)
    * feat(menu): implement `getRenderState` and `getWidgetRenderState`
    
    * Removed duplicated declaration of `jsHelper`, moved `cachedToggleShowMore` binding to `init` to avoid it happens more than once

--- src/connectors/menu/__tests__/connectMenu-test.js
+++ src/connectors/menu/__tests__/connectMenu-test.js
@@ -2,8 +2,13 @@ import jsHelper, {
   SearchResults,
   SearchParameters,
 } from 'algoliasearch-helper';
+import { createSearchClient } from '../../../../test/mock/createSearchClient';
 import { createSingleSearchResponse } from '../../../../test/mock/createAPIResponse';
 import { createInstantSearch } from '../../../../test/mock/createInstantSearch';
+import {
+  createInitOptions,
+  createRenderOptions,
+} from '../../../../test/mock/createWidget';
 import connectMenu from '../connectMenu';
 
 describe('connectMenu', () => {
@@ -435,6 +440,117 @@ See documentation: https://www.algolia.com/doc/api-reference/widgets/menu/js/#co
       widget.getWidgetSearchParameters(new SearchParameters(), { uiState: {} })
     );
     expect(() => widget.dispose({ helper, state: helper.state })).not.toThrow();
+  });
+
+  describe('getRenderState', () => {
+    test('returns the render state', () => {
+      const renderFn = jest.fn();
+      const unmountFn = jest.fn();
+      const createMenu = connectMenu(renderFn, unmountFn);
+      const menu = createMenu({
+        attribute: 'brand',
+      });
+      const helper = jsHelper(
+        createSearchClient(),
+        'indexName',
+        menu.getWidgetSearchParameters(new SearchParameters(), { uiState: {} })
+      );
+
+      const renderState1 = menu.getRenderState(
+        { menu: {} },
+        createInitOptions({ helper })
+      );
+
+      expect(renderState1.menu).toEqual({
+        items: [],
+        createURL: undefined,
+        refine: undefined,
+        canRefine: false,
+        isShowingMore: false,
+        toggleShowMore: expect.any(Function),
+        canToggleShowMore: false,
+        widgetParams: { attribute: 'brand' },
+      });
+    });
+
+    test('returns the render state with results', () => {
+      const renderFn = jest.fn();
+      const unmountFn = jest.fn();
+      const createMenu = connectMenu(renderFn, unmountFn);
+      const menu = createMenu({
+        attribute: 'brand',
+      });
+      const helper = jsHelper(
+        createSearchClient(),
+        'indexName',
+        menu.getWidgetSearchParameters(new SearchParameters(), {
+          uiState: {},
+        })
+      );
+
+      menu.init(createInitOptions({ helper }));
+
+      expect(
+        menu.getRenderState(
+          {},
+          createRenderOptions({
+            helper,
+            results: new SearchResults(helper.state, [
+              createSingleSearchResponse({
+                hits: [],
+                facets: {
+                  brand: 300,
+                },
+              }),
+            ]),
+          })
+        )
+      ).toEqual({
+        menu: {
+          items: [],
+          canRefine: false,
+          refine: expect.any(Function),
+          sendEvent: expect.any(Function),
+          createURL: expect.any(Function),
+          widgetParams: { attribute: 'brand' },
+          isShowingMore: false,
+          toggleShowMore: expect.any(Function),
+          canToggleShowMore: false,
+        },
+      });
+    });
+  });
+
+  describe('getWidgetRenderState', () => {
+    test('returns the widget render state', () => {
+      const renderFn = jest.fn();
+      const unmountFn = jest.fn();
+      const createMenu = connectMenu(renderFn, unmountFn);
+      const menu = createMenu({
+        attribute: 'brand',
+      });
+      const helper = jsHelper(
+        createSearchClient(),
+        'indexName',
+        menu.getWidgetSearchParameters(new SearchParameters(), { uiState: {} })
+      );
+
+      const renderState1 = menu.getWidgetRenderState(
+        {},
+        createInitOptions({ helper })
+      );
+
+      expect(renderState1).toEqual({
+        items: [],
+        createURL: undefined,
+        refine: undefined,
+        canRefine: false,
+        isShowingMore: false,
+        toggleShowMore: expect.any(Function),
+        canToggleShowMore: false,
+        widgetParams: { attribute: 'brand' },
+      });
+    });
   });
 
   describe('showMore', () => {
