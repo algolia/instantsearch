332ad8f8836c64d89390bd22e512c3b079613c04
Haroen Viaene <hello@haroen.me>
2020-11-09 11:39:25 +0100

    feat(vue): update vue template for vue-instantsearch 3 (algolia/create-instantsearch-app#452)
    * feat(vue): update vue template for vue-instantsearch 3
    
    not much is different, except the version of the library and the compatibility with algoliasearch@4
    
    * update unit test

--- src/templates/Vue InstantSearch 2/.template.js
+++ src/templates/Vue InstantSearch 2/.template.js
@@ -4,8 +4,8 @@ const teardown = require('../../tasks/node/teardown');
 module.exports = {
   category: 'Web',
   libraryName: 'vue-instantsearch',
-  templateName: 'vue-instantsearch',
-  supportedVersion: '>= 3.0.0 < 4.0.0',
+  templateName: 'vue-instantsearch-2.x',
+  supportedVersion: '>= 2.0.0 < 3.0.0',
   appName: 'vue-instantsearch-app',
   keywords: ['algolia', 'InstantSearch', 'Vue', 'vue-instantsearch'],
   tasks: {
