023fda635712d712cc152161ba2a5e2ba4f9f12e
Haroen Viaene <hello@haroen.me>
2020-12-03 15:32:36 +0100

    chore(setUiState): remove warning
    we haven't needed to make changes here, so i don't think the warning is still useful :)

--- src/lib/InstantSearch.ts
+++ src/lib/InstantSearch.ts
@@ -522,24 +522,12 @@ See ${createDocumentationLink({
     }
   }
 
-  public setUiState(
-    uiState: UiState | ((uiState: UiState) => UiState),
-    suppressWarning = false
-  ): void {
+  public setUiState(uiState: UiState | ((uiState: UiState) => UiState)): void {
     if (!this.mainHelper) {
       throw new Error(
         withUsage('The `start` method needs to be called before `setUiState`.')
       );
     }
-
-    warning(
-      suppressWarning,
-      `
-\`setUiState\` provides a powerful way to manage the UI state. This is considered experimental as the API might change in a next minor version.
-
-Feel free to give us feedback on GitHub: https://github.com/algolia/instantsearch.js/issues/new
-    `
-    );
 
     // We refresh the index UI state to update the local UI state that the
     // main index passes to the function form of `setUiState`.
