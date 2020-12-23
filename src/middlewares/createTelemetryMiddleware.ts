74d3c13b2824dea258dc6fa0111b0ba6741b04cf
Haroen Viaene <hello@haroen.me>
2020-12-23 15:38:51 +0100

    extract payload recursively & safer

--- src/middlewares/createTelemetryMiddleware.ts
+++ src/middlewares/createTelemetryMiddleware.ts
@@ -1,10 +1,69 @@
-import { Middleware } from '../types';
+import { InstantSearch, Middleware, Widget } from '../types';
+import { Index } from '../widgets/index/index';
 
 type TelemetryWidget = {
   type: string;
   params: string[];
   officialWidget: boolean;
 };
+
+type TelemetryPayload = {
+  widgets: TelemetryWidget[];
+};
+
+function extractPayload(
+  widgets: Array<Widget<{ renderState: any }>>,
+  instantSearchInstance: InstantSearch,
+  payload: TelemetryPayload
+) {
+  const parent = instantSearchInstance.mainIndex;
+
+  const initOptions = {
+    instantSearchInstance,
+    parent,
+    scopedResults: parent.getScopedResults(),
+    state: parent.getHelper()!.state,
+    helper: parent.getHelper()!,
+    createURL: parent.createURL,
+    uiState: instantSearchInstance._initialUiState,
+    renderState: instantSearchInstance.renderState,
+    templatesConfig: instantSearchInstance.templatesConfig,
+    searchMetadata: {
+      isSearchStalled: instantSearchInstance._isSearchStalled,
+    },
+  };
+
+  widgets.forEach(widget => {
+    let widgetParams = {};
+
+    if (widget.getWidgetRenderState) {
+      const renderState = widget.getWidgetRenderState(initOptions);
+
+      if (renderState && renderState.widgetParams) {
+        widgetParams = renderState.widgetParams;
+      }
+    }
+
+    // since we destructure in all widgets, the parameters with defaults are set to "undefined"
+    const params = Object.keys(widgetParams).filter(
+      key => widgetParams[key] !== undefined
+    );
+
+    payload.widgets.push({
+      type: widget.$$type || 'custom.widget',
+      params,
+      officialWidget: Boolean(widget.$$officialWidget),
+    });
+
+    if (widget.$$type === 'ais.index') {
+      extractPayload(
+        (widget as Index).getWidgets(),
+        instantSearchInstance,
+        payload
+      );
+    }
+  });
+}
 
 export function createTelemetryMiddleware(): Middleware {
   const isTelemetryEnabled =
@@ -19,7 +78,7 @@ export function createTelemetryMiddleware(): Middleware {
     });
   }
 
-  const payload: { widgets: TelemetryWidget[] } = {
+  const payload: TelemetryPayload = {
     widgets: [],
   };
 
@@ -33,41 +92,11 @@ export function createTelemetryMiddleware(): Middleware {
       onStateChange() {},
       subscribe() {
         setTimeout(() => {
-          const widgets = instantSearchInstance.mainIndex.getWidgets();
-
-          const parent = instantSearchInstance.mainIndex;
-
-          const initOptions = {
+          extractPayload(
+            instantSearchInstance.mainIndex.getWidgets(),
             instantSearchInstance,
-            parent,
-            scopedResults: parent.getScopedResults(),
-            state: parent.getHelper()!.state,
-            helper: parent.getHelper()!,
-            createURL: parent.createURL,
-            uiState: instantSearchInstance._initialUiState,
-            renderState: instantSearchInstance.renderState,
-            templatesConfig: instantSearchInstance.templatesConfig,
-            searchMetadata: {
-              isSearchStalled: instantSearchInstance._isSearchStalled,
-            },
-          };
-
-          widgets.forEach(widget => {
-            const widgetParams = widget.getWidgetRenderState
-              ? (widget.getWidgetRenderState(initOptions) as any).widgetParams
-              : {};
-
-            // since we destructure in all widgets, the parameters with defaults are set to "undefined"
-            const params = Object.keys(widgetParams).filter(
-              key => widgetParams[key] !== undefined
-            );
-
-            payload.widgets.push({
-              type: widget.$$type || 'custom.widget',
-              params,
-              officialWidget: Boolean(widget.$$officialWidget),
-            });
-          });
+            payload
+          );
 
           payloadContainer.content = JSON.stringify(payload);
         }, 0);
