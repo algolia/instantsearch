bf6d953fe87ce3a3fff66a6d20756d0fa5150788
eunjae-lee <karis612@gmail.com>
2021-01-25 15:07:53 +0100

    fix eslint rule for EXPERIMENTAL_

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,12 +7,7 @@ module.exports = {
     'new-cap': [
       'error',
       {
-        capIsNewExceptions: [
-          'EXPERIMENTAL_use',
-          'EXPERIMENTAL_connectConfigureRelatedItems',
-          'EXPERIMENTAL_configureRelatedItems',
-          'EXPERIMENTAL_answers',
-        ],
+        capIsNewExceptionPattern: '(\\.|^)EXPERIMENTAL_.+',
       },
     ],
     'react/no-string-refs': 'error',
