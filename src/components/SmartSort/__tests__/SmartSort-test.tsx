7db105546e8192157a1b9f859dcfb7296462ac5f
eunjae-lee <karis612@gmail.com>
2021-02-10 23:32:13 +0100

    add tests for component and widget

---
+++ src/components/SmartSort/__tests__/SmartSort-test.tsx
@@ -0,0 +1,109 @@
+/** @jsx h */
+
+import { h } from 'preact';
+import { render, fireEvent } from '@testing-library/preact';
+
+import SmartSort from '../SmartSort';
+
+const cssClasses = {
+  root: 'root',
+};
+
+const templates = {
+  text: ({ isSmartSorted }) => {
+    return isSmartSorted ? 'See all results' : 'See relevant results';
+  },
+};
+
+describe('SmartSort', () => {
+  it('render the default status', () => {
+    const { container } = render(
+      <SmartSort
+        cssClasses={cssClasses}
+        templates={templates}
+        isSmartSorted={false}
+        refine={() => {}}
+      />
+    );
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <button
+          class="root"
+          type="button"
+        >
+          <span>
+            See relevant results
+          </span>
+        </button>
+      </div>
+    `);
+  });
+
+  it('refine on button click', () => {
+    const refine = jest.fn();
+    const { getByText } = render(
+      <SmartSort
+        cssClasses={cssClasses}
+        templates={templates}
+        isSmartSorted={false}
+        refine={refine}
+      />
+    );
+    fireEvent.click(getByText('See relevant results'));
+    expect(refine).toHaveBeenCalledTimes(1);
+  });
+
+  it('refine with given value', () => {
+    const refine = jest.fn();
+    const { getByText } = render(
+      <SmartSort
+        cssClasses={cssClasses}
+        templates={templates}
+        isSmartSorted={false}
+        relevancyStrictness={30}
+        refine={refine}
+      />
+    );
+    fireEvent.click(getByText('See relevant results'));
+    expect(refine).toHaveBeenCalledTimes(1);
+    expect(refine).toHaveBeenCalledWith(30);
+  });
+
+  it('render sorted status', () => {
+    const { container } = render(
+      <SmartSort
+        cssClasses={cssClasses}
+        templates={templates}
+        isSmartSorted={true}
+        refine={() => {}}
+      />
+    );
+    expect(container).toMatchInlineSnapshot(`
+<div>
+  <button
+    class="root"
+    type="button"
+  >
+    <span>
+      See all results
+    </span>
+  </button>
+</div>
+`);
+  });
+
+  it('refine with zero when seeing all results', () => {
+    const refine = jest.fn();
+    const { getByText } = render(
+      <SmartSort
+        cssClasses={cssClasses}
+        templates={templates}
+        isSmartSorted={true}
+        refine={refine}
+      />
+    );
+    fireEvent.click(getByText('See all results'));
+    expect(refine).toHaveBeenCalledTimes(1);
+    expect(refine).toHaveBeenCalledWith(0);
+  });
+});
---
+++ src/widgets/smart-sort/__tests__/smart-sort-test.ts
@@ -0,0 +1,111 @@
+import { render } from 'preact';
+import smartSort from '../smart-sort';
+import algoliasearchHelper, { SearchResults } from 'algoliasearch-helper';
+import { createSearchClient } from '../../../../test/mock/createSearchClient';
+import {
+  createInitOptions,
+  createRenderOptions,
+} from '../../../../test/mock/createWidget';
+import { createSingleSearchResponse } from '../../../../test/mock/createAPIResponse';
+
+jest.mock('preact', () => {
+  const module = require.requireActual('preact');
+
+  module.render = jest.fn();
+
+  return module;
+});
+
+const templates = {
+  text: ({ isSmartSorted }) => {
+    return isSmartSorted ? 'See all results' : 'See relevant results';
+  },
+};
+
+describe('smartSort', () => {
+  beforeEach(() => {
+    (render as jest.Mock).mockReset();
+  });
+
+  describe('Usage', () => {
+    it('throws without container', () => {
+      expect(() => {
+        // @ts-ignore wrong options
+        smartSort({ container: undefined });
+      }).toThrowErrorMatchingInlineSnapshot(`
+"The \`container\` option is required.
+
+See documentation: https://www.algolia.com/doc/api-reference/widgets/smart-sort/js/"
+`);
+    });
+
+    it('throws without templates.text', () => {
+      expect(() => {
+        // @ts-ignore wrong options
+        smartSort({
+          container: document.createElement('div'),
+        });
+      }).toThrowErrorMatchingInlineSnapshot(`
+"The \`templates.text\` option is required.
+
+See documentation: https://www.algolia.com/doc/api-reference/widgets/smart-sort/js/"
+`);
+    });
+  });
+
+  it('render', () => {
+    const helper = algoliasearchHelper(createSearchClient(), '', {});
+    const widget = smartSort({
+      container: document.createElement('div'),
+      cssClasses: {
+        root: 'my-SmartSort',
+      },
+      templates,
+    });
+    widget.init!(createInitOptions({ helper, state: helper.state }));
+
+    const results = {
+      nbHits: 20,
+      nbSortedHits: 14,
+      appliedRelevancyStrictness: 70,
+    };
+
+    widget.render!(
+      createRenderOptions({
+        results: new SearchResults(helper.state, [
+          createSingleSearchResponse(results),
+        ]),
+      })
+    );
+    const [, secondRender] = (render as jest.Mock).mock.calls;
+
+    expect(render).toHaveBeenCalledTimes(2);
+    expect(secondRender[0].props).toEqual(
+      expect.objectContaining({
+        cssClasses: { root: 'ais-SmartSort my-SmartSort' },
+        isSmartSorted: true,
+        refine: expect.any(Function),
+        relevancyStrictness: undefined,
+        templates,
+      })
+    );
+  });
+
+  it('pass relevancyStrictness', () => {
+    const helper = algoliasearchHelper(createSearchClient(), '', {});
+    const widget = smartSort({
+      container: document.createElement('div'),
+      relevancyStrictness: 20,
+      templates,
+    });
+    widget.init!(createInitOptions({ helper, state: helper.state }));
+
+    const [firstRender] = (render as jest.Mock).mock.calls;
+    expect(render).toHaveBeenCalledTimes(1);
+    expect(firstRender[0].props).toEqual(
+      expect.objectContaining({
+        relevancyStrictness: 20,
+      })
+    );
+  });
+});
