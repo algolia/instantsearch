ec8dfdfd1a558db48880c7987dbc0d486c86fad4
eunjae-lee <karis612@gmail.com>
2021-02-11 10:30:22 +0100

    remove test code that is no longer needed

--- stories/smart-sort.stories.ts
+++ stories/smart-sort.stories.ts
@@ -1,5 +1,4 @@
 import algoliasearch from 'algoliasearch/lite';
-import { CallEnum } from '@algolia/transporter';
 import { storiesOf } from '@storybook/html';
 import { withHits } from '../.storybook/decorators';
 import smartSort from '../src/widgets/smart-sort/smart-sort';
@@ -8,19 +7,7 @@ const searchOptions = {
   appId: 'C7RIRJRYR9',
   apiKey: '77af6d5ffb27caa5ff4937099fcb92e8',
   indexName: 'test_Bestbuy_vr_price_asc',
-  searchClient: algoliasearch(
-    'C7RIRJRYR9',
-    '77af6d5ffb27caa5ff4937099fcb92e8',
-    {
-      hosts: [
-        {
-          protocol: 'https',
-          url: 'c7rirjryr9-3.algolianet.com',
-          accept: CallEnum.Read,
-        },
-      ],
-    }
-  ),
+  searchClient: algoliasearch('C7RIRJRYR9', '77af6d5ffb27caa5ff4937099fcb92e8'),
 };
 
 storiesOf('Sorting/SmartSort', module)
