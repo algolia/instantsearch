f681b8778dc1f9f90c8c3e0c1fc17425c7b28269
eunjae-lee <karis612@gmail.com>
2021-02-16 17:27:07 +0100

    clean up the story

--- stories/smart-sort.stories.ts
+++ stories/smart-sort.stories.ts
@@ -1,4 +1,3 @@
-import algoliasearch from 'algoliasearch/lite';
 import { storiesOf } from '@storybook/html';
 import { withHits } from '../.storybook/decorators';
 import smartSort from '../src/widgets/smart-sort/smart-sort';
@@ -7,7 +6,6 @@ const searchOptions = {
   appId: 'C7RIRJRYR9',
   apiKey: '77af6d5ffb27caa5ff4937099fcb92e8',
   indexName: 'test_Bestbuy_vr_price_asc',
-  searchClient: algoliasearch('C7RIRJRYR9', '77af6d5ffb27caa5ff4937099fcb92e8'),
 };
 
 storiesOf('Sorting/SmartSort', module)
