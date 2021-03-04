067dabf2066769e77a44ec662c9719b7c74bb987
Haroen Viaene <hello@haroen.me>
2021-03-04 11:28:17 +0100

    add possible connector

--- stories/stack.stories.ts
+++ stories/stack.stories.ts
@@ -1,7 +1,8 @@
 import { storiesOf } from '@storybook/html';
 import { SearchResults } from 'algoliasearch-helper';
 import { withHits } from '../.storybook/decorators';
-import { Widget } from '../src/types';
+import { noop } from '../src/lib/utils';
+import { Widget, Connector } from '../src/types';
 import { refinementList, menu } from '../src/widgets';
 
 type DynamicWidgetsParams = {
@@ -18,6 +19,107 @@ type DynamicWidgets = Widget & {
   ): { container: HTMLElement; apply(widget: Widget): void };
 };
 
+type DynamicWidgetsRendererOptions = {
+  attributesToRender: string[];
+};
+type DynamicWidgetsConnectorParams = {
+  transformItems(items: string[], results: SearchResults): string[];
+};
+
+type DynamicWidgetsConnector = Connector<
+  DynamicWidgetsRendererOptions,
+  DynamicWidgetsConnectorParams
+>;
+
+const connectDynamicWidgets: DynamicWidgetsConnector = function connectDynamicWidgets(
+  renderFn,
+  unmountFn = noop
+) {
+  return widgetParams => {
+    const {
+      transformItems = (items =>
+        items) as DynamicWidgetsConnectorParams['transformItems'],
+    } = widgetParams || {};
+
+    const localWidgets: Map<
+      string,
+      { widget: Widget; isMounted: boolean }
+    > = new Map();
+
+    return {
+      $$type: 'ais.dynamicWidgets',
+      $$widgetType: 'ais.dynamicWidgets',
+      init() {},
+      render(renderOptions) {
+        const { parent } = renderOptions;
+        const renderState = this.getWidgetRenderState(renderOptions);
+
+        const widgetsToUnmount: Widget[] = [];
+        const widgetsToMount: Widget[] = [];
+
+        localWidgets.forEach(({ widget, isMounted }, attribute) => {
+          const shouldMount =
+            renderState.attributesToRender.indexOf(attribute) > -1;
+
+          if (!isMounted && shouldMount) {
+            widgetsToMount.push(widget);
+            localWidgets.set(attribute, {
+              widget,
+              isMounted: true,
+            });
+          } else if (isMounted && !shouldMount) {
+            widgetsToUnmount.push(widget);
+
+            localWidgets.set(attribute, {
+              widget,
+              isMounted: false,
+            });
+          }
+        });
+
+        parent!.addWidgets(widgetsToMount);
+        // make sure this only happens after the regular render, otherwise it
+        // happens too quick, since render is "deferred" for the next microtask,
+        // so this needs to be a whole task later
+        setTimeout(() => parent!.removeWidgets(widgetsToUnmount), 0);
+
+        renderFn(
+          {
+            ...renderState,
+            instantSearchInstance: renderOptions.instantSearchInstance,
+          },
+          false
+        );
+      },
+      dispose() {
+        unmountFn();
+      },
+      getRenderState(renderState) {
+        // @TODO: decide whether it makes sense to return anything,
+        // knowing that you could have multiple instances of dynamic
+        // and they are hard/impossible to distinguish at this point
+        return renderState;
+      },
+      getWidgetRenderState({ results }) {
+        if (!results) {
+          return { attributesToRender: [], widgetParams };
+        }
+
+        // retrieve the facet order out of the results:
+        // results.facetOrder.map(facet => facet.attribute)
+        const attributesToRender = transformItems([], results);
+
+        return { attributesToRender, widgetParams };
+      },
+      addWidget(attribute: string, widget: Widget) {
+        localWidgets.set(attribute, { widget, isMounted: false });
+        return this;
+      },
+      // removeWidget(attribute, widget) {},
+    };
+  };
+};
+
 function dynamicWidgets({
   container: rootContainer,
   transformItems,
@@ -28,9 +130,7 @@ function dynamicWidgets({
   > = new Map();
 
   return {
-    // @ts-expect-error
     $$type: 'ais.dynamicWidgets',
-    // @ts-expect-error
     $$widgetType: 'ais.dynamicWidgets',
     init() {},
     render({ results, parent }) {
@@ -38,21 +138,22 @@ function dynamicWidgets({
       // results.facetOrder.map(facet => facet.attribute)
       const attributesToRender = transformItems([], results);
 
+      const widgetsToUnmount: Widget[] = [];
+      const widgetsToMount: Widget[] = [];
+
       localWidgets.forEach(({ widget, isMounted, container }, attribute) => {
         const shouldMount = attributesToRender.indexOf(attribute) > -1;
 
         if (!isMounted && shouldMount) {
-          parent!.addWidgets([widget]);
+          widgetsToMount.push(widget);
           localWidgets.set(attribute, {
             widget,
             container,
             isMounted: true,
           });
         } else if (isMounted && !shouldMount) {
-          // make sure this only happens after the regular render, otherwise it happens too quick
-          // render is "deferred" for the next microtask
-          // so this needs to be a whole task later
-          setTimeout(() => parent!.removeWidgets([widget]), 0);
+          widgetsToUnmount.push(widget);
+
           localWidgets.set(attribute, {
             widget,
             container,
@@ -60,6 +161,12 @@ function dynamicWidgets({
           });
         }
       });
+
+      parent!.addWidgets(widgetsToMount);
+      // make sure this only happens after the regular render, otherwise it happens too quick
+      // render is "deferred" for the next microtask
+      // so this needs to be a whole task later
+      setTimeout(() => parent!.removeWidgets(widgetsToUnmount), 0);
 
       attributesToRender.forEach(attribute => {
         if (!localWidgets.has(attribute)) {
@@ -85,40 +192,95 @@ function dynamicWidgets({
   };
 }
 
-storiesOf('Basics/DynamicWidgets', module).add(
-  'default',
-  withHits(({ search, container }) => {
-    const stack = dynamicWidgets({
-      transformItems(_attributes, results) {
-        if (results._state.query === 'dog') {
-          return ['categories'];
-        }
-        if (results._state.query === 'lego') {
-          return ['categories', 'brand'];
-        }
-        return ['brand', 'categories'];
-      },
-      container,
-    });
+function transformItems(_attributes, results) {
+  if (results._state.query === 'dog') {
+    return ['categories'];
+  }
+  if (results._state.query === 'lego') {
+    return ['categories', 'brand'];
+  }
+  return ['brand', 'categories'];
+}
 
-    const categoryContainer = stack.createContainer('categories');
+storiesOf('Basics/DynamicWidgets', module)
+  .add(
+    'default',
+    withHits(({ search, container }) => {
+      const stack = dynamicWidgets({
+        transformItems,
+        container,
+      });
 
-    categoryContainer.apply(
-      menu({
-        container: categoryContainer.container,
-        attribute: 'categories',
-      })
-    );
+      const categoryContainer = stack.createContainer('categories');
 
-    const brandContainer = stack.createContainer('brand');
+      categoryContainer.apply(
+        menu({
+          container: categoryContainer.container,
+          attribute: 'categories',
+        })
+      );
 
-    brandContainer.apply(
-      refinementList({
-        container: brandContainer.container,
-        attribute: 'brand',
-      })
-    );
+      const brandContainer = stack.createContainer('brand');
 
-    search.addWidgets([stack]);
-  })
-);
+      brandContainer.apply(
+        refinementList({
+          container: brandContainer.container,
+          attribute: 'brand',
+        })
+      );
+
+      search.addWidgets([stack]);
+    })
+  )
+  .add(
+    'connector',
+    withHits(({ search, container }) => {
+      const brandContainer = document.createElement('div');
+      const categoriesContainer = document.createElement('div');
+
+      container.appendChild(brandContainer);
+      container.appendChild(categoriesContainer);
+
+      search.addWidgets([
+        connectDynamicWidgets<{
+          container: HTMLElement;
+          containers: Map<string, HTMLElement>;
+        }>(
+          ({
+            attributesToRender,
+            widgetParams: { container: rootContainer, containers },
+          }) => {
+            attributesToRender.forEach(attribute => {
+              if (!containers.has(attribute)) {
+                return;
+              }
+              const container = containers.get(attribute)!;
+              rootContainer.appendChild(container);
+            });
+          }
+        )({
+          transformItems,
+          container,
+          containers: new Map([
+            ['brand', brandContainer],
+            ['categories', categoriesContainer],
+          ]),
+        })
+          // @ts-expect-error haven't yet found a
+          .addWidget(
+            'brand',
+            refinementList({
+              container: brandContainer,
+              attribute: 'brand',
+            })
+          )
+          .addWidget(
+            'categories',
+            menu({
+              container: categoriesContainer,
+              attribute: 'categories',
+            })
+          ),
+      ]);
+    })
+  );
