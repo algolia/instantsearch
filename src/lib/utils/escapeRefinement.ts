37763a89cf492fad2e20361dc78e7fe478a73b46
Haroen Viaene <hello@haroen.me>
2021-04-20 11:23:24 +0200

    chore(ts): more detailed type for escapeRefinement

--- src/lib/utils/escapeRefinement.ts
+++ src/lib/utils/escapeRefinement.ts
@@ -1,3 +1,5 @@
+function escapeRefinement(value: number): string | number;
+function escapeRefinement(value: string): string;
 function escapeRefinement(value: string | number): string | number {
   if (typeof value === 'number' && value < 0) {
     value = String(value).replace(/^-/, '\\-');
