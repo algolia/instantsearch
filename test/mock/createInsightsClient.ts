47063e9579389a2834adc652e7a854eca1808503
Eunjae Lee <eunjae.lee@algolia.com>
2021-04-26 15:05:39 +0200

    chore(test): use real search-insights to test the insights middleware (#4712)

--- test/mock/createInsightsClient.ts
+++ test/mock/createInsightsClient.ts
@@ -1,89 +1,55 @@
-export const ANONYMOUS_TOKEN = 'anonymous-user-id-1';
+import AlgoliaAnalytics from 'search-insights/lib/insights';
+import { processQueue } from 'search-insights/lib/_processQueue';
+import { getFunctionalInterface } from 'search-insights/lib/_getFunctionalInterface';
 
-export type AlgoliaAnalytics = {
-  setUserToken(userToken: string): void;
-  init({ appId, apiKey }): void;
-  getUserToken(
-    options: any,
-    callback: (error: any, userToken: string) => void
-  ): void;
-  onUserTokenChange(
-    callback: (value: string) => void,
-    options?: { immediate?: boolean }
-  ): void;
-  viewedObjectIDs(...args: any[]): void;
-};
-
-export function createAlgoliaAnalytics(): AlgoliaAnalytics {
-  let values: any = {};
-  const setValues = obj => {
-    values = {
-      ...values,
-      ...obj,
-    };
-  };
-  let userTokenCallback;
-  const setUserToken = userToken => {
-    setValues({ _userToken: userToken });
-    if (userTokenCallback) {
-      userTokenCallback(userToken);
-    }
-  };
-  const init = ({ appId, apiKey }) => {
-    setValues({ _hasCredentials: true, _appId: appId, _apiKey: apiKey });
-    setUserToken(ANONYMOUS_TOKEN);
-  };
-  const getUserToken = (_options, callback) =>
-    callback(null, values._userToken);
-  const onUserTokenChange = (callback, { immediate = false } = {}) => {
-    userTokenCallback = callback;
-    if (immediate) {
-      callback(values._userToken);
-    }
-  };
-  const viewedObjectIDs = jest.fn();
+export function createInsights() {
+  const analytics = mockSendingEvents(
+    new AlgoliaAnalytics({
+      requestFn: jest.fn(),
+    })
+  );
+  const insightsClient = jest.fn(getFunctionalInterface(analytics));
 
   return {
-    setUserToken,
-    init,
-    getUserToken,
-    onUserTokenChange,
-    viewedObjectIDs,
+    analytics,
+    insightsClient,
   };
 }
 
-export function createInsightsClient(instance = createAlgoliaAnalytics()) {
-  return (methodName, ...args) => {
-    if (!instance[methodName]) {
-      throw new Error(`${methodName} doesn't exist in this mocked instance`);
-    }
-    instance[methodName](...args);
-  };
-}
-
-export function createInsightsUmdVersion(
-  algoliaAnalytics = createAlgoliaAnalytics()
-) {
+export function createInsightsUmdVersion() {
   const globalObject: any = {};
+  globalObject.AlgoliaAnalyticsObject = 'aa';
   globalObject.aa = (...args) => {
     globalObject.aa.queue = globalObject.aa.queue || [];
     globalObject.aa.queue.push(args);
   };
+  const analytics = mockSendingEvents(
+    new AlgoliaAnalytics({
+      requestFn: jest.fn(),
+    })
+  );
 
   return {
+    analytics,
     insightsClient: globalObject.aa,
     libraryLoadedAndProcessQueue: () => {
-      const _aa = createInsightsClient(algoliaAnalytics);
-      const queue = globalObject.aa.queue;
-      queue.forEach(([methodName, ...args]) => {
-        _aa(methodName, ...args);
-      });
-      queue.push = ([methodName, ...args]) => {
-        _aa(methodName, ...args);
-      };
-      return {
-        algoliaAnalytics,
-      };
+      processQueue.call(analytics, globalObject);
     },
   };
+}
+
+function mockSendingEvents(analytics: AlgoliaAnalytics) {
+  analytics.viewedFilters = jest.fn(analytics.viewedFilters);
+  analytics.viewedObjectIDs = jest.fn(analytics.viewedObjectIDs);
+  analytics.clickedFilters = jest.fn(analytics.clickedFilters);
+  analytics.clickedObjectIDs = jest.fn(analytics.clickedObjectIDs);
+  analytics.clickedObjectIDsAfterSearch = jest.fn(
+    analytics.clickedObjectIDsAfterSearch
+  );
+  analytics.convertedFilters = jest.fn(analytics.convertedFilters);
+  analytics.convertedObjectIDs = jest.fn(analytics.convertedObjectIDs);
+  analytics.convertedObjectIDsAfterSearch = jest.fn(
+    analytics.convertedObjectIDsAfterSearch
+  );
+  return analytics;
 }
