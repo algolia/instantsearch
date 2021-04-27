ad13acb2b833d045ba3dbc724f01c2637061511b
eunjae-lee <karis612@gmail.com>
2021-04-27 16:46:19 +0200

    fix duplicate types

--- src/types/insights.ts
+++ src/types/insights.ts
@@ -44,7 +44,6 @@ export type InsightsInit = (
 
 export type InsightsClient = InsightsSendEvent &
   InsightsOnUserTokenChange &
-  InsightsGetUserToken &
   InsightsInit &
   InsightsSetUserToken &
   InsightsGetUserToken & {
