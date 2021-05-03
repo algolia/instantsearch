477446e5da6034bb20c082e97c6f2bf1e751d5ba
Haroen Viaene <hello@haroen.me>
2021-05-03 14:23:15 +0200

    refactor(types): reorganise instantsearch type files (#4731)
    * refactor(types): reorganise instantsearch type files
    
    * chore: fix lint

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,16 @@
+// internal
+export * from './utils';
+
+// Algolia-related
+export * from './algoliasearch';
+export * from './results';
+
+// instantsearch-related
 export * from './instantsearch';
+export * from './middleware';
+export * from './router';
+export * from './insights';
+
+// widget-related
 export * from './connector';
 export * from './widget';
-export * from './insights';
-export * from './algoliasearch';
-export * from './middleware';
-export * from './utils';
