85d5164364ac5e4f3db0408099a536cd736eb9a1
Haroen Viaene <hello@haroen.me>
2021-06-01 14:45:09 +0200

    feat(vue 3): compat for both versions of v-model (algolia/vue-instantsearch#849)
    v-model used to be @input / :value, but is now @update:modelValue / :modelValue. Since those don't overlap, it's possible to support both, by using them as fallback
    
    "emits" is now also required in v3, so I added this to all components emitting
    
    docs: https://v3.vuejs.org/guide/migration/v-model.html

--- packages/vue-instantsearch/src/components/SearchBox.vue
+++ packages/vue-instantsearch/src/components/SearchBox.vue
@@ -78,6 +78,10 @@ export default {
       type: String,
       default: undefined,
     },
+    modelValue: {
+      type: String,
+      default: undefined,
+    },
   },
   data() {
     return {
@@ -86,25 +90,33 @@ export default {
   },
   computed: {
     isControlled() {
-      return typeof this.value !== 'undefined';
+      return (
+        typeof this.value !== 'undefined' ||
+        typeof this.modelValue !== 'undefined'
+      );
+    },
+    model() {
+      return this.value || this.modelValue;
     },
     currentRefinement: {
       get() {
         // if the input is controlled, but not up to date
         // this means it didn't search, and we should pretend it was `set`
-        if (this.isControlled && this.value !== this.localValue) {
+        if (this.isControlled && this.model !== this.localValue) {
           // eslint-disable-next-line vue/no-side-effects-in-computed-properties
-          this.localValue = this.value;
-          this.$emit('input', this.value);
-          this.state.refine(this.value);
+          this.localValue = this.model;
+          this.$emit('input', this.model);
+          this.$emit('update:modelValue', this.model);
+          this.state.refine(this.model);
         }
-        return this.value || this.state.query || '';
+        return this.model || this.state.query || '';
       },
       set(val) {
         this.localValue = val;
         this.state.refine(val);
         if (this.isControlled) {
           this.$emit('input', val);
+          this.$emit('update:modelValue', val);
         }
       },
     },
