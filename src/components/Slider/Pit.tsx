ecb7c2b1a73cb27775e86894673d864f3d2dd0a0
Haroen Viaene <hello@haroen.me>
2021-06-09 09:20:57 +0200

    refactor(ts): migrate rangeSlider  (#4770)
    * refactor(ts): migrate slider component
    
    * refactor(ts): migrate rangeSlider
    
    DX-1706
    
    * chore(ts): fix error in test

---
+++ src/components/Slider/Pit.tsx
@@ -0,0 +1,27 @@
+/** @jsx h */
+
+import { h } from 'preact';
+import cx from 'classnames';
+import { PitProps } from './Rheostat';
+
+const Pit = ({ style, children }: PitProps) => {
+  // first, end & middle
+  const positionValue = Math.round(parseFloat(style.left as string));
+  const shouldDisplayValue = [0, 50, 100].includes(positionValue);
+
+  const value = children as string;
+  const pitValue = Math.round(parseInt(value, 10) * 100) / 100;
+
+  return (
+    <div
+      style={{ ...style, marginLeft: positionValue === 100 ? '-2px' : 0 }}
+      className={cx('rheostat-marker', 'rheostat-marker-horizontal', {
+        'rheostat-marker-large': shouldDisplayValue,
+      })}
+    >
+      {shouldDisplayValue && <div className={'rheostat-value'}>{pitValue}</div>}
+    </div>
+  );
+};
+
+export default Pit;
