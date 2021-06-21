368dd553bdaaf6567399fc3db5a669d1204fb991
Haroen Viaene <hello@haroen.me>
2021-06-21 11:05:05 +0200

    ci(test): store junit results for e2e tests (#4788)
    * ci(test): store junit results for unit tests
    
    inspired by https://github.com/algolia/npm-search/pull/626 by @bodinsamuel
    
    * chore: jest subfolder
    
    * ci(test): store junit results for e2e tests
    
    * indentation?
    
    * xml format
    
    * build only one example
    
    * correct copy
    
    * correct file names
    
    * fix examples script
    
    * simplified

--- wdio.saucelabs.conf.js
+++ wdio.saucelabs.conf.js
@@ -2,4 +2,22 @@
 
 const { saucelabs } = require('instantsearch-e2e-tests');
 
-exports.config = saucelabs;
+exports.config = {
+  ...saucelabs,
+  reporters: [
+    'spec',
+    [
+      'junit',
+      {
+        outputDir: `${__dirname}/junit/wdio`,
+        outputFileFormat({
+          cid,
+          capabilities: { browserName, browserVersion },
+        }) {
+          return `results-${cid}.${browserName}-${browserVersion}.xml`;
+        },
+        addFileAttribute: true,
+      },
+    ],
+  ],
+};
