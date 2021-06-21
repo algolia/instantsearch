258abb0919db8471d30bad696ba3bd283557075c
Haroen Viaene <hello@haroen.me>
2021-06-21 09:33:33 +0200

    ci(test): store junit results for e2e tests

--- wdio.saucelabs.conf.js
+++ wdio.saucelabs.conf.js
@@ -2,4 +2,15 @@
 
 const { saucelabs } = require('instantsearch-e2e-tests');
 
-exports.config = saucelabs;
+exports.config = {
+  ...saucelabs,
+  reporters: [
+    'spec',
+    [
+      'junit',
+      {
+        outputDir: `${__dirname}/junit/wdio`,
+      },
+    ],
+  ],
+};
