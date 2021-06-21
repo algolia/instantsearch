b4e452f51ab13ab05986f67a102c4abb46ed3c88
Haroen Viaene <hello@haroen.me>
2021-06-21 10:08:21 +0200

    build only one example

--- scripts/website/build-examples.sh
+++ scripts/website/build-examples.sh
@@ -7,11 +7,21 @@ set -e # exit when error
 rm -rf website/examples
 mkdir website/examples
 
-for example in examples/*; do
-  if [ -d "$example" ]; then
-    name=$(basename "$example")
-    echo "Building $name example..."
+build_example() {
+  name=$1
+  echo "Building $name example..."
 
-    (cd "$example" && yarn && yarn build && cp -r "dist/" "../../website/examples/$name/")
-  fi
-done
+  (cd "$example" && yarn && yarn build && cp -r "dist/" "../../website/examples/$name/")
+}
+
+if [ -z "$1" ]; then
+  for example in examples/*; do
+    if [ -d "$example" ]; then
+      name=$(basename "$example")
+      build_example $name
+    fi
+  done
+else
+  cd examples
+  build_example $1
+fi
