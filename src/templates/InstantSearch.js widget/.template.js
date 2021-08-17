81b1f130642b701c623b6b6c43df33becbc429c2
Haroen Viaene <hello@haroen.me>
2021-08-17 16:28:25 +0200

    feat(is widget): upgrade to standard types (algolia/create-instantsearch-app#463)
    * feat(is widget): upgrade to standard types
    
    This makes the TS integration no longer need `experimental-typescript`
    
    * update e2e

--- src/templates/InstantSearch.js widget/.template.js
+++ src/templates/InstantSearch.js widget/.template.js
@@ -4,7 +4,7 @@ const teardown = require('../../tasks/node/teardown');
 module.exports = {
   category: 'Widget',
   libraryName: 'instantsearch.js',
-  supportedVersion: '>= 4.21.0 < 5.0.0',
+  supportedVersion: '>= 4.27.1 < 5.0.0',
   templateName: 'instantsearch.js-widget',
   packageNamePrefix: 'instantsearch-widget-',
   keywords: [
