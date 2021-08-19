932ae3a868340a32ccaacb276c862921fee41a93
Haroen Viaene <hello@haroen.me>
2021-08-19 15:43:28 +0200

    fix(svg): remove xmlns (#4839)
    xml namespace is not required for svg elements (and has never been https://stackoverflow.com/a/18468348/3185307), only for svg documents.
    
    This saves a probably insignificant amount of bundlesize

--- src/widgets/voice-search/defaultTemplates.ts
+++ src/widgets/voice-search/defaultTemplates.ts
@@ -24,7 +24,6 @@ const getButtonInnerElement = (
 const defaultTemplates: VoiceSearchComponentTemplates = {
   buttonText({ status, errorCode, isListening }) {
     return `<svg
-       xmlns="http://www.w3.org/2000/svg"
        width="16"
        height="16"
        viewBox="0 0 24 24"
