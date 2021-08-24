d37aca8c8acb9c9111199dd887078a72e527416f
Haroen Viaene <hello@haroen.me>
2021-08-24 15:23:12 +0200

    feat(vue): support Vue InstantSearch 4 (algolia/create-instantsearch-app#465)
    not yet added a new template for Vue 3, that can be done later by @eunjae-lee

--- src/templates/Vue InstantSearch/.template.js
+++ src/templates/Vue InstantSearch/.template.js
@@ -5,7 +5,7 @@ module.exports = {
   category: 'Web',
   libraryName: 'vue-instantsearch',
   templateName: 'vue-instantsearch',
-  supportedVersion: '>= 3.0.0 < 4.0.0',
+  supportedVersion: '>= 3.0.0 < 5.0.0',
   appName: 'vue-instantsearch-app',
   keywords: ['algolia', 'InstantSearch', 'Vue', 'vue-instantsearch'],
   tasks: {
