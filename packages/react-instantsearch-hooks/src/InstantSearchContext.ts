fa654883616919ce75124dc364263a578e06527b
François Chalifour <francoischalifour@users.noreply.github.com>
2021-10-19 11:54:29 +0200

    feat(hooks): expose `displayName` on Contexts (#3168)

--- packages/react-instantsearch-hooks/src/InstantSearchContext.ts
+++ packages/react-instantsearch-hooks/src/InstantSearchContext.ts
@@ -3,3 +3,7 @@ import { createContext } from 'react';
 import type { InstantSearch } from 'instantsearch.js';
 
 export const InstantSearchContext = createContext<InstantSearch | null>(null);
+
+if (__DEV__) {
+  InstantSearchContext.displayName = 'InstantSearch';
+}
