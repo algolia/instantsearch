c1ca836083705be5284e9b538d258ca6540159a7
François Chalifour <francoischalifour@users.noreply.github.com>
2021-11-24 11:22:45 +0100

    docs(hooks): add `Highlight` component (algolia/react-instantsearch#3208)

--- examples/hooks/default-theme/App.tsx
+++ examples/hooks/default-theme/App.tsx
@@ -6,6 +6,7 @@ import { InstantSearch } from 'react-instantsearch-hooks';
 import {
   Configure,
   HierarchicalMenu,
+  Highlight,
   Hits,
   Pagination,
   Panel,
@@ -33,15 +34,7 @@ type HitProps = {
 function Hit({ hit }: HitProps) {
   return (
     <>
-      <span
-        className="Hit-label"
-        dangerouslySetInnerHTML={{
-          __html:
-            hit._highlightResult && hit._highlightResult.name
-              ? hit._highlightResult.name.value
-              : '',
-        }}
-      />
+      <Highlight hit={hit} attribute="name" className="Hit-label" />
       <span className="Hit-price">${hit.price}</span>
     </>
   );
---
+++ examples/hooks/default-theme/components/Highlight.tsx
@@ -0,0 +1,98 @@
+import { Hit as AlgoliaHit } from '@algolia/client-search';
+import {
+  getHighlightedParts,
+  getPropertyByPath,
+} from 'instantsearch.js/es/lib/utils';
+import React from 'react';
+
+import { cx } from '../cx';
+
+type HighlightPartProps = {
+  children: React.ReactNode;
+  highlightedTagName: React.ReactType;
+  nonHighlightedTagName: React.ReactType;
+  isHighlighted: boolean;
+};
+
+function HighlightPart({
+  children,
+  highlightedTagName,
+  isHighlighted,
+  nonHighlightedTagName,
+}: HighlightPartProps) {
+  const TagName = isHighlighted ? highlightedTagName : nonHighlightedTagName;
+
+  return (
+    <TagName
+      className={
+        isHighlighted
+          ? 'ais-Highlight--highlighted'
+          : 'ais-Highlight--nonHighlighted'
+      }
+    >
+      {children}
+    </TagName>
+  );
+}
+
+export type HighlightProps<THit> = {
+  hit: THit;
+  attribute: keyof THit | string[];
+  highlightedTagName?: React.ReactType;
+  nonHighlightedTagName?: React.ReactType;
+  className?: string;
+  separator?: string;
+};
+
+export function Highlight<THit extends AlgoliaHit<Record<string, unknown>>>({
+  hit,
+  attribute,
+  highlightedTagName = 'mark',
+  nonHighlightedTagName = 'span',
+  separator = ', ',
+  ...rest
+}: HighlightProps<THit>) {
+  const { value: attributeValue = '' } =
+    getPropertyByPath(hit._highlightResult, attribute as string) || {};
+  const parts = getHighlightedParts(attributeValue);
+
+  return (
+    <span {...rest} className={cx('ais-Highlight', rest.className)}>
+      {parts.map((part, partIndex) => {
+        if (Array.isArray(part)) {
+          const isLastPart = partIndex === parts.length - 1;
+
+          return (
+            <span key={partIndex}>
+              {part.map((subPart, subPartIndex) => (
+                <HighlightPart
+                  key={subPartIndex}
+                  highlightedTagName={highlightedTagName}
+                  nonHighlightedTagName={nonHighlightedTagName}
+                  isHighlighted={subPart.isHighlighted}
+                >
+                  {subPart.value}
+                </HighlightPart>
+              ))}
+
+              {!isLastPart && (
+                <span className="ais-Highlight-separator">{separator}</span>
+              )}
+            </span>
+          );
+        }
+
+        return (
+          <HighlightPart
+            key={partIndex}
+            highlightedTagName={highlightedTagName}
+            nonHighlightedTagName={nonHighlightedTagName}
+            isHighlighted={part.isHighlighted}
+          >
+            {part.value}
+          </HighlightPart>
+        );
+      })}
+    </span>
+  );
+}
