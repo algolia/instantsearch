5ee71c30c2de0120e717b3360690ede94d4dd75f
Haroen Viaene <hello@haroen.me>
2021-12-09 20:43:13 +0100

    rewrite extension rewriter

--- scripts/babel/__tests__/extension-resolver-test.js
+++ scripts/babel/__tests__/extension-resolver-test.js
@@ -1,31 +1,16 @@
 import { transformAsync } from '@babel/core';
 
 import plugin from '../extension-resolver';
+import fs from 'fs';
+jest.mock('fs');
 
 describe('babel-plugin-extension-resolver', () => {
-  let files;
-  let options;
-
-  beforeEach(() => {
-    files = {};
-    options = {
-      filename: '/path/to/src/file.js',
-      configFile: false,
-      babelrc: false,
-      plugins: [
-        [
-          plugin,
-          {
-            resolveOptions: {
-              isFile: jest.fn((filename) => Boolean(files[filename])),
-              isDirectory: jest.fn(() => false),
-              readFileSync: jest.fn((filename) => files[filename]),
-            },
-          },
-        ],
-      ],
-    };
-  });
+  const options = {
+    filename: '/path/to/src/file.js',
+    configFile: false,
+    babelrc: false,
+    plugins: [plugin],
+  };
 
   afterEach(() => {
     jest.resetAllMocks();
@@ -36,227 +21,46 @@ describe('babel-plugin-extension-resolver', () => {
       'babel-plugin-extension-resolver'
     ));
 
-  it('ignores empty code', async () =>
-    expect(await transformAsync('', options)).toHaveProperty('code', ''));
+  it('ignores empty code', async () => {
+    expect(await transformAsync('', options)).toHaveProperty('code', '');
+  });
 
-  it('ignores module imports', async () =>
+  it('ignores module imports', async () => {
     expect(
       await transformAsync('import path from "path";', options)
-    ).toHaveProperty('code', 'import path from "path";'));
-
-  it('finds .node.mjs files', async () => {
-    files = {
-      '/path/to/src/other.node.mjs': true,
-      '/path/to/src/other.mjs': true,
-      '/path/to/src/other.node.js': true,
-      '/path/to/src/other.js': true,
-      '/path/to/src/other.node.ts': true,
-      '/path/to/src/other.ts': true,
-      '/path/to/src/other.node.tsx': true,
-      '/path/to/src/other.tsx': true,
-      '/path/to/src/other.json': true,
-      '/path/to/src/other.node.jsx': true,
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
-
-    expect(
-      await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.node.mjs";');
-  });
-
-  it('finds .mjs files', async () => {
-    files = {
-      '/path/to/src/other.mjs': true,
-      '/path/to/src/other.node.js': true,
-      '/path/to/src/other.js': true,
-      '/path/to/src/other.node.ts': true,
-      '/path/to/src/other.ts': true,
-      '/path/to/src/other.node.tsx': true,
-      '/path/to/src/other.tsx': true,
-      '/path/to/src/other.json': true,
-      '/path/to/src/other.node.jsx': true,
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
-
-    expect(
-      await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.mjs";');
-  });
-
-  it('finds .node.js files', async () => {
-    files = {
-      '/path/to/src/other.node.js': true,
-      '/path/to/src/other.js': true,
-      '/path/to/src/other.node.ts': true,
-      '/path/to/src/other.ts': true,
-      '/path/to/src/other.node.tsx': true,
-      '/path/to/src/other.tsx': true,
-      '/path/to/src/other.json': true,
-      '/path/to/src/other.node.jsx': true,
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
-
-    expect(
-      await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.node.js";');
+    ).toHaveProperty('code', 'import path from "path";');
   });
 
   it('finds .js files', async () => {
-    files = {
-      '/path/to/src/other.js': true,
-      '/path/to/src/other.node.ts': true,
-      '/path/to/src/other.ts': true,
-      '/path/to/src/other.node.tsx': true,
-      '/path/to/src/other.tsx': true,
-      '/path/to/src/other.json': true,
-      '/path/to/src/other.node.jsx': true,
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
+    fs.__setMockFiles([
+      '/path/to/src/other.js',
+      '/path/to/src/other.ts',
+      '/path/to/src/other.tsx',
+    ]);
 
     expect(
       await transformAsync('import other from "./other";', options)
     ).toHaveProperty('code', 'import other from "./other.js";');
   });
 
-  it('finds .node.ts files', async () => {
-    files = {
-      '/path/to/src/other.node.ts': true,
-      '/path/to/src/other.ts': true,
-      '/path/to/src/other.node.tsx': true,
-      '/path/to/src/other.tsx': true,
-      '/path/to/src/other.json': true,
-      '/path/to/src/other.node.jsx': true,
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
-
-    expect(
-      await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.node.ts";');
-  });
-
   it('finds .ts files', async () => {
-    files = {
-      '/path/to/src/other.ts': true,
-      '/path/to/src/other.node.tsx': true,
-      '/path/to/src/other.tsx': true,
-      '/path/to/src/other.json': true,
-      '/path/to/src/other.node.jsx': true,
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
+    fs.__setMockFiles(['/path/to/src/other.ts', '/path/to/src/other.tsx']);
 
     expect(
       await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.ts";');
-  });
-
-  it('finds .node.tsx files', async () => {
-    files = {
-      '/path/to/src/other.node.tsx': true,
-      '/path/to/src/other.tsx': true,
-      '/path/to/src/other.json': true,
-      '/path/to/src/other.node.jsx': true,
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
-
-    expect(
-      await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.node.tsx";');
+    ).toHaveProperty('code', 'import other from "./other.js";');
   });
 
   it('finds .tsx files', async () => {
-    files = {
-      '/path/to/src/other.tsx': true,
-      '/path/to/src/other.json': true,
-      '/path/to/src/other.node.jsx': true,
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
+    fs.__setMockFiles(['/path/to/src/other.tsx']);
 
     expect(
       await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.tsx";');
-  });
-
-  it('finds .json files', async () => {
-    files = {
-      '/path/to/src/other.json': true,
-      '/path/to/src/other.node.jsx': true,
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
-
-    expect(
-      await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.json";');
-  });
-
-  it('finds .node.jsx files', async () => {
-    files = {
-      '/path/to/src/other.node.jsx': true,
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
-
-    expect(
-      await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.node.jsx";');
-  });
-
-  it('finds .jsx files', async () => {
-    files = {
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
-
-    expect(
-      await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.jsx";');
-  });
-
-  it('finds .node files', async () => {
-    files = {
-      '/path/to/src/other.node': true,
-    };
-
-    expect(
-      await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.node";');
-  });
-
-  it('uses extensions override', async () => {
-    options.plugins[0][1].extensions = ['.jsx'];
-    files = {
-      '/path/to/src/other.node.mjs': true,
-      '/path/to/src/other.mjs': true,
-      '/path/to/src/other.node.js': true,
-      '/path/to/src/other.js': true,
-      '/path/to/src/other.node.ts': true,
-      '/path/to/src/other.ts': true,
-      '/path/to/src/other.node.tsx': true,
-      '/path/to/src/other.tsx': true,
-      '/path/to/src/other.json': true,
-      '/path/to/src/other.node.jsx': true,
-      '/path/to/src/other.jsx': true,
-      '/path/to/src/other.node': true,
-    };
-
-    expect(
-      await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other.jsx";');
+    ).toHaveProperty('code', 'import other from "./other.js";');
   });
 
   it('finds files in parent directory', async () => {
-    files = {
-      '/path/to/other.js': true,
-      '/path/to/src/other.js': true,
-    };
+    fs.__setMockFiles(['/path/to/other.js', '/path/to/src/other.js']);
 
     expect(
       await transformAsync('import other from "../other";', options)
@@ -264,10 +68,7 @@ describe('babel-plugin-extension-resolver', () => {
   });
 
   it('finds files in child directory', async () => {
-    files = {
-      '/path/to/src/child/other.js': true,
-      '/path/to/src/other.js': true,
-    };
+    fs.__setMockFiles(['/path/to/src/other.js', '/path/to/src/child/other.js']);
 
     expect(
       await transformAsync('import other from "./child/other";', options)
@@ -275,20 +76,15 @@ describe('babel-plugin-extension-resolver', () => {
   });
 
   it('uses index file', async () => {
-    files = {
-      '/path/to/src/other/index.mjs': true,
-    };
+    fs.__setMockFiles(['/path/to/src/other/index.js']);
 
     expect(
       await transformAsync('import other from "./other";', options)
-    ).toHaveProperty('code', 'import other from "./other/index.mjs";');
+    ).toHaveProperty('code', 'import other from "./other/index.js";');
   });
 
   it('works with multiple imports', async () => {
-    files = {
-      '/path/to/src/other.js': true,
-      '/path/to/src/another.js': true,
-    };
+    fs.__setMockFiles(['/path/to/src/other.js', '/path/to/src/another.js']);
 
     expect(
       await transformAsync(
@@ -301,11 +97,16 @@ describe('babel-plugin-extension-resolver', () => {
     );
   });
 
+  it('works with export from', async () => {
+    fs.__setMockFiles(['/path/to/src/other.js']);
+
+    expect(
+      await transformAsync('export * from "./other"', options)
+    ).toHaveProperty('code', 'export * from "./other.js";');
+  });
+
   it('ignores require()', async () => {
-    files = {
-      '/path/to/src/other.node.mjs': true,
-      '/path/to/src/other.js': true,
-    };
+    fs.__setMockFiles(['/path/to/src/other.js', '/path/to/src/another.js']);
 
     expect(await transformAsync('require("./other");', options)).toHaveProperty(
       'code',
@@ -313,59 +114,21 @@ describe('babel-plugin-extension-resolver', () => {
     );
   });
 
-  it('ignores dynamic require()', async () => {
-    files = {
-      '/path/to/src/other.node.mjs': true,
-      '/path/to/src/other.js': true,
-    };
-
-    expect(
-      await transformAsync(
-        'const other = "./other";\n\nrequire(other);',
-        options
-      )
-    ).toHaveProperty('code', 'const other = "./other";\n\nrequire(other);');
-  });
-
-  it('ignores multiple arguments', async () => {
-    files = {
-      '/path/to/src/other.node.mjs': true,
-      '/path/to/src/other.js': true,
-    };
-
-    expect(
-      await transformAsync('require("./other", true);', options)
-    ).toHaveProperty('code', 'require("./other", true);');
-  });
-
   it('ignores other function calls', async () => {
-    files = {
-      '/path/to/src/other.node.mjs': true,
-      '/path/to/src/other.js': true,
-    };
+    fs.__setMockFiles(['/path/to/src/other.js']);
 
     expect(
       await transformAsync('requireOOPS("./other");', options)
     ).toHaveProperty('code', 'requireOOPS("./other");');
   });
 
-  it('works with export from', async () => {
-    files = {
-      '/path/to/src/other.js': true,
-    };
+  it('leaves as-is if file not found', async () => {
+    fs.__setMockFiles([]);
 
-    expect(
-      await transformAsync('export * from "./other"', options)
-    ).toHaveProperty('code', 'export * from "./other.js";');
+    await expect(() =>
+      transformAsync('import other from "./other";', options)
+    ).rejects.toThrow(
+      '/path/to/src/file.js: local import for "./other" could not be resolved'
+    );
   });
-
-  /*
-   * TODO package.json resolution
-   * BODY https://github.com/browserify/resolve/blob/master/test/mock_sync.js#L49
-   */
-
-  /*
-   * TODO dynamic imports
-   * BODY https://babeljs.io/docs/en/babel-plugin-syntax-dynamic-import
-   */
 });
--- scripts/babel/extension-resolver.js
+++ scripts/babel/extension-resolver.js
@@ -1,78 +1,33 @@
 /* eslint-disable import/no-commonjs */
-// original source: https://github.com/saiichihashimoto/babel-plugin-extension-resolver
+// original source: https://github.com/shimataro/babel-plugin-module-extension-resolver
 // To create proper ES Modules, the paths imported need to be fully-specified,
 // and can't be resolved like is possible with CommonJS. To have large compatibility
 // without much hassle, we don't use extensions *inside* the source code, but add
 // them with this plugin.
 
+const fs = require('fs');
 const path = require('path');
-const resolve = require('resolve');
 
-const DEFAULT_EXTENSIONS = [
-  '.node.mjs',
-  '.mjs',
-  '.node.js',
-  '.js',
-  '.node.ts',
-  '.ts',
-  '.node.tsx',
-  '.tsx',
-  '.json',
-  '.node.jsx',
-  '.jsx',
-  '.node',
-];
+const PLUGIN_NAME = 'babel-plugin-extension-resolver';
 
-function extensionResolver(
-  { types },
-  { extensions = DEFAULT_EXTENSIONS, resolveOptions = {} }
-) {
+const srcExtensions = ['.js', '.ts', '.tsx'];
+const dstExtension = '.js';
+
+module.exports = function extensionResolver(babel) {
+  const { types } = babel;
   return {
-    name: 'babel-plugin-extension-resolver',
+    name: PLUGIN_NAME,
     visitor: {
       Program: {
-        enter(programPath, state) {
-          const {
-            file: {
-              opts: { filename },
-            },
-          } = state;
-
-          function replaceSource(source) {
-            if (!types.isStringLiteral(source)) {
-              return;
-            }
-
-            const sourcePath = source.node.value;
-            if (sourcePath[0] !== '.') {
-              return;
-            }
-
-            const basedir = path.dirname(filename);
-
-            const resolvedPath = path.relative(
-              basedir,
-              resolve.sync(sourcePath, {
-                ...resolveOptions,
-                basedir,
-                extensions,
-              })
-            );
-
-            source.replaceWith(
-              types.stringLiteral(
-                resolvedPath[0] === '.' ? resolvedPath : `./${resolvedPath}`
-              )
-            );
-          }
-
+        enter: (programPath, state) => {
+          const { filename } = state;
           programPath.traverse(
             {
               ImportDeclaration(declaration) {
-                replaceSource(declaration.get('source'));
+                handleImportDeclaration(types, declaration, filename);
               },
               ExportDeclaration(declaration) {
-                replaceSource(declaration.get('source'));
+                handleExportDeclaration(types, declaration, filename);
               },
             },
             state
@@ -81,6 +36,73 @@ function extensionResolver(
       },
     },
   };
+};
+
+function handleImportDeclaration(types, declaration, fileName) {
+  const source = declaration.get('source');
+  replaceSource(types, source, fileName);
 }
 
-module.exports = extensionResolver;
+function handleExportDeclaration(types, declaration, fileName) {
+  const source = declaration.get('source');
+  if (Array.isArray(source)) {
+    return;
+  }
+  replaceSource(types, source, fileName);
+}
+
+function replaceSource(types, source, fileName) {
+  if (!source.isStringLiteral()) {
+    return;
+  }
+  const sourcePath = source.node.value;
+  if (sourcePath[0] !== '.') {
+    return;
+  }
+  const baseDir = path.dirname(fileName);
+  const resolvedPath = resolvePath(baseDir, sourcePath);
+  const normalizedPath = normalizePath(resolvedPath);
+  source.replaceWith(types.stringLiteral(normalizedPath));
+}
+
+function resolvePath(baseDir, sourcePath) {
+  for (const title of [sourcePath, path.join(sourcePath, 'index')]) {
+    const resolvedPath = resolveExtension(baseDir, title);
+    if (resolvedPath !== null) {
+      return resolvedPath;
+    }
+  }
+  throw new Error(`local import for "${sourcePath}" could not be resolved`);
+}
+
+function resolveExtension(baseDir, sourcePath) {
+  const absolutePath = path.join(baseDir, sourcePath);
+  if (isFile(absolutePath)) {
+    return sourcePath;
+  }
+  for (const extension of srcExtensions) {
+    if (isFile(`${absolutePath}${extension}`)) {
+      return path.relative(baseDir, `${absolutePath}${dstExtension}`);
+    }
+  }
+  return null;
+}
+
+function normalizePath(originalPath) {
+  let normalizedPath = originalPath;
+  if (path.sep === '\\') {
+    normalizedPath = normalizedPath.split(path.sep).join('/');
+  }
+  if (normalizedPath[0] !== '.') {
+    normalizedPath = `./${normalizedPath}`;
+  }
+  return normalizedPath;
+}
+
+function isFile(pathName) {
+  try {
+    return fs.statSync(pathName).isFile();
+  } catch (err) {
+    return false;
+  }
+}
