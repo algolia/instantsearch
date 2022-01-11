7d10586f8f21f18f19831b0f7780a3060664c72a
Haroen Viaene <hello@haroen.me>
2022-01-11 20:23:46 +0100

    fix(hooks): allow importing via require (algolia/react-instantsearch#3257)
    * fix(hooks): allow importing via require
    
    * cjs test

--- scripts/prepare-cjs.sh
+++ scripts/prepare-cjs.sh
@@ -7,3 +7,7 @@ find dist/cjs -type f -exec sed -i.bak 's|instantsearch.js/es|instantsearch.js/c
 
 # clean up the .bak files, as the set -i'' can not be enabled on BSD/Mac and Linux with the same syntax
 find dist/cjs -name '*.bak' -type f -exec rm {} +
+
+# create a package.json to be able to import using require
+touch dist/cjs/package.json
+echo '{ "type": "commonjs", "sideEffects": false }' > dist/cjs/package.json
---
+++ test/module/packages-are-cjs-modules.cjs
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-commonjs */
+
+const assert = require('assert');
+
+const ReactInstantSearchHooks = require('react-instantsearch-hooks');
+const ReactInstantSearchHooksServer = require('react-instantsearch-hooks-server');
+
+assert.ok(ReactInstantSearchHooks);
+assert.ok(ReactInstantSearchHooksServer);
