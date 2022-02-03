ceaf620fadea07e0bb5e1a9f87c6dc8adacf0cd2
Dhaya <154633+dhayab@users.noreply.github.com>
2022-02-03 17:23:40 +0100

    chore: update e2e tests

--- wdio.saucelabs.conf.js
+++ wdio.saucelabs.conf.js
@@ -2,22 +2,4 @@
 
 const { saucelabs } = require('instantsearch-e2e-tests');
 
-exports.config = {
-  ...saucelabs,
-  reporters: [
-    'spec',
-    [
-      'junit',
-      {
-        outputDir: `${__dirname}/junit/wdio`,
-        outputFileFormat({
-          cid,
-          capabilities: { browserName, browserVersion },
-        }) {
-          return `results-${cid}.${browserName}-${browserVersion}.xml`;
-        },
-        addFileAttribute: true,
-      },
-    ],
-  ],
-};
+exports.config = saucelabs;
