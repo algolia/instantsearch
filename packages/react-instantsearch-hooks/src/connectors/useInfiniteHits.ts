a36628237b17d68fa0e28cb7a247ce554028a492
Haroen Viaene <hello@haroen.me>
2022-02-24 11:01:58 +0100

    feat(hooks): allow useHits and useInfiniteHit to be generic (algolia/react-instantsearch#3364)
    * feat(hooks): allow useHits and useInfiniteHit to be generic
    
    This requires https://github.com/algolia/instantsearch.js/pull/5019 and enables better generics for the Hits component (https://github.com/algolia/react-instantsearch/pull/3363)
    
    * Apply suggestions from code review
    
    * Update useInfiniteHits.ts
    
    * latest IS
    
    * use BaseHit from IS

--- packages/react-instantsearch-hooks/src/connectors/useInfiniteHits.ts
+++ packages/react-instantsearch-hooks/src/connectors/useInfiniteHits.ts
@@ -3,19 +3,26 @@ import connectInfiniteHits from 'instantsearch.js/es/connectors/infinite-hits/co
 import { useConnector } from '../hooks/useConnector';
 
 import type { AdditionalWidgetProperties } from '../hooks/useConnector';
+import type { BaseHit } from 'instantsearch.js';
 import type {
   InfiniteHitsConnectorParams,
   InfiniteHitsWidgetDescription,
+  InfiniteHitsConnector,
 } from 'instantsearch.js/es/connectors/infinite-hits/connectInfiniteHits';
 
-export type UseInfiniteHitsProps = InfiniteHitsConnectorParams;
+export type UseInfiniteHitsProps<THit extends BaseHit = BaseHit> =
+  InfiniteHitsConnectorParams<THit>;
 
-export function useInfiniteHits(
-  props?: UseInfiniteHitsProps,
+export function useInfiniteHits<THit extends BaseHit = BaseHit>(
+  props?: UseInfiniteHitsProps<THit>,
   additionalWidgetProperties?: AdditionalWidgetProperties
 ) {
   return useConnector<
-    InfiniteHitsConnectorParams,
-    InfiniteHitsWidgetDescription
-  >(connectInfiniteHits, props, additionalWidgetProperties);
+    InfiniteHitsConnectorParams<THit>,
+    InfiniteHitsWidgetDescription<THit>
+  >(
+    connectInfiniteHits as InfiniteHitsConnector<THit>,
+    props,
+    additionalWidgetProperties
+  );
 }
