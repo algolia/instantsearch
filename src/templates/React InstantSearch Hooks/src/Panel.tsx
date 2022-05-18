836609baa8ee5d8ad5011cfbc147bd965667bd50
Dhaya <154633+dhayab@users.noreply.github.com>
2022-05-18 10:56:04 +0200

    feat(hooks): add react instantsearch hooks template (algolia/create-instantsearch-app#568)
    * feat(hooks): add react instantsearch hooks template
    
    * update test snapshots
    
    * add panel component to mark clear separation between refinement lists
    
    * apply suggestions
    
    Co-authored-by: Haroen Viaene <hello@haroen.me>
    
    * fresh new snapshots
    
    Co-authored-by: Haroen Viaene <hello@haroen.me>

---
+++ src/templates/React InstantSearch Hooks/src/Panel.tsx
@@ -0,0 +1,14 @@
+type PanelProps = React.PropsWithChildren<{
+  header: string;
+}>;
+
+export function Panel({ header, children }: PanelProps) {
+  return (
+    <div className="ais-Panel">
+      <div className="ais-Panel-header">
+        <span>{header}</span>
+      </div>
+      <div className="ais-Panel-body">{children}</div>
+    </div>
+  );
+}
