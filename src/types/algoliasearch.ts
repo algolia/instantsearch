11004ae5f03569e8dc1cf62f5a615ff6a26fed15
Haroen Viaene <hello@haroen.me>
2022-06-16 13:41:12 +0200

    let's do it this way

--- src/types/algoliasearch.ts
+++ src/types/algoliasearch.ts
@@ -1,5 +1,3 @@
-// this is a type-only file, and thus isn't resolvable
-// eslint-disable-next-line import/no-unresolved
-export * from 'algoliasearch-helper/types/algoliasearch';
+export * from 'algoliasearch-helper/types/algoliasearch.js';
 
 export {};
