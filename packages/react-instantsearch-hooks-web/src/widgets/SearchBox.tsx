2ffc893ed304fda1eb0c93cc484a5d2624c53a51
François Chalifour <francoischalifour@users.noreply.github.com>
2022-07-07 10:39:51 +0200

    refactor(hooks): remove `useEffect()` from `<SearchBox>` (algolia/react-instantsearch#3551)

--- packages/react-instantsearch-hooks-web/src/widgets/SearchBox.tsx
+++ packages/react-instantsearch-hooks-web/src/widgets/SearchBox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { useSearchBox } from 'react-instantsearch-hooks';
 
 import { SearchBox as SearchBoxUiComponent } from '../ui/SearchBox';
@@ -24,45 +24,36 @@ export function SearchBox({ queryHook, ...props }: SearchBoxProps) {
     { queryHook },
     { $$widgetType: 'ais.searchBox' }
   );
-  const [value, setValue] = useState(query);
+  const [inputValue, setInputValue] = useState(query);
   const inputRef = useRef<HTMLInputElement>(null);
 
+  function setQuery(newQuery: string) {
+    setInputValue(newQuery);
+    refine(newQuery);
+  }
+
   function onReset() {
-    setValue('');
+    setQuery('');
   }
 
   function onChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setValue(event.currentTarget.value);
+    setQuery(event.currentTarget.value);
   }
-
-  // Track when the value coming from the React state changes to synchronize
-  // it with InstantSearch.
-  useEffect(() => {
-    if (query !== value) {
-      refine(value);
-    }
-    // We don't want to track when the InstantSearch query changes.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value, refine]);
 
   // Track when the InstantSearch query changes to synchronize it with
   // the React state.
-  useEffect(() => {
-    // We bypass the state update if the input is focused to avoid concurrent
-    // updates when typing.
-    if (document.activeElement !== inputRef.current && query !== value) {
-      setValue(query);
-    }
-    // We don't want to track when the React state value changes.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [query]);
+  // We bypass the state update if the input is focused to avoid concurrent
+  // updates when typing.
+  if (query !== inputValue && document.activeElement !== inputRef.current) {
+    setInputValue(query);
+  }
 
   const uiProps: UiProps = {
     inputRef,
     isSearchStalled,
     onChange,
     onReset,
-    value,
+    value: inputValue,
     translations: {
       submitTitle: 'Submit the search query.',
       resetTitle: 'Clear the search query.',
