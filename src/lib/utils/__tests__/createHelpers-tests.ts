743ffb2ad39edc5d4112a76f6021b92051c1dc28
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-07-25 15:57:25 +0200

    feat: introduce `html` templating

--- src/lib/utils/__tests__/createHelpers-tests.ts
+++ src/lib/utils/__tests__/createHelpers-tests.ts
@@ -34,7 +34,7 @@ describe('insights hogan helper', () => {
     })}>Add to cart</button>
     `;
 
-    expect(output.trim()).toEqual(expected.trim());
+    expect((output as string).trim()).toEqual(expected.trim());
 
     expect(warn).toHaveBeenLastCalledWith(
       `[InstantSearch.js]: \`insights\` function has been deprecated. It is still supported in 4.x releases, but not further. It is replaced by the \`insights\` middleware.
