3b46490259f1f46177ec463262e470277acc71a6
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-07-25 15:57:25 +0200

    feat: introduce `html` templating

--- src/types/templates.ts
+++ src/types/templates.ts
@@ -23,6 +23,17 @@ export type TemplateParams = BindEventForHits & {
   sendEvent?: SendEventForHits;
 };
 
+export type TemplateParams = BindEventForHits & {
+  html: typeof html;
+  components: {
+    Highlight: typeof Highlight;
+    ReverseHighlight: typeof ReverseHighlight;
+    Snippet: typeof Snippet;
+    ReverseSnippet: typeof ReverseSnippet;
+  };
+  sendEvent?: SendEventForHits;
+};
+
 export type TemplateWithBindEvent<TTemplateData = void> =
   | string
   | ((data: TTemplateData, params: TemplateParams) => VNode | VNode[] | string);
