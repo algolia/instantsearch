e33070e3882a8dcaa2ab83a6dac1d97d89d72732
Dhaya <154633+dhayab@users.noreply.github.com>
2022-09-30 11:59:46 +0200

    simplify menu item link class assignation

--- src/widgets/hierarchical-menu/defaultTemplates.tsx
+++ src/widgets/hierarchical-menu/defaultTemplates.tsx
@@ -7,12 +7,14 @@ import type { HierarchicalMenuComponentTemplates } from './hierarchical-menu';
 
 const defaultTemplates: HierarchicalMenuComponentTemplates = {
   item({ url, label, count, cssClasses, isRefined }) {
-    const linkCssClasses = ([] as Array<string | undefined>)
-      .concat(cssClasses.link, isRefined ? cssClasses.selectedItemLink : [])
-      .filter(Boolean) as string[];
-
     return (
-      <a className={cx(linkCssClasses)} href={url}>
+      <a
+        className={cx([
+          cx(cssClasses.link),
+          cx(isRefined ? cssClasses.selectedItemLink : undefined),
+        ])}
+        href={url}
+      >
         <span className={cx(cssClasses.label)}>{label}</span>
         <span className={cx(cssClasses.count)}>{formatNumber(count)}</span>
       </a>
