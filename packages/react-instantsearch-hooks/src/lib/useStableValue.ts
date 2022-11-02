44c6492fd7faa644d44a46de16dc750b66dd3e9b
Haroen Viaene <hello@haroen.me>
2022-11-02 09:25:54 +0100

    fix(hooks): avoid effect in useStableValue (algolia/react-instantsearch#3670)
    There's no tests for this, so make sure that the timings we had are
    still the same.
    
    specifically used in:
    
    https://github.com/algolia/react-instantsearch/blob/ae55a8b621f4124431d2eaa8fb263475926c8721/packages/react-instantsearch-hooks/src/hooks/useConnector.ts#L26-L29
    https://github.com/algolia/react-instantsearch/blob/c24ffc4c1f52c4a9d3a148b47f843e097b55f46f/packages/react-instantsearch-hooks/src/lib/useIndex.ts#L22
    
    same as https://github.com/algolia/predict/pull/45

--- packages/react-instantsearch-hooks/src/lib/useStableValue.ts
+++ packages/react-instantsearch-hooks/src/lib/useStableValue.ts
@@ -1,16 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { dequal } from '../lib/dequal';
 
 export function useStableValue<TValue>(value: TValue) {
   const [stableValue, setStableValue] = useState<TValue>(() => value);
 
-  useEffect(() => {
-    if (!dequal(stableValue, value)) {
-      setStableValue(value);
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value]);
+  if (!dequal(stableValue, value)) {
+    setStableValue(value);
+  }
 
   return stableValue;
 }
