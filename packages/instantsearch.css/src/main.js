e025ab1954a85f4eab8d93dbdbcb543eedcea595
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-12-14 00:06:19 +0100

    chore(instantsearch.css): lint

--- packages/instantsearch.css/src/main.js
+++ packages/instantsearch.css/src/main.js
@@ -1,95 +1,95 @@
-const changeThemeSelect = document.getElementById("js-changeTheme");
-const customThemeStyleSheet = document.getElementById("js-customTheme");
-const localStorageSelectChoice = localStorage.getItem("selectChoice") || 0;
-const hoverClass = document.querySelectorAll(".js-hoverClass");
-const codeWrapperTabs = document.querySelectorAll(".code-wrapper-tab");
+const changeThemeSelect = document.getElementById('js-changeTheme');
+const customThemeStyleSheet = document.getElementById('js-customTheme');
+const localStorageSelectChoice = localStorage.getItem('selectChoice') || 0;
+const hoverClass = document.querySelectorAll('.js-hoverClass');
+const codeWrapperTabs = document.querySelectorAll('.code-wrapper-tab');
 
 // Store current CSS theme in localStorage
 
 customThemeStyleSheet.setAttribute(
-  "href",
+  'href',
   changeThemeSelect.options[localStorageSelectChoice].value
 );
 
 if (changeThemeSelect) {
   changeThemeSelect.value =
     changeThemeSelect.options[localStorageSelectChoice].value;
-  changeThemeSelect.addEventListener("change", (e) => {
+  changeThemeSelect.addEventListener('change', (e) => {
     customThemeStyleSheet.setAttribute(
-      "href",
+      'href',
       changeThemeSelect.options[e.target.options.selectedIndex].value
     );
-    localStorage.setItem("selectChoice", e.target.options.selectedIndex);
+    localStorage.setItem('selectChoice', e.target.options.selectedIndex);
   });
 }
 
 // Hover list of classes to highligh them
 
-function targetHtmlElement(item) {
-  const queue = [document.getElementById("js-pre")];
-  const word = item.getAttribute("data-value").replace(/\./g, "");
+function highlightMatches(item) {
+  const queue = [document.getElementById('js-pre')];
+  const word = item.getAttribute('data-value').replace(/\./g, '');
   let curr;
+
   while ((curr = queue.pop())) {
-    if (!curr.textContent.match(word)) continue;
-    curr.childNodes.forEach((currentNode) => {
-      const currentTextContent = currentNode.textContent;
-      const textWithoutQuotes = currentTextContent.substring(
-        1,
-        currentTextContent.length - 1
-      );
-      const ArrFromText = textWithoutQuotes.split(" ");
-      switch (currentNode.nodeType) {
-        case Node.TEXT_NODE:
-          if (ArrFromText.indexOf(word) > -1) {
-            curr.classList.toggle("highlighted-text");
-          }
-          break;
-        case Node.ELEMENT_NODE:
-          queue.push(currentNode);
-          break;
-      }
-    });
+    if (curr.textContent.match(word)) {
+      // eslint-disable-next-line no-loop-func
+      curr.childNodes.forEach((currentNode) => {
+        const currentTextContent = currentNode.textContent;
+        const textWithoutQuotes = currentTextContent.substring(
+          1,
+          currentTextContent.length - 1
+        );
+        const arrFromText = textWithoutQuotes.split(' ');
+
+        switch (currentNode.nodeType) {
+          case Node.TEXT_NODE:
+            if (arrFromText.indexOf(word) > -1) {
+              curr.classList.toggle('highlighted-text');
+            }
+            break;
+          case Node.ELEMENT_NODE:
+            queue.push(currentNode);
+            break;
+          // No Default
+        }
+      });
+    }
   }
 }
 
 hoverClass.forEach((item) => {
-  item.addEventListener("mouseenter", () => {
-    targetHtmlElement(item);
+  item.addEventListener('mouseenter', () => {
+    highlightMatches(item);
   });
-  item.addEventListener("mouseleave", () => {
-    targetHtmlElement(item);
+
+  item.addEventListener('mouseleave', () => {
+    highlightMatches(item);
   });
 });
 
 // Change HTML tabs
 
-codeWrapperTabs.forEach((item) => {
-  const dataHTML = item.getAttribute("data-html");
-  item.addEventListener("click", () => {
-    codeWrapperTabs.forEach((item) => {
-      item.classList.remove("selected");
-    });
-    item.classList.add("selected");
-    document.querySelectorAll(".code-wrapper-content").forEach((item) => {
-      if (item.getAttribute("data-html") === dataHTML) {
-        item.style.display = "block";
+codeWrapperTabs.forEach((tab) => {
+  const dataHTML = tab.getAttribute('data-html');
 
-        // have the possibility of styling certain elements as their dark version
-        if (item.querySelector('[class$="--dark"]')) {
-          item.parentNode.classList.add("dark");
-        } else {
-          item.parentNode.classList.remove("dark");
-        }
-      } else {
-        item.style.display = "none";
-      }
+  tab.addEventListener('click', () => {
+    codeWrapperTabs.forEach((_tab) => _tab.classList.remove('selected'));
+
+    tab.classList.add('selected');
+
+    document.querySelectorAll('.code-wrapper-content').forEach((content) => {
+      const isActive = content.getAttribute('data-html') === dataHTML;
+
+      content.style.display = isActive ? 'block' : 'none';
+      content.parentNode.classList.toggle(
+        'dark',
+        isActive && content.querySelector('[class$="--dark"]')
+      );
     });
-    document.querySelectorAll(".code-output").forEach((item) => {
-      if (item.getAttribute("data-html") === dataHTML) {
-        item.style.display = "block";
-      } else {
-        item.style.display = "none";
-      }
+
+    document.querySelectorAll('.code-output').forEach((output) => {
+      output.style.display =
+        output.getAttribute('data-html') === dataHTML ? 'block' : 'none';
     });
   });
 });
