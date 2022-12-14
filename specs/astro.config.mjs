f2f58a5d0dd30293a621a342889554bae6c75410
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2022-12-14 18:30:25 +0100

    chore(specs): fix output directory

--- specs/astro.config.mjs
+++ specs/astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://instantsearchjs.netlify.app/',
   base: '/specs',
-  outDir: '../../specs',
+  outDir: '../website/specs',
 });
