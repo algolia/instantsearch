2c94c3aeb95eb0316f5ed71c0cdff60613024e19
Haroen Viaene <hello@haroen.me>
2022-12-28 10:00:17 +0100

    chore(scripts): right file written in Dom-maps version
    see: https://github.com/algolia/instantsearch.js/pull/5364/files#r1058174310

--- packages/react-instantsearch-dom-maps/scripts/version.js
+++ packages/react-instantsearch-dom-maps/scripts/version.js
@@ -2,9 +2,13 @@
 /* eslint-disable import/no-commonjs */
 
 const fs = require('fs');
+const path = require('path');
 
 const package = require('../package.json');
 
 package.peerDependencies['react-instantsearch-dom'] = package.version;
 
-fs.writeFileSync('../package.json', JSON.stringify(package, null, 2));
+fs.writeFileSync(
+  path.resolve(__dirname, '../package.json'),
+  JSON.stringify(package, null, 2)
+);
