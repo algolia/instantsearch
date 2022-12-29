30decf9528029c3b99da33823e8c7b055a3cf6ac
Haroen Viaene <hello@haroen.me>
2022-12-29 12:57:45 +0100

    ci(vue3): avoid using utilities

---
+++ scripts/prepare-vue3.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const shell = require('shelljs');
+
+console.log('swapping Vue 3 dependency');
+shell.sed(
+  '-i',
+  /"vue": "2.*"(,?)/,
+  '"vue": "3.1.2"$1',
+  path.resolve(__dirname, '../packages/vue-instantsearch/package.json')
+);
+
+console.log('replacing vue-compat aliases');
+fs.writeFileSync(
+  path.resolve(
+    __dirname,
+    '../packages/vue-instantsearch/src/util/vue-compat/index.js'
+  ),
+  "export * from './index-vue3';"
+);
+
+console.log('switching to Vue 3 for Jest');
+shell.sed(
+  '-i',
+  /vue2-jest/,
+  'vue3-jest',
+  path.resolve(__dirname, '../jest.config.js')
+);
+
+console.log('installing');
+shell.exec('yarn');
