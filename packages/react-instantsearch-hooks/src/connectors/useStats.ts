90b707dd590e1c6cca4d3ab17b4363d2b3f3dda0
Christopher Hawke <69921547+cdhawke@users.noreply.github.com>
2023-01-10 16:32:48 -0500

    fix: linter

--- packages/react-instantsearch-hooks/src/connectors/useStats.ts
+++ packages/react-instantsearch-hooks/src/connectors/useStats.ts
@@ -1,12 +1,12 @@
+import connectStats from 'instantsearch.js/es/connectors/stats/connectStats';
+
+import { useConnector } from '../hooks/useConnector';
+
+import type { AdditionalWidgetProperties } from '../hooks/useConnector';
 import type {
   StatsConnectorParams,
   StatsWidgetDescription,
 } from 'instantsearch.js/es/connectors/stats/connectStats';
-import connectStats from 'instantsearch.js/es/connectors/stats/connectStats';
-import {
-  AdditionalWidgetProperties,
-  useConnector,
-} from '../hooks/useConnector';
 
 export type UseStatsProps = StatsConnectorParams;
 
