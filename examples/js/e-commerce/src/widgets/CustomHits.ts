79e88218ae00f0c85aeaab75088132d178307cfb
Dhaya <154633+dhayab@users.noreply.github.com>
2023-01-24 15:54:32 +0100

    wip: manual event binding in connectors

--- examples/js/e-commerce/src/widgets/CustomHits.ts
+++ examples/js/e-commerce/src/widgets/CustomHits.ts
@@ -1,13 +1,28 @@
-import { connectHits } from 'instantsearch.js/es/connectors';
+import type { BaseHit, Hit, Renderer } from 'instantsearch.js';
+import type { HitsWidgetDescription } from 'instantsearch.js/es/connectors/hits/connectHits';
+import type { HitsWidgetParams } from 'instantsearch.js/es/widgets/hits/hits';
 
-function getHitByObjectID(hits, objectID) {
+import { connectHits } from 'instantsearch.js/es/connectors';
+import {
+  getContainerNode,
+  SendEventForHits,
+} from 'instantsearch.js/es/lib/utils';
+
+function getHitByObjectID(
+  hits: Array<Hit<BaseHit>>,
+  objectID: BaseHit['objectID']
+) {
   return hits.find((hit) => hit.objectID === objectID);
 }
 
-const renderHitsWithSelector = (renderOptions, isFirstRender) => {
+const renderHitsWithSelector: Renderer<
+  HitsWidgetDescription['renderState'],
+  HitsWidgetParams
+> = (renderOptions, _isFirstRender) => {
   const { hits, widgetParams, sendEvent } = renderOptions;
+  const container = getContainerNode(widgetParams.container);
 
-  widgetParams.container.innerHTML = `
+  container.innerHTML = `
     <ul>
       ${hits
         .map(
@@ -20,7 +35,7 @@ const renderHitsWithSelector = (renderOptions, isFirstRender) => {
     </ul>
   `;
 
-  [...widgetParams.container.querySelectorAll('li')].forEach((el) => {
+  [...container.querySelectorAll('li')].forEach((el) => {
     el.addEventListener('click', () => {
       sendEvent(
         'click',
@@ -31,21 +46,74 @@ const renderHitsWithSelector = (renderOptions, isFirstRender) => {
   });
 };
 
-const renderHitsWithBindEvent = (renderOptions, isFirstRender) => {
-  const { hits, widgetParams, bindEvent } = renderOptions;
+class HitsWrapper {
+  hits: any[] = [];
 
-  widgetParams.container.innerHTML = `
+  constructor(
+    readonly container: HTMLElement,
+    readonly sendEvent: SendEventForHits
+  ) {}
+
+  push(hit: Hit<BaseHit>, template: string): string {
+    const root = document.createElement('template');
+    root.innerHTML = template;
+    root.content.querySelector<HTMLElement>('*')!.dataset.productId =
+      hit.objectID;
+
+    this.hits.push(hit);
+    return root.innerHTML;
+  }
+
+  bind() {
+    this.container
+      .querySelectorAll<HTMLElement>('[data-product-id]')
+      .forEach((el) => {
+        el.addEventListener('click', () => {
+          this.sendEvent(
+            'click',
+            this.hits.find((hit) => hit.objectID === el.dataset.productId),
+            'CustomHits widget: Hit clicked'
+          );
+        });
+      });
+  }
+}
+
+const renderHitsWithWrapper: Renderer<
+  HitsWidgetDescription['renderState'],
+  HitsWidgetParams
+> = (renderOptions, _isFirstRender) => {
+  const { hits, widgetParams, sendEvent } = renderOptions;
+  const container = getContainerNode(widgetParams.container);
+  const wrapper = new HitsWrapper(container, sendEvent);
+
+  container.innerHTML = `
     <ul>
       ${hits
-        .map(
-          (item) =>
-            `<li ${bindEvent('click', item, 'CustomHits widget: Hit clicked')}>
-              ${item.name}
-            </li>`
-        )
+        .map((item) => wrapper.push(item, `<li>${item.name}</li>`))
         .join('')}
     </ul>
   `;
+
+  wrapper.bind();
 };
 
-export const customHits = connectHits(renderHitsWithBindEvent);
+// const renderHitsWithBindEvent = (renderOptions, isFirstRender) => {
+//   const { hits, widgetParams, bindEvent } = renderOptions;
+
+//   widgetParams.container.innerHTML = `
+//     <ul>
+//       ${hits
+//         .map(
+//           (item) =>
+//             `<li ${bindEvent('click', item, 'CustomHits widget: Hit clicked')}>
+//               ${item.name}
+//             </li>`
+//         )
+//         .join('')}
+//     </ul>
+//   `;
+// };
+
+export const customHits = connectHits(renderHitsWithSelector);
+// export const customHits = connectHits(renderHitsWithWrapper);
