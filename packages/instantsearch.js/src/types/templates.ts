68152d0665ad67954ab7351069798e6fd530aa4b
Haroen Viaene <hello@haroen.me>
2023-01-24 18:15:40 +0100

    fix(sendEvent):  correct types in template (#5449)
    * fix(sendEvent):  correct types in template
    
    sendEvent is only passed in cases bindEvent is as well (hits, infinitehits), and this means  `?.` in the docs can be removed before sendEvent
    
    * make types pass

--- packages/instantsearch.js/src/types/templates.ts
+++ packages/instantsearch.js/src/types/templates.ts
@@ -6,13 +6,17 @@ import type {
   Snippet,
 } from '../helpers/components';
 import type { html } from 'htm/preact';
-import type { BindEventForHits, SendEventForHits } from '../lib/utils';
+import type {
+  BuiltInBindEventForHits,
+  CustomBindEventForHits,
+  SendEventForHits,
+} from '../lib/utils';
 
 export type Template<TTemplateData = void> =
   | string
   | ((data: TTemplateData, params: TemplateParams) => VNode | VNode[] | string);
 
-export type TemplateParams = BindEventForHits & {
+export type TemplateParams = {
   html: typeof html;
   components: {
     Highlight: typeof Highlight;
@@ -23,9 +27,24 @@ export type TemplateParams = BindEventForHits & {
   sendEvent?: SendEventForHits;
 };
 
+interface TemplateWithBindEventParams extends TemplateParams {
+  /** @deprecated use sendEvent instead */
+  (
+    ...args: Parameters<BuiltInBindEventForHits>
+  ): ReturnType<BuiltInBindEventForHits>;
+  /** @deprecated use sendEvent instead */
+  (
+    ...args: Parameters<CustomBindEventForHits>
+  ): ReturnType<CustomBindEventForHits>;
+  sendEvent: SendEventForHits;
+}
+
 export type TemplateWithBindEvent<TTemplateData = void> =
   | string
-  | ((data: TTemplateData, params: TemplateParams) => VNode | VNode[] | string);
+  | ((
+      data: TTemplateData,
+      params: TemplateWithBindEventParams
+    ) => VNode | VNode[] | string);
 
 export type Templates = {
   [key: string]: Template<any> | TemplateWithBindEvent<any> | undefined;
