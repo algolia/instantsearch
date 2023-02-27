372a7a7fd620818ec4ddee193e81b9c3441c6c35
Haroen Viaene <hello@haroen.me>
2023-02-27 13:50:15 +0100

    chore: remove dead code

--- packages/instantsearch.js/src/lib/utils/detect-insights-client.ts
+++
@@ -1,8 +0,0 @@
-import { safelyRunOnBrowser } from './safelyRunOnBrowser';
-
-export function hasDetectedInsightsClient(): boolean {
-  return safelyRunOnBrowser<boolean>(
-    ({ window }) => Boolean((window as any).AlgoliaAnalyticsObject),
-    { fallback: () => false }
-  );
-}
