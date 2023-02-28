85dfbc9ebd722fbe6a7e1bd056950fdbcc16d8d9
Haroen Viaene <hello@haroen.me>
2023-02-28 17:04:37 +0100

    fix(userToken): prevent search waterfall (#5512)
    This makes sure when the same userToken is set multiple times, it doesn't cause multiple search requests, as could be the case if something would call setUserToken without condition.
    
    Discovered in #5500, but should be merged separately.
    
    Co-authored-by: Fabien Motte <fabien.motte@algolia.com>

--- packages/instantsearch.js/src/middlewares/__tests__/createInsightsMiddleware.ts
+++ packages/instantsearch.js/src/middlewares/__tests__/createInsightsMiddleware.ts
@@ -7,11 +7,13 @@ import {
   createInsightsUmdVersion,
   createSearchClient,
 } from '@instantsearch/mocks';
+import { castToJestMock } from '@instantsearch/testutils';
 import { wait } from '@instantsearch/testutils/wait';
 import { fireEvent } from '@testing-library/dom';
 
 import { createInsightsMiddleware } from '..';
 import { createInstantSearch } from '../../../test/createInstantSearch';
+import { connectSearchBox } from '../../connectors';
 import instantsearch from '../../index.es';
 import { history } from '../../lib/routers';
 import { warning } from '../../lib/utils';
@@ -45,6 +47,7 @@ describe('insights', () => {
     started = true,
     insights = false,
   } = {}) => {
+    castToJestMock(searchClient.search).mockClear();
     const { analytics, insightsClient } = createInsights();
     const indexName = 'my-index';
     const instantSearchInstance = instantsearch({
@@ -109,6 +112,8 @@ describe('insights', () => {
     (window as any).aa = undefined;
 
     document.body.innerHTML = '';
+
+    document.cookie = '_ALGOLIA=;';
   });
 
   describe('usage', () => {
@@ -623,6 +628,76 @@ See documentation: https://www.algolia.com/doc/guides/building-search-ui/going-f
       ).toEqual('def');
     });
 
+    it('searches once per unique userToken', async () => {
+      const { insightsClient, instantSearchInstance } = createTestEnvironment();
+
+      instantSearchInstance.addWidgets([connectSearchBox(() => ({}))({})]);
+
+      await wait(0);
+      expect(instantSearchInstance.client.search).toHaveBeenCalledTimes(1);
+
+      insightsClient('setUserToken', 'abc');
+      instantSearchInstance.use(
+        createInsightsMiddleware({
+          insightsClient,
+        })
+      );
+
+      await wait(0);
+      expect(instantSearchInstance.client.search).toHaveBeenCalledTimes(2);
+
+      insightsClient('setUserToken', 'abc');
+
+      await wait(0);
+      expect(instantSearchInstance.client.search).toHaveBeenCalledTimes(2);
+    });
+
+    it("doesn't search when userToken is falsy", async () => {
+      const { insightsClient, instantSearchInstance } = createTestEnvironment();
+
+      instantSearchInstance.addWidgets([connectSearchBox(() => ({}))({})]);
+
+      await wait(0);
+      expect(instantSearchInstance.client.search).toHaveBeenCalledTimes(1);
+      expect(instantSearchInstance.client.search).toHaveBeenLastCalledWith([
+        {
+          indexName: 'my-index',
+          params: {
+            facets: [],
+            query: '',
+            tagFilters: '',
+          },
+        },
+      ]);
+
+      insightsClient('setUserToken', 0);
+      instantSearchInstance.use(
+        createInsightsMiddleware({
+          insightsClient,
+          insightsInitParams: { useCookie: false },
+        })
+      );
+
+      await wait(0);
+      expect(instantSearchInstance.client.search).toHaveBeenCalledTimes(2);
+      expect(instantSearchInstance.client.search).toHaveBeenLastCalledWith([
+        {
+          indexName: 'my-index',
+          params: {
+            clickAnalytics: true,
+            facets: [],
+            query: '',
+            tagFilters: '',
+          },
+        },
+      ]);
+
+      insightsClient('setUserToken', undefined);
+
+      await wait(0);
+      expect(instantSearchInstance.client.search).toHaveBeenCalledTimes(2);
+    });
+
     describe('umd', () => {
       it('applies userToken from queue if exists', () => {
         const {
@@ -818,8 +893,6 @@ See documentation: https://www.algolia.com/doc/guides/building-search-ui/going-f
     await wait(100);
     // url should not get cleared
     expect(document.location.href).toEqual(url);
-
-    document.cookie = '';
   });
 
   test('does not throw error when document or cookie are undefined', () => {
