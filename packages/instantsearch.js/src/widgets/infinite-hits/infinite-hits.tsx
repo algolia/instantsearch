7f415a876d04e4a1bbe8de6b5e5b58dd12749876
Haroen Viaene <hello@haroen.me>
2023-03-08 15:54:50 +0100

    refactor(bindEvent): move listener inside components (#5528)
    * refactor(bindEvent): move listener inside components
    
    This was needlessly abstracted with a HOC, not allowing us to use this same function inside onClick of an individual hit, which could help with deduplicating sendEvent + bindEvent
    
    I didn't remove the `withInsightsListener` HOC, as it could (in theory) be used externally.
    
    * remove unused data

--- packages/instantsearch.js/src/widgets/infinite-hits/infinite-hits.tsx
+++ packages/instantsearch.js/src/widgets/infinite-hits/infinite-hits.tsx
@@ -5,7 +5,7 @@ import { h, render } from 'preact';
 
 import InfiniteHits from '../../components/InfiniteHits/InfiniteHits';
 import connectInfiniteHits from '../../connectors/infinite-hits/connectInfiniteHits';
-import { withInsights, withInsightsListener } from '../../lib/insights';
+import { withInsights } from '../../lib/insights';
 import { component } from '../../lib/suit';
 import { prepareTemplateProps } from '../../lib/templating';
 import {
@@ -40,7 +40,6 @@ const withUsage = createDocumentationMessageGenerator({
   name: 'infinite-hits',
 });
 const suit = component('InfiniteHits');
-const InfiniteHitsWithInsightsListener = withInsightsListener(InfiniteHits);
 
 export type InfiniteHitsCSSClasses = Partial<{
   /**
@@ -182,11 +181,11 @@ const renderer =
     }
 
     render(
-      <InfiniteHitsWithInsightsListener
+      <InfiniteHits
         cssClasses={cssClasses}
         hits={hits}
-        results={results}
-        hasShowPrevious={hasShowPrevious}
+        results={results!}
+        hasShowPrevious={hasShowPrevious!}
         showPrevious={showPrevious}
         showMore={showMore}
         templateProps={renderState.templateProps!}
