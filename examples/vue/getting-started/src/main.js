5bb94fa03e2a791e1addf9d9be618ce343e8d8bf
Dhaya <154633+dhayab@users.noreply.github.com>
2023-04-12 18:06:14 +0200

    chore(examples): vue getting started

---
+++ examples/vue/getting-started/src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import InstantSearch from 'vue-instantsearch';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+Vue.use(InstantSearch);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
