22e7ef7b0d09aa0c31e39b40e70e66f018c7bdb2
Mike Davis <mikedavisis@gmail.com>
2023-04-12 15:50:33 -0700

    feat(refinementList): updated react test

--- packages/react-instantsearch-hooks/src/connectors/__tests__/useRefinementList.test.tsx
+++ packages/react-instantsearch-hooks/src/connectors/__tests__/useRefinementList.test.tsx
@@ -21,6 +21,7 @@ describe('useRefinementList', () => {
       hasExhaustiveItems: true,
       isFromSearch: false,
       isShowingMore: false,
+      showMoreCount: 0,
       items: [],
       refine: expect.any(Function),
       searchForItems: expect.any(Function),
