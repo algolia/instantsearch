ca3de84d7828cb48696e8710efe051c2e12c0750
Haroen Viaene <hello@haroen.me>
2023-06-16 10:06:32 +0200

    chore(devDeps): remove lodash.keyBy (algolia/algoliasearch-helper-js#945)
    only used in the jsdoc-data plugin, and can easily be replaced without dependency

--- packages/algoliasearch-helper/documentation-src/plugins/jsdoc-data.js
+++ packages/algoliasearch-helper/documentation-src/plugins/jsdoc-data.js
@@ -1,6 +1,17 @@
 'use strict';
 var jsdoc = require('jsdoc-to-markdown');
-var keyBy = require('lodash.keyby');
+
+/**
+ * Same as lodash keyBy, turns an array of objects into an object keyed by the
+ * value of the given key. When there are multiple objects with the same key,
+ * the last one wins.
+ */
+function keyBy(arr, key) {
+  return arr.reduce(function(obj, o) {
+    obj[o[key]] = o;
+    return obj;
+  }, {});
+}
 
 module.exports = function(opts) {
   if (!opts.src) throw new Error('opts.src must be defined');
@@ -8,8 +19,10 @@ module.exports = function(opts) {
 
   return function(_files, metalsmith) {
     var files = metalsmith.path(opts.src);
-    return jsdoc.getTemplateData({files: files}).then(data => {
-      var filteredData = data.filter(function(o) {return !o.deprecated;});
+    return jsdoc.getTemplateData({files: files}).then((data) => {
+      var filteredData = data.filter(function(o) {
+        return !o.deprecated;
+      });
       var metadata = metalsmith.metadata();
       if (!namespace) metadata.jsdoc = keyBy(filteredData, 'longname');
       else {
