2e2d2109f58e04d535e150004f8346ad1ab555be
Dhaya <154633+dhayab@users.noreply.github.com>
2023-07-27 15:25:00 +0200

    remove dom and native packages

--- packages/react-instantsearch-dom-maps/src/index.js
+++
@@ -1,8 +0,0 @@
-export { default as GeoSearch } from './GeoSearch';
-export { default as Marker } from './Marker';
-export { default as CustomMarker } from './CustomMarker';
-export { default as Redo } from './Redo';
-export { default as Control } from './Control';
-export { default as GeoSearchContext } from './GeoSearchContext';
-export { default as GoogleMapsLoader } from './GoogleMapsLoader';
-export { default as withGoogleMaps } from './withGoogleMaps';
