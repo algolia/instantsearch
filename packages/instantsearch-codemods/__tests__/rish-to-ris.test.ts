a6da32cd13626e39fc5905d3c0f29c857370b728
Haroen Viaene <hello@haroen.me>
2023-07-27 09:50:02 +0200

    fix(compat): codemod for use -> addMiddlewares (#5782)
    * fix(compat): codemod for use -> addMiddlewares
    
    * Update packages/instantsearch-codemods/__tests__/rish-to-ris.test.ts

--- packages/instantsearch-codemods/__tests__/rish-to-ris.test.ts
+++ packages/instantsearch-codemods/__tests__/rish-to-ris.test.ts
@@ -3,3 +3,4 @@ const define = require('jscodeshift/dist/testUtils').defineTest;
 
 define(__dirname, 'src/rish-to-ris', null, 'rish-to-ris/import');
 define(__dirname, 'src/rish-to-ris', null, 'rish-to-ris/path');
+define(__dirname, 'src/rish-to-ris', null, 'rish-to-ris/use');
--- packages/instantsearch-codemods/src/rish-to-ris.ts
+++ packages/instantsearch-codemods/src/rish-to-ris.ts
@@ -1,6 +1,13 @@
 import { replaceImports } from './replaceImports';
 
-import type { API, FileInfo, Options } from 'jscodeshift';
+import type {
+  API,
+  FileInfo,
+  Identifier,
+  MemberExpression,
+  ObjectProperty,
+  Options,
+} from 'jscodeshift';
 
 export default function transformer(
   file: FileInfo,
@@ -10,6 +17,77 @@ export default function transformer(
   const source = j(file.source);
   const printOptions = options.printOptions || {
     quote: 'single',
+  };
+
+  const replaceUse = () => {
+    // destructured
+    source
+      .find(j.VariableDeclarator, {
+        id: {
+          type: 'ObjectPattern',
+          properties: [
+            {
+              key: {
+                name: 'use',
+              },
+            },
+          ],
+        },
+        init: {
+          type: 'CallExpression',
+          callee: {
+            name: 'useInstantSearch',
+          },
+        },
+      })
+      .forEach((path) => {
+        const useProperty =
+          path.value.id.type === 'ObjectPattern' &&
+          (path.value.id.properties.find((prop) => {
+            return (
+              prop.type === 'Property' &&
+              prop.key.type === 'Identifier' &&
+              prop.key.name === 'use'
+            );
+          }) as ObjectProperty);
+
+        if (!useProperty) {
+          return;
+        }
+
+        const originalKeyValue = (useProperty.value as Identifier).name;
+        (useProperty.key as Identifier).name = 'addMiddlewares';
+
+        // If the property is reassigned to another variable we don't have to transform it
+        if (originalKeyValue !== 'use') {
+          return;
+        }
+
+        useProperty.shorthand = false;
+        (useProperty.value as Identifier).name = 'use';
+      });
+
+    // dot property
+    source
+      .find(j.VariableDeclarator, {
+        init: {
+          type: 'MemberExpression',
+          object: {
+            type: 'CallExpression',
+            callee: {
+              name: 'useInstantSearch',
+            },
+          },
+          property: {
+            type: 'Identifier',
+            name: 'use',
+          },
+        },
+      })
+      .forEach((path) => {
+        ((path.value.init as MemberExpression).property as Identifier).name =
+          'addMiddlewares';
+      });
   };
 
   replaceImports(
@@ -38,6 +116,8 @@ export default function transformer(
     'react-instantsearch-hooks-server',
     'react-instantsearch'
   );
+
+  replaceUse();
 
   return source.toSource(printOptions);
 }
