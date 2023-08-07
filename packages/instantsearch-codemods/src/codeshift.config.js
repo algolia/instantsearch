432b57d8fbd36471951a393ef15148790777811f
Aymeric Giraudet <aymeric.giraudet@algolia.com>
2023-08-07 18:00:41 +0200

    fix(codemods): add empty transforms object so that script does not fail

--- packages/instantsearch-codemods/src/codeshift.config.js
+++ packages/instantsearch-codemods/src/codeshift.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   description: 'Codemods for InstantSearch libraries',
+  transforms: {},
   presets: {
     'ris-v6-to-v7': require('./ris-v6-to-v7'),
     'rish-to-ris': require('./rish-to-ris'),
