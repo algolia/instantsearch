fdf974a0d2d4807bf6b0f53140d36e1c6365e457
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2023-08-21 16:55:01 +0200

    test: reduce flavor-specific tests

---
+++ packages/instantsearch.js/src/widgets/hits-per-page/__tests__/hits-per-page.test.tsx
@@ -0,0 +1,60 @@
+/**
+ * @jest-environment jsdom
+ */
+/** @jsx h */
+import { createSearchClient } from '@instantsearch/mocks';
+
+import instantsearch from '../../../index.es';
+import hitsPerPage from '../hits-per-page';
+
+describe('hitsPerPage', () => {
+  describe('options', () => {
+    test('throws without a `container`', () => {
+      expect(() => {
+        const searchClient = createSearchClient();
+
+        const search = instantsearch({
+          indexName: 'indexName',
+          searchClient,
+        });
+
+        search.addWidgets([
+          hitsPerPage({
+            // @ts-expect-error
+            container: undefined,
+          }),
+        ]);
+      }).toThrowErrorMatchingInlineSnapshot(`
+"The \`container\` option is required.
+
+See documentation: https://www.algolia.com/doc/api-reference/widgets/hits-per-page/js/"
+`);
+    });
+
+    test('warns with an item without a `value`', () => {
+      const searchClient = createSearchClient();
+
+      const search = instantsearch({
+        indexName: 'indexName',
+        searchClient,
+      });
+
+      search.addWidgets([
+        hitsPerPage({
+          container: document.createElement('div'),
+          items: [
+            // @ts-expect-error Incomplete item
+            { label: '10', default: true },
+          ],
+        }),
+      ]);
+
+      expect(() => search.start())
+        .toWarnDev(`[InstantSearch.js]: The \`items\` option of \`hitsPerPage\` does not contain the "hits per page" value coming from the state: undefined.
+
+You may want to add another entry to the \`items\` option with this value.`);
+    });
+
+    //test('');
+  });
+});
--- packages/react-instantsearch/src/widgets/__tests__/HitsPerPage.test.tsx
+++ packages/react-instantsearch/src/widgets/__tests__/HitsPerPage.test.tsx
@@ -2,127 +2,13 @@
  * @jest-environment jsdom
  */
 
-import { createSearchClient } from '@instantsearch/mocks';
 import { InstantSearchTestWrapper } from '@instantsearch/testutils';
-import { render, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 import React from 'react';
 
 import { HitsPerPage } from '../HitsPerPage';
 
 describe('HitsPerPage', () => {
-  test('renders with props', async () => {
-    const searchClient = createSearchClient({});
-    const { container } = render(
-      <InstantSearchTestWrapper searchClient={searchClient}>
-        <HitsPerPage
-          items={[
-            { label: '10', value: 10, default: true },
-            { label: '20', value: 20 },
-            { label: '30', value: 30 },
-          ]}
-        />
-      </InstantSearchTestWrapper>
-    );
-
-    await waitFor(() => expect(searchClient.search).toHaveBeenCalledTimes(1));
-
-    expect(container).toMatchInlineSnapshot(`
-      <div>
-        <div
-          class="ais-HitsPerPage"
-        >
-          <select
-            class="ais-HitsPerPage-select"
-          >
-            <option
-              class="ais-HitsPerPage-option"
-              value="10"
-            >
-              10
-            </option>
-            <option
-              class="ais-HitsPerPage-option"
-              value="20"
-            >
-              20
-            </option>
-            <option
-              class="ais-HitsPerPage-option"
-              value="30"
-            >
-              30
-            </option>
-          </select>
-        </div>
-      </div>
-    `);
-  });
-
-  test('selects current value', async () => {
-    const searchClient = createSearchClient({});
-    const { getByRole } = render(
-      <InstantSearchTestWrapper
-        searchClient={searchClient}
-        initialUiState={{
-          indexName: {
-            hitsPerPage: 20,
-          },
-        }}
-      >
-        <HitsPerPage
-          items={[
-            { label: '10', value: 10, default: true },
-            { label: '20', value: 20 },
-            { label: '30', value: 30 },
-          ]}
-        />
-      </InstantSearchTestWrapper>
-    );
-
-    await waitFor(() => expect(searchClient.search).toHaveBeenCalledTimes(1));
-
-    expect(
-      (getByRole('option', { name: '10' }) as HTMLOptionElement).selected
-    ).toBe(false);
-    expect(
-      (getByRole('option', { name: '20' }) as HTMLOptionElement).selected
-    ).toBe(true);
-    expect(
-      (getByRole('option', { name: '30' }) as HTMLOptionElement).selected
-    ).toBe(false);
-  });
-
-  test('refines on select', async () => {
-    const searchClient = createSearchClient({});
-    const { getByRole } = render(
-      <InstantSearchTestWrapper searchClient={searchClient}>
-        <HitsPerPage
-          items={[
-            { label: '10', value: 10, default: true },
-            { label: '20', value: 20 },
-            { label: '30', value: 30 },
-          ]}
-        />
-      </InstantSearchTestWrapper>
-    );
-
-    await waitFor(() => expect(searchClient.search).toHaveBeenCalledTimes(1));
-
-    userEvent.selectOptions(getByRole('combobox'), ['30']);
-
-    expect(searchClient.search).toHaveBeenCalledTimes(2);
-    expect(searchClient.search).toHaveBeenLastCalledWith(
-      expect.arrayContaining([
-        expect.objectContaining({
-          params: expect.objectContaining({
-            hitsPerPage: 30,
-          }),
-        }),
-      ])
-    );
-  });
-
   test('forwards custom class names and `div` props to the root element', () => {
     const { container } = render(
       <InstantSearchTestWrapper>
