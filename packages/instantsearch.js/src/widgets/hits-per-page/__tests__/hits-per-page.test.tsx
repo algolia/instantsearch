5d6cbb565fb4a1de7f745573652e7ef8b29e33fa
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2023-08-22 10:20:44 +0200

    test: test custom class names

--- packages/instantsearch.js/src/widgets/hits-per-page/__tests__/hits-per-page.test.tsx
+++ packages/instantsearch.js/src/widgets/hits-per-page/__tests__/hits-per-page.test.tsx
@@ -3,6 +3,8 @@
  */
 /** @jsx h */
 import { createSearchClient } from '@instantsearch/mocks';
+import { wait } from '@instantsearch/testutils';
+import { findAllByRole, findByRole } from '@testing-library/dom';
 
 import instantsearch from '../../../index.es';
 import hitsPerPage from '../hits-per-page';
@@ -29,6 +31,44 @@ describe('hitsPerPage', () => {
 
 See documentation: https://www.algolia.com/doc/api-reference/widgets/hits-per-page/js/"
 `);
+    });
+
+    test('add custom CSS classes', async () => {
+      const container = document.createElement('div');
+      const searchClient = createSearchClient();
+
+      const search = instantsearch({
+        indexName: 'indexName',
+        searchClient,
+      });
+
+      search.addWidgets([
+        hitsPerPage({
+          container,
+          items: [
+            { label: '10', value: 10, default: true },
+            { label: '20', value: 20 },
+            { label: '30', value: 30 },
+          ],
+          cssClasses: {
+            root: 'ROOT',
+            select: 'SELECT',
+            option: 'OPTION',
+          },
+        }),
+      ]);
+
+      search.start();
+
+      await wait(0);
+
+      const root = container.firstChild;
+      const select = await findByRole(container, 'combobox');
+      const options = await findAllByRole(container, 'option');
+
+      expect(root).toHaveClass('ROOT');
+      expect(select).toHaveClass('SELECT');
+      options.forEach((option) => expect(option).toHaveClass('OPTION'));
     });
 
     test('warns with an item without a `value`', () => {
