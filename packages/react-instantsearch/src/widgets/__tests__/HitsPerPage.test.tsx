2c6d262b3beecad266f981f11a2cac0536ae10f5
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2023-08-22 10:23:09 +0200

    test: test all custom class names

--- packages/react-instantsearch/src/widgets/__tests__/HitsPerPage.test.tsx
+++ packages/react-instantsearch/src/widgets/__tests__/HitsPerPage.test.tsx
@@ -3,18 +3,18 @@
  */
 
 import { InstantSearchTestWrapper } from '@instantsearch/testutils';
-import { render } from '@testing-library/react';
+import { findAllByRole, findByRole, render } from '@testing-library/react';
 import React from 'react';
 
 import { HitsPerPage } from '../HitsPerPage';
 
 describe('HitsPerPage', () => {
-  test('forwards custom class names and `div` props to the root element', () => {
+  test('forwards custom class names and `div` props to the root element', async () => {
     const { container } = render(
       <InstantSearchTestWrapper>
         <HitsPerPage
           className="MyHitsPerPage"
-          classNames={{ root: 'ROOT' }}
+          classNames={{ root: 'ROOT', select: 'SELECT', option: 'OPTION' }}
           title="Some custom title"
           items={[
             { label: '10', value: 10, default: true },
@@ -26,7 +26,12 @@ describe('HitsPerPage', () => {
     );
 
     const root = container.firstChild;
+    const select = await findByRole(container, 'combobox');
+    const options = await findAllByRole(container, 'option');
+
     expect(root).toHaveClass('MyHitsPerPage', 'ROOT');
+    expect(select).toHaveClass('SELECT');
+    options.forEach((option) => expect(option).toHaveClass('OPTION'));
     expect(root).toHaveAttribute('title', 'Some custom title');
   });
 });
