9d0ab94ab1d691a04f38f2a6d993e068bf4ebc80
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2023-08-22 11:10:07 +0200

    test: move test to connector tests

--- packages/instantsearch.js/src/widgets/hits-per-page/__tests__/hits-per-page.test.tsx
+++ packages/instantsearch.js/src/widgets/hits-per-page/__tests__/hits-per-page.test.tsx
@@ -70,29 +70,5 @@ See documentation: https://www.algolia.com/doc/api-reference/widgets/hits-per-pa
       expect(select).toHaveClass('SELECT');
       options.forEach((option) => expect(option).toHaveClass('OPTION'));
     });
-
-    test('warns with an item without a `value`', () => {
-      const searchClient = createSearchClient();
-
-      const search = instantsearch({
-        indexName: 'indexName',
-        searchClient,
-      });
-
-      search.addWidgets([
-        hitsPerPage({
-          container: document.createElement('div'),
-          items: [
-            // @ts-expect-error Incomplete item
-            { label: '10', default: true },
-          ],
-        }),
-      ]);
-
-      expect(() => search.start())
-        .toWarnDev(`[InstantSearch.js]: The \`items\` option of \`hitsPerPage\` does not contain the "hits per page" value coming from the state: undefined.
-
-You may want to add another entry to the \`items\` option with this value.`);
-    });
   });
 });
