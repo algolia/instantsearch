28ae4315a0a343ed20946652201f0858b0be79c6
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2023-09-08 14:32:53 +0200

    test: clean up InstantSearch.js tests

--- packages/instantsearch.js/src/widgets/pagination/__tests__/pagination.test.tsx
+++ packages/instantsearch.js/src/widgets/pagination/__tests__/pagination.test.tsx
@@ -14,12 +14,118 @@ import configure from '../../configure/configure';
 import pagination from '../pagination';
 
 import type { SearchResponse } from '../../../../src/types';
+import { findByLabelText, findByRole } from '@testing-library/dom';
 
 beforeEach(() => {
   document.body.innerHTML = '';
 });
 
 describe('pagination', () => {
+  describe('options', () => {
+    test('throws without a `container`', () => {
+      expect(() => {
+        const searchClient = createSearchClient();
+
+        const search = instantsearch({
+          indexName: 'indexName',
+          searchClient,
+        });
+
+        search.addWidgets([
+          pagination({
+            // @ts-expect-error
+            container: undefined,
+          }),
+        ]);
+      }).toThrowErrorMatchingInlineSnapshot(`
+"The \`container\` option is required.
+
+See documentation: https://www.algolia.com/doc/api-reference/widgets/pagination/js/"
+`);
+    });
+
+    test('add custom CSS classes', async () => {
+      const container = document.createElement('div');
+      const searchClient = createSearchClient();
+
+      const search = instantsearch({
+        indexName: 'indexName',
+        searchClient,
+      });
+
+      search.addWidgets([
+        pagination({
+          container,
+          cssClasses: {
+            root: 'ROOT',
+            noRefinementRoot: 'NO_REFINEMENT_ROOT',
+            list: 'LIST',
+            item: 'ITEM',
+            firstPageItem: 'FIRST_PAGE_ITEM',
+            lastPageItem: 'LAST_PAGE_ITEM',
+            previousPageItem: 'PREVIOUS_PAGE_ITEM',
+            nextPageItem: 'NEXT_PAGE_ITEM',
+            pageItem: 'PAGE_ITEM',
+            selectedItem: 'SELECTED_ITEM',
+            disabledItem: 'DISABLED_ITEM',
+            link: 'LINK',
+          },
+        }),
+      ]);
+
+      search.start();
+
+      await wait(0);
+
+      const root = container.firstChild;
+
+      const [
+        list,
+        firstPageItem,
+        previousPageItem,
+        nextPageItem,
+        lastPageItem,
+        pageLink,
+      ] = await Promise.all([
+        findByRole(container, 'list'),
+        (await findByLabelText(container, 'First')).parentNode,
+        (await findByLabelText(container, 'Previous')).parentNode,
+        (await findByLabelText(container, 'Next')).parentNode,
+        (await findByLabelText(container, 'Last')).parentNode,
+        findByRole(container, 'link', { name: 'Page 1' }),
+      ]);
+
+      expect(root).toHaveClass('ROOT', 'NO_REFINEMENT_ROOT');
+      expect(list).toHaveClass('LIST');
+      expect(firstPageItem).toHaveClass(
+        'FIRST_PAGE_ITEM',
+        'ITEM',
+        'DISABLED_ITEM'
+      );
+      expect(previousPageItem).toHaveClass(
+        'PREVIOUS_PAGE_ITEM',
+        'ITEM',
+        'DISABLED_ITEM'
+      );
+      expect(nextPageItem).toHaveClass(
+        'NEXT_PAGE_ITEM',
+        'ITEM',
+        'DISABLED_ITEM'
+      );
+      expect(lastPageItem).toHaveClass(
+        'LAST_PAGE_ITEM',
+        'ITEM',
+        'DISABLED_ITEM'
+      );
+      expect(pageLink.parentNode).toHaveClass(
+        'ITEM',
+        'PAGE_ITEM',
+        'SELECTED_ITEM'
+      );
+      expect(pageLink).toHaveClass('LINK');
+    });
+  });
+
   describe('templates', () => {
     test('does not warn with default templates', async () => {
       const container = document.createElement('div');
