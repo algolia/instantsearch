f5a81d5cf75cf1f81b0e337365b0a242920d541a
Dhaya <154633+dhayab@users.noreply.github.com>
2023-10-19 17:21:19 +0200

    revert example changes from poc

--- examples/js/getting-started/src/app.js
+++ examples/js/getting-started/src/app.js
@@ -2,10 +2,10 @@ import algoliasearch from 'algoliasearch/lite';
 import instantsearch from 'instantsearch.js';
 import {
   configure,
-  hierarchicalMenu,
   hits,
-  index,
+  pagination,
   panel,
+  refinementList,
   searchBox,
 } from 'instantsearch.js/es/widgets';
 
@@ -24,124 +24,28 @@ search.addWidgets([
   searchBox({
     container: '#searchbox',
   }),
-  index({ indexName: 'instant_search', indexId: 'sendEvent' }).addWidgets([
-    configure({
-      filters: 'brand:Apple',
-    }),
-    hits({
-      container: '#hits-sendevent',
-      templates: {
-        item: (hit, { html, components, sendEvent }) => html`
-          <article>
-            <h1>${components.Highlight({ hit, attribute: 'name' })}</h1>
-            <p>${components.Highlight({ hit, attribute: 'description' })}</p>
-            <p>$${hit.price}</p>
-            <button
-              type="button"
-              onClick=${() =>
-                sendEvent('conversion', hit, 'Add to cart', {
-                  eventSubtype: 'addToCart',
-                  objectData: [
-                    {
-                      queryId: hit.__queryID,
-                      price: hit.price,
-                      discount: 0,
-                      quantity: 1,
-                    },
-                  ],
-                  value: hit.price,
-                  currency: 'USD',
-                })}
-            >
-              Add to cart
-            </button>
-            ${' • '}
-            <button
-              type="button"
-              onClick=${() =>
-                sendEvent('conversion', hit, 'Buy product', {
-                  eventSubtype: 'purchase',
-                  objectData: [
-                    {
-                      queryId: hit.__queryID,
-                      price: hit.price,
-                      discount: 0,
-                      quantity: 1,
-                    },
-                  ],
-                  value: hit.price,
-                  currency: 'USD',
-                })}
-            >
-              Buy now
-            </button>
-          </article>
-        `,
-      },
-    }),
-  ]),
-  index({ indexName: 'instant_search', indexId: 'bindEvent' }).addWidgets([
-    configure({
-      filters: 'brand:Samsung',
-    }),
-    hits({
-      container: '#hits-bindevent',
-      templates: {
-        item(hit, bindEvent) {
-          return `<article>
-          <h1>${instantsearch.highlight({ hit, attribute: 'name' })}</h1>
-          <p>${instantsearch.highlight({ hit, attribute: 'description' })}</p>
-          <p>$${hit.price}</p>
-          <button
-          type="button"
-          ${bindEvent('conversion', hit, 'Add to cart', {
-            eventSubtype: 'addToCart',
-            objectData: [
-              {
-                queryId: hit.__queryID,
-                price: hit.price,
-                discount: 0,
-                quantity: 1,
-              },
-            ],
-            value: hit.price,
-            currency: 'USD',
-          })}
-        >
-          Add to cart
-        </button>
-        ${' • '}
-        <button
-          type="button"
-          ${bindEvent('conversion', hit, 'Buy product', {
-            eventSubtype: 'purchase',
-            objectData: [
-              {
-                queryId: hit.__queryID,
-                price: hit.price,
-                discount: 0,
-                quantity: 1,
-              },
-            ],
-            value: hit.price,
-            currency: 'USD',
-          })}
-        >
-          Buy now
-        </button>
-        </article>`;
-        },
-      },
-    }),
-  ]),
+  hits({
+    container: '#hits',
+    templates: {
+      item: (hit, { html, components }) => html`
+        <article>
+          <h1>${components.Highlight({ hit, attribute: 'name' })}</h1>
+          <p>${components.Highlight({ hit, attribute: 'description' })}</p>
+        </article>
+      `,
+    },
+  }),
   configure({
-    hitsPerPage: 2,
+    hitsPerPage: 8,
   }),
   panel({
-    templates: { header: 'Category' },
-  })(hierarchicalMenu)({
-    container: '#category',
-    attributes: ['hierarchicalCategories.lvl0', 'hierarchicalCategories.lvl1'],
+    templates: { header: 'brand' },
+  })(refinementList)({
+    container: '#brand-list',
+    attribute: 'brand',
+  }),
+  pagination({
+    container: '#pagination',
   }),
 ]);
 
