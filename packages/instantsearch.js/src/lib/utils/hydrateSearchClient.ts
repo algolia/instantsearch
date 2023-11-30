162d52978a0cce8cd9dc8321cbf2e74ea660e7de
Aymeric Giraudet <aymeric.giraudet@algolia.com>
2023-11-30 16:35:00 +0100

    stop using qs and added typings for file

--- packages/instantsearch.js/src/lib/utils/hydrateSearchClient.ts
+++ packages/instantsearch.js/src/lib/utils/hydrateSearchClient.ts
@@ -1,10 +1,18 @@
-// @ts-nocheck (types to be fixed during actual implementation)
-import qs from 'qs';
+import type {
+  SearchClient,
+  InitialResults,
+  ClientV3_4,
+  SearchOptions,
+  SearchResponse,
+} from '../../types';
 
-import type { InitialResults, SearchClient } from '../../types';
+type ClientWithCache = SearchClient & { cache: Record<string, string> };
 
 export function hydrateSearchClient(
-  client: SearchClient,
+  client: SearchClient & {
+    _cacheHydrated?: boolean;
+    _useCache?: boolean;
+  },
   results?: InitialResults
 ) {
   if (!results) {
@@ -16,11 +24,26 @@ export function hydrateSearchClient(
   // - Third party clients (detected by the `addAlgoliaAgent` function missing)
 
   if (
-    (!client.transporter || client._cacheHydrated) &&
+    (!('transporter' in client) || client._cacheHydrated) &&
     (!client._useCache || typeof client.addAlgoliaAgent !== 'function')
   ) {
     return;
   }
+
+  const cachedRequest = Object.keys(results).map((key) =>
+    results[key].results.map((result) => ({
+      indexName: result.index,
+      // We normalize the params received from the server as they can
+      // be serialized differently depending on the engine.
+      params: serializeQueryParameters(
+        deserializeQueryParameters(result.params)
+      ),
+    }))
+  );
+  const cachedResults = Object.keys(results).reduce<Array<SearchResponse<any>>>(
+    (acc, key) => acc.concat(results[key].results),
+    []
+  );
 
   // Algoliasearch API Client >= v4
   // To hydrate the client we need to populate the cache with the data from
@@ -29,17 +52,17 @@ export function hydrateSearchClient(
   // for us to compute the key the same way as `algoliasearch-client` we need
   // to populate it on a custom key and override the `search` method to
   // search on it first.
-  if (client.transporter && !client._cacheHydrated) {
+  if ('transporter' in client && !client._cacheHydrated) {
     client._cacheHydrated = true;
 
     const baseMethod = client.search;
     client.search = (requests, ...methodArgs) => {
       const requestsWithSerializedParams = requests.map((request) => ({
         ...request,
-        params: serializeQueryParameters(request.params),
+        params: serializeQueryParameters(request.params!),
       }));
 
-      return client.transporter.responsesCache.get(
+      return (client as ClientV3_4).transporter.responsesCache.get(
         {
           method: 'search',
           args: [requestsWithSerializedParams, ...methodArgs],
@@ -50,30 +73,13 @@ export function hydrateSearchClient(
       );
     };
 
-    // Populate the cache with the data from the server
-    client.transporter.responsesCache.set(
+    (client as ClientV3_4).transporter.responsesCache.set(
       {
         method: 'search',
-        args: [
-          Object.keys(results).reduce(
-            (acc, key) =>
-              acc.concat(
-                results[key].results.map((result) => ({
-                  indexName: result.index,
-                  // We normalize the params received from the server as they can
-                  // be serialized differently depending on the engine.
-                  params: serializeQueryParameters(qs.parse(result.params)),
-                }))
-              ),
-            []
-          ),
-        ],
+        args: cachedRequest,
       },
       {
-        results: Object.keys(results).reduce(
-          (acc, key) => acc.concat(results[key].results),
-          []
-        ),
+        results: cachedResults,
       }
     );
   }
@@ -84,40 +90,36 @@ export function hydrateSearchClient(
   // a single-index result. You can find more information about the
   // computation of the key inside the client (see link below).
   // https://github.com/algolia/algoliasearch-client-javascript/blob/c27e89ff92b2a854ae6f40dc524bffe0f0cbc169/src/AlgoliaSearchCore.js#L232-L240
-  if (!client.transporter) {
+  if (!('transporter' in client)) {
     const cacheKey = `/1/indexes/*/queries_body_${JSON.stringify({
-      requests: Object.keys(results).reduce(
-        (acc, key) =>
-          acc.concat(
-            results[key].rawResults.map((result) => ({
-              indexName: result.index,
-              params: serializeQueryParameters(qs.parse(result.params)),
-            }))
-          ),
-        []
-      ),
+      requests: cachedRequest,
     })}`;
 
-    client.cache = {
-      ...client.cache,
+    (client as ClientWithCache).cache = {
+      ...(client as ClientWithCache).cache,
       [cacheKey]: JSON.stringify({
-        results: Object.keys(results).reduce(
-          (acc, key) => acc.concat(results[key].rawResults),
-          []
-        ),
+        results: Object.keys(results).map((key) => results[key].results),
       }),
     };
   }
 }
 
+function deserializeQueryParameters(parameters: string) {
+  return parameters.split('&').reduce<Record<string, any>>((acc, parameter) => {
+    const [key, value] = parameter.split('=');
+    acc[key] = value ? decodeURIComponent(value) : '';
+    return acc;
+  }, {});
+}
+
 // This function is copied from the algoliasearch v4 API Client. If modified,
 // consider updating it also in `serializeQueryParameters` from `@algolia/transporter`.
-function serializeQueryParameters(parameters) {
-  const isObjectOrArray = (value) =>
+function serializeQueryParameters(parameters: SearchOptions) {
+  const isObjectOrArray = (value: any) =>
     Object.prototype.toString.call(value) === '[object Object]' ||
     Object.prototype.toString.call(value) === '[object Array]';
 
-  const encode = (format, ...args) => {
+  const encode = (format: string, ...args: [string, any]) => {
     let i = 0;
     return format.replace(/%s/g, () => encodeURIComponent(args[i++]));
   };
@@ -127,9 +129,9 @@ function serializeQueryParameters(parameters) {
       encode(
         '%s=%s',
         key,
-        isObjectOrArray(parameters[key])
-          ? JSON.stringify(parameters[key])
-          : parameters[key]
+        isObjectOrArray(parameters[key as keyof SearchOptions])
+          ? JSON.stringify(parameters[key as keyof SearchOptions])
+          : parameters[key as keyof SearchOptions]
       )
     )
     .join('&');
