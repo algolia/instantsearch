51cf9257f0e2f12372b466bd516012a871e47599
Dhaya <154633+dhayab@users.noreply.github.com>
2024-02-27 16:54:51 +0100

    fix weird parcel bundle issue with react and detect browser from wdio

--- tests/e2e/all-flavors.spec.ts
+++ tests/e2e/all-flavors.spec.ts
@@ -1,11 +1,11 @@
-import { flavors } from './flavors';
+import { getFilteredFlavors } from './flavors';
 import { createBrandAndQueryTestSuite } from './specs/brand-and-query.spec';
 import { createCategoryTestSuite } from './specs/category.spec';
 import { createInitialStateFromRouteTestSuite } from './specs/initial-state-from-route.spec';
 import { createPaginationTestSuite } from './specs/pagination.spec';
 import { createPriceRangeTestSuite } from './specs/price-range.spec';
 
-flavors.forEach((flavor) => {
+getFilteredFlavors(browser).forEach((flavor) => {
   describe(`${flavor}`, () => {
     createBrandAndQueryTestSuite(flavor);
     createCategoryTestSuite(flavor);
