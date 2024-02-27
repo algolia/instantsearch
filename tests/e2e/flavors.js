a2d01ff87bfa95e34a29ed1c1777f54f249b9513
Dhaya <154633+dhayab@users.noreply.github.com>
2024-02-27 13:49:13 +0100

    disable IE11 tests on non-js flavors

--- tests/e2e/flavors.js
+++ tests/e2e/flavors.js
@@ -1,7 +1,16 @@
+/**
+ * Returns the list of flavors to be tested on.
+ * When E2E_FLAVOR is set, it only tests the requested flavor.
+ * When it is not set, it will tests all flavors, except on Internet Explorer
+ * where it will only test the JS flavors.
+ */
 module.exports = {
   flavors: ['js', 'js-umd', 'react', 'vue'].filter(
     !process.env.E2E_FLAVOR
-      ? Boolean
+      ? (flavor) =>
+          process.env.E2E_BROWSER !== 'internet explorer'
+            ? true
+            : flavor.startsWith('js')
       : (flavor) => flavor === process.env.E2E_FLAVOR
   ),
 };
