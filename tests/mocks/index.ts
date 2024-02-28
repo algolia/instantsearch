0d14ed0b0d9c94a57c8724c1e81f4c836d9dd130
Dhaya <154633+dhayab@users.noreply.github.com>
2024-02-28 14:25:41 +0100

    revert: accept optional Recommend client (#6053) (#6080)

--- tests/mocks/index.ts
+++ tests/mocks/index.ts
@@ -1,5 +1,4 @@
 export * from './createAPIResponse';
 export * from './createInsightsClient';
-export * from './createRecommendClient';
 export * from './createSearchClient';
 export * from './createAlgoliaSearchClient';
