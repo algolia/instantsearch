05a01e5986deab15dc8d183ad0208c9d5437a1b5
Raed <raed.chammam@algolia.com>
2024-04-15 15:15:50 +0200

    feat(instantsearch-ui-components): introduce `FrequentlyBoughtTogether` component (#6114)
    Co-authored-by: Dhaya <154633+dhayab@users.noreply.github.com>
    Co-authored-by: Aymeric Giraudet <aymeric.giraudet@algolia.com>
    Co-authored-by: Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>

---
+++ packages/instantsearch-ui-components/src/types/Recommend.ts
@@ -0,0 +1,101 @@
+import type { SendEventForHits } from '.';
+
+export type RecommendClassNames = {
+  /**
+   * Class names to apply to the root element
+   */
+  root: string;
+  /**
+   * Class names to apply to the title element
+   */
+  title: string;
+  /**
+   * Class names to apply to the container element
+   */
+  container: string;
+  /**
+   * Class names to apply to the list element
+   */
+  list: string;
+  /**
+   * Class names to apply to each item element
+   */
+  item: string;
+};
+
+export type RecommendTranslations = {
+  /**
+   * The title of the recommendation section
+   */
+  title: string;
+  /**
+   * The label of the horizontal slider
+   */
+  sliderLabel: string;
+};
+
+export type RecommendViewProps<
+  TItem extends RecordWithObjectID,
+  TTranslations extends Record<string, string>,
+  TClassNames extends Record<string, string>
+> = {
+  classNames: TClassNames;
+  itemComponent: <
+    TComponentProps extends Record<string, unknown> = Record<string, unknown>
+  >(
+    props: RecommendItemComponentProps<RecordWithObjectID<TItem>> &
+      TComponentProps
+  ) => JSX.Element;
+  items: TItem[];
+  translations: TTranslations;
+  sendEvent: SendEventForHits;
+};
+
+export type RecommendComponentProps<
+  TObject,
+  TComponentProps extends Record<string, unknown> = Record<string, unknown>
+> = {
+  itemComponent: (
+    props: RecommendItemComponentProps<RecordWithObjectID<TObject>> &
+      TComponentProps
+  ) => JSX.Element;
+  items: Array<RecordWithObjectID<TObject>>;
+  classNames?: Partial<RecommendClassNames>;
+  fallbackComponent?: (props: TComponentProps) => JSX.Element;
+  headerComponent?: (
+    props: RecommendInnerComponentProps<TObject> & TComponentProps
+  ) => JSX.Element;
+  status: RecommendStatus;
+  translations?: Partial<RecommendTranslations>;
+  sendEvent: SendEventForHits;
+  view?: (
+    props: RecommendViewProps<
+      RecordWithObjectID<TObject>,
+      Required<RecommendTranslations>,
+      Record<string, string>
+    > &
+      TComponentProps
+  ) => JSX.Element;
+};
+
+export type RecommendInnerComponentProps<TObject> = {
+  classNames: Partial<RecommendClassNames>;
+  recommendations: TObject[];
+  translations: Partial<RecommendTranslations>;
+};
+
+export type RecordWithObjectID<TObject = Record<string, unknown>> = TObject & {
+  objectID: string;
+  __position: number;
+  __queryID?: string;
+};
+
+export type RecommendItemComponentProps<TObject> = {
+  item: TObject;
+  onClick?: () => void;
+  onAuxClick?: () => void;
+};
+
+// Should be equal to InstantSearchStatus and imported
+// from a shared package in the future
+export type RecommendStatus = 'loading' | 'stalled' | 'idle' | 'error';
