5a7ed9ea7282a14b2ec8210e506184574a11721f
Raed <raed.chammam@algolia.com>
2024-04-15 15:15:50 +0200

    feat(instantsearch-ui-components): introduce `FrequentlyBoughtTogether` component (#6114)
    Co-authored-by: Dhaya <154633+dhayab@users.noreply.github.com>
    Co-authored-by: Aymeric Giraudet <aymeric.giraudet@algolia.com>
    Co-authored-by: Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>

---
+++ packages/instantsearch-ui-components/src/components/FrequentlyBoughtTogether.tsx
@@ -0,0 +1,85 @@
+/** @jsx createElement */
+
+import { cx } from '../lib';
+
+import { createDefaultHeaderComponent } from './recommend-shared/DefaultHeader';
+import { createListViewComponent } from './recommend-shared/ListView';
+
+import type {
+  RecommendTranslations,
+  Renderer,
+  ComponentProps,
+  RecommendComponentProps,
+} from '../types';
+
+export type FrequentlyBoughtTogetherProps<
+  TObject,
+  TComponentProps extends Record<string, unknown> = Record<string, unknown>
+> = ComponentProps<'div'> & RecommendComponentProps<TObject, TComponentProps>;
+
+export function createFrequentlyBoughtTogetherComponent({
+  createElement,
+  Fragment,
+}: Renderer) {
+  return function FrequentlyBoughtTogether<TObject>(
+    userProps: FrequentlyBoughtTogetherProps<TObject>
+  ) {
+    const {
+      classNames = {},
+      fallbackComponent: FallbackComponent = () => null,
+      headerComponent: HeaderComponent = createDefaultHeaderComponent({
+        createElement,
+        Fragment,
+      }),
+      itemComponent: ItemComponent,
+      view: View = createListViewComponent({ createElement, Fragment }),
+      items,
+      status,
+      translations: userTranslations,
+      sendEvent,
+      ...props
+    } = userProps;
+
+    const translations: Required<RecommendTranslations> = {
+      title: 'Frequently bought together',
+      sliderLabel: 'Frequently bought together products',
+      ...userTranslations,
+    };
+
+    if (items.length === 0 && status === 'idle') {
+      return <FallbackComponent />;
+    }
+
+    return (
+      <section
+        {...props}
+        className={cx('ais-FrequentlyBoughtTogether', classNames.root)}
+      >
+        <HeaderComponent
+          classNames={{
+            ...classNames,
+            title: cx('ais-FrequentlyBoughtTogether-title', classNames.title),
+          }}
+          recommendations={items}
+          translations={translations}
+        />
+
+        <View
+          classNames={{
+            ...classNames,
+            container: cx(
+              'ais-FrequentlyBoughtTogether-container',
+              classNames.container
+            ),
+            list: cx('ais-FrequentlyBoughtTogether-list', classNames.list),
+            item: cx('ais-FrequentlyBoughtTogether-item', classNames.item),
+          }}
+          translations={translations}
+          itemComponent={ItemComponent}
+          items={items}
+          sendEvent={sendEvent}
+        />
+      </section>
+    );
+  };
+}
---
+++ packages/instantsearch-ui-components/src/components/__tests__/FrequentlyBoughtTogether.test.tsx
@@ -0,0 +1,340 @@
+/**
+ * @jest-environment jsdom
+ */
+/** @jsx createElement */
+import { render } from '@testing-library/preact';
+import userEvent from '@testing-library/user-event';
+import { createElement, Fragment } from 'preact';
+
+import { createFrequentlyBoughtTogetherComponent } from '../FrequentlyBoughtTogether';
+
+import type { RecordWithObjectID } from '../../types';
+import type { FrequentlyBoughtTogetherProps } from '../FrequentlyBoughtTogether';
+
+const FrequentlyBoughtTogether = createFrequentlyBoughtTogetherComponent({
+  createElement,
+  Fragment,
+});
+
+const ItemComponent: FrequentlyBoughtTogetherProps<RecordWithObjectID>['itemComponent'] =
+  ({ item, ...itemProps }) => (
+    <li {...itemProps}>
+      <div>{item.objectID}</div>
+    </li>
+  );
+
+describe('FrequentlyBoughtTogether', () => {
+  test('renders items with default view and header', () => {
+    const { container } = render(
+      <FrequentlyBoughtTogether
+        status="idle"
+        items={[
+          {
+            objectID: '1',
+            __position: 1,
+          },
+          {
+            objectID: '2',
+            __position: 2,
+          },
+        ]}
+        itemComponent={ItemComponent}
+        sendEvent={jest.fn()}
+      />
+    );
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <section
+          class="ais-FrequentlyBoughtTogether"
+        >
+          <h3
+            class="ais-FrequentlyBoughtTogether-title"
+          >
+            Frequently bought together
+          </h3>
+          <div
+            class="ais-FrequentlyBoughtTogether-container"
+          >
+            <ol
+              class="ais-FrequentlyBoughtTogether-list"
+            >
+              <li
+                class="ais-FrequentlyBoughtTogether-item"
+              >
+                <div>
+                  1
+                </div>
+              </li>
+              <li
+                class="ais-FrequentlyBoughtTogether-item"
+              >
+                <div>
+                  2
+                </div>
+              </li>
+            </ol>
+          </div>
+        </section>
+      </div>
+    `);
+  });
+
+  test('renders default fallback', () => {
+    const { container } = render(
+      <FrequentlyBoughtTogether
+        status="idle"
+        items={[]}
+        itemComponent={ItemComponent}
+        sendEvent={jest.fn()}
+      />
+    );
+
+    expect(container).toMatchInlineSnapshot(`<div />`);
+  });
+
+  test('renders custom header', () => {
+    const { container } = render(
+      <FrequentlyBoughtTogether
+        status="idle"
+        items={[{ objectID: '1', __position: 1 }]}
+        headerComponent={({ classNames }) => (
+          <div className={classNames.title}>My custom header</div>
+        )}
+        itemComponent={ItemComponent}
+        sendEvent={jest.fn()}
+      />
+    );
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <section
+          class="ais-FrequentlyBoughtTogether"
+        >
+          <div
+            class="ais-FrequentlyBoughtTogether-title"
+          >
+            My custom header
+          </div>
+          <div
+            class="ais-FrequentlyBoughtTogether-container"
+          >
+            <ol
+              class="ais-FrequentlyBoughtTogether-list"
+            >
+              <li
+                class="ais-FrequentlyBoughtTogether-item"
+              >
+                <div>
+                  1
+                </div>
+              </li>
+            </ol>
+          </div>
+        </section>
+      </div>
+    `);
+  });
+
+  test('renders custom view', () => {
+    const { container } = render(
+      <FrequentlyBoughtTogether
+        status="idle"
+        items={[{ objectID: '1', __position: 1 }]}
+        itemComponent={ItemComponent}
+        view={(props) => (
+          <div className={props.classNames.container}>
+            <ol className={props.classNames.list}>
+              {props.items.map((item) => (
+                <li key={item.objectID} className={props.classNames.item}>
+                  <props.itemComponent
+                    item={item}
+                    onClick={jest.fn()}
+                    onAuxClick={jest.fn()}
+                  />
+                </li>
+              ))}
+            </ol>
+          </div>
+        )}
+        sendEvent={jest.fn()}
+      />
+    );
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <section
+          class="ais-FrequentlyBoughtTogether"
+        >
+          <h3
+            class="ais-FrequentlyBoughtTogether-title"
+          >
+            Frequently bought together
+          </h3>
+          <div
+            class="ais-FrequentlyBoughtTogether-container"
+          >
+            <ol
+              class="ais-FrequentlyBoughtTogether-list"
+            >
+              <li
+                class="ais-FrequentlyBoughtTogether-item"
+              >
+                <li>
+                  <div>
+                    1
+                  </div>
+                </li>
+              </li>
+            </ol>
+          </div>
+        </section>
+      </div>
+    `);
+  });
+
+  test('renders custom fallback', () => {
+    const { container } = render(
+      <FrequentlyBoughtTogether
+        status="idle"
+        items={[]}
+        fallbackComponent={() => <div>My custom fallback</div>}
+        itemComponent={ItemComponent}
+        sendEvent={jest.fn()}
+      />
+    );
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <div>
+          My custom fallback
+        </div>
+      </div>
+    `);
+  });
+
+  test('sends a `click` event when clicking on an item', () => {
+    const sendEvent = jest.fn();
+    const items = [{ objectID: '1', __position: 1 }];
+
+    const { container } = render(
+      <FrequentlyBoughtTogether
+        status="idle"
+        items={items}
+        itemComponent={ItemComponent}
+        sendEvent={sendEvent}
+      />
+    );
+
+    userEvent.click(
+      container.querySelectorAll('.ais-FrequentlyBoughtTogether-item')[0]!
+    );
+
+    expect(sendEvent).toHaveBeenCalledTimes(1);
+  });
+
+  test('accepts custom title translation', () => {
+    const { container } = render(
+      <FrequentlyBoughtTogether
+        status="idle"
+        items={[{ objectID: '1', __position: 1 }]}
+        translations={{ title: 'My custom title' }}
+        itemComponent={ItemComponent}
+        sendEvent={jest.fn()}
+      />
+    );
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <section
+          class="ais-FrequentlyBoughtTogether"
+        >
+          <h3
+            class="ais-FrequentlyBoughtTogether-title"
+          >
+            My custom title
+          </h3>
+          <div
+            class="ais-FrequentlyBoughtTogether-container"
+          >
+            <ol
+              class="ais-FrequentlyBoughtTogether-list"
+            >
+              <li
+                class="ais-FrequentlyBoughtTogether-item"
+              >
+                <div>
+                  1
+                </div>
+              </li>
+            </ol>
+          </div>
+        </section>
+      </div>
+    `);
+  });
+
+  test('forwards `div` props to the root element', () => {
+    const { container } = render(
+      <FrequentlyBoughtTogether
+        status="idle"
+        items={[{ objectID: '1', __position: 1 }]}
+        hidden={true}
+        itemComponent={ItemComponent}
+        sendEvent={jest.fn()}
+      />
+    );
+
+    expect(
+      container.querySelector<HTMLDivElement>('.ais-FrequentlyBoughtTogether')!
+        .hidden
+    ).toBe(true);
+  });
+
+  test('accepts custom class names', () => {
+    const { container } = render(
+      <FrequentlyBoughtTogether
+        status="idle"
+        items={[{ objectID: '1', __position: 1 }]}
+        classNames={{
+          root: 'ROOT',
+          title: 'TITLE',
+          container: 'CONTAINER',
+          list: 'LIST',
+          item: 'ITEM',
+        }}
+        itemComponent={ItemComponent}
+        sendEvent={jest.fn()}
+      />
+    );
+
+    expect(container).toMatchInlineSnapshot(`
+      <div>
+        <section
+          class="ais-FrequentlyBoughtTogether ROOT"
+        >
+          <h3
+            class="ais-FrequentlyBoughtTogether-title TITLE"
+          >
+            Frequently bought together
+          </h3>
+          <div
+            class="ais-FrequentlyBoughtTogether-container CONTAINER"
+          >
+            <ol
+              class="ais-FrequentlyBoughtTogether-list LIST"
+            >
+              <li
+                class="ais-FrequentlyBoughtTogether-item ITEM"
+              >
+                <div>
+                  1
+                </div>
+              </li>
+            </ol>
+          </div>
+        </section>
+      </div>
+    `);
+  });
+});
