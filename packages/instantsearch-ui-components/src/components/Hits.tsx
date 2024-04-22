05574584ddd597ecea8c53c233c92c2cf89eb6d7
Taylor Johnson <taylor.johnson@algolia.com>
2024-04-21 22:18:06 -0600

    feat(hits): add banner to hits UI component

--- packages/instantsearch-ui-components/src/components/Hits.tsx
+++ packages/instantsearch-ui-components/src/components/Hits.tsx
@@ -8,6 +8,57 @@ type Hit = Record<string, unknown> & {
   objectID: string;
 };
 type SendEventForHits = (...props: unknown[]) => void;
+type Banner = {
+  image: {
+    urls: Array<{
+      url: string;
+    }>;
+    title?: string;
+  };
+  link?: {
+    url?: string;
+    target?: '_blank' | '_self';
+  };
+};
+
+type BannerProps = ComponentProps<'aside'> & {
+  banner: Banner;
+  classNames: Pick<
+    Partial<HitsClassNames>,
+    'bannerRoot' | 'bannerLink' | 'bannerImage'
+  >;
+};
+
+function createDefaultBannerComponent({ createElement }: Renderer) {
+  return function DefaultBanner({ classNames, banner }: BannerProps) {
+    if (!banner.image.urls[0].url) {
+      return null;
+    }
+    return (
+      <aside className={cx('ais-Hits-banner', classNames.bannerRoot)}>
+        {banner.link?.url ? (
+          <a
+            className={cx('ais-Hits-banner-link', classNames.bannerLink)}
+            href={banner.link.url}
+            target={banner.link.target}
+          >
+            <img
+              className={cx('ais-Hits-banner-image', classNames.bannerImage)}
+              src={banner.image.urls[0].url}
+              alt={banner.image.title}
+            />
+          </a>
+        ) : (
+          <img
+            className={cx('ais-Hits-banner-image', classNames.bannerImage)}
+            src={banner.image.urls[0].url}
+            alt={banner.image.title}
+          />
+        )}
+      </aside>
+    );
+  };
+}
 
 export type HitsProps<THit> = ComponentProps<'div'> & {
   hits: THit[];
@@ -20,7 +71,12 @@ export type HitsProps<THit> = ComponentProps<'div'> & {
   }) => JSX.Element;
   sendEvent: SendEventForHits;
   classNames?: Partial<HitsClassNames>;
-  emptyComponent?: (props: { className: string }) => JSX.Element;
+  emptyComponent?: (props: { className?: string }) => JSX.Element;
+  banner?: Banner;
+  bannerComponent?: (props: {
+    className: string;
+    banner: Banner;
+  }) => JSX.Element;
 };
 
 export type HitsClassNames = {
@@ -40,9 +96,26 @@ export type HitsClassNames = {
    * Class names to apply to each item element
    */
   item: string | string[];
+  /**
+   * Class names to apply to the banner element
+   */
+  bannerRoot: string | string[];
+  /**
+   * Class names to apply to the banner image element
+   */
+  bannerImage: string | string[];
+  /**
+   * Class names to apply to the banner link element
+   */
+  bannerLink: string | string[];
 };
 
-export function createHitsComponent({ createElement }: Renderer) {
+export function createHitsComponent({ createElement, Fragment }: Renderer) {
+  const DefaultBannerComponent = createDefaultBannerComponent({
+    createElement,
+    Fragment,
+  });
+
   return function Hits<THit extends Hit>(userProps: HitsProps<THit>) {
     const {
       classNames = {},
@@ -50,21 +123,11 @@ export function createHitsComponent({ createElement }: Renderer) {
       itemComponent: ItemComponent,
       sendEvent,
       emptyComponent: EmptyComponent,
+      banner,
+      bannerComponent: BannerComponent,
       ...props
     } = userProps;
 
-    if (hits.length === 0 && EmptyComponent) {
-      return (
-        <EmptyComponent
-          className={cx(
-            'ais-Hits',
-            classNames.root,
-            cx('ais-Hits--empty', classNames.emptyRoot),
-            props.className
-          )}
-        />
-      );
-    }
     return (
       <div
         {...props}
@@ -75,22 +138,35 @@ export function createHitsComponent({ createElement }: Renderer) {
           props.className
         )}
       >
-        <ol className={cx('ais-Hits-list', classNames.list)}>
-          {hits.map((hit, index) => (
-            <ItemComponent
-              key={hit.objectID}
-              hit={hit}
-              index={index}
-              className={cx('ais-Hits-item', classNames.item)}
-              onClick={() => {
-                sendEvent('click:internal', hit, 'Hit Clicked');
-              }}
-              onAuxClick={() => {
-                sendEvent('click:internal', hit, 'Hit Clicked');
-              }}
+        {banner &&
+          (BannerComponent ? (
+            <BannerComponent
+              className={cx('ais-Hits-banner', classNames.bannerRoot)}
+              banner={banner}
             />
+          ) : (
+            <DefaultBannerComponent classNames={classNames} banner={banner} />
           ))}
-        </ol>
+        {hits.length === 0 && EmptyComponent ? (
+          <EmptyComponent />
+        ) : (
+          <ol className={cx('ais-Hits-list', classNames.list)}>
+            {hits.map((hit, index) => (
+              <ItemComponent
+                key={hit.objectID}
+                hit={hit}
+                index={index}
+                className={cx('ais-Hits-item', classNames.item)}
+                onClick={() => {
+                  sendEvent('click:internal', hit, 'Hit Clicked');
+                }}
+                onAuxClick={() => {
+                  sendEvent('click:internal', hit, 'Hit Clicked');
+                }}
+              />
+            ))}
+          </ol>
+        )}
       </div>
     );
   };
