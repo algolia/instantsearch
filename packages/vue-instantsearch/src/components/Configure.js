93cfd60f4a4e9517ec942faf8b8e349041e260cb
Aymeric Giraudet <aymeric.giraudet@algolia.com>
2024-04-26 13:01:50 +0200

    fix(vue): avoid passing a proxied object in Configure (#6166)
    * fix(vue): avoid passing a proxied object in Configure
    
    * Update packages/vue-instantsearch/src/components/Configure.js
    
    Co-authored-by: Dhaya <154633+dhayab@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Dhaya <154633+dhayab@users.noreply.github.com>

--- packages/vue-instantsearch/src/components/Configure.js
+++ packages/vue-instantsearch/src/components/Configure.js
@@ -21,7 +21,7 @@ export default {
   computed: {
     widgetParams() {
       return {
-        searchParameters: this.$attrs,
+        searchParameters: Object.assign({}, this.$attrs),
       };
     },
   },
