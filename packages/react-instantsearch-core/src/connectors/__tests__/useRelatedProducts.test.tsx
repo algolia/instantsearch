85b77c9c364b0fce56a54dff768f4087aa92bbb9
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2024-04-26 14:50:27 +0200

    feat(react-instantsearch): introduce `useRelatedProducts` Hook (#6159)

---
+++ packages/react-instantsearch-core/src/connectors/__tests__/useRelatedProducts.test.tsx
@@ -0,0 +1,24 @@
+import { createInstantSearchTestWrapper } from '@instantsearch/testutils';
+import { renderHook } from '@testing-library/react-hooks';
+
+import { useRelatedProducts } from '../useRelatedProducts';
+
+describe('useRelatedProducts', () => {
+  test('returns the connector render state', async () => {
+    const wrapper = createInstantSearchTestWrapper();
+    const { result, waitForNextUpdate } = renderHook(
+      () => useRelatedProducts({ objectIDs: ['1'] }),
+      {
+        wrapper,
+      }
+    );
+
+    // Initial render state from manual `getWidgetRenderState`
+    expect(result.current).toEqual({ recommendations: expect.any(Array) });
+
+    await waitForNextUpdate();
+
+    // InstantSearch.js state from the `render` lifecycle step
+    expect(result.current).toEqual({ recommendations: expect.any(Array) });
+  });
+});
