3b99dffc02b09871caa1312dcbfb218c59597442
Sarah Dayan <5370675+sarahdayan@users.noreply.github.com>
2024-04-26 14:27:49 +0200

    test: fix test

--- packages/react-instantsearch/src/widgets/__tests__/RelatedProducts.test.tsx
+++ packages/react-instantsearch/src/widgets/__tests__/RelatedProducts.test.tsx
@@ -2,16 +2,22 @@
  * @jest-environment jsdom
  */
 
-import { createSearchClient } from '@instantsearch/mocks';
+import {
+  createMultiSearchResponse,
+  createSearchClient,
+  createSingleSearchResponse,
+} from '@instantsearch/mocks';
 import { InstantSearchTestWrapper } from '@instantsearch/testutils';
 import { render, waitFor } from '@testing-library/react';
 import React from 'react';
 
 import { RelatedProducts } from '../RelatedProducts';
 
+import type { SearchClient } from 'instantsearch.js';
+
 describe('RelatedProducts', () => {
   test('renders with translations', async () => {
-    const client = createSearchClient();
+    const client = createMockedSearchClient();
     const { container } = render(
       <InstantSearchTestWrapper searchClient={client}>
         <RelatedProducts
@@ -29,9 +35,35 @@ describe('RelatedProducts', () => {
       expect(container.querySelector('.ais-RelatedProducts'))
         .toMatchInlineSnapshot(`
         <section
-          class="ais-RelatedProducts ais-RelatedProducts--empty"
+          class="ais-RelatedProducts"
         >
-          No results
+          <h3
+            class="ais-RelatedProducts-title"
+          >
+            My related products
+          </h3>
+          <div
+            class="ais-RelatedProducts-container"
+          >
+            <ol
+              class="ais-RelatedProducts-list"
+            >
+              <li
+                class="ais-RelatedProducts-item"
+              >
+                {
+          "objectID": "1"
+        }
+              </li>
+              <li
+                class="ais-RelatedProducts-item"
+              >
+                {
+          "objectID": "2"
+        }
+              </li>
+            </ol>
+          </div>
         </section>
       `);
     });
@@ -54,3 +86,25 @@ describe('RelatedProducts', () => {
     expect(root).toHaveAttribute('aria-hidden', 'true');
   });
 });
+
+function createMockedSearchClient() {
+  return createSearchClient({
+    getRecommendations: jest.fn((requests) =>
+      Promise.resolve(
+        createMultiSearchResponse(
+          // @ts-ignore
+          // `request` will be implicitly typed as `any` in type-check:v3
+          // since `getRecommendations` is not available there
+          ...requests.map((request) => {
+            return createSingleSearchResponse<any>({
+              hits:
+                request.maxRecommendations === 0
+                  ? []
+                  : [{ objectID: '1' }, { objectID: '2' }],
+            });
+          })
+        )
+      )
+    ) as SearchClient['getRecommendations'],
+  });
+}
