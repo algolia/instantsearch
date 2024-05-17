1ba6e9f9c6896a68129495f4e251264bf14deae7
Haroen Viaene <hello@haroen.me>
2024-05-17 12:17:53 +0200

    test(react): take method in account

--- packages/react-instantsearch-core/src/connectors/__tests__/useTrendingItems.test.tsx
+++ packages/react-instantsearch-core/src/connectors/__tests__/useTrendingItems.test.tsx
@@ -1,3 +1,4 @@
+import { createRecommendSearchClient } from '@instantsearch/mocks/fixtures';
 import { createInstantSearchTestWrapper } from '@instantsearch/testutils';
 import { renderHook } from '@testing-library/react-hooks';
 
@@ -5,7 +6,9 @@ import { useTrendingItems } from '../useTrendingItems';
 
 describe('useTrendingItems', () => {
   test('returns the connector render state', async () => {
-    const wrapper = createInstantSearchTestWrapper();
+    const wrapper = createInstantSearchTestWrapper({
+      searchClient: createRecommendSearchClient({ minimal: true }),
+    });
     const { result, waitForNextUpdate } = renderHook(
       () => useTrendingItems({}),
       {
@@ -14,11 +17,13 @@ describe('useTrendingItems', () => {
     );
 
     // Initial render state from manual `getWidgetRenderState`
-    expect(result.current).toEqual({ items: expect.any(Array) });
+    expect(result.current).toEqual({ items: [] });
 
     await waitForNextUpdate();
 
     // InstantSearch.js state from the `render` lifecycle step
-    expect(result.current).toEqual({ items: expect.any(Array) });
+    expect(result.current).toEqual({
+      items: expect.arrayContaining([{ objectID: '1' }, { objectID: '2' }]),
+    });
   });
 });
