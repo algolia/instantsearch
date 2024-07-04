b8d276a7ecf71b031d6d7e7d0579d540675ab7bd
Aymeric Giraudet <aymeric.giraudet@algolia.com>
2024-07-04 10:16:25 +0200

    update comment

--- packages/instantsearch.js/src/lib/utils/getAppIdAndApiKey.ts
+++ packages/instantsearch.js/src/lib/utils/getAppIdAndApiKey.ts
@@ -1,7 +1,7 @@
 // typed as any, since it accepts the _real_ js clients, not the interface we otherwise expect
 export function getAppIdAndApiKey(searchClient: any): [string, string] {
   if (searchClient.transporter) {
-    // searchClient v4
+    // searchClient v4 or v5
     const transporter = searchClient.transporter;
     const headers = transporter.headers || transporter.baseHeaders;
     const queryParameters =
