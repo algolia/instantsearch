b421f185ef5407bf8cd9846a8ab1664ea20e6da4
Shahmir Ejaz <shaejaz5@gmail.com>
2024-08-09 13:08:04 +0100

    test(react-is) test layout for recommend components

--- packages/react-instantsearch/src/widgets/__tests__/FrequentlyBoughtTogether.test.tsx
+++ packages/react-instantsearch/src/widgets/__tests__/FrequentlyBoughtTogether.test.tsx
@@ -7,6 +7,7 @@ import { InstantSearchTestWrapper } from '@instantsearch/testutils';
 import { render, waitFor } from '@testing-library/react';
 import React from 'react';
 
+import { Carousel } from '../../templates/Carousel';
 import { FrequentlyBoughtTogether } from '../FrequentlyBoughtTogether';
 
 describe('FrequentlyBoughtTogether', () => {
@@ -80,5 +81,150 @@ describe('FrequentlyBoughtTogether', () => {
     const root = container.firstChild;
     expect(root).toHaveClass('MyFrequentlyBoughtTogether', 'ROOT');
     expect(root).toHaveAttribute('aria-hidden', 'true');
+  });
+
+  test('renders custom layout component', async () => {
+    const client = createRecommendSearchClient({
+      minimal: true,
+    });
+    const { container } = render(
+      <InstantSearchTestWrapper searchClient={client}>
+        <FrequentlyBoughtTogether
+          objectIDs={['1']}
+          layoutComponent={({ items }) => (
+            <ul>
+              {items.map((item) => (
+                <li key={item.objectID}>
+                  <p>{item.objectID}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        />
+      </InstantSearchTestWrapper>
+    );
+
+    await waitFor(() => {
+      expect(client.getRecommendations).toHaveBeenCalledTimes(1);
+    });
+
+    await waitFor(() => {
+      expect(container.querySelector('.ais-FrequentlyBoughtTogether'))
+        .toMatchInlineSnapshot(`
+        <section
+          class="ais-FrequentlyBoughtTogether"
+        >
+          <h3
+            class="ais-FrequentlyBoughtTogether-title"
+          >
+            Frequently bought together
+          </h3>
+          <ul>
+            <li>
+              <p>
+                1
+              </p>
+            </li>
+            <li>
+              <p>
+                2
+              </p>
+            </li>
+          </ul>
+        </section>
+        `);
+    });
+  });
+
+  test('renders Carousel as a layout component', async () => {
+    const client = createRecommendSearchClient({
+      minimal: true,
+    });
+    const { container } = render(
+      <InstantSearchTestWrapper searchClient={client}>
+        <FrequentlyBoughtTogether
+          objectIDs={['1']}
+          itemComponent={({ item }) => <p>{item.objectID}</p>}
+          layoutComponent={(props) => (
+            <Carousel
+              {...props}
+              previousIconComponent={() => <p>Previous</p>}
+              nextIconComponent={() => <p>Next</p>}
+            />
+          )}
+        />
+      </InstantSearchTestWrapper>
+    );
+
+    await waitFor(() => {
+      expect(client.getRecommendations).toHaveBeenCalledTimes(1);
+    });
+
+    await waitFor(() => {
+      expect(container.querySelector('.ais-FrequentlyBoughtTogether'))
+        .toMatchInlineSnapshot(`
+        <section
+          class="ais-FrequentlyBoughtTogether"
+        >
+          <h3
+            class="ais-FrequentlyBoughtTogether-title"
+          >
+            Frequently bought together
+          </h3>
+          <div
+            class="ais-Carousel ais-FrequentlyBoughtTogether"
+          >
+            <button
+              aria-controls="ais-Carousel-0"
+              aria-label="Previous"
+              class="ais-Carousel-navigation ais-Carousel-navigation--previous"
+              hidden=""
+              title="Previous"
+            >
+              <p>
+                Previous
+              </p>
+            </button>
+            <ol
+              aria-label="Items"
+              aria-live="polite"
+              aria-roledescription="carousel"
+              class="ais-Carousel-list ais-FrequentlyBoughtTogether-list"
+              id="ais-Carousel-0"
+              tabindex="0"
+            >
+              <li
+                aria-label="1 of 2"
+                aria-roledescription="slide"
+                class="ais-Carousel-item ais-FrequentlyBoughtTogether-item"
+              >
+                <p>
+                  1
+                </p>
+              </li>
+              <li
+                aria-label="2 of 2"
+                aria-roledescription="slide"
+                class="ais-Carousel-item ais-FrequentlyBoughtTogether-item"
+              >
+                <p>
+                  2
+                </p>
+              </li>
+            </ol>
+            <button
+              aria-controls="ais-Carousel-0"
+              aria-label="Next"
+              class="ais-Carousel-navigation ais-Carousel-navigation--next"
+              title="Next"
+            >
+              <p>
+                Next
+              </p>
+            </button>
+          </div>
+        </section>
+        `);
+    });
   });
 });
--- packages/react-instantsearch/src/widgets/__tests__/LookingSimilar.test.tsx
+++ packages/react-instantsearch/src/widgets/__tests__/LookingSimilar.test.tsx
@@ -7,6 +7,7 @@ import { InstantSearchTestWrapper } from '@instantsearch/testutils';
 import { render, waitFor } from '@testing-library/react';
 import React from 'react';
 
+import { Carousel } from '../../templates/Carousel';
 import { LookingSimilar } from '../LookingSimilar';
 
 describe('LookingSimilar', () => {
@@ -77,5 +78,150 @@ describe('LookingSimilar', () => {
     const root = container.firstChild;
     expect(root).toHaveClass('MyLookingSimilar', 'ROOT');
     expect(root).toHaveAttribute('aria-hidden', 'true');
+  });
+
+  test('renders custom layout component', async () => {
+    const client = createRecommendSearchClient({
+      minimal: true,
+    });
+    const { container } = render(
+      <InstantSearchTestWrapper searchClient={client}>
+        <LookingSimilar
+          objectIDs={['1']}
+          layoutComponent={({ items }) => (
+            <ul>
+              {items.map((item) => (
+                <li key={item.objectID}>
+                  <p>{item.objectID}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        />
+      </InstantSearchTestWrapper>
+    );
+
+    await waitFor(() => {
+      expect(client.getRecommendations).toHaveBeenCalledTimes(1);
+    });
+
+    await waitFor(() => {
+      expect(container.querySelector('.ais-LookingSimilar'))
+        .toMatchInlineSnapshot(`
+        <section
+          class="ais-LookingSimilar"
+        >
+          <h3
+            class="ais-LookingSimilar-title"
+          >
+            Looking similar
+          </h3>
+          <ul>
+            <li>
+              <p>
+                1
+              </p>
+            </li>
+            <li>
+              <p>
+                2
+              </p>
+            </li>
+          </ul>
+        </section>
+        `);
+    });
+  });
+
+  test('renders Carousel as a layout component', async () => {
+    const client = createRecommendSearchClient({
+      minimal: true,
+    });
+    const { container } = render(
+      <InstantSearchTestWrapper searchClient={client}>
+        <LookingSimilar
+          objectIDs={['1']}
+          itemComponent={({ item }) => <p>{item.objectID}</p>}
+          layoutComponent={(props) => (
+            <Carousel
+              {...props}
+              previousIconComponent={() => <p>Previous</p>}
+              nextIconComponent={() => <p>Next</p>}
+            />
+          )}
+        />
+      </InstantSearchTestWrapper>
+    );
+
+    await waitFor(() => {
+      expect(client.getRecommendations).toHaveBeenCalledTimes(1);
+    });
+
+    await waitFor(() => {
+      expect(container.querySelector('.ais-LookingSimilar'))
+        .toMatchInlineSnapshot(`
+        <section
+          class="ais-LookingSimilar"
+        >
+          <h3
+            class="ais-LookingSimilar-title"
+          >
+            Looking similar
+          </h3>
+          <div
+            class="ais-Carousel ais-LookingSimilar"
+          >
+            <button
+              aria-controls="ais-Carousel-0"
+              aria-label="Previous"
+              class="ais-Carousel-navigation ais-Carousel-navigation--previous"
+              hidden=""
+              title="Previous"
+            >
+              <p>
+                Previous
+              </p>
+            </button>
+            <ol
+              aria-label="Items"
+              aria-live="polite"
+              aria-roledescription="carousel"
+              class="ais-Carousel-list ais-LookingSimilar-list"
+              id="ais-Carousel-0"
+              tabindex="0"
+            >
+              <li
+                aria-label="1 of 2"
+                aria-roledescription="slide"
+                class="ais-Carousel-item ais-LookingSimilar-item"
+              >
+                <p>
+                  1
+                </p>
+              </li>
+              <li
+                aria-label="2 of 2"
+                aria-roledescription="slide"
+                class="ais-Carousel-item ais-LookingSimilar-item"
+              >
+                <p>
+                  2
+                </p>
+              </li>
+            </ol>
+            <button
+              aria-controls="ais-Carousel-0"
+              aria-label="Next"
+              class="ais-Carousel-navigation ais-Carousel-navigation--next"
+              title="Next"
+            >
+              <p>
+                Next
+              </p>
+            </button>
+          </div>
+        </section>
+        `);
+    });
   });
 });
