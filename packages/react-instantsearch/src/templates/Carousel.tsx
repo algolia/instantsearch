37ab1fce37a9c821e26b87e4f1ff743de70564da
Shahmir Ejaz <shaejaz5@gmail.com>
2024-08-09 13:10:43 +0100

    fix typo

--- packages/react-instantsearch/src/templates/Carousel.tsx
+++ packages/react-instantsearch/src/templates/Carousel.tsx
@@ -4,23 +4,26 @@ import {
 } from 'instantsearch-ui-components';
 import React, { createElement, Fragment, useRef } from 'react';
 
-import type { CarouselProps, Pragma } from 'instantsearch-ui-components';
+import type {
+  CarouselProps as CarouselUiProps,
+  Pragma,
+} from 'instantsearch-ui-components';
 
 const CarouselUiComponent = createCarouselComponent({
   createElement: createElement as Pragma,
   Fragment,
 });
 
-export type CarouseulProps<TObject extends Record<string, unknown>> = Omit<
-  CarouselProps<TObject>,
+export type CarouselProps<TObject extends Record<string, unknown>> = Omit<
+  CarouselUiProps<TObject>,
   'listRef' | 'nextButtonRef' | 'previousButtonRef' | 'carouselIdRef'
 >;
 
 export function Carousel<TObject extends Record<string, unknown>>(
-  props: CarouseulProps<TObject>
+  props: CarouselProps<TObject>
 ) {
   const carouselRefs: Pick<
-    CarouselProps<TObject>,
+    CarouselUiProps<TObject>,
     'listRef' | 'nextButtonRef' | 'previousButtonRef' | 'carouselIdRef'
   > = {
     listRef: useRef(null),
