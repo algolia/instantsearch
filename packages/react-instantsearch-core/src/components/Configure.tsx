393c969f36865aeaa7515bae65cd6a705889794e
Haroen Viaene <hello@haroen.me>
2024-10-07 12:30:06 +0200

    fix(configure): ignore data attributes

--- packages/react-instantsearch-core/src/components/Configure.tsx
+++ packages/react-instantsearch-core/src/components/Configure.tsx
@@ -5,7 +5,17 @@ import type { UseConfigureProps } from '../connectors/useConfigure';
 export type ConfigureProps = UseConfigureProps;
 
 export function Configure(props: ConfigureProps) {
-  useConfigure({ ...props }, { $$widgetType: 'ais.configure' });
+  useConfigure(
+    Object.keys(props).reduce<ConfigureProps>((acc, key) => {
+      if (key.slice(0, 6) === 'data-') {
+        return acc;
+      }
+      // @ts-ignore
+      acc[key] = props[key];
+      return acc;
+    }, {}),
+    { $$widgetType: 'ais.configure' }
+  );
 
   return null;
 }
