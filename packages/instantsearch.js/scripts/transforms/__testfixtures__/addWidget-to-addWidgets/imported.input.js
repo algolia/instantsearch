6f58eb5083dfa2e9aff9978f7817953a4d57fac4
Haroen Viaene <hello@haroen.me>
2024-10-18 11:23:12 +0200

    chore(codemods): remove codemods from instantsearch.js package
    They are already in place in the instantsearch-codemods package
    
    BREAKING CHANGE: use codemods from `instantsearch-codemods` instead of instantsearch.js

--- packages/instantsearch.js/scripts/transforms/__testfixtures__/addWidget-to-addWidgets/imported.input.js
+++
@@ -1,6 +0,0 @@
-import instantsearch from 'instantsearch.js';
-
-const search = instantsearch();
-
-search.addWidget(instantsearch.widgets.hits({}));
-search.addWidget(instantsearch.widgets.hits({}));
