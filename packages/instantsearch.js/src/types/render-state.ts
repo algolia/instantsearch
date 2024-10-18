1cec9b6a136789ca59e0db0204d0d3c3aba6ff1a
Haroen Viaene <hello@haroen.me>
2024-10-18 13:00:21 +0200

    fix(widgets): remove analytics widget (#6398)
    * fix(widgets): remove analytics widget
    
    The analytics widget has long been deprecated, and is removed with the insights middleware as alternative. You can use the `onEvent` hook to use a different service.
    
    BREAKING CHANGE: the analytics widget no longer exists
    
    * !fixup stories

--- packages/instantsearch.js/src/types/render-state.ts
+++ packages/instantsearch.js/src/types/render-state.ts
@@ -23,7 +23,6 @@ import type { SortByWidgetDescription } from '../connectors/sort-by/connectSortB
 import type { StatsWidgetDescription } from '../connectors/stats/connectStats';
 import type { ToggleRefinementWidgetDescription } from '../connectors/toggle-refinement/connectToggleRefinement';
 import type { VoiceSearchWidgetDescription } from '../connectors/voice-search/connectVoiceSearch';
-import type { AnalyticsWidgetDescription } from '../widgets/analytics/analytics';
 import type { PlacesWidgetDescription } from '../widgets/places/places';
 
 type ConnectorRenderStates = AnswersWidgetDescription['indexRenderState'] &
@@ -52,8 +51,7 @@ type ConnectorRenderStates = AnswersWidgetDescription['indexRenderState'] &
   ToggleRefinementWidgetDescription['indexRenderState'] &
   VoiceSearchWidgetDescription['indexRenderState'];
 
-type WidgetRenderStates = AnalyticsWidgetDescription['indexRenderState'] &
-  PlacesWidgetDescription['indexRenderState'];
+type WidgetRenderStates = PlacesWidgetDescription['indexRenderState'];
 
 export type IndexRenderState = Partial<
   ConnectorRenderStates & WidgetRenderStates
