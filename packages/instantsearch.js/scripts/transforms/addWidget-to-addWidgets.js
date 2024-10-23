978243a782e76eb0aafa5a0b8f203c9976f1fad5
Haroen Viaene <hello@haroen.me>
2024-10-23 13:06:16 +0200

    chore(codemods): remove codemods from instantsearch.js package (#6396)
    They are already in place in the instantsearch-codemods package
    
    BREAKING CHANGE: use codemods from `instantsearch-codemods` instead of instantsearch.js

--- packages/instantsearch.js/scripts/transforms/addWidget-to-addWidgets.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console, import/no-commonjs */
-
-// @MAJOR: remove this file and only keep only the `instantsearch-codemods` one
-// also ensure this is removed from package.json
-console.warn(
-  "This file is deprecated. Please use `npx @codeshift/cli --packages 'instantsearch-codemods#addWidget-to-addWidgets' <path>` instead."
-);
-
-module.exports = require('instantsearch-codemods/src/addWidget-to-addWidgets');
