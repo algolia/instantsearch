448a00e15297cb9846177b35de9c20c5000a6239
Dhaya <154633+dhayab@users.noreply.github.com>
2024-10-29 09:58:52 +0100

    feat(tests): remove ie11 targeting tests (#6415)

--- tests/e2e/helpers/setHitsPerPage.ts
+++ tests/e2e/helpers/setHitsPerPage.ts
@@ -10,7 +10,6 @@ browser.addCommand('setHitsPerPage', async (label: string) => {
 
   await hitsPerPage.selectByVisibleText(label);
 
-  // Changing the URL will also change the page element IDs in Internet Explorer
   // Not waiting for the URL to be properly updated before continuing can make the next tests fail
   await browser.waitUntil(
     async () => (await browser.getUrl()) !== oldUrl,
