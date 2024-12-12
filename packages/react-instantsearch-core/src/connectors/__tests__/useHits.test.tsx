9aa59d8664c7bf552799b492ef2967f8e608d95d
Haroen Viaene <hello@haroen.me>
2024-12-12 12:06:23 +0100

    fix(insights): remove bindEvent (#6471)
    bindEvent is no longer handled in the component since #6403 (https://github.com/algolia/instantsearch/pull/6403/files#diff-e7717d55fdbbc94c7dd996f78b6f0f860332545d6fa8f6b843c940798334532cL94)
    
    [FX-3191]
    
    BREAKING CHANGE: the `bindEvent` related functions are non longer usable, use `sendEvent` instead

--- packages/react-instantsearch-core/src/connectors/__tests__/useHits.test.tsx
+++ packages/react-instantsearch-core/src/connectors/__tests__/useHits.test.tsx
@@ -17,7 +17,6 @@ describe('useHits', () => {
     // Initial render state from manual `getWidgetRenderState`
     expect(result.current).toEqual({
       banner: undefined,
-      bindEvent: expect.any(Function),
       hits: [],
       items: [],
       results: expect.objectContaining({ nbHits: 0 }),
@@ -28,7 +27,6 @@ describe('useHits', () => {
       // InstantSearch.js state from the `render` lifecycle step
       expect(result.current).toEqual({
         banner: undefined,
-        bindEvent: expect.any(Function),
         hits: [],
         items: [],
         results: expect.objectContaining({ nbHits: 0 }),
