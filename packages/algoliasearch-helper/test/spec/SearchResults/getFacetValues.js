e00015bba411a9fbc9ea158802419d519607e6c5
Haroen Viaene <hello@haroen.me>
2024-12-20 10:08:57 +0100

    feat(helper): persistHierarchicalRootCount: true (#6478)
    * feat(helper): persistHierarchicalRootCount: true
    
    BREAKING CHANGE: the default behaviour of the helper is now persistHierarchicalRootCount: true
    
    * fix(options): remove persistHierarchicalRootCount option
    
    It behaves as persistHierarchicalRootCount: true

--- packages/algoliasearch-helper/test/spec/SearchResults/getFacetValues.js
+++ packages/algoliasearch-helper/test/spec/SearchResults/getFacetValues.js
@@ -751,60 +751,16 @@ test('getFacetValues(facetName) prefers the "main" facet result (disjunctive)', 
   expect(facetValues).toEqual(expected);
 });
 
-test('getFacetValues(facetName) prefers the "main" facet result (hierarchical) (persistHierarchicalRootCount: true)', function () {
+test('getFacetValues(facetName) prefers the "main" facet result (hierarchical)', function () {
   var data = require('./getFacetValues/hierarchical-non-exhaustive.json');
   var searchParams = new SearchParameters(data.state);
-  var result = new SearchResults(searchParams, data.content.results, {
-    persistHierarchicalRootCount: true,
-  });
+  var result = new SearchResults(searchParams, data.content.results);
 
   var facetValues = result.getFacetValues('brand').data;
 
   var expected = [
     {
       count: 1000,
-      data: null,
-      isRefined: true,
-      name: 'Apple',
-      escapedValue: 'Apple',
-      path: 'Apple',
-      exhaustive: true,
-    },
-    {
-      count: 551,
-      data: null,
-      isRefined: false,
-      name: 'Insignia™',
-      escapedValue: 'Insignia™',
-      path: 'Insignia™',
-      exhaustive: true,
-    },
-    {
-      count: 551,
-      data: null,
-      isRefined: false,
-      name: 'Samsung',
-      escapedValue: 'Samsung',
-      path: 'Samsung',
-      exhaustive: true,
-    },
-  ];
-
-  expect(facetValues).toEqual(expected);
-});
-
-test('getFacetValues(facetName) prefers the "main" facet result (hierarchical) (persistHierarchicalRootCount: false)', function () {
-  var data = require('./getFacetValues/hierarchical-non-exhaustive.json');
-  var searchParams = new SearchParameters(data.state);
-  var result = new SearchResults(searchParams, data.content.results, {
-    persistHierarchicalRootCount: false,
-  });
-
-  var facetValues = result.getFacetValues('brand').data;
-
-  var expected = [
-    {
-      count: 50,
       data: null,
       isRefined: true,
       name: 'Apple',
