392a65bd92465e3b392632597dd82ac3120bdfda
Haroen Viaene <hello@haroen.me>
2024-12-26 15:23:49 +0100

    fix(insights): detect appId and apiKey from client directly in v5 (#6492)

--- packages/instantsearch.js/src/lib/utils/getAppIdAndApiKey.ts
+++ packages/instantsearch.js/src/lib/utils/getAppIdAndApiKey.ts
@@ -1,6 +1,11 @@
 // typed as any, since it accepts the _real_ js clients, not the interface we otherwise expect
-export function getAppIdAndApiKey(searchClient: any): [string, string] {
-  if (searchClient.transporter) {
+export function getAppIdAndApiKey(
+  searchClient: any
+): [appId: string, apiKey: string] | [appId: undefined, apiKey: undefined] {
+  if (searchClient.appId && searchClient.apiKey) {
+    // searchClient v5
+    return [searchClient.appId, searchClient.apiKey];
+  } else if (searchClient.transporter) {
     // searchClient v4 or v5
     const transporter = searchClient.transporter;
     const headers = transporter.headers || transporter.baseHeaders;
