e082aa279d9586e333e0fb9c8d7ed0890846c4c0
Emmanuel <e-krebs@users.noreply.github.com>
2025-01-07 11:35:15 +0100

    feat: add tests for server

--- tests/mocks/createCompositionClient.ts
+++ tests/mocks/createCompositionClient.ts
@@ -18,3 +18,50 @@ export const createCompositionClient = <T extends Record<string, unknown>>(
   apiKey: 'apiKey',
   ...args,
 });
+
+type ControlledCompositionClient = {
+  searchClient: CompositionClient;
+  searches: Array<{
+    promise: ReturnType<CompositionClient['search']>;
+    resolver: () => void;
+    rejecter: (value: any) => void;
+  }>;
+};
+
+export const createControlledCompositionClient = <
+  T extends Record<string, unknown>
+>(
+  args: Partial<CompositionClient & T> = {},
+  createResponse = (..._params: Parameters<CompositionClient['search']>) =>
+    Promise.resolve({ results: [createSingleSearchResponse()] })
+): ControlledCompositionClient => {
+  const searches: ControlledCompositionClient['searches'] = [];
+  const searchClient = createCompositionClient({
+    search: jest.fn((...params) => {
+      let resolver: () => void;
+      let rejecter: (value: any) => void;
+      const promise: ReturnType<CompositionClient['search']> = new Promise(
+        (resolve, reject) => {
+          resolver = () => resolve(createResponse(...params));
+          rejecter = (value) => reject(value);
+        }
+      );
+
+      searches.push({
+        promise,
+        // @ts-expect-error actually being assigned in the promise constructor
+        resolver,
+        // @ts-expect-error actually being assigned in the promise constructor
+        rejecter,
+      });
+
+      return promise;
+    }),
+    ...args,
+  });
+
+  return {
+    searchClient,
+    searches,
+  };
+};
