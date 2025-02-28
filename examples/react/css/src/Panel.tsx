43ccae9b0e757d3a60f8dd864b20a3ffab61b19e
Fabien Motte <662153+FabienMotte@users.noreply.github.com>
2025-02-28 10:54:08 +0100

    poc: add `Panel` footer

--- examples/react/css/src/Panel.tsx
+++ examples/react/css/src/Panel.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 type PanelProps = React.PropsWithChildren<{
   header: string;
+  footer?: React.ReactNode;
 }>;
 
-export function Panel({ header, children }: PanelProps) {
+export function Panel({ header, children, footer }: PanelProps) {
   return (
     <div className="ais-Panel">
       <div className="ais-Panel-header">
         <span>{header}</span>
       </div>
       <div className="ais-Panel-body">{children}</div>
+      <div className="ais-Panel-footer">{footer}</div>
     </div>
   );
 }
