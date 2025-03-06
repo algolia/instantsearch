cf06d9a49258f54c33a807ba518822d18c715abe
Aymeric Giraudet <aymeric.giraudet@algolia.com>
2025-03-06 17:42:50 +0100

    fix(ssr-example): responsesCache instead of requestsCache

--- examples/react/ssr/src/searchClient.js
+++ examples/react/ssr/src/searchClient.js
@@ -1,11 +1,11 @@
 import { createMemoryCache } from '@algolia/client-common';
 import { liteClient as algoliasearch } from 'algoliasearch/lite';
 
-export const requestsCache = createMemoryCache();
+export const responsesCache = createMemoryCache();
 export const searchClient = algoliasearch(
   'latency',
   '6be0576ff61c053d5f9a3225e2a90f76',
   {
-    requestsCache,
+    responsesCache,
   }
 );
