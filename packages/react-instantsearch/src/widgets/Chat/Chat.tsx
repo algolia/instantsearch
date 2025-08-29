d01536bf900f2ba76cc46e6759a7aa837f7dea9e
Haroen Viaene <hello@haroen.me>
2025-08-29 15:41:15 +0200

    demo uses v4

--- packages/react-instantsearch/src/widgets/Chat/Chat.tsx
+++ packages/react-instantsearch/src/widgets/Chat/Chat.tsx
@@ -1,7 +1,6 @@
-import { DefaultChatTransport } from 'ai';
+import { useChat } from '@ai-sdk/react';
 import { createChatComponent } from 'instantsearch-ui-components';
 import React, { createElement, Fragment } from 'react';
-import { useChat } from 'react-instantsearch-core';
 
 import type { Pragma } from 'instantsearch-ui-components';
 
@@ -16,25 +15,17 @@ export type ChatProps = {
 
 export function Chat({ token }: ChatProps) {
   const [open, setOpen] = React.useState(false);
-  const [input, setInput] = React.useState('');
-  const { messages, sendMessage } = useChat({
-    transport: new DefaultChatTransport({
-      api: `https://askai.algolia.com/chat`,
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Algolia-Application-Id': 'PMZUYBQDAK',
-        'X-Algolia-API-Key': '24b09689d5b4223813d9b8e48563c8f6',
-        'X-Algolia-Index-Name': 'docsearch-markdown',
-        'X-Algolia-Assistant-Id': 'askAIDemo',
-        Authorization: `TOKEN ${token}`,
-      },
-      prepareSendMessagesRequest: ({ id, messages: m }) => ({
-        body: {
-          id,
-          messages: m,
-        },
-      }),
-    }),
+  const { messages, handleSubmit, input, setInput } = useChat({
+    api: `https://askai.algolia.com/chat`,
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Algolia-Application-Id': 'PMZUYBQDAK',
+      'X-Algolia-API-Key': '24b09689d5b4223813d9b8e48563c8f6',
+      'X-Algolia-Index-Name': 'docsearch-markdown',
+      'X-Algolia-Assistant-Id': 'askAIDemo',
+      Authorization: `TOKEN ${token}`,
+    },
+    experimental_prepareRequestBody: (body) => body,
   });
 
   return (
@@ -55,9 +46,8 @@ export function Chat({ token }: ChatProps) {
         onInput: (event) => {
           setInput(event);
         },
-        onSubmit: (event) => {
-          sendMessage({ text: event });
-          setInput('');
+        onSubmit: () => {
+          handleSubmit();
         },
       }}
     />
