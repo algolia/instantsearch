166db0903cd98b186ce361b10834fc2090dc9cab
Aymeric Giraudet <aymeric.giraudet@algolia.com>
2025-09-15 18:17:16 +0200

    feat(autocomplete): indices prop for React component (#6703)

--- packages/react-instantsearch/src/components/Autocomplete.tsx
+++ packages/react-instantsearch/src/components/Autocomplete.tsx
@@ -1,19 +1,30 @@
 import React from 'react';
-import {
-  EXPERIMENTAL_AutocompleteWrapper as AutocompleteWrapper,
-  EXPERIMENTAL_useAutocomplete as useAutocomplete,
-} from 'react-instantsearch-core';
+import { Index } from 'react-instantsearch-core';
 
-export function EXPERIMENTAL_Autocomplete() {
+import { Hits, SearchBox } from '../widgets';
+
+import type { BaseHit, Hit } from 'instantsearch.js';
+
+type IndexConfig<TItem extends Hit<BaseHit> = Hit<BaseHit>> = {
+  indexName: string;
+  getQuery?: (item: TItem) => string;
+  getURL?: (item: TItem) => string;
+  itemComponent: React.ComponentType<TItem> | React.ComponentType<any>;
+};
+
+export type AutocompleteProps = {
+  indices: IndexConfig[];
+};
+
+export function EXPERIMENTAL_Autocomplete({ indices }: AutocompleteProps) {
   return (
-    <AutocompleteWrapper>
-      <AutocompleteInner />
-    </AutocompleteWrapper>
+    <Index EXPERIMENTAL_isolated>
+      <SearchBox />
+      {indices.map((index) => (
+        <Index key={index.indexName} indexName={index.indexName}>
+          <Hits hitComponent={({ hit }) => <index.itemComponent {...hit} />} />
+        </Index>
+      ))}
+    </Index>
   );
-}
-
-function AutocompleteInner() {
-  useAutocomplete();
-
-  return null;
 }
