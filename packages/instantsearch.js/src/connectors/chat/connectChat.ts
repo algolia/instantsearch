49e4d4fa8acbeb84b502055890a9fb38d7122730
Dhaya <154633+dhayab@users.noreply.github.com>
2025-09-17 13:53:16 +0200

    fine-tune connector implementation

--- packages/instantsearch.js/src/connectors/chat/connectChat.ts
+++ packages/instantsearch.js/src/connectors/chat/connectChat.ts
@@ -1,53 +1,63 @@
 import { DefaultChatTransport } from 'ai';
 
 import { Chat } from '../../lib/chat';
-import { getAppIdAndApiKey, noop } from '../../lib/utils';
+import {
+  checkRendering,
+  createDocumentationMessageGenerator,
+  createSendEventForHits,
+  getAppIdAndApiKey,
+  noop,
+} from '../../lib/utils';
 
 import type {
   AbstractChat,
   ChatInit as ChatInitAi,
   UIMessage,
 } from '../../lib/chat';
+import type { SendEventForHits } from '../../lib/utils';
 import type {
   Connector,
   Renderer,
   Unmounter,
   UnknownWidgetParams,
-  InitOptions,
-  RenderOptions,
+  InstantSearch,
+  WidgetRenderState,
 } from '../../types';
-import type { ChatStatus } from 'instantsearch-ui-components';
+
+const withUsage = createDocumentationMessageGenerator({
+  name: 'chat',
+  connector: true,
+});
 
 export type ChatRenderState<TUiMessage extends UIMessage = UIMessage> = {
+  input: string;
+  open: boolean;
   /**
-   * The id of the chat.
+   * Sends an event to the Insights middleware.
    */
-  readonly id: string;
-
+  sendEvent: SendEventForHits;
+  setInput: (input: string) => void;
+  setOpen: (open: boolean) => void;
   /**
-   * Update the `messages` state locally. This is useful when you want to
+   * Updates the `messages` state locally. This is useful when you want to
    * edit the messages on the client, and then trigger the `reload` method
    * manually to regenerate the AI response.
    */
   setMessages: (
     messages: TUiMessage[] | ((m: TUiMessage[]) => TUiMessage[])
   ) => void;
-
-  open: boolean;
-  input: string;
-  setOpen: (open: boolean) => void;
-  setInput: (input: string) => void;
-  getMessages: () => TUiMessage[];
-  getError: () => Error | undefined;
-  getStatus: () => ChatStatus;
 } & Pick<
   AbstractChat<TUiMessage>,
-  | 'sendMessage'
-  | 'regenerate'
-  | 'stop'
-  | 'resumeStream'
   | 'addToolResult'
   | 'clearError'
+  | 'error'
+  | 'id'
+  | 'messages'
+  | 'regenerate'
+  | 'resumeStream'
+  | 'sendMessage'
+  | 'status'
+  | 'stop'
 >;
 
 export type ChatInitWithoutTransport<TUiMessage extends UIMessage> = Omit<
@@ -63,16 +73,6 @@ export type ChatTransport = {
 export type ChatInit<TUiMessage extends UIMessage> =
   ChatInitWithoutTransport<TUiMessage> & ChatTransport;
 
-export type UseChatOptions<TUiMessage extends UIMessage> = (
-  | { chat: Chat<TUiMessage> }
-  | ChatInit<TUiMessage>
-) & {
-  /**
-   * Whether to resume an ongoing chat generation stream.
-   */
-  resume?: boolean;
-};
-
 export type ChatConnectorParams<TUiMessage extends UIMessage = UIMessage> = (
   | { chat: Chat<TUiMessage> }
   | ChatInit<TUiMessage>
@@ -83,9 +83,15 @@ export type ChatConnectorParams<TUiMessage extends UIMessage = UIMessage> = (
   resume?: boolean;
 };
 
-export type ChatWidgetDescription<TUiMessage extends UIMessage> = {
+export type ChatWidgetDescription<TUiMessage extends UIMessage = UIMessage> = {
   $$type: 'ais.chat';
   renderState: ChatRenderState<TUiMessage>;
+  indexRenderState: {
+    chat: WidgetRenderState<
+      ChatRenderState<TUiMessage>,
+      ChatConnectorParams<TUiMessage>
+    >;
+  };
 };
 
 export type ChatConnector<TUiMessage extends UIMessage = UIMessage> = Connector<
@@ -97,176 +103,169 @@ export default (function connectChat<TWidgetParams extends UnknownWidgetParams>(
   renderFn: Renderer<ChatRenderState, TWidgetParams & ChatConnectorParams>,
   unmountFn: Unmounter = noop
 ) {
+  checkRendering(renderFn, withUsage());
+
   return <TUiMessage extends UIMessage = UIMessage>(
     widgetParams: TWidgetParams & ChatConnectorParams<TUiMessage>
   ) => {
     const { resume = false, ...options } = widgetParams || {};
-    let _chatInstance: Chat<TUiMessage>;
-    let open = false;
-    let input = '';
-    let setOpen: (o: boolean) => void;
-    let setInput: (i: string) => void;
 
-    let setMessages: (
+    let _chatInstance: Chat<TUiMessage>;
+    let input = '';
+    let open = false;
+    let sendEvent: SendEventForHits;
+    let setInput: ChatRenderState<TUiMessage>['setInput'];
+    let setOpen: ChatRenderState<TUiMessage>['setOpen'];
+
+    const setMessages = (
       messagesParam: TUiMessage[] | ((m: TUiMessage[]) => TUiMessage[])
-    ) => void;
-    const latestRenderOptions: { value: InitOptions | RenderOptions | null } = {
-      value: null,
+    ) => {
+      if (typeof messagesParam === 'function') {
+        messagesParam = messagesParam(_chatInstance.messages);
+      }
+      _chatInstance.messages = messagesParam;
+    };
+
+    const makeChatInstance = (instantSearchInstance: InstantSearch) => {
+      let transport;
+      const [appId, apiKey] = getAppIdAndApiKey(instantSearchInstance.client);
+      if ('transport' in options && options.transport) {
+        transport = new DefaultChatTransport(options.transport);
+      }
+      if ('agentId' in options && options.agentId) {
+        const { agentId } = options;
+        if (!appId || !apiKey) {
+          throw new Error(
+            'The `connectChat` requires an `appId` and `apiKey` to be set on the `InstantSearch` component when using the `agentId` option.'
+          );
+        }
+        transport = new DefaultChatTransport({
+          api: `https://${appId}.algolia.net/agent-studio/1/agents/${agentId}/completions?compatibilityMode=ai-sdk-5`,
+          headers: {
+            'x-algolia-application-id': appId,
+            'x-algolia-api-Key': apiKey,
+          },
+        });
+      }
+      if (!transport) {
+        throw new Error(
+          'You need to provide either an `agentId` or a `transport`.'
+        );
+      }
+
+      const optionsWithTransport =
+        'chat' in options
+          ? options
+          : {
+              ...options,
+              transport,
+            };
+
+      return 'chat' in optionsWithTransport
+        ? optionsWithTransport.chat
+        : new Chat(optionsWithTransport);
     };
 
     return {
       $$type: 'ais.chat',
 
       init(initOptions) {
-        latestRenderOptions.value = initOptions;
+        const { instantSearchInstance } = initOptions;
 
-        const [appId, apiKey] = getAppIdAndApiKey(
-          initOptions.instantSearchInstance.client
-        );
+        _chatInstance = makeChatInstance(instantSearchInstance);
 
-        const transport = (() => {
-          if ('transport' in options && options.transport) {
-            return new DefaultChatTransport(options.transport);
-          }
-          if ('agentId' in options && options.agentId) {
-            const { agentId } = options;
-            if (!appId || !apiKey) {
-              throw new Error(
-                'The `connectChat` requires an `appId` and `apiKey` to be set on the `InstantSearch` component when using the `agentId` option.'
-              );
-            }
-            return new DefaultChatTransport({
-              api: `https://${appId}.algolia.net/agent-studio/1/agents/${agentId}/completions?compatibilityMode=ai-sdk-5`,
-              headers: {
-                'x-algolia-application-id': appId,
-                'x-algolia-api-Key': apiKey,
-              },
-            });
-          }
-
-          throw new Error(
-            'You need to provide either an `agentId` or a `transport`.'
-          );
-        })();
-
-        const optionsWithTransport = (() => {
-          if ('chat' in options) {
-            return options;
-          }
-          return {
-            ...options,
-            transport,
-          };
-        })();
-
-        _chatInstance =
-          'chat' in optionsWithTransport
-            ? (optionsWithTransport.chat as Chat<TUiMessage>) // TODO: fix assertion
-            : new Chat(optionsWithTransport);
-
-        setMessages = (
-          messagesParam: TUiMessage[] | ((m: TUiMessage[]) => TUiMessage[])
-        ) => {
-          if (typeof messagesParam === 'function') {
-            messagesParam = messagesParam(_chatInstance.messages);
-          }
-          _chatInstance.messages = messagesParam;
-        };
-
-        const rerender = () => {
+        const render = () => {
           renderFn(
             {
-              ...this.getWidgetRenderState!(latestRenderOptions.value!),
+              ...this.getWidgetRenderState(initOptions),
               instantSearchInstance: initOptions.instantSearchInstance,
-            } as any, // TODO: fix assertion
+            },
             false
           );
         };
 
-        setOpen = (o: boolean) => {
+        setOpen = (o) => {
           open = o;
-          rerender();
+          render();
         };
 
-        setInput = (i: string) => {
+        setInput = (i) => {
           input = i;
-          rerender();
+          render();
         };
 
-        _chatInstance['~registerMessagesCallback'](() => {
-          rerender();
-        });
-        _chatInstance['~registerErrorCallback'](() => {
-          rerender();
-        });
-        _chatInstance['~registerStatusCallback'](() => {
-          rerender();
-        });
+        _chatInstance['~registerErrorCallback'](render);
+        _chatInstance['~registerMessagesCallback'](render);
+        _chatInstance['~registerStatusCallback'](render);
+
+        if (resume) {
+          _chatInstance.resumeStream();
+        }
 
         renderFn(
           {
-            ...this.getWidgetRenderState!(initOptions),
-            instantSearchInstance: initOptions.instantSearchInstance,
-          } as any, // TODO: fix assertion
+            ...this.getWidgetRenderState(initOptions),
+            instantSearchInstance,
+          },
           true
         );
       },
 
       render(renderOptions) {
-        const renderState = this.getWidgetRenderState!(renderOptions);
-        latestRenderOptions.value = renderOptions;
-
         renderFn(
           {
-            ...renderState,
+            ...this.getWidgetRenderState(renderOptions),
             instantSearchInstance: renderOptions.instantSearchInstance,
-          } as any, // TODO: fix assertion
+          },
           false
         );
       },
 
-      getRenderState(renderState) {
-        return renderState;
+      getRenderState(renderState, renderOptions) {
+        return {
+          ...renderState,
+          chat: this.getWidgetRenderState(renderOptions),
+        };
       },
 
-      getWidgetRenderState() {
+      getWidgetRenderState(renderState) {
         if (!_chatInstance) {
-          throw new Error(
-            'The `chat` instance is not available yet. Did you forget to call `init`?'
-          );
+          this.init!({ ...renderState, uiState: {}, results: undefined });
         }
 
-        // TODO: make sure resume works as expected / same as useChat
-        if (resume) {
-          _chatInstance.resumeStream();
+        if (!sendEvent) {
+          sendEvent = createSendEventForHits({
+            instantSearchInstance: renderState.instantSearchInstance,
+            helper: renderState.helper,
+            widgetType: this.$$type,
+          });
         }
 
         return {
-          widgetParams,
-          id: _chatInstance.id,
-          open,
           input,
-          setOpen,
+          open,
+          sendEvent,
           setInput,
-          getMessages: () => _chatInstance.messages,
+          setOpen,
           setMessages,
-          sendMessage: _chatInstance.sendMessage,
-          getError: () => _chatInstance.error,
-          regenerate: _chatInstance.regenerate,
-          clearError: _chatInstance.clearError,
-          stop: _chatInstance.stop,
-          getStatus: () => _chatInstance.status,
-          resumeStream: _chatInstance.resumeStream,
+          widgetParams,
+
+          // Chat instance render state
           addToolResult: _chatInstance.addToolResult,
+          clearError: _chatInstance.clearError,
+          error: _chatInstance.error,
+          id: _chatInstance.id,
+          messages: _chatInstance.messages,
+          regenerate: _chatInstance.regenerate,
+          resumeStream: _chatInstance.resumeStream,
+          sendMessage: _chatInstance.sendMessage,
+          status: _chatInstance.status,
+          stop: _chatInstance.stop,
         };
       },
 
       dispose() {
         unmountFn();
-      },
-
-      getWidgetParameters(state) {
-        return state;
       },
     };
   };
