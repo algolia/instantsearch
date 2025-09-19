43716b24f74d4e61348c8474307b071cc0cdde7d
Fabien Motte <662153+FabienMotte@users.noreply.github.com>
2025-09-19 16:21:23 +0200

    test: fix

--- packages/instantsearch-ui-components/src/components/chat/__tests__/ChatToggleButton.test..tsx
+++ packages/instantsearch-ui-components/src/components/chat/__tests__/ChatToggleButton.test..tsx
@@ -22,7 +22,21 @@ describe('ChatToggleButton', () => {
           class="ais-ChatToggleButton ais-ChatToggleButton--open"
           type="button"
         >
-          Close chat
+          <svg
+            fill="none"
+            height="28"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            viewBox="0 0 24 24"
+            width="28"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="m18 15-6-6-6 6"
+            />
+          </svg>
         </button>
       </div>
     `);
@@ -38,7 +52,26 @@ describe('ChatToggleButton', () => {
           class="ais-ChatToggleButton"
           type="button"
         >
-          Open chat
+          <svg
+            fill="none"
+            height="28"
+            viewBox="0 0 20 20"
+            width="28"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clipRule="evenodd"
+              d="M10 1.875c.27 0 .51.173.594.43l1.593 4.844a1.043 1.043 0 0 0 .664.664l4.844 1.593a.625.625 0 0 1 0 1.188l-4.844 1.593a1.043 1.043 0 0 0-.664.664l-1.593 4.844a.625.625 0 0 1-1.188 0l-1.593-4.844a1.042 1.042 0 0 0-.664-.664l-4.844-1.593a.625.625 0 0 1 0-1.188l4.844-1.593a1.042 1.042 0 0 0 .664-.664l1.593-4.844a.625.625 0 0 1 .594-.43ZM9 7.539A2.292 2.292 0 0 1 7.54 9L4.5 10l3.04 1A2.292 2.292 0 0 1 9 12.46l1 3.04 1-3.04A2.293 2.293 0 0 1 12.46 11l3.04-1-3.04-1A2.292 2.292 0 0 1 11 7.54L10 4.5 9 7.54ZM4.167 1.875c.345 0 .625.28.625.625v3.333a.625.625 0 0 1-1.25 0V2.5c0-.345.28-.625.625-.625ZM15.833 13.542c.345 0 .625.28.625.625V17.5a.625.625 0 1 1-1.25 0v-3.333c0-.345.28-.625.625-.625Z"
+              fill="currentColor"
+              fillRule="evenodd"
+            />
+            <path
+              clipRule="evenodd"
+              d="M1.875 4.167c0-.346.28-.625.625-.625h3.333a.625.625 0 1 1 0 1.25H2.5a.625.625 0 0 1-.625-.625ZM13.542 15.833c0-.345.28-.625.625-.625H17.5a.625.625 0 0 1 0 1.25h-3.333a.625.625 0 0 1-.625-.625Z"
+              fill="currentColor"
+              fillRule="evenodd"
+            />
+          </svg>
         </button>
       </div>
     `);
