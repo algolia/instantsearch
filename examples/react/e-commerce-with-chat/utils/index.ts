e8a8eab5aef784423c3f1f6bcb97d48e4493c556
Fabien Motte <662153+FabienMotte@users.noreply.github.com>
2025-09-22 11:26:00 +0200

    chore: remove example

--- examples/react/e-commerce-with-chat/utils/index.ts
+++
@@ -1,9 +0,0 @@
-export function formatNumber(number: string | number): string {
-  return Number(number).toLocaleString();
-}
-
-export function cx(
-  ...classNames: Array<string | number | boolean | undefined | null>
-) {
-  return classNames.filter(Boolean).join(' ');
-}
