b1c39e08aeb15522fa741665130966dfe110c1f4
Dhaya <154633+dhayab@users.noreply.github.com>
2025-09-24 14:03:05 +0200

    fix(umd): remove ai components from react umd bundle

---
+++ packages/react-instantsearch/src/index.umd.ts
@@ -0,0 +1,3 @@
+export * from 'react-instantsearch-core/src/index.umd';
+export * from './widgets/index.umd';
+export * from './components';
---
+++ packages/react-instantsearch/src/widgets/index.umd.ts
@@ -0,0 +1,32 @@
+export * from './Breadcrumb';
+
+export const Chat = () => {
+  throw new Error(
+    `"<Chat>" is not available from the UMD build.
+
+Please use React InstantSearch with a packaging system:
+https://www.algolia.com/doc/guides/building-search-ui/installation/react/#install-react-instantsearch-as-an-npm-package`
+  );
+};
+
+export * from './ClearRefinements';
+export * from './CurrentRefinements';
+export * from './FrequentlyBoughtTogether';
+export * from './HierarchicalMenu';
+export * from './Highlight';
+export * from './Hits';
+export * from './HitsPerPage';
+export * from './InfiniteHits';
+export * from './LookingSimilar';
+export * from './Menu';
+export * from './Pagination';
+export * from './PoweredBy';
+export * from './RangeInput';
+export * from './RefinementList';
+export * from './RelatedProducts';
+export * from './SearchBox';
+export * from './Snippet';
+export * from './SortBy';
+export * from './Stats';
+export * from './ToggleRefinement';
+export * from './TrendingItems';
