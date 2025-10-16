5b5e904fe6d93bfbb5f3dc5d53a9e74d65e9c825
Shahmir Ejaz <shaejaz5@gmail.com>
2025-10-16 11:41:52 +0100

    add translation tests

--- tests/common/widgets/chat/options.tsx
+++ tests/common/widgets/chat/options.tsx
@@ -514,6 +514,334 @@ export function createOptionsTests(
       });
     });
 
+    describe('translations', () => {
+      describe('header', () => {
+        test('renders with custom translations', async () => {
+          const searchClient = createSearchClient();
+
+          const commonWidgetParams = createDefaultWidgetParams();
+          const jsWidgetParams: JSChatWidgetParams = {
+            ...commonWidgetParams,
+            templates: {
+              header: {
+                titleText: 'Custom title',
+                closeLabelText: 'Custom close button label',
+                clearLabelText: 'Custom clear button label',
+                maximizeLabelText: 'Custom maximize button label',
+                minimizeLabelText: 'Custom minimize button label',
+              },
+            },
+          };
+          const reactWidgetParams: ReactChatWidgetParams = {
+            ...commonWidgetParams,
+            translations: {
+              header: {
+                title: 'Custom title',
+                closeLabel: 'Custom close button label',
+                clearLabel: 'Custom clear button label',
+                maximizeLabel: 'Custom maximize button label',
+                minimizeLabel: 'Custom minimize button label',
+              },
+            },
+          };
+          await setup({
+            instantSearchOptions: {
+              indexName: 'indexName',
+              searchClient,
+            },
+            widgetParams: {
+              javascript: jsWidgetParams,
+              react: reactWidgetParams,
+              vue: {},
+            }[flavor],
+          });
+
+          await openChat(act);
+
+          expect(
+            document.querySelector('.ais-ChatHeader-title')!.textContent
+          ).toBe('Custom title');
+          expect(
+            document
+              .querySelector('.ais-ChatHeader-close')!
+              .getAttribute('aria-label')
+          ).toBe('Custom close button label');
+          expect(
+            document.querySelector('.ais-ChatHeader-clear')!.textContent
+          ).toBe('Custom clear button label');
+          expect(
+            document
+              .querySelector('.ais-ChatHeader-maximize')!
+              .getAttribute('aria-label')
+          ).toBe('Custom maximize button label');
+
+          userEvent.click(document.querySelector('.ais-ChatHeader-maximize')!);
+          expect(
+            document
+              .querySelector('.ais-ChatHeader-maximize')!
+              .getAttribute('aria-label')
+          ).toBe('Custom minimize button label');
+        });
+      });
+
+      describe('prompt', () => {
+        test('renders with custom textarea translations', async () => {
+          const searchClient = createSearchClient();
+
+          const chat = new Chat({});
+          const commonWidgetParams = createDefaultWidgetParams(chat);
+          const jsWidgetParams: JSChatWidgetParams = {
+            ...commonWidgetParams,
+            templates: {
+              prompt: {
+                textareaLabelText: 'Custom message input',
+                textareaPlaceholderText: 'Custom placeholder',
+                disclaimerText: 'Custom disclaimer',
+              },
+            },
+          };
+          const reactWidgetParams: ReactChatWidgetParams = {
+            ...commonWidgetParams,
+            translations: {
+              prompt: {
+                textareaLabel: 'Custom message input',
+                textareaPlaceholder: 'Custom placeholder',
+                disclaimer: 'Custom disclaimer',
+              },
+            },
+          };
+
+          await setup({
+            instantSearchOptions: {
+              indexName: 'indexName',
+              searchClient,
+            },
+            widgetParams: {
+              javascript: jsWidgetParams,
+              react: reactWidgetParams,
+              vue: {},
+            }[flavor],
+          });
+
+          await openChat(act);
+
+          expect(
+            document
+              .querySelector('.ais-ChatPrompt-textarea')
+              ?.getAttribute('aria-label')
+          ).toBe('Custom message input');
+          expect(
+            document
+              .querySelector('.ais-ChatPrompt-textarea')
+              ?.getAttribute('placeholder')
+          ).toBe('Custom placeholder');
+          expect(
+            document.querySelector('.ais-ChatPrompt-footer')!.textContent
+          ).toBe('Custom disclaimer');
+        });
+
+        test('renders with custom empty message translation', async () => {
+          const searchClient = createSearchClient();
+
+          const chat = new Chat({});
+          const commonWidgetParams = createDefaultWidgetParams(chat);
+          const jsWidgetParams: JSChatWidgetParams = {
+            ...commonWidgetParams,
+            templates: {
+              prompt: {
+                emptyMessageTooltipText: 'Custom empty message',
+              },
+            },
+          };
+          const reactWidgetParams: ReactChatWidgetParams = {
+            ...commonWidgetParams,
+            translations: {
+              prompt: {
+                emptyMessageTooltip: 'Custom empty message',
+              },
+            },
+          };
+
+          await setup({
+            instantSearchOptions: {
+              indexName: 'indexName',
+              searchClient,
+            },
+            widgetParams: {
+              javascript: jsWidgetParams,
+              react: reactWidgetParams,
+              vue: {},
+            }[flavor],
+          });
+
+          await openChat(act);
+
+          expect(
+            document
+              .querySelector('.ais-ChatPrompt-submit')
+              ?.getAttribute('aria-label')
+          ).toBe('Custom empty message');
+        });
+
+        test('renders with custom send message translation', async () => {
+          const searchClient = createSearchClient();
+
+          const commonWidgetParams = createDefaultWidgetParams();
+          const jsWidgetParams: JSChatWidgetParams = {
+            ...commonWidgetParams,
+            templates: {
+              prompt: {
+                sendMessageTooltipText: 'Custom send message',
+              },
+            },
+          };
+          const reactWidgetParams: ReactChatWidgetParams = {
+            ...commonWidgetParams,
+            translations: {
+              prompt: {
+                sendMessageTooltip: 'Custom send message',
+              },
+            },
+          };
+
+          await setup({
+            instantSearchOptions: {
+              indexName: 'indexName',
+              searchClient,
+            },
+            widgetParams: {
+              javascript: jsWidgetParams,
+              react: reactWidgetParams,
+              vue: {},
+            }[flavor],
+          });
+
+          await openChat(act);
+
+          userEvent.type(
+            document.querySelector('.ais-ChatPrompt-textarea')!,
+            'Hello, world!'
+          );
+
+          expect(
+            document
+              .querySelector('.ais-ChatPrompt-submit')
+              ?.getAttribute('aria-label')
+          ).toBe('Custom send message');
+        });
+
+        test('renders with custom stop response translation', async () => {
+          const searchClient = createSearchClient();
+
+          const chat = new Chat({});
+          Object.defineProperty(chat, 'status', {
+            get: () => 'streaming',
+          });
+
+          const commonWidgetParams = createDefaultWidgetParams(chat);
+          const jsWidgetParams: JSChatWidgetParams = {
+            ...commonWidgetParams,
+            templates: {
+              prompt: {
+                stopResponseTooltipText: 'Custom stop response',
+              },
+            },
+          };
+          const reactWidgetParams: ReactChatWidgetParams = {
+            ...commonWidgetParams,
+            translations: {
+              prompt: {
+                stopResponseTooltip: 'Custom stop response',
+              },
+            },
+          };
+
+          await setup({
+            instantSearchOptions: {
+              indexName: 'indexName',
+              searchClient,
+            },
+            widgetParams: {
+              javascript: jsWidgetParams,
+              react: reactWidgetParams,
+              vue: {},
+            }[flavor],
+          });
+
+          await openChat(act);
+
+          expect(
+            document
+              .querySelector('.ais-ChatPrompt-submit')
+              ?.getAttribute('aria-label')
+          ).toBe('Custom stop response');
+        });
+      });
+
+      describe('messages', () => {
+        test('renders with custom actions translations', async () => {
+          const searchClient = createSearchClient();
+
+          const commonWidgetParams = createDefaultWidgetParams(
+            new Chat({
+              messages: [
+                {
+                  id: '0',
+                  role: 'assistant',
+                  parts: [
+                    {
+                      type: 'text',
+                      text: 'hello',
+                    },
+                  ],
+                },
+              ],
+            })
+          );
+          const jsWidgetParams: JSChatWidgetParams = {
+            ...commonWidgetParams,
+            templates: {
+              messages: {
+                copyToClipboardLabelText: 'Custom copy action',
+                regenerateLabelText: 'Custom regenerate action',
+              },
+            },
+          };
+          const reactWidgetParams: ReactChatWidgetParams = {
+            ...commonWidgetParams,
+            translations: {
+              messages: {
+                copyToClipboardLabel: 'Custom copy action',
+                regenerateLabel: 'Custom regenerate action',
+              },
+            },
+          };
+
+          await setup({
+            instantSearchOptions: {
+              indexName: 'indexName',
+              searchClient,
+            },
+            widgetParams: {
+              javascript: jsWidgetParams,
+              react: reactWidgetParams,
+              vue: {},
+            }[flavor],
+          });
+
+          await openChat(act);
+
+          const actions = document.querySelectorAll('.ais-ChatMessage-action');
+          expect(actions[0].getAttribute('aria-label')).toBe(
+            'Custom copy action'
+          );
+          expect(actions[1].getAttribute('aria-label')).toBe(
+            'Custom regenerate action'
+          );
+        });
+      });
+    });
+
     describe('tools', () => {
       test('renders with default tools', async () => {
         const searchClient = createSearchClient();
