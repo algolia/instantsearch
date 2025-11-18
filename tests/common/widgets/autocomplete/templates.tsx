25af487f7d142ae19298f00fa2ff63516d219d6c
Dhaya <154633+dhayab@users.noreply.github.com>
2025-11-18 10:28:54 +0100

    add recent searches to panel template test

--- tests/common/widgets/autocomplete/templates.tsx
+++ tests/common/widgets/autocomplete/templates.tsx
@@ -17,10 +17,7 @@ import type { TestOptions } from '../../common';
 function normalizeSnapshot(expectedId: string) {
   return function _normalizeSnapshot(html: string) {
     // `useId()` in Preact and React have different patterns.
-    return commonNormalizeSnapshot(html).replace(
-      /(P\d{3}|:r\d+:)/g,
-      expectedId
-    );
+    return commonNormalizeSnapshot(html).replace(/(P\d{3}|:r.:)/g, expectedId);
   };
 }
 
@@ -119,6 +116,10 @@ export function createTemplatesTests(
     });
 
     test('renders custom panel', async () => {
+      jest
+        .spyOn(Storage.prototype, 'getItem')
+        .mockReturnValue(JSON.stringify(['hello']));
+
       const searchClient = createMockedSearchClient(
         createMultiSearchResponse(
           createSingleSearchResponse({
@@ -157,11 +158,14 @@ export function createTemplatesTests(
               indexName: 'indexName2',
               templates: { item: (props) => props.item.query },
             },
+            showRecent: true,
             templates: {
               panel: ({ elements }, { html }) => html`
                 <div>
                   <h1>My custom panel</h1>
-                  <div class="left">${elements.suggestions}</div>
+                  <div class="left">
+                    ${elements.recent} ${elements.suggestions}
+                  </div>
                   <div class="right">${elements.indexName}</div>
                 </div>
               `,
@@ -178,10 +182,13 @@ export function createTemplatesTests(
               indexName: 'indexName2',
               itemComponent: (props) => props.item.query,
             },
+            showRecent: true,
             panelComponent: ({ elements }) => (
               <div>
                 <h1>My custom panel</h1>
-                <div className="left">{elements.suggestions}</div>
+                <div className="left">
+                  {elements.recent} {elements.suggestions}
+                </div>
                 <div className="right">{elements.indexName}</div>
               </div>
             ),
@@ -215,6 +222,63 @@ export function createTemplatesTests(
               <div
                 class="left"
               >
+                <div
+                  class="ais-AutocompleteIndex ais-AutocompleteRecentSearches"
+                >
+                  <ol
+                    class="ais-AutocompleteIndexList ais-AutocompleteRecentSearchesList"
+                  >
+                    <li
+                      aria-selected="false"
+                      class="ais-AutocompleteIndexItem ais-AutocompleteRecentSearchesItem"
+                      id="autocompleteP484item:recent-searches:0"
+                      role="row"
+                    >
+                      <div
+                        class="ais-AutocompleteItemWrapper ais-AutocompleteRecentSearchWrapper"
+                      >
+                        <div
+                          class="ais-AutocompleteItemContent ais-AutocompleteRecentSearchItemContent"
+                        >
+                          <div
+                            class="ais-AutocompleteItemIcon ais-AutocompleteRecentSearchItemIcon"
+                          >
+                            <svg
+                              fill="currentColor"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                d="M12.516 6.984v5.25l4.5 2.672-0.75 1.266-5.25-3.188v-6h1.5zM12 20.016q3.281 0 5.648-2.367t2.367-5.648-2.367-5.648-5.648-2.367-5.648 2.367-2.367 5.648 2.367 5.648 5.648 2.367zM12 2.016q4.125 0 7.055 2.93t2.93 7.055-2.93 7.055-7.055 2.93-7.055-2.93-2.93-7.055 2.93-7.055 7.055-2.93z"
+                              />
+                            </svg>
+                          </div>
+                          <div
+                            class="ais-AutocompleteItemContentBody ais-AutocompleteRecentSearchItemContentBody"
+                          >
+                            hello
+                          </div>
+                        </div>
+                        <div
+                          class="ais-AutocompleteItemActions ais-AutocompleteRecentSearchItemActions"
+                        >
+                          <button
+                            class="ais-AutocompleteItemActionButton ais-AutocompleteRecentSearchItemDeleteButton"
+                            title="Remove hello from recent searches"
+                          >
+                            <svg
+                              fill="currentColor"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                d="M18 7v13c0 0.276-0.111 0.525-0.293 0.707s-0.431 0.293-0.707 0.293h-10c-0.276 0-0.525-0.111-0.707-0.293s-0.293-0.431-0.293-0.707v-13zM17 5v-1c0-0.828-0.337-1.58-0.879-2.121s-1.293-0.879-2.121-0.879h-4c-0.828 0-1.58 0.337-2.121 0.879s-0.879 1.293-0.879 2.121v1h-4c-0.552 0-1 0.448-1 1s0.448 1 1 1h1v13c0 0.828 0.337 1.58 0.879 2.121s1.293 0.879 2.121 0.879h10c0.828 0 1.58-0.337 2.121-0.879s0.879-1.293 0.879-2.121v-13h1c0.552 0 1-0.448 1-1s-0.448-1-1-1zM9 5v-1c0-0.276 0.111-0.525 0.293-0.707s0.431-0.293 0.707-0.293h4c0.276 0 0.525 0.111 0.707 0.293s0.293 0.431 0.293 0.707v1zM9 11v6c0 0.552 0.448 1 1 1s1-0.448 1-1v-6c0-0.552-0.448-1-1-1s-1 0.448-1 1zM13 11v6c0 0.552 0.448 1 1 1s1-0.448 1-1v-6c0-0.552-0.448-1-1-1s-1 0.448-1 1z"
+                              />
+                            </svg>
+                          </button>
+                        </div>
+                      </div>
+                    </li>
+                  </ol>
+                </div>
                 <div
                   class="ais-AutocompleteIndex ais-AutocompleteSuggestions"
                 >
