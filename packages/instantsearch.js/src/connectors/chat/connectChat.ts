4409688452db1dad00c98508ec05b84641c086c3
Fabien Motte <662153+FabienMotte@users.noreply.github.com>
2025-12-08 10:45:04 +0100

    fix: remove

--- packages/instantsearch.js/src/connectors/chat/connectChat.ts
+++ packages/instantsearch.js/src/connectors/chat/connectChat.ts
@@ -268,7 +268,6 @@ export default (function connectChat<TWidgetParams extends UnknownWidgetParams>(
             },
             false
           );
-          instantSearchInstance.scheduleRender(false);
         };
 
         setOpen = (o) => {
