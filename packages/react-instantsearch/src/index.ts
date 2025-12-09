531858f202905e4671da89f55aaef6564d2497bc
Fabien Motte <662153+FabienMotte@users.noreply.github.com>
2025-12-09 14:05:12 +0100

    chore: add comment

--- packages/react-instantsearch/src/index.ts
+++ packages/react-instantsearch/src/index.ts
@@ -1,4 +1,5 @@
 export * from 'react-instantsearch-core';
 export * from './widgets';
 export * from './components';
+// @internal Internal exports (not part of the public API)
 export * from './lib/useStickToBottom';
