d88ff3ec42bce18eb3e037ed2d81e49bd5b17ade
Fabien Motte <662153+FabienMotte@users.noreply.github.com>
2025-12-09 12:46:08 +0100

    feat: export `useStickToBottom` hook

--- packages/react-instantsearch/src/index.ts
+++ packages/react-instantsearch/src/index.ts
@@ -1,3 +1,4 @@
 export * from 'react-instantsearch-core';
 export * from './widgets';
 export * from './components';
+export * from './lib/useStickToBottom';
--- packages/react-instantsearch/src/index.umd.ts
+++ packages/react-instantsearch/src/index.umd.ts
@@ -1,3 +1,4 @@
 export * from 'react-instantsearch-core/dist/es/index.umd';
 export * from './widgets/index.umd';
 export * from './components';
+export * from './lib/useStickToBottom';
