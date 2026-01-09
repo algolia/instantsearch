3d46672656c0c5e7303dc5bfb43e191c13758291
Aymeric Giraudet <aymeric.giraudet@algolia.com>
2026-01-09 11:59:32 +0100

    support IE11 by mocking each function instead of using proxy

--- packages/instantsearch.js/scripts/rollup/emptyModule.js
+++ packages/instantsearch.js/scripts/rollup/emptyModule.js
@@ -2,17 +2,95 @@
 export const ZodFirstPartyTypeKind = {};
 export const toJSONSchema = {};
 export const safeParseAsync = {};
-export const z = new Proxy(
-  {},
-  {
-    get: (_, name) =>
-      name === '~standard'
-        ? { validate: (value) => Promise.resolve({ issues: null, value }) }
-        : function () {
-            return this;
-          },
-  }
-);
+export const z = {
+  string() {
+    return this;
+  },
+  instanceof() {
+    return this;
+  },
+  custom() {
+    return this;
+  },
+  object() {
+    return this;
+  },
+  array() {
+    return this;
+  },
+  union() {
+    return this;
+  },
+  literal() {
+    return this;
+  },
+  lazy() {
+    return this;
+  },
+  null() {
+    return this;
+  },
+  number() {
+    return this;
+  },
+  boolean() {
+    return this;
+  },
+  record() {
+    return this;
+  },
+  optional() {
+    return this;
+  },
+  unknown() {
+    return this;
+  },
+  discriminatedUnion() {
+    return this;
+  },
+  strictObject() {
+    return this;
+  },
+  startsWith() {
+    return this;
+  },
+  looseObject() {
+    return this;
+  },
+  loose() {
+    return this;
+  },
+  base64() {
+    return this;
+  },
+  extend() {
+    return this;
+  },
+  default() {
+    return this;
+  },
+  or() {
+    return this;
+  },
+  int() {
+    return this;
+  },
+  merge() {
+    return this;
+  },
+  strict() {
+    return this;
+  },
+  enum() {
+    return this;
+  },
+  never() {
+    return this;
+  },
+  '~standard': {
+    validate: (value) => Promise.resolve({ issues: null, value }),
+  },
+};
 
 // Placeholder export for 'react' module
 export const cloneElement = {};
