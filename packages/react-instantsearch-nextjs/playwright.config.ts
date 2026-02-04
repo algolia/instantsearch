994434e061f0c3155425b899848c140c38908777
Aymeric Giraudet <aymeric.giraudet@algolia.com>
2026-02-04 13:35:42 +0100

    remove firefox tests for next.js

--- packages/react-instantsearch-nextjs/playwright.config.ts
+++ packages/react-instantsearch-nextjs/playwright.config.ts
@@ -19,10 +19,6 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
   ],
   webServer: {
     command:
