21cf69f021e72a67d1c871631a27b9db63354105
Shahmir Ejaz <shaejaz5@gmail.com>
2026-03-05 13:28:58 +0000

    address comment

--- packages/instantsearch-ui-components/src/lib/utils/chat.ts
+++ packages/instantsearch-ui-components/src/lib/utils/chat.ts
@@ -1,6 +1,4 @@
-import { startsWith } from './startsWith';
-
-import type { ChatMessageBase, ChatToolMessage } from '../../components';
+import type { ChatMessageBase } from '../../components';
 
 export const getTextContent = (message: ChatMessageBase) => {
   return message.parts
@@ -12,28 +10,8 @@ export const hasTextContent = (message: ChatMessageBase) => {
   return getTextContent(message).trim() !== '';
 };
 
-export const getToolParts = (message: ChatMessageBase) => {
-  return message.parts.filter((part) => startsWith(part.type, 'tool-'));
-};
-
-export const hasToolParts = (message: ChatMessageBase) => {
-  return getToolParts(message).length > 0;
-};
-
 export const isPartText = (
   part: ChatMessageBase['parts'][number]
 ): part is Extract<ChatMessageBase['parts'][number], { type: 'text' }> => {
   return part.type === 'text';
-};
-
-export const isPartTool = (
-  part: ChatMessageBase['parts'][number]
-): part is ChatToolMessage => {
-  return startsWith(part.type, 'tool-');
-};
-
-export const toolHasOutput = (message: ChatMessageBase) => {
-  return message.parts.some(
-    (part) => isPartTool(part) && part.state === 'output-available'
-  );
 };
