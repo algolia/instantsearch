3962328bf06df019add7fe0e8bf43ac36c15c0f5
Haroen Viaene <hello@haroen.me>
2026-04-09 16:23:33 +0200

    chore(ci): attempt fixing ie11 test

--- examples/js/e-commerce-umd/vite.config.mjs
+++ examples/js/e-commerce-umd/vite.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
+import legacy from '@vitejs/plugin-legacy';
 import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
-  plugins: [commonjs()],
+  plugins: [
+    commonjs(),
+    legacy({
+      targets: ['ie >= 11'],
+    }),
+  ],
   build: {
     commonjsOptions: {
       requireReturnsDefault: 'preferred',
