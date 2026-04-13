d00311f6598127d13a12cbc74028b498485e72ce
Fabien Motte <662153+FabienMotte@users.noreply.github.com>
2026-04-13 14:52:56 +0900

    chore(examples): add IE 11 legacy browser support to e-commerce examples

--- examples/js/e-commerce/vite.config.mjs
+++ examples/js/e-commerce/vite.config.mjs
@@ -1,8 +1,14 @@
+import legacy from '@vitejs/plugin-legacy';
 import { defineConfig } from 'vite';
 import commonjs from 'vite-plugin-commonjs';
 
 export default defineConfig({
-  plugins: [commonjs()],
+  plugins: [
+    commonjs(),
+    legacy({
+      targets: ['defaults', 'IE 11'],
+    }),
+  ],
   build: {
     commonjsOptions: {
       requireReturnsDefault: 'preferred',
