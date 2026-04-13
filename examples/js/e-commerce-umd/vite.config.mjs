9ce64fc6bf44ea8b758712c9f640e29a331dda63
Fabien Motte <662153+FabienMotte@users.noreply.github.com>
2026-04-13 17:49:21 +0900

    fix(examples): fix IE 11 legacy bundle loading and add error overlay

--- examples/js/e-commerce-umd/vite.config.mjs
+++ examples/js/e-commerce-umd/vite.config.mjs
@@ -2,12 +2,27 @@ import legacy from '@vitejs/plugin-legacy';
 import { defineConfig } from 'vite';
 import commonjs from 'vite-plugin-commonjs';
 
+function removeCrossOrigin() {
+  return {
+    name: 'remove-crossorigin',
+    enforce: 'post',
+    transformIndexHtml: {
+      order: 'post',
+      handler(html) {
+        return html.replace(/ crossorigin/g, '');
+      },
+    },
+  };
+}
+
 export default defineConfig({
   plugins: [
     commonjs(),
     legacy({
       targets: ['defaults', 'IE 11'],
+      renderModernChunks: false,
     }),
+    removeCrossOrigin(),
   ],
   build: {
     commonjsOptions: {
