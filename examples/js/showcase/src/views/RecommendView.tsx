434c41ac24837af58e526738306670f8cd57a58c
Fabien Motte <662153+FabienMotte@users.noreply.github.com>
2026-05-20 13:39:25 +0200

    chore(examples): add recommend view to showcase

---
+++ examples/js/showcase/src/views/RecommendView.tsx
@@ -0,0 +1,77 @@
+import { liteClient as algoliasearch } from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+import { useRef, useEffect } from 'preact/hooks';
+
+import { WidgetFrequentlyBoughtTogether } from '../components/widgets/WidgetFrequentlyBoughtTogether';
+import { WidgetLookingSimilar } from '../components/widgets/WidgetLookingSimilar';
+import { WidgetRelatedProducts } from '../components/widgets/WidgetRelatedProducts';
+import { WidgetTrendingFacets } from '../components/widgets/WidgetTrendingFacets';
+import { WidgetTrendingItems } from '../components/widgets/WidgetTrendingItems';
+import { WidgetSwitcher } from '../components/WidgetSwitcher';
+import { SearchContext } from '../context/search';
+
+const searchClient = algoliasearch(
+  'latency',
+  '6be0576ff61c053d5f9a3225e2a90f76'
+);
+
+const SEED_OBJECT_ID = '5723537';
+const TRENDING_FACET_NAME = 'brand';
+
+export function RecommendView() {
+  const searchRef = useRef<ReturnType<typeof instantsearch> | null>(null);
+  if (searchRef.current === null) {
+    searchRef.current = instantsearch({
+      indexName: 'instant_search',
+      searchClient,
+    });
+  }
+
+  useEffect(() => {
+    const search = searchRef.current!;
+    search.start();
+    return () => search.dispose();
+  }, []);
+
+  return (
+    <SearchContext.Provider value={searchRef.current}>
+      <div class="flex flex-col gap-2">
+        <WidgetSwitcher
+          widgets={[
+            {
+              title: 'frequentlyBoughtTogether',
+              body: () => (
+                <WidgetFrequentlyBoughtTogether objectIDs={[SEED_OBJECT_ID]} />
+              ),
+            },
+            {
+              title: 'relatedProducts',
+              body: () => (
+                <WidgetRelatedProducts objectIDs={[SEED_OBJECT_ID]} />
+              ),
+            },
+            {
+              title: 'lookingSimilar',
+              body: () => <WidgetLookingSimilar objectIDs={[SEED_OBJECT_ID]} />,
+            },
+          ]}
+        />
+
+        <WidgetSwitcher
+          widgets={[{ title: 'trendingItems', body: WidgetTrendingItems }]}
+        />
+
+        <WidgetSwitcher
+          widgets={[
+            {
+              title: 'trendingFacets',
+              body: () => (
+                <WidgetTrendingFacets facetName={TRENDING_FACET_NAME} />
+              ),
+            },
+          ]}
+        />
+      </div>
+    </SearchContext.Provider>
+  );
+}
