2042cba495d1472db359518f9f6d7a90c3d23746
Dhaya <154633+dhayab@users.noreply.github.com>
2026-08-25 16:56:35 +0200

    revert disabling analytics

--- packages/instantsearch.js/src/widgets/index.ts
+++ packages/instantsearch.js/src/widgets/index.ts
@@ -24,7 +24,8 @@ export const EXPERIMENTAL_dynamicWidgets = deprecate(
 );
 export { dynamicWidgets };
 
-// export { default as analytics } from './analytics/analytics';
+// test comment
+export { default as analytics } from './analytics/analytics';
 export { autocomplete };
 /** @deprecated use autocomplete instead */
 export const EXPERIMENTAL_autocomplete = deprecate(
